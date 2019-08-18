623b06be27cb55eddcab016b38cac6ef1b4feed8
lucria <jerryzhang28eclipse@gmail.com>
2019-08-19 00:50:25 +0800

    [Feat] Finished A-Gradle

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
 public class Duke {
+    /**
+     * Main class.
+     *
+     * @param args empty
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
