57dad05ea37195fc36fb06b2729353df65887cb1
lucria <jerryzhang28eclipse@gmail.com>
2019-08-20 13:11:39 +0800

    [Feat] Finished Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     /**
      * Main class.
+     * Level 1 Completed
      *
-     * @param args empty
+     * @param args Refers to CLI arguments
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(command);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
