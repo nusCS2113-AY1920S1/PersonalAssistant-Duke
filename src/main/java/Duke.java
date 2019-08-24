72cd2cb62b7e321cc58df9a51135cdee8986c0a0
TehZiHuai <tehzihuai97@gmail.com>
2019-08-25 00:03:07 +0800

    Level-3: Mark as Done duke allows for marking as done for items type "done" followed by a valid number in the list to mark the item as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-   public static final String LINE_BREAK = "___________________________";
+   public static final String LINE_BREAK = "    ___________________________";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,7 +14,7 @@ public class Duke {
         System.out.println(LINE_BREAK);
         boolean isExit = false;
         Scanner userInput = new Scanner(System.in);
-        ArrayList<String> toDoList = new ArrayList<>();
+        ArrayList<Task> toDoList = new ArrayList<>();
 
         // begin repeated inputs of commands
         while (!isExit) {
@@ -23,23 +23,34 @@ public class Duke {
                 // user indicated bye to exit duke
                 isExit = true;
                 System.out.println(LINE_BREAK);
-                System.out.println(temp);
+                System.out.println("    " + temp);
                 System.out.println(LINE_BREAK);
                 userInput.close();
             } else if (temp.toLowerCase().equals("list")) {
                 // user indicates to list items, iterate through toDoList and print
                 int listCount = 1;
                 System.out.println(LINE_BREAK);
-                for (String output : toDoList) {
-                    System.out.println(listCount + ". " + output);
+                for (Task output : toDoList) {
+                    System.out.println("    " + listCount + ". " + output.getStatusIcon() + output.getDescription());
                     listCount += 1;
                 }
                 System.out.println(LINE_BREAK);
+            } else if (temp.toLowerCase().contains("done")) {
+                // user indicates item is done, mark item as done
+                String[] splitTemp = temp.split(" ");
+                int index = Integer.parseInt(splitTemp[1]) - 1; // reduce by 1 to match entries in toDoList
+                toDoList.get(index).setDone();
+                Task tempTask = toDoList.get(index);
+                System.out.println(LINE_BREAK);
+                System.out.println("    " + "Nice!, I've marked this task as done");
+                System.out.println("    " + tempTask.getStatusIcon() + " " + tempTask.getDescription());
+                System.out.println(LINE_BREAK);
             } else {
                 // user is not exiting duke, indicate item has been added
-                toDoList.add(temp);
+                Task newTask = new Task(temp);
+                toDoList.add(newTask);
                 System.out.println(LINE_BREAK);
-                System.out.println("added: " + temp);
+                System.out.println("    added: " + temp);
                 System.out.println(LINE_BREAK);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[\u2713] " : "[\u2718] "); //return tick or X symbols
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDone() {
+        isDone = true;
+    }
+}
