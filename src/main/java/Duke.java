1081755422def02246b678d392123a44b2fe4a23
zyleet <zylee@ymail.com>
2019-08-24 22:54:24 +0800

    Level-1, Testing commands from user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,23 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String separator = "____________________________________________________________";
         System.out.println("Hello from\n" + logo);
+        System.out.println(separator);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+
+        inputCommand();
+    }
+
+    private static void inputCommand() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        System.out.println("You entered " + number);
     }
 }
+
+
