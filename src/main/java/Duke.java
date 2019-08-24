a6eb81e3aab1561c43ecdfed2b466b8093dd0385
TehZiHuai <tehzihuai97@gmail.com>
2019-08-24 23:38:34 +0800

    Level-1: Greet, Echo,Exit duke greets user, and echoes commands to exit, type "bye" (not case sensitive)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+   public static final String LINE_BREAK = "___________________________";
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("How may I help you?");
+        System.out.println(LINE_BREAK);
+        boolean isExit = false;
+        Scanner userInput = new Scanner(System.in);
+
+        // begin repeated inputs of commands
+        while (!isExit) {
+            String temp = userInput.nextLine();
+            if (temp.toLowerCase().equals("bye")) {
+                // user indicated bye to exit duke
+                isExit = true;
+                System.out.println(LINE_BREAK);
+                System.out.println(temp);
+                System.out.println(LINE_BREAK);
+                userInput.close();
+            } else {
+                // user is not exiting duke, echo back the command
+                System.out.println(LINE_BREAK);
+                System.out.println(temp);
+                System.out.println(LINE_BREAK);
+            }
+        }
     }
 }
