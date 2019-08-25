08dca0a1f4786be26ba7087a1828ed94873cf119
Riyas97 <rockingriyas97@yahoo.com>
2019-08-25 22:33:52 +0800

    Level-5: Done with Exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,129 +6,219 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.CopyOnWriteArrayList;
+import Exception.DukeException;
 
 
 public class Duke {
 
     protected static List<Tasks> userToDoList = new ArrayList<>();
+    protected static String logo =
+            " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+    protected static String line = "   ____________________________________________________________";
+    protected static String space = "    ";
+    static int todolist_number = 1;
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws DukeException {
 
-        String logo =
+        /**String logo =
                 " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                        + "|  _ \\ _   _| | _____ \n"
+                        + "| | | | | | | |/ / _ \\\n"
+                        + "| |_| | |_| |   <  __/\n"
+                        + "|____/ \\__,_|_|\\_\\___|\n";
         String line = "   ____________________________________________________________";
         String space = "    ";
         System.out.println(line);
+         **/
         System.out.println(space + "Hello from\n" + logo + "\n" + space + "Hello I am " +
                 "Duke.");
+
         System.out.println(space + "What can I do for you?");
         System.out.println(line);
 
-        int todolist_number = 1;
 
         while (true) {
             Scanner in = new Scanner(System.in);
             String s = in.nextLine();
-
-            // when user type bye
+            String arr[] = s.split(" ", 2);
+            String firstWord = arr[0];
             if (s.equals("bye")) {
                 System.out.println(line + "\n" + space + "Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
-
-            } else if (s.equals("list")) {
-                //when user type list
-                System.out.println(line + "\n" + space + "Here are the tasks in your list:");
-                for (int i = 0; i < userToDoList.size(); i++) {
-                    String message = userToDoList.get(i).getDescription();
-                    int j = i + 1;
-                    System.out.println(space + j + ".[" + userToDoList.get(i).getType()
-                            + "][" + userToDoList.get(i).getStatusIcon()
-                            + "] " + message);
-                }
-                System.out.println(line);
-
-            } else if (s.startsWith("done")) { //when user type done
-                String[] tokens = s.split(" ");
-                int num = Integer.parseInt(tokens[1]);
-                userToDoList.get(num - 1).setDone(true);
-                System.out.println(line + "\n" + space + "Nice! I've marked this task as done:");
-                System.out.println(space + " [" + userToDoList.get(num - 1).getType()
-                        + "][" + userToDoList.get(num - 1).getStatusIcon()
-                        + "] " + userToDoList.get(num - 1).getDescription());
-                System.out.println(line);
-            } else if (s.startsWith("todo")) {
-                String joinTokens = s.substring(s.indexOf(" ")+ 1);
-                // System.out.println(joinTokens);
-                Tasks newToDo = new Todo(joinTokens, "T");
-                userToDoList.add(newToDo);
-                System.out.println(line + "\n" + space + "Got it. I've added this task:" + "\n" + space + " [" + userToDoList.get(todolist_number - 1).getType()
-                        + "][" + userToDoList.get(todolist_number - 1).getStatusIcon()
-                        + "] " + joinTokens);
-                if (todolist_number > 1) {
-                    System.out.println(space + "Now you have " + todolist_number + " tasks in the list.");
-                } else {
-                    System.out.println(space + "Now you have " + todolist_number + " task in the list.");
-                }
-                System.out.println(line);
-                todolist_number += 1;
-            } else if (s.startsWith("deadline")) {
-
-                String todoTask = s.substring(9, s.indexOf("/by"));
-                String time = s.substring(s.indexOf("/by") + 4);
-                //System.out.println(time);
-
-                Deadline task = new Deadline(todoTask, "D", time);
-                String newtodoTask = task.toMessage();
-                Tasks newToDo = new Deadline(newtodoTask, "D", time) ;
-                userToDoList.add(newToDo);
-
-                System.out.println(line + "\n" + space + "Got it. I've added this task:" + "\n" + space + " [" + userToDoList.get(todolist_number - 1).getType()
-                        + "][" + userToDoList.get(todolist_number - 1).getStatusIcon()
-                        + "] " + todoTask + "(by: " + time + ")");
-
-
-                if (todolist_number > 1) {
-                    System.out.println(space + "Now you have " + todolist_number + " tasks in the list.");
-                } else {
-                    System.out.println(space + "Now you have " + todolist_number + " task in the list.");
-                }
-                System.out.println(line);
-                todolist_number += 1;
-
-            } else if (s.startsWith("event")) {
-
-                //String todoTask = s.substring(6, s.indexOf("/at"));
-                String[] split = s.split("/at");
-                String todoTask = split[0].substring(6);
-                String time = s.substring(s.indexOf("/at") + 4);
-                //System.out.println(time);
-
-                Event task = new Event(todoTask, "E", time);
-                String newtodoTask = task.toMessage();
-
-                Tasks newToDo = new Event(newtodoTask, "E", time);
-                userToDoList.add(newToDo);
-
-                System.out.println(line + "\n" + space + "Got it. I've added this task:" + "\n" + space + " [" + userToDoList.get(todolist_number - 1).getType()
-                        + "][" + userToDoList.get(todolist_number - 1).getStatusIcon()
-                        + "] " + todoTask + "(at: " + time + ")");
-
-
-                if (todolist_number > 1) {
-                    System.out.println(space + "Now you have " + todolist_number + " tasks in the list.");
-                } else {
-                    System.out.println(space + "Now you have " + todolist_number + " task in the list.");
-                }
-                System.out.println(line);
-                todolist_number += 1;
-
             }
+            try {
+                switch (firstWord) {
+                    case "list":
+                    System.out.println(line + "\n" + space + "Here are the tasks in your list:");
+                    for (int i = 0; i < userToDoList.size(); i++) {
+                        String message = userToDoList.get(i).getDescription();
+                        int j = i + 1;
+                        System.out.println(space + j + ".[" + userToDoList.get(i).getType()
+                                + "][" + userToDoList.get(i).getStatusIcon()
+                                + "] " + message);
+                    }
+                    System.out.println(line);
+                    break;
+                    //================================================
+                    case "done":
+                        doneCommand(s);
+                        break;
+                    //================================================
+                    case "todo":
+                        todoCommand(s);
+                        break;
+                    //================================================
+                    case "deadline":
+                        deadlineCommand(s);
+                        break;
+                    //================================================
+                    case "event":
+                        eventCommand(s);
+                    break;
+                    //================================================
+                    default:
+                        throw DukeException.UNKNOWN_COMMAND;
+
+                }
+            } catch (DukeException e) {
+                System.out.println(line + "\n" + space + e.getError() + "\n" + line);
+            }
+        }
+    }
+
+    private static void eventCommand(String s) throws DukeException {
+        try {
+            String todoTask1 = s.substring(6, s.indexOf("/at"));
+            String time1 = s.substring(s.indexOf("/at") + 4);
+            if (todoTask1.isEmpty()) {
+                throw DukeException.EMPTY_TASK_IN_EVENT;
+            }
+            if (todoTask1.equals(" ")) {
+                throw DukeException.EMPTY_TASK_IN_EVENT;
+            }
+            if (time1.isEmpty()) {
+                throw DukeException.EMPTY_TIME_IN_EVENT;
+            }
+            if (time1.equals(" ")) {
+                throw DukeException.EMPTY_TIME_IN_EVENT;
+            }
+
+            Event task1 = new Event(todoTask1, "E", time1);
+            String newtodoTask1 = task1.toMessage();
+
+            Tasks newToDo1 = new Event(newtodoTask1, "E", time1);
+            userToDoList.add(newToDo1);
+
+            System.out.println(line + "\n" + space + "Got it. I've added this task:" + "\n" + space + " [" + userToDoList.get(todolist_number - 1).getType()
+                    + "][" + userToDoList.get(todolist_number - 1).getStatusIcon()
+                    + "] " + todoTask1 + "(at: " + time1 + ")");
+
+
+            if (todolist_number > 1) {
+                System.out.println(space + "Now you have " + todolist_number + " tasks in the list.");
+            } else {
+                System.out.println(space + "Now you have " + todolist_number + " task in the list.");
+            }
+            System.out.println(line);
+            todolist_number += 1;
+        } catch (StringIndexOutOfBoundsException e) {
+            throw DukeException.INVALID_FORMAT_IN_EVENT;
+        }
+    }
+
+
+    private static void deadlineCommand(String s) throws DukeException {
+        try {
+            String todoTask = s.substring(9, s.indexOf("/by"));
+            String time = s.substring(s.indexOf("/by") + 4);
+            if (todoTask.isEmpty()) {
+                throw DukeException.EMPTY_TASK_IN_DEADLINE;
+            }
+            if (todoTask.equals(" ")) {
+                throw DukeException.EMPTY_TASK_IN_DEADLINE;
+            }
+            if (time.isEmpty()) {
+                throw DukeException.EMPTY_TIME_IN_DEADLINE;
+            }
+            if (time.equals(" ")) {
+                throw DukeException.EMPTY_TIME_IN_DEADLINE;
+            }
+            Deadline task = new Deadline(todoTask, "D", time);
+            String newtodoTask = task.toMessage();
+            Tasks newToDo2 = new Deadline(newtodoTask, "D", time);
+            userToDoList.add(newToDo2);
+            System.out.println(line + "\n" + space + "Got it. I've added this task:" + "\n" + space + " [" + userToDoList.get(todolist_number - 1).getType()
+                    + "][" + userToDoList.get(todolist_number - 1).getStatusIcon()
+                    + "] " + todoTask + "(by: " + time + ")");
+            if (todolist_number > 1) {
+                System.out.println(space + "Now you have " + todolist_number + " tasks in the list.");
+            } else {
+                System.out.println(space + "Now you have " + todolist_number + " task in the list.");
+            }
+            System.out.println(line);
+            todolist_number += 1;
+
+        } catch (StringIndexOutOfBoundsException e) {
+            throw DukeException.INVALID_FORMAT_IN_DEADLINE;
+        }
+    }
+
+    private static void todoCommand(String s) throws DukeException {
+
+        try {
+            String joinTokens = s.substring(s.indexOf(" ") + 1);
+            if (joinTokens.equals(" ")) {
+                throw DukeException.EMPTY_TASK_IN_TODO;
+            }
+            if (s.length() == 4) {
+                throw DukeException.EMPTY_TASK_IN_TODO;
+            }
+            if (joinTokens.isEmpty()) {
+                throw DukeException.EMPTY_TASK_IN_TODO;
+            }
+
+            Tasks newToDo = new Todo(joinTokens, "T");
+            userToDoList.add(newToDo);
+            System.out.println(line + "\n" + space + "Got it. I've added this task:" + "\n" + space + " [" + userToDoList.get(todolist_number - 1).getType()
+                    + "][" + userToDoList.get(todolist_number - 1).getStatusIcon()
+                    + "] " + joinTokens);
+
+            if (todolist_number > 1) {
+                System.out.println(space + "Now you have " + todolist_number + " tasks in the list.");
+            } else {
+                System.out.println(space + "Now you have " + todolist_number + " task in the list.");
+            }
+            todolist_number += 1;
+            System.out.println(line);
+
+
+        } catch (DukeException e) {
+            System.out.println(line + "\n" + space + e.getError() + "\n" + line);
+        }
+    }
+
+    private static void doneCommand(String s) throws DukeException {
+        try {
+            String[] tokens = s.split(" ");
+            int num = Integer.parseInt(tokens[1]);
+            userToDoList.get(num - 1).setDone(true);
+            System.out.println(line + "\n" + space + "Nice! I've marked this task as done:");
+            System.out.println(space + " [" + userToDoList.get(num - 1).getType()
+                    + "][" + userToDoList.get(num - 1).getStatusIcon()
+                    + "] " + userToDoList.get(num - 1).getDescription());
+            System.out.println(line);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw DukeException.TASK_NO_MISSING;
+        } catch (NumberFormatException e) {
+            throw DukeException.TASK_DOES_NOT_EXIST;
+        } catch (IndexOutOfBoundsException e) {
+            throw DukeException.TASK_DOES_NOT_EXIST;
         }
     }
 }
---
+++ src/main/java/Exception/DukeException.java
@@ -0,0 +1,28 @@
+package Exception;
+
+public class DukeException extends Exception{
+
+    public static final DukeException UNKNOWN_COMMAND = new DukeException("☹ OOPS!!! I'm sorry, but I don't understand what you mean :-(");
+    public static final DukeException EMPTY_TASK_IN_TODO = new DukeException(" ☹ OOPS!!! The description of a todo cannot be empty, e.g. todo borrow book.");
+    public static final DukeException EMPTY_TASK_IN_DEADLINE = new DukeException(" ☹ OOPS!!! The description of a deadline cannot be empty, e.g. deadline return book /by Sunday");
+    public static final DukeException INVALID_FORMAT_IN_DEADLINE = new DukeException(" ☹ OOPS!!! Invalid format. Please key in the task in the correct structure, e.g. deadline return book /by Sunday");
+    public static final DukeException EMPTY_TIME_IN_DEADLINE = new DukeException("☹ OOPS!!! Task must have a valid deadline, e.g. deadline return book /by Sunday");
+    public static final DukeException EMPTY_TASK_IN_EVENT = new DukeException(" ☹ OOPS!!! The description of a event cannot be empty, e.g. event project meeting /at Mon 2-4pm");
+    public static final DukeException EMPTY_TIME_IN_EVENT = new DukeException(" ☹ OOPS!!! Task must have a valid time, e.g. event project meeting /at Mon 2-4pm");
+    public static final DukeException INVALID_FORMAT_IN_EVENT = new DukeException(" ☹ OOPS!!! Invalid format. Please key in the task in the correct structure, e.g. event project meeting /at Mon 2-4pm");
+    public static final DukeException TASK_DOES_NOT_EXIST = new DukeException("☹ OOPS!!! Task does not exist.");
+    public static final DukeException TASK_NO_MISSING = new DukeException("☹ OOPS!!! Please provide a task number, e.g. done 1.");
+
+
+    private String message;
+
+    private DukeException(String message) {
+        this.message = message;
+    }
+
+    public String getError() {
+        return message;
+    }
+
+
+}
