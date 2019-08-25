443eab0131ef5239592a7e5df996107bd2f20af9
zyleet <zylee@ymail.com>
2019-08-25 12:22:43 +0800

    Level-3, Added Task Class with methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
---
+++ src/main/java/Task.java
@@ -0,0 +1,17 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) { //initialization
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void markAsDone() { //marks a task as done
+        this.isDone = true;
+    }
+}
