970eb105196e12ef939724943a375cd0799ef664
zylee <zylee@ymail.com>
2019-08-27 15:58:40 +0800

    Level-4: Cleaning up main duke class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,29 +3,27 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String SEPARATOR = "____________________________________________________________";
+
         System.out.println("Hello from\n" + logo);
-        System.out.println(SEPARATOR);
+        separator();
         System.out.println("Hello! I'm Duke"); //introduction
         System.out.println("What can I do for you?");
-        System.out.println(SEPARATOR);
+        separator();
 
-        String myString = inputCommand(); //it will either be the description or "done [some number]"
+        String myString = inputCommand(); //get raw input from user
         ArrayList<Task> myList = new ArrayList<>(); //Instantiate an array list of a dynamic size and class Task
 
         // as long as input is not bye, keep running
         while (!myString.equals("bye")) {
-            System.out.println(SEPARATOR);
+            separator();
             if (myString.equals("list")) { //request for list, output the storage
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < myList.size(); i++) { //Standard for-each loop: for (String element: myList)
-                    System.out.println((i + 1) + "." + myList.get(i).getStatusIcon());
-                }
+                getList(myList);
             }
             //if not requesting for list, check for done, event, todos, deadline
             else {
@@ -40,14 +38,12 @@ public class Duke {
                     System.out.println("Nice! I've marked this task as done:");
                     System.out.println((taskNumber) + "." + myList.get(taskNumber - 1).getStatusIcon());
                 }
+
                 //First word is not done, hence the user is adding a task
                 //check if its a todos, adds a standard task description with no timing
                 else if (bufferArray[0].equals("todo")) {
                     Task newTask = new Todo(bufferArray[1]); //Create a new todos with description from user input
-                    myList.add(newTask); //adds the task to the list
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(newTask.getStatusIcon());
-                    System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                    addToList(newTask, myList);
 
                 }
                 //check if its a deadline, there will be a "/by "
@@ -60,12 +56,9 @@ public class Duke {
 
                     //Create a new deadline with description from user input
                     Task newTask = new Deadline(bufferDescription[1], bufferDeadline[1]);
-                    myList.add(newTask); //adds the task to the list
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(newTask.getStatusIcon());
-                    System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                    addToList(newTask, myList);
                 }
-                //check if its an event
+                //check if its an event, there should be a "/at "
                 else if (bufferArray[0].equals("event")) {
 
                     //Split the input into 2, before and after /at
@@ -75,28 +68,17 @@ public class Duke {
 
                     //Create a new deadline with description from user input
                     Task newTask = new Event(bufferDescription[1], bufferEvent[1]);
-                    myList.add(newTask); //adds the task to the list
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(newTask.getStatusIcon());
-                    System.out.println("Now you have " + myList.size() + " tasks in the list.");
-                }
-
-
-                //first word is not "done", hence it is a task description
-                else {
-                    System.out.println("Added: " + myString);
-                    Task newTask = new Task(myString); //Create a new task with description from user input
-                    myList.add(newTask); //adds the task to the list
+                    addToList(newTask, myList);
                 }
             }
-            System.out.println(SEPARATOR);
+            separator();
             myString = inputCommand(); //after processing command, wait for new input form user
         }
 
         //indicates end of duke
-        System.out.println(SEPARATOR);
+        separator();
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(SEPARATOR);
+        separator();
     }
 
     private static String inputCommand() { //read input and returns that input to be processed in main
@@ -104,6 +86,28 @@ public class Duke {
 
         return input.nextLine();
     }
+
+    private static void separator() {
+        String SEPARATOR = "____________________________________________________________";
+        System.out.println(SEPARATOR);
+    }
+
+    //Method to add a task to a list and output the size of list its size
+    private static void addToList(Task taskData, ArrayList<Task> myTasks) {
+        myTasks.add(taskData);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(taskData.getStatusIcon());
+        System.out.println("Now you have " + myTasks.size() + " task(s) in the list.");
+    }
+
+    //Method to get the tasks in a list
+    private static void getList(ArrayList<Task> myTasks) {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < myTasks.size(); i++) { //Standard for-each loop: for (String element: myList)
+            System.out.println((i + 1) + "." + myTasks.get(i).getStatusIcon());
+        }
+    }
+
 }
 
 
