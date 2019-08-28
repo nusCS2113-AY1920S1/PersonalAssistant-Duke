2284eefd5b0b359049043d97f22c04aa35f69e99
tejas123 <vasavada.tejas@gmail.com>
2019-08-28 19:15:16 +0800

    Handled some errors related to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -55,81 +55,126 @@ public class Duke {
     private static void takeInput() {
         Scanner sc = new Scanner(System.in);
         String str;
-        //To continue taking inputs until the user says bye
         while (true) {
             str = sc.nextLine();
-            printSpaces();
-            printDash();
-            printSpaces();
-            List<String> splitInput = new ArrayList<String>(
-                    Arrays.asList(str.split(" ")));
             if (str.equals("bye")) {
+                printSpaces();
+                printDash();
+                printSpaces();
                 System.out.println(" Bye. Hope to see you again"
                         + " soon.");
                 printSpaces();
                 printDash();
                 break;
             }
-
-            //to print the list
-            else if (str.equals("list")) {
-                printList();
+            try {
+                processInput(str);
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+                printSpaces();
+                printDash();
             }
+        }
+    }
 
-            //marking a task as done
-            else if (splitInput.get(0).equals("done")) {
-                int temp =  Integer.parseInt(splitInput.get(1));
+    private static void processInput(String str) throws DukeException {
+        Scanner sc = new Scanner(System.in);
+        //To process the various forms on input given to DUKE
+        printSpaces();
+        printDash();
+        printSpaces();
+        List<String> splitInput = new ArrayList<String>(
+                Arrays.asList(str.split(" ")));
+
+        //to print the list
+        if (str.equals("list")) {
+            printList();
+        }
+
+        //marking a task as done
+        else if (splitInput.get(0).equals("done")) {
+            try {
+                int temp = Integer.parseInt(splitInput.get(1));
                 printTaskDone(temp - 1);
+            } catch (NumberFormatException obj) {
+                throw new DukeException("OOPS! Enter a "
+                        + "positive integer after \"done\"");
+            } catch (IndexOutOfBoundsException obj) {
+                throw new DukeException("OOPS! Enter a number that "
+                        + "is present in the list");
             }
+        }
 
-            //adding a task which has no timing constraints
-            else if (splitInput.get(0).equals("todo")) {
+        //adding a task which has no timing constraints
+        else if (splitInput.get(0).equals("todo")) {
+            try {
                 storeList.add(new Todo(str.substring(5)));
                 addTask(storeList.get(storeList.size() - 1));
             }
-
-            //adding a task that has a deadline
-            else if (splitInput.get(0).equals("deadline")) {
-                int i;
-                int k = 0;
-                String split1 = "";
-                String split2 = "";
-                for (i = 1; i < splitInput.size(); i++) {
-                    if (splitInput.get(i).equals("/by")) {
-                        k = 1;
-                        continue;
-                    }
-                    if (k == 0) {
-                        split1 += splitInput.get(i) + " ";
-                    } else {
-                        split2 += splitInput.get(i) + " ";
-                    }
-                }
-                storeList.add(new Deadline(split1.trim(), split2.trim()));
-                addTask(storeList.get(storeList.size() - 1));
+            catch (StringIndexOutOfBoundsException e) {
+                throw new DukeException("OOPS! The description of"
+                        + " a todo list cannot be empty");
             }
+        }
 
-            //adding an event to the list
-            else if (splitInput.get(0).equals("event")) {
-                int i;
-                int k = 0;
-                String split1 = "";
-                String split2 = "";
-                for (i = 1; i < splitInput.size(); i++) {
-                    if (splitInput.get(i).equals("/at")) {
-                        k = 1;
-                        continue;
-                    }
-                    if (k == 0) {
-                        split1 += splitInput.get(i) + " ";
-                    } else {
-                        split2 += splitInput.get(i) + " ";
-                    }
-                }
-                storeList.add(new Event(split1.trim(), split2.trim()));
-                addTask(storeList.get(storeList.size() - 1));
+        //adding a task that has a deadline
+        else if (splitInput.get(0).equals("deadline")) {
+            int i;
+            int k = 0;
+            String split1 = "";
+            String split2 = "";
+            if (splitInput.size() == 1) {
+                throw new DukeException("OOPS! The description of deadline"
+                       + " cannot be empty");
             }
+            for (i = 1; i < splitInput.size(); i++) {
+                if (splitInput.get(i).equals("/by")) {
+                    k = 1;
+                    continue;
+                }
+                if (k == 0) {
+                    split1 += splitInput.get(i) + " ";
+                } else {
+                    split2 += splitInput.get(i) + " ";
+                }
+            }
+            if (k == 0) {
+                throw new DukeException("Please make sure that you have "
+                        + "entered \"/by\" to separate task and time");
+            }
+            storeList.add(new Deadline(split1.trim(), split2.trim()));
+            addTask(storeList.get(storeList.size() - 1));
+        }
 
+        //adding an event to the list
+        else if (splitInput.get(0).equals("event")) {
+            int i;
+            int k = 0;
+            String split1 = "";
+            String split2 = "";
+            if (splitInput.size() == 1) {
+                throw new DukeException("OOPS! the description for an "
+                      + "event cannot be empty");
+            }
+            for (i = 1; i < splitInput.size(); i++) {
+                if (splitInput.get(i).equals("/at")) {
+                    k = 1;
+                    continue;
+                }
+                if (k == 0) {
+                    split1 += splitInput.get(i) + " ";
+                } else {
+                    split2 += splitInput.get(i) + " ";
+                }
+            }
+            if (k == 0) {
+                throw new DukeException("Please make sure you have used "
+                       +  "\"at\" to separate task and time");
+            }
+            storeList.add(new Event(split1.trim(), split2.trim()));
+            addTask(storeList.get(storeList.size() - 1));
+        } else {
+            throw new DukeException("Please enter a valid command");
         }
     }
     /** This function is responsible for the proper execution
