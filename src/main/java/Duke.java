bd9590e8e681743a1e7a0cb133820f9924ac0103
tejas123 <vasavada.tejas@gmail.com>
2019-08-28 17:30:13 +0800

    Added level-4 using inheritance

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Duke {
     private static final int LINE_LENGTH = 50;
     private static final int NUM_SPACE = 4;
     private static ArrayList<Task> storeList =
-             new ArrayList<Task>();
+             new ArrayList<>();
 
     private static void printDash() {
         for (int i = 0; i < LINE_LENGTH; i++) {
@@ -22,10 +24,8 @@ public class Duke {
 
     private static void printList() {
         for (int i = 0; i < storeList.size(); i++) {
-            System.out.print(" " + (i + 1) + ". " +"["
-                    + storeList.get(i).getStatusIcon());
-            System.out.println("] "
-                    + storeList.get(i).description);
+            System.out.println(" " + (i + 1) + ". "
+                    + storeList.get(i).toString());
             printSpaces();
         }
         printDash();
@@ -36,8 +36,18 @@ public class Duke {
                 + "task as done:");
         printSpaces();
         storeList.get(num).markAsDone();
-        System.out.println(" [" + "\u2713"
-                + "] " + storeList.get(num).description);
+        System.out.println(" " + storeList.get(num).toString());
+        printSpaces();
+        printDash();
+    }
+
+    private static void addTask(Task taskA) {
+        System.out.println(" Got it. I have added this task:");
+        printSpaces();
+        System.out.println("  " + taskA.toString());
+        printSpaces();
+        System.out.println(" Now you have " + storeList.size()
+                + " tasks in the list.");
         printSpaces();
         printDash();
     }
@@ -51,24 +61,75 @@ public class Duke {
             printSpaces();
             printDash();
             printSpaces();
+            List<String> splitInput = new ArrayList<String>(
+                    Arrays.asList(str.split(" ")));
             if (str.equals("bye")) {
                 System.out.println(" Bye. Hope to see you again"
                         + " soon.");
                 printSpaces();
                 printDash();
                 break;
-            } else if (str.equals("list")) {
-                printList();
-                continue;
-            } else if (str.length() > 3 && str.substring(0,4).equals("done")) {
-                int temp =  Integer.parseInt(str.substring(5));
-                printTaskDone(temp - 1);
-                continue;
             }
-            storeList.add(new Task(str));
-            System.out.println(" added: " + str);
-            printSpaces();
-            printDash();
+
+            //to print the list
+            else if (str.equals("list")) {
+                printList();
+            }
+
+            //marking a task as done
+            else if (splitInput.get(0).equals("done")) {
+                int temp =  Integer.parseInt(splitInput.get(1));
+                printTaskDone(temp - 1);
+            }
+
+            //adding a task which has no timing constraints
+            else if (splitInput.get(0).equals("todo")) {
+                storeList.add(new Todo(str.substring(5)));
+                addTask(storeList.get(storeList.size() - 1));
+            }
+
+            //adding a task that has a deadline
+            else if (splitInput.get(0).equals("deadline")) {
+                int i;
+                int k = 0;
+                String split1 = "";
+                String split2 = "";
+                for (i = 1; i < splitInput.size(); i++) {
+                    if (splitInput.get(i).equals("/by")) {
+                        k = 1;
+                        continue;
+                    }
+                    if (k == 0) {
+                        split1 += splitInput.get(i) + " ";
+                    } else {
+                        split2 += splitInput.get(i) + " ";
+                    }
+                }
+                storeList.add(new Deadline(split1.trim(), split2.trim()));
+                addTask(storeList.get(storeList.size() - 1));
+            }
+
+            //adding an event to the list
+            else if (splitInput.get(0).equals("event")) {
+                int i;
+                int k = 0;
+                String split1 = "";
+                String split2 = "";
+                for (i = 1; i < splitInput.size(); i++) {
+                    if (splitInput.get(i).equals("/at")) {
+                        k = 1;
+                        continue;
+                    }
+                    if (k == 0) {
+                        split1 += splitInput.get(i) + " ";
+                    } else {
+                        split2 += splitInput.get(i) + " ";
+                    }
+                }
+                storeList.add(new Event(split1.trim(), split2.trim()));
+                addTask(storeList.get(storeList.size() - 1));
+            }
+
         }
     }
     /** This function is responsible for the proper execution
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,16 +1,24 @@
 public class Task {
     protected String description;
     protected boolean isDone;
-    private final String TICKS = "\u2713";
-    private final String CROSS = "\u2718";
+    private final String ticks = "\u2713";
+    private final String cross = "\u2718";
+
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
+
     public String getStatusIcon() {
-        return (isDone? TICKS: CROSS);
+        return (isDone ? ticks : cross);
     }
+
     public void markAsDone() {
         this.isDone = true;
+    }
+
+    public String toString() {
+        return ("[" + getStatusIcon() + "] "
+                + description);
     }
 }
