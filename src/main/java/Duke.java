daf02ba0f61cf0a3feb68b63fbb91b29bbc556ea
tejas123 <vasavada.tejas@gmail.com>
2019-08-28 11:41:09 +0800

    added level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static final int LINE_LENGTH = 50;
     private static final int NUM_SPACE = 4;
+    private static ArrayList<String> storeList =
+             new ArrayList<String>();
 
     private static void printDash() {
         for (int i = 0; i < LINE_LENGTH; i++) {
@@ -15,6 +18,15 @@ public class Duke {
         for (int i = 0; i < NUM_SPACE; i++) {
             System.out.print(" ");
         }
+    }
+
+    private static void printList() {
+        for (int i = 0; i < storeList.size(); i++) {
+            System.out.println(" " + (i + 1) + ". "
+                    + storeList.get(i));
+            printSpaces();
+        }
+        printDash();
     }
 
     private static void takeInput() {
@@ -32,12 +44,19 @@ public class Duke {
                 printSpaces();
                 printDash();
                 break;
+            } else if (str.equals("list")) {
+                printList();
+                continue;
             }
-            System.out.println(" " + str);
+            storeList.add(str);
+            System.out.println(" added: " + str);
             printSpaces();
             printDash();
         }
     }
+    /** This function is responsible for the proper execution
+     *  of Duke.
+     */
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -48,8 +67,8 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.print("____");
         printDash();
-        System.out.println("Hello! I am Duke \n" +
-                "What can I do for you?");
+        System.out.println("Hello! I am Duke \n"
+                + "What can I do for you?");
         System.out.print("____");
         printDash();
         takeInput();
