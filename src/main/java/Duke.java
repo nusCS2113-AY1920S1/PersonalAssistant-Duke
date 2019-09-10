6c8f165a9b2b05bbc5b581364d932fd25870fbc9
Sfloydzy <42894517+Sfloydzy@users.noreply.github.com>
2019-09-10 12:59:55 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke\n" +
+        "What can I do for you?");
     }
 }
