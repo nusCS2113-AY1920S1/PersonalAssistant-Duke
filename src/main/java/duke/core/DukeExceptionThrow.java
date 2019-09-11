9cdfbd2eefe8405effedfa1a4a57e0174f75724d
HUANG XUAN KUN <hxk123123123@gmail.com>
2019-09-11 16:47:54 +0800

    Added prefix "Oops" to warning of DukeExceptionThrow

--- src/main/java/duke/core/DukeExceptionThrow.java
+++ src/main/java/duke/core/DukeExceptionThrow.java
@@ -3,6 +3,6 @@ package duke.core;
 public class DukeExceptionThrow extends Exception
 {
     public DukeExceptionThrow(String message) {
-        super(message);
+        super("Oops !!!" + message);
     }
 }
