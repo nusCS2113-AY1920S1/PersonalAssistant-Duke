b60c0eef7889481fec003c06be8a8b13021f31ad
JasonLeeWeiHern <e0309651@u.nus.edu>
2019-09-11 16:39:02 +0800

    Jason push

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class DukeTest {
-
     @Test
     public void testUi() {
         Ui ui = new Ui();
