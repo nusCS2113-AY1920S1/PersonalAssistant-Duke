3ac01a03d8052f037853b3f087932206f25b15ad
JasonLeeWeiHern <e0309651@u.nus.edu>
2019-09-11 16:58:44 +0800

    Cr8 new UI package

---
+++ src/main/java/UI/Ui.java
@@ -0,0 +1,34 @@
+package UI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ui {
+    protected String FullCommand;
+
+    protected String ReadCommand() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FullCommand = reader.readLine();
+        return FullCommand;
+    }
+
+    protected String showWelcome() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        return logo;
+    }
+    protected void showDateFormatError (){
+        System.err.println("Date Time has to be in YYYY-MM-DD HH:mm:ss format");
+    }
+    protected  void showIOErrorMessage(Exception e) {
+        System.err.println("An IOException was caught :"+e.getMessage());
+    }
+    protected void showErrorMessage(Exception e){
+        System.err.println(e.getMessage());
+    }
+}
