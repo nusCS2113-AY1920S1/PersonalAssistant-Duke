fdffe88d5205b3e1f84d6e010bea0132f0e31bc0
tejas123 <vasavada.tejas@gmail.com>
2019-09-11 19:22:18 +0800

    Fix checkstyle errors

--- src/main/java/duke/command/AddDeadlineCommand.java
+++ src/main/java/duke/command/AddDeadlineCommand.java
@@ -16,7 +16,7 @@ public class AddDeadlineCommand extends Command {
     }
 
     @Override
-    public void execute(TaskList tasks,Ui ui,FileHandling storage)throws DukeException{
+    public void execute(TaskList tasks,Ui ui,FileHandling storage)throws DukeException {
         int i;
         int k = 0;
         String split1 = "";
