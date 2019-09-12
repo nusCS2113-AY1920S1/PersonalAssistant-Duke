73a2dc9e588f9b16f969842460d9d696ad1d2886
Ryan Tay <tay.yee.en@gmail.com>
2019-09-12 16:39:21 +0800

    Sample duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +11,32 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greeting();
+        userCmd();
+    }
+
+    public static void greeting() {
+        String line = "_____________________________________________\n";
+        System.out.println("Hello I'm Duke\n" + "What can I do for you?\n" + line);
+    }
+
+    public static void userCmd() {
+        Scanner input = new Scanner(System.in);
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy HHmm");
+        formatDate.setLenient(false);
+
+        while (true) {
+            String userCmd = input.nextLine();
+            int indexSpace = userCmd.indexOf(" ");
+            String command = (indexSpace != -1) ? userCmd.substring(0, indexSpace) : userCmd;
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println("I do not recogise this command. Please try again");
+                continue;
+            }
+        }
     }
 }
