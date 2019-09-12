d03eeab2c3faa30af11483c8840f8815ab7b689f
zyleet <zylee@ymail.com>
2019-09-12 14:05:01 +0800

    A-JUnit: add test cases for each classes

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -3,8 +3,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
+
     @Test
-    public void dummyTest(){
-        assertEquals(2, 2);
+    void getSave() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void getResponse() {
     }
 }
---
+++ src/test/java/TodoTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoTest {
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void getStatusIcon() {
+    }
+}
