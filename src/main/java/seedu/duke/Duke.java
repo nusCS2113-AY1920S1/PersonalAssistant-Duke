dfde57e7250ee40596a151df95d6539adaec543e
JokerY <770792058@qq.com>
2019-09-12 12:08:07 +0800

    Initial Codebase

---
+++ src/main/java/seedu/duke/Duke.java
@@ -0,0 +1,46 @@
+package seedu.duke;
+
+import seedu.duke.command.Command;
+import seedu.duke.command.ExitCommand;
+
+import java.util.Scanner;
+
+/**
+ * The main class of the program, which provides the entry point.
+ */
+public class Duke {
+    private static TaskList taskList;
+    private static UI ui;
+
+    /**
+     * The main function of the program, which is the entry point.
+     *
+     * @param args the arguments from the console when running
+     */
+    public static void main(String[] args) {
+        ui = new UI();
+        run();
+    }
+
+    public static TaskList getTaskList() {
+        return taskList;
+    }
+
+    public static UI getUI() {
+        return ui;
+    }
+
+    private static void run() {
+        taskList = Storage.readTasks();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Command command = Parser.parseCommand(input);
+        while (!(command instanceof ExitCommand)) {
+            command.execute();
+            input = scanner.nextLine();
+            command = Parser.parseCommand(input);
+        }
+        Storage.saveTasks(taskList);
+        ui.showMessage("Bye. Hope to see you again!");
+    }
+}
---
+++ src/main/java/seedu/duke/task/Event.java
@@ -0,0 +1,56 @@
+package seedu.duke.task;
+
+import java.util.Date;
+
+/**
+ * Event class is a typ of task with a date/time when the event is going to happen.
+ */
+public class Event extends Task {
+    private Date time;
+
+    /**
+     * Instantiate the Event class with name and time. Time must be passed in during the instantiation as it
+     * cannot be changed later.
+     *
+     * @param name name of the Event
+     * @param time time of the Event that is going to happen
+     */
+    public Event(String name, Date time) {
+        super(name);
+        this.time = time;
+        this.taskType = TaskType.Event;
+    }
+
+    /**
+     * Function that convert the Event to a human readable string containing important information about the
+     * Event, including the type and time of this Event.
+     *
+     * @return a human readable string containing the important information
+     */
+    @Override
+    public String toString() {
+        return "[E]" + this.getStatus() + " (at: " + formatDate() + ")";
+    }
+
+    /**
+     * Functions that output a string with all the information of this Event to be stored in a file for future
+     * usage.
+     *
+     * @return a string containing all information of this Event
+     */
+    @Override
+    public String toFileString() {
+        return (this.isDone ? "1" : "0") + " event " + this.name + " /at "
+                + formatDate();
+    }
+
+    /**
+     * A function that outputs a formatted string of the time of this Event. The format is the same as input
+     * format and is shared by all tasks.
+     *
+     * @return a formatted string of the time of this Event
+     */
+    protected String formatDate() {
+        return format.format(this.time);
+    }
+}
