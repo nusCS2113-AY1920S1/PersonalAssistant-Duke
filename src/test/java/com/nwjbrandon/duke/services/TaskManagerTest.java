32203e81e77c975210abe43cfd20ad600af3d39a
nwjbrandon <nwjbrandon@gmail.com>
2019-09-15 17:48:28 +0800

    redo junit test for io

--- src/test/java/com/nwjbrandon/duke/services/TaskManagerTest.java
+++ src/test/java/com/nwjbrandon/duke/services/TaskManagerTest.java
@@ -1,19 +1,24 @@
 package com.nwjbrandon.duke.services;
 
-import com.nwjbrandon.duke.TestExtender;
+import com.nwjbrandon.duke.SystemIO;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-class TaskManagerTest extends TestExtender {
+class TaskManagerTest extends SystemIO {
+
+    @RegisterExtension
+    SystemIO io = new SystemIO();
 
     @Test
     void testTodo() {
         final String testString = "todo borrow book";
-        provideInput(testString);
+        io.provideInput(testString);
         TaskManager taskManager = new TaskManager();
         taskManager.run();
-        String output = getOutput();
+        String output = io.getOutput();
         String expected = "\t____________________________________________________________\n"
                         + "\t Got it. I've added this task:\n"
                         + "\t   [T][✗] borrow book\n"
