faf8878d1a13d5584d1477b3c24cfb8ba739b846
RusdiHaizim <rusdihaizim@gmail.com>
2019-09-15 14:53:40 +0800

    B-FindFreeTimes: +Added FreeTimeCommand +Editted Parser to include 'freetime <TIME_IN_HOURS/>' command +Editted Command() to include enums for ReminderCommand and FreeTimeCommand +Included natty for Event class

--- src/main/java/duke/tasks/Event.java
+++ src/main/java/duke/tasks/Event.java
@@ -1,8 +1,13 @@
 package duke.tasks;
 
+import com.joestelmach.natty.DateGroup;
+import com.joestelmach.natty.Parser;
+import duke.DukeException;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class Event extends Task {
     private String at;
@@ -14,16 +19,16 @@ public class Event extends Task {
      *                    of the task inputted by user
      * @param at The details of when task is to be done
      */
-    public Event(String description, String at) {
+    public Event(String description, String at) throws DukeException {
         super(description);
         this.at = at;
         taskType = TaskType.EVENT;
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HHmm");
-        sdf.setLenient(false);
         try {
-            dateNow = sdf.parse(at);
-        } catch (ParseException e) {
-            //throw new DukeException("Tasks.Task does not have dd/MM/yyyy HHmm date-time format!");
+            Parser parser = new Parser();
+            List<DateGroup> groups = parser.parse(at);
+            dateNow = groups.get(0).getDates().get(0);
+        } catch (Exception e) {
+            throw new DukeException("   Date cannot be parsed: " + at);
         }
     }
 
