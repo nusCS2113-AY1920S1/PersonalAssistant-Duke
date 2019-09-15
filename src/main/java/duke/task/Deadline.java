f2448926fac0fae2dee9778975da3998ca068802
WEIFENG-NUSCEG <weifeng5655@hotmail.com>
2019-09-16 00:55:11 +0800

    added the reminder function
    The reminder function will keep repeating the task reminder every 20 seconds if the duration between the current time and the deadline is less than one day. To stop the reminder the user have to mark the task as done.

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -23,6 +23,7 @@ public class Deadline extends Task {
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        super.updateLocalDateTime(by);
     }
 
     /**
