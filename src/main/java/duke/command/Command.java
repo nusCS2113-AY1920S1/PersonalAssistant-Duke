6fa7d5d242a7abf7a2a1aec8cc73d0c91d376bba
wjlingg <e0309488@u.nus.edu>
2019-09-15 11:23:49 +0800

    A-Packages: Divide classes into packages

---
+++ src/main/java/duke/command/Command.java
@@ -0,0 +1,16 @@
+package duke.command;
+
+import duke.exception.DukeException;
+import duke.storage.Storage;
+import duke.tasklist.TaskList;
+import duke.ui.Ui;
+
+/**
+ * Abstract class to represent command
+ */
+public abstract class Command {
+    protected String userInputCommand;
+
+    public abstract void execute(TaskList taskList, Ui ui, Storage storage) throws DukeException;
+    public abstract boolean isExit();
+}
