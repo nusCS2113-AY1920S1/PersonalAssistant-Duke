ee5e01cd96b022b754e0fa011acf4dbfed9543c5
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2019-09-15 17:01:16 +0800

    Added test for After class

---
+++ src/test/java/AfterTest.java
@@ -0,0 +1,34 @@
+import Task.After;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Represents a test for After class.
+ */
+public class AfterTest {
+    @Test
+    public void afterTest() {
+        String cross = "\u2718";
+        System.out.println("Start afterTest");
+
+        /**
+         * First test is for after a specific task
+         */
+        String info = "return book";
+        String endDate = "exam is done";
+        After after = new After(info, false, endDate);
+        assertEquals("[A][" + cross + "] return book (after: exam is done)",after.toString());
+
+        /**
+         * Second test is for after a specific date
+         */
+        String info2 = "buy bread";
+        String endDate2 = "15/9/2019";
+        After after2 = new After(info2, false, endDate2);
+        assertEquals("[A][" + cross + "] buy bread (after: 15/9/2019)",after2.toString());
+
+        System.out.println("Passed afterTest");
+    }
+}
