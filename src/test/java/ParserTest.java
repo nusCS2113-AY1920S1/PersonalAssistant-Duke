c5a26c27d78dc65fc39b1a0c521919dd58c1ea1b
danielcyc <daniel.c.y.cyc@gmail.com>
2019-09-17 20:12:44 +0800

    changed startup screen

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,6 +1,6 @@
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Tests the Parser class by feeding it an input String and comparing it against the
@@ -22,48 +22,46 @@ public class ParserTest {
      * Tests the Parser by feeding it a String input to create a Deadline, and comparing it
      * against the ArrayList of String commands that we should obtain.
      */
-    //@Test
+    @Test
     public void testDeadline() {
-        //try {
+        try {
             testOutput.add("deadline");
             testOutput.add("cry");
             testOutput.add("23/01/2019 1800");
-            //assertEquals(testOutput, parser.parseInput("deadline cry /by 23/01/2019 1800"));
-       // }
-        //catch (DukeException e) {
-          //  e.showError();
-        //}
+            assertEquals(testOutput, parser.parseInput("deadline cry /by 23/01/2019 1800"));
+        } catch (DukeException e) {
+            e.showError();
+        }
     }
 
     /**
      * Tests the Parser by feeding it a String input to create an Event, and comparing it
      * against the ArrayList of String commands that we should obtain.
      */
-    //@Test
+    @Test
     public void testEvent() {
-       // try {
+        try {
             testOutput.add("event");
             testOutput.add("cry");
             testOutput.add("23/01/2019 1800");
-            //assertEquals(testOutput, parser.parseInput("event cry /at 23/01/2019 1800"));
-
-        //catch (DukeException e) {
-          //  e.showError();
-        //}
+            assertEquals(testOutput, parser.parseInput("event cry /at 23/01/2019 1800"));
+        } catch (DukeException e) {
+            e.showError();
+        }
     }
 
     /**
      * Tests the Parser by feeding it a String input to create a Todo, and comparing it
      * against the ArrayList of String commands that we should obtain.
      */
-    //@Test
+    @Test
     public void testTodo() {
-       // try {
+        try {
             testOutput.add("todo");
             testOutput.add("cry");
-            //assertEquals(testOutput, parser.parseInput("todo cry"));
+            assertEquals(testOutput, parser.parseInput("todo cry"));
+        } catch (DukeException e) {
+            e.showError();
         }
-        //catch (DukeException e) {
-           // e.showError();
-       // }
     }
+}
