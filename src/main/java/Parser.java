95b2a5fa118529fbd3be4e66dfb39524365c3a0c
AmosChan97 <amoszzchan@outlook.com>
2019-09-18 00:50:36 +0800

    Resolve merge conflicts

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,15 +1,4 @@
-import command.ByeCommand;
-import command.Command;
-import command.DeadlineCommand;
-import command.DeleteCommand;
-import command.DoneCommand;
-import command.EventCommand;
-import command.FindCommand;
-import command.ListCommand;
-import command.SnoozeCommand;
-import command.TodoCommand;
-import command.UnSnoozeCommand;
-import command.ViewScheduleCommand;
+import command.*;
 import exception.DukeException;
 
 import java.text.ParseException;
