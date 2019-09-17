287bd1948f1f6cd7c1144cfebb726f794d6b03bf
Mohammad Yousuf Minhaj Zia <e0381439@u.nus.edu>
2019-09-17 22:04:10 +0800

    added Checkstyle plugin

--- src/main/java/seedu/duke/command/Parser.java
+++ src/main/java/seedu/duke/command/Parser.java
@@ -4,87 +4,78 @@ import seedu.duke.task.TaskList;
 import seedu.duke.ui.Ui;
 
 /**
- * Takes raw user input as string, makes sense out of the input using regex and then performs operations based on the input.
+ * Takes raw user input as string, makes sense out of the input using
+ * regex and then performs operations based on the input.
  */
 public class Parser {
-  
-  /**
-   * Takes raw input and splits it into task type (eg. todo) and task description (eg. finish work). In cases like task type: list, bye,
-   * the output array only contains task type.
-   *
-   * @param raw_input users single line string input
-   * @return an array of Strings of length 2 or 1, either task type and task description or just task type based on raw input.
-   */
-  public String[] split (String raw_input) {
-    String[] user_input = raw_input.split(" ", 2);
-    return user_input;
-  }
-  
-  /**
-   * Takes raw user input and the current list of tasks and based on the user input performs operations like find, delete, done, list, add and bye.
-   * Outputs a task list back after performing the operation.
-   *
-   * @param raw_input users single line string input
-   * @param list existing list of tasks to perform operations upon
-   * @return list after an operation has been performed
-   * @see TaskList
-   */
-  public TaskList parse (String raw_input, TaskList list) {
-    Ui ui = new Ui();
-    String[] user_input = this.split(raw_input); 
-    /* return output; */
-
-    // If user inputs list
-    if (user_input[0].equals("find")) {
-      if (user_input.length == 1) {
-        ui.empty_description_error();
-      }
-      else {
-        list.findTask(user_input[1]);
-      }
+    /**
+     * Takes raw input and splits it into task type (eg. todo) and task
+     * description (eg. finish work). In cases like task type: list, bye,
+     * the output array only contains task type.
+     *
+     * @param rawInput users single line string input
+     * @return an array split into task type and task description
+     */
+    public String[] split(String rawInput) {
+        String[] userInput = rawInput.split(" ", 2);
+        return userInput;
     }
+    /**
+     * Default constructor.
+     */
 
-    else if (user_input[0].equals("delete")) {
-      if (user_input.length == 1) {
-        ui.empty_description_error();
-      }
-      else {
-        int task_id = Integer.parseInt(user_input[1]) - 1;
-        list.removeTask(task_id);
-      }
-    }
+    /**
+     * Takes raw user input and the current list of tasks and based on
+     * the user input performs operations like find, delete, done, list,
+     * add and bye.
+     * Outputs a task list back after performing the operation.
+     *
+     * @param rawInput users single line string input
+     * @param list existing list of tasks to perform operations upon
+     * @return list after an operation has been performed
+     * @see TaskList
+     */
+    public TaskList parse(String rawInput, TaskList list) {
+        Ui ui = new Ui();
+        String[] userInput = this.split(rawInput);
+        /* return output; */
 
-    // list tasks
-    else if (user_input[0].equals("list")) {
-      list.displayList();
-    }
-
-    // Do task.
-    else if (user_input[0].equals("done")) {
-      // Extracting which task to do from the rest of the line after inputting only task type in line 20
-      if (user_input.length == 1) {
-        ui.empty_description_error(); 
-      }
-      else {
-        int task_id = Integer.parseInt(user_input[1]) - 1;
-        list.doTask(task_id);
-      }
-    }
-
-    else {
-      // add task to list
-      if (user_input[0].equals("todo") || user_input[0].equals("deadline") || user_input[0].equals("event")) {
-        if (user_input.length == 1) {
-          ui.empty_description_error();
+        // If user inputs list
+        if (userInput[0].equals("find")) {
+            if (userInput.length == 1) {
+                ui.empty_description_error();
+            } else {
+                list.findTask(userInput[1]);
+            }
+        } else if (userInput[0].equals("delete")) {
+            if (userInput.length == 1) {
+                ui.empty_description_error();
+            } else {
+                int taskId = Integer.parseInt(userInput[1]) - 1;
+                list.removeTask(taskId);
+            }
+        } else if (userInput[0].equals("list")) {
+            list.displayList();
+        } else if (userInput[0].equals("done")) {
+            if (userInput.length == 1) {
+                ui.empty_description_error();
+            } else {
+                int taskId = Integer.parseInt(userInput[1]) - 1;
+                list.doTask(taskId);
+            }
+        } else {
+            // add task to list
+            if (userInput[0].equals("todo") || userInput[0].equals("deadline")
+                || userInput[0].equals("event")) {
+                if (userInput.length == 1) {
+                    ui.empty_description_error();
+                } else {
+                    list.add(userInput[0], userInput[1]);
+                }
+            } else if (!userInput[0].equals("bye")) {
+                ui.correct_command_error();
+            }
         }
-        else {
-          list.add(user_input[0], user_input[1]);
-        }
-      }
-      else if (!user_input[0].equals("bye")) {
-        ui.correct_command_error();
-      }
+        return list;
     }
-    return list;
-  }
 }
--- src/main/java/seedu/duke/task/Event.java
+++ src/main/java/seedu/duke/task/Event.java
@@ -10,89 +10,104 @@ import java.util.Locale;
  * This task type inherits from Task. It specifies an event at a particular time.
  */
 public class Event extends Task {
-  
-  protected String at;
-  // translated date version
-  protected String date_at;
-  private Ui ui = new Ui();
 
-  public Event(String description, String at) {
-    super(description);
-    this.at = at;
-    this.to_date();
-  }
+    /**
+     * String that denotes when the event is going to happen.
+     */
+    protected String at;
 
-  /**
-   * Makes use of the DateTimeFormatter and LocalDateTime class to parse the user input date time and initializes the date_at member variable.
-   */
-  public void to_date() {
-    // splitting date
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/uuuu HHmm");
-    LocalDateTime parsedDate;
-    try {
-      parsedDate = LocalDateTime.parse(this.at, formatter);
-    }
-    catch (DateTimeParseException e) {
-      ui.date_time_error(); 
-      this.date_at = this.at;
-      return;
-    }
-   
-    String suffix;
-    switch (parsedDate.getDayOfMonth() % 10)
-    {
-      case 1:
-        suffix = "st";
-        break;
-      case 2:
-        suffix = "nd";
-        break;
-      case 3:
-        suffix = "rd";
-        break;
-      default:
-        suffix = "th";
-        break;
+    /**
+     * String that denotes a date time parsed at variable.
+     */
+    protected String dateAt;
+
+    /**
+     * Object of the Ui class that that is used to
+     * communicate errors to the user.
+     */
+    private Ui ui = new Ui();
+
+    /**
+     * Initializes description and at variable.
+     */
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+        this.to_date();
     }
 
-    if (parsedDate.getDayOfMonth() > 3 && parsedDate.getDayOfMonth() < 21) {
-      suffix = "th";
+    /**
+     * Makes use of the DateTimeFormatter and LocalDateTime class to parse
+     * the user input date time and initializes the date_at member variable.
+     */
+    public void to_date() {
+        // splitting date
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/uuuu HHmm");
+        LocalDateTime parsedDate;
+        try {
+            parsedDate = LocalDateTime.parse(this.at, formatter);
+        } catch (DateTimeParseException e) {
+            ui.date_time_error();
+            this.dateAt = this.at;
+            return;
+        }
+
+        String suffix;
+        switch (parsedDate.getDayOfMonth() % 10) {
+        case 1:
+            suffix = "st";
+            break;
+        case 2:
+            suffix = "nd";
+            break;
+        case 3:
+            suffix = "rd";
+            break;
+        default:
+            suffix = "th";
+            break;
+        }
+
+        if (parsedDate.getDayOfMonth() > 3 && parsedDate.getDayOfMonth() < 21) {
+            suffix = "th";
+        }
+
+        DateTimeFormatter printFormat = DateTimeFormatter.ofPattern("d'"
+            + suffix + "' 'of' MMMM uuuu',' h:mma", Locale.ENGLISH);
+
+        this.dateAt = parsedDate.format(printFormat);
     }
 
-    DateTimeFormatter print_format = DateTimeFormatter.ofPattern("d'" + suffix + "' 'of' MMMM uuuu',' h:mma", Locale.ENGLISH);
-
-    this.date_at = parsedDate.format(print_format);
-  }
-
-  /**
-   * Overrides the toString method in Task to display task type and date time.
-   *
-   * @return a string with the target info.
-   */
-  public String toString() {
-    return "[E]" + super.toString() + " (at: " + this.date_at + ")";
-  }
-
-
-  /**
-   * Overrides the toSaveFormat function to include task type and date time.
-   *
-   * @return a string with pipe separated info.
-   */
-  public String toSaveFormat() {
-    return "E|" + super.toSaveFormat() + "|" + this.at;
-  }
-
-  /**
-   * Checks equality with another Event instance.
-   *
-   * @param temp the instance to compare against.
-   * @return true or false to the comparison.
-   */
-  public boolean equals(Event temp) {
-    if (this.description == temp.description && this.at == temp.at && this.date_at == temp.date_at) {
-      return true; 
+    /**
+     * Overrides the toString method in Task to display task type and date time.
+     *
+     * @return a string with the target info.
+     */
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + this.dateAt + ")";
     }
-    return false;
-  }
+
+
+    /**
+     * Overrides the toSaveFormat function to include task type and date time.
+     *
+     * @return a string with pipe separated info.
+     */
+    public String toSaveFormat() {
+        return "E|" + super.toSaveFormat() + "|" + this.at;
+    }
+
+    /**
+     * Checks equality with another Event instance.
+     *
+     * @param temp the instance to compare against.
+     * @return true or false to the comparison.
+     */
+    public boolean equals(Event temp) {
+        if (this.description == temp.description && this.at == temp.at
+                && this.dateAt == temp.dateAt) {
+            return true;
+        }
+        return false;
+    }
 }
