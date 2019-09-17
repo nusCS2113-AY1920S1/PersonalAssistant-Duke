9de6e74929e28d75b1c82d7b14f8888ddadabf8d
gowgos5 <gowgos5@gmail.com>
2019-09-18 00:54:41 +0800

    Edit Task classes to accept reminders

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -9,9 +9,17 @@ abstract class Task {
     char type;
     private String name;
     private Boolean isDone;
+    private Reminder reminder;
 
     Task(String name) {
         this.name = name;
+        isDone = false;
+        reminder = null;
+    }
+
+    public Task(String name, Reminder reminder) {
+        this.name = name;
+        this.reminder = reminder;
         isDone = false;
     }
 
@@ -20,6 +28,7 @@ abstract class Task {
             throw new DukeException("You already did that task!");
         } else {
             isDone = true;
+            reminder = null;
         }
     }
 
@@ -42,7 +51,12 @@ abstract class Task {
      */
     @Override
     public String toString() {
-        return "[" + (isDone ? "\u2713" : "\u2718") + "] " + name; //ternary operator returns tick or X
+        String remind = "";
+        if (reminder != null) {
+            remind = reminder.toString();
+        }
+
+        return "[" + (isDone ? "\u2713" : "\u2718") + "]" + remind + " " + name; //ternary operator returns tick or X
     }
 
     /**
@@ -51,6 +65,23 @@ abstract class Task {
      * @return Data-formatted (tab-separated) task description.
      */
     public String toData() {
-        return type + "\t" + (isDone ? "1" : "0") + "\t" + name;
+        String remind = "-";
+        if (reminder != null) {
+            remind = reminder.toData();
+        }
+
+        return type + "\t" + (isDone ? "1" : "0") + "\t" + remind + "\t" + name;
+    }
+
+    public Reminder getReminder() {
+        return reminder;
+    }
+
+    public void setReminder(Reminder reminder) throws DukeException {
+        if (isDone) {
+            throw new DukeException("This task has already been completed! I can't set a reminder for it.");
+        }
+
+        this.reminder = reminder;
     }
 }
