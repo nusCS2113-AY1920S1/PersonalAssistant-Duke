378150a478e6f2e9e45f7190ae066f64f124f287
nicholasnovakovic <nicholas.n.novakovic@gmail.com>
2019-09-18 22:06:58 +0800

    Add capability to package app as JAR file

---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,15 @@
+import duke.parser.Parser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+/**
+ * Class for JUnit testing for Parser class
+ */
+public class ParserTest {
+	private final Parser parser = new Parser();
+
+	@Test
+	public void magicTest() {
+	}
+
+}
