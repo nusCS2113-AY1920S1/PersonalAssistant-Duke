beb413df3409bc53e344c6c5be220b134d2a8550
limsiying <siying1899@gmail.com>
2019-09-18 12:11:23 +0800

    im so technically challenged

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,19 @@
-import duke.command.*;
+import duke.command.AddDeadlineCommand;
+import duke.command.AddEventCommand;
+import duke.command.AddTodoCommand;
+import duke.command.ByeCommand;
+import duke.command.Command;
+import duke.command.DeleteCommand;
+import duke.command.DoneCommand;
+import duke.command.FindCommand;
+import duke.command.ListCommand;
+import duke.command.ReminderCommand;
+import duke.command.SnoozeCommand;
+import duke.command.ViewScheduleCommand;
 import duke.dukeexception.DukeException;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 class Parser {
     /**
