8457ae47836432ab00dd7bd8fe443555eae1f037
John Khoo <john_khoo@u.nus.edu>
2019-09-19 00:26:59 +0800

    Add failure check to SnoozeCommandTest

--- src/test/java/SnoozeCommandTest.java
+++ src/test/java/SnoozeCommandTest.java
@@ -42,12 +42,36 @@ public class SnoozeCommandTest extends CommandTest {
     @Test
     public void snooze_validCommand_correctDateTimeSet() {
         SnoozeCommand uut = new SnoozeCommand();
+        String taskListStr = null;
         try {
             uut.parse("3 19/09/2019 1400");
             uut.execute(ctx);
+            taskListStr = ctx.taskList.listTasks();
         } catch (DukeException excp) {
             System.out.println(excp.getMessage());
             fail("Exception thrown on valid command!");
+        }
+        assertEquals(taskListStr, System.lineSeparator() + "1.[T][N] JUnit tests"
+                + System.lineSeparator() + "2.[E][N] tutorial (at: Thu, 12 Sep 2019 2:00 PM)"
+                + System.lineSeparator() + "3.[D][N] submission (by: Thu, 19 Sep 2019 2:00 PM)");
+    }
+
+    @Test
+    public void snooze_invalidTarget_exceptionThrown() {
+        SnoozeCommand uut = new SnoozeCommand();
+        try {
+            uut.parse("4 19/09/2019 1400");
+            assertThrows(DukeException.class, () -> {
+                uut.execute(ctx);
+            });
+            uut.parse("1 19/09/2019 1400");
+            assertThrows(DukeException.class, () -> {
+                uut.execute(ctx);
+            });
+        } catch (AssertionError excp) {
+            fail("Failed to catch command snoozing invalid targets!");
+        } catch (DukeException excp) {
+            fail("Failed to parse command with valid delimiter!");
         }
     }
 }
