ccd15744ea501949adab4826a809e5d0df09179f
Tejasvas123 <42727292+Tejasvas123@users.noreply.github.com>
2019-09-18 19:25:48 +0800

    [CS2113T-F11-1]-Tejasvas123-B-DoAfterTasks (#8)
    * Improve coding style
    
    * Add Do-After Task
    
    * Handle checkStyle errors
    
    * make a release
    
    * Add test cases for DoAfter
    
    * Enable SpongeBob to recognize FixDurationTask
    
    * Minor BugFix
    
    * Enable testing for FixedCommand

---
+++ src/test/java/duketest/command/AddFixedCommandTest.java
@@ -0,0 +1,42 @@
+package duketest.command;
+
+import duke.command.AddDoAfterCommand;
+import duke.exceptions.DukeException;
+import duke.storage.FileHandling;
+import duke.tasks.TaskList;
+import duke.ui.Ui;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AddFixedCommandTest {
+    private final TaskList taskList = new TaskList();
+    private final Ui ui = new Ui();
+    private final FileHandling storage = new FileHandling("storeData.txt");
+
+    @Test
+    public void checkValidCommand() {
+        List<String> splitInput = new ArrayList<>();
+        splitInput.add("fixed");
+        AddDoAfterCommand obj = new AddDoAfterCommand(splitInput);
+
+        assertThrows(DukeException.class, () ->
+                obj.execute(taskList, ui, storage));
+        splitInput.add("return book");
+    }
+
+    @Test
+    public void checkValidFormat() {
+        List<String> splitInput = new ArrayList<>();
+        splitInput.add("fixed");
+        splitInput.add("read");
+        splitInput.add("journal");
+        splitInput.add("/needs");
+        AddDoAfterCommand obj = new AddDoAfterCommand(splitInput);
+        assertThrows(DukeException.class, () ->
+                obj.execute(taskList, ui, storage));
+    }
+}
