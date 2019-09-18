9673a750904e92681fccc8155c96760143258fe1
JustinChia1997 <justinchiajj@hotmail.com>
2019-09-19 02:40:22 +0800

    Testing Travis

--- src/main/java/tasks/ToDo.java
+++ src/main/java/tasks/ToDo.java
@@ -9,32 +9,33 @@ public class ToDo extends Task {
     /**
      * default constructor of ToDo
      */
-    public ToDo() {}
+    public ToDo() {
+    }
 
     /**
      * another constructor of ToDo
-     * @param description
-     *                  description of this task
+     *
+     * @param description description of this task
      */
-    public ToDo(String description){
+    public ToDo(String description) {
         super(description);
     }
 
     /**
+     * @return string
+     * how task list will print out in console
      * @Override toString() in Task
      * a method to format the output of the task list
-     * @return string
-     *              how task list will print out in console
      */
     public String toString() {
         return "[T]" + super.toString();
     }
 
     /**
+     * @return string
+     * a string which will show in data file that store the task list
      * @Override dataString() in Task
      * a method to format the data list data store in file
-     * @return string
-     *              a string which will show in data file that store the task list
      */
     public String dataString() {
         return "T | " + (this.isDone ? 1 : 0) + " | " + this.description + " | " + this.getPrecondition();
