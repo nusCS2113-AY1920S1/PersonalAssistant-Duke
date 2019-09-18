b267ddf08852507a4cbbf3872dd57d9d1b57e0ee
limsiying <siying1899@gmail.com>
2019-09-18 12:24:19 +0800

    Add test for Schedule

--- src/test/java/ViewScheduleCommandTest.java
+++ src/test/java/ViewScheduleCommandTest.java
@@ -1,16 +1,38 @@
+import duke.command.Schedule;
 import duke.command.ViewScheduleCommand;
 import duke.dukeexception.DukeException;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ViewScheduleCommandTest {
+
+    void getStartString_returnsCorrectly() throws ParseException {
+        String dateString = "12/12/2019 1400";
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HHmm");
+        Date time = formatter.parse(dateString);
+        String toString = "[D][✘] test";
+        assertEquals("1400", new Schedule(time, toString).getStartString());
+    }
+
+    void getTask_returnsCorrectly() throws ParseException {
+        String dateString = "12/12/2019 1400";
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HHmm");
+        Date time = formatter.parse(dateString);
+        String toString = "[D][✘] test";
+        assertEquals("[D][✘] test", new Schedule(time, toString).toString());
+        }
+
     @Test
-    void viewScheduleCommand_invalidDateTime_dukeExceptionThrown() {
-        List<String> invalidSchedule = Arrays.asList("schedule", "/from", "16/9/2019");
+    void viewScheduleCommand_emptySchedule_dukeExceptionThrown() {
+        List<String> invalidSchedule = Arrays.asList("schedule", "/for", "16/9/");
         assertThrows(DukeException.class, () -> new ViewScheduleCommand(invalidSchedule));
     }
 }
