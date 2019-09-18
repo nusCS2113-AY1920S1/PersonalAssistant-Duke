113f42e72756046672da7b78986eafc5fd2897bb
ngjiewu <ngjiewu@gmail.com>
2019-09-18 16:00:02 +0800

    Added few tests

--- src/test/java/seedu/duke/task/EventTest.java
+++ src/test/java/seedu/duke/task/EventTest.java
@@ -1,6 +1,9 @@
 package seedu.duke.task;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -15,15 +18,22 @@ public class EventTest {
      */
     public void dummyTest() {
         Event task1 = new Event("meet Kartike.", "5/5/2019 1800");
-
         assertEquals(task1.description, "meet Kartike.");
         assertEquals(task1.dateAt, "5th of May 2019, 6:00PM");
         assertEquals(task1.at, "5/5/2019 1800");
+        assertEquals(task1.getDateTime(), LocalDateTime.of(2019,5,5,18,0));
 
         Event task2 = new Event("meet Kartike.", "next week");
         assertEquals(task2.description, "meet Kartike.");
         assertEquals(task2.dateAt, "next week");
         assertEquals(task2.at, "next week");
 
+        Event task3 = new Event("meet Kartike.", "5/5/2019 1200");
+        task3.setDateTime(LocalDateTime.of(2019,5,5,18,0));
+        assertEquals(task3.getDateTime(), task1.getDateTime());
+
+        Event task4 = new Event("meet Kartike.", "5/5/2019 1200");
+        task4.setDateTime("5/5/2019 1800");
+        assertEquals(task4.getDateTime(), task1.getDateTime());
     }
 }
