f723dbe521f018f4c5b5e90ad26ee934572df935
yuyanglin28 <annielin9908@gmail.com>
2019-09-18 19:48:36 +0800

    add javaDoc to Last and Period

--- src/main/java/tasks/Last.java
+++ src/main/java/tasks/Last.java
@@ -7,25 +7,55 @@ package tasks;
 
 public class Last extends Task{
 
+    /**
+     * a String store the duration time
+     */
     private String duration;
 
+    /**
+     * default constructor of Last
+     */
     public Last(){}
 
+
+    /**
+     * another constructor of Last
+     * @param description
+     *                  description of this task
+     * @param duration
+     *                  duration of this task
+     */
     public Last(String description, String duration){
         super(description);
         this.duration = duration;
     }
 
+
+    /**
+     * a method to set the duration
+     * @param duration
+     *              duration of this task
+     */
     public void setDuration(String duration){
         this.duration = duration;
     }
 
-    @Override
+    /**
+     * @Override toString() in Task
+     * a method to format the output of the task list
+     * @return string
+     *              how task list will print out in console
+     */
     public String toString() {
         return "[L]" + super.toString() + " (last: " + duration + ")";
     }
 
-    @Override
+    /**
+     * @Override dataString() in Task
+     * a method to format the data list data store in file
+     * @return string
+     *              a string which will show in data file that store the task list
+     */
     public String dataString() {
 
         return "L | " + (this.isDone ? 1 : 0) + " | " + this.description + " | " + this.duration;
--- src/main/java/tasks/Period.java
+++ src/main/java/tasks/Period.java
@@ -8,31 +8,72 @@ import java.util.Date;
  * e.g., collect certificate between Jan 15 and 25th.
  */
 public class Period extends Task{
+
+    /**
+     * the start time of the task
+     */
     Date start;
+
+    /**
+     * the end time of the task
+     */
     Date end;
 
-    public Period(){super();}
+    /**
+     * default constructor of Period
+     */
+    public Period(){}
 
+
+    /**
+     * another constructor of Period
+     * @param description
+     *                  the description of this task
+     * @param start
+     *                  the starting time of this task
+     * @param end
+     *                  the end time of this task
+     */
     public Period(String description, Date start, Date end){
         super(description);
         this.start = start;
         this.end = end;
     }
 
+    /**
+     * set the start time
+     * @param start
+     *              starting time
+     */
     public void setStart(Date start){
         this.start = start;
     }
 
+    /**
+     * set the end time
+     * @param end
+     *              end time
+     */
     public void setEnd(Date end){
         this.end = end;
     }
 
-    @Override
+    /**
+     * @Override toString() in Task
+     * a method to format the output of the task list
+     * @return string
+     *              how task list will print out in console
+     */
     public String toString() {
         return "[P]" + super.toString() + " (from: " + start + " to: " + end + ")";
     }
 
-    @Override
+    /**
+     * @Override dataString() in Task
+     * a method to format the data list data store in file
+     * @return string
+     *              a string which will show in data file that store the task list
+     */
     public String dataString() {
         SimpleDateFormat ft = new SimpleDateFormat("dd/MM/yyyy hhmm");
         return "P | " + (this.isDone ? 1 : 0) + " | " + this.description + " | " + ft.format(this.start)+" | " + ft.format(this.end);
