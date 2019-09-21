29232d2931be76c3fab6763040496ca475a23d96
Mohammad Yousuf Minhaj Zia <e0381439@u.nus.edu>
2019-09-21 17:10:05 +0800

    created Command class and the classes that depend on it

--- src/main/java/seedu/duke/parser/CommandParser.java
+++ src/main/java/seedu/duke/parser/CommandParser.java
@@ -1,9 +1,20 @@
-package seedu.duke.command;
+package seedu.duke.parser;
 
 import seedu.duke.data.Schedule;
+import java.util.Arrays;
 import seedu.duke.task.Reminders;
 import seedu.duke.task.TaskList;
 import seedu.duke.ui.Ui;
+import seedu.duke.command.Command;
+import seedu.duke.command.DeleteCommand;
+import seedu.duke.command.ListCommand;
+import seedu.duke.command.DoneCommand;
+import seedu.duke.command.FindCommand;
+import seedu.duke.command.InvalidCommand;
+import seedu.duke.command.ScheduleCommand;
+import seedu.duke.command.SnoozeCommand;
+import seedu.duke.command.RemindCommand;
+import seedu.duke.command.AddCommand;
 
 import java.text.ParseException;
 import java.util.Date;
@@ -12,9 +23,7 @@ import java.util.Date;
  * Takes raw user input as string, makes sense out of the input using
  * regex and then performs operations based on the input.
  */
-public class Parser {
-
-    private Schedule schedule = new Schedule();
+public class CommandParser extends Parser {
 
     /**
      * Takes raw input and splits it into task type (eg. todo) and task
@@ -41,33 +50,29 @@ public class Parser {
      * @return an instruction, of type Command, to be executed.
      */
     public Command parse(String rawInput) {
-        Ui ui = new Ui();
         String[] userInput = this.split(rawInput);
-        /* return output; */
 
-        // If user inputs list
+        String[] taskCommands = {"todo", "deadline", "event", "range", "doafter"};
+        Arrays.sort(taskCommands);
+
         if (userInput[0].equals("find")) {
-            return new commandFindTask(userInput[1]);
+            return new FindCommand(userInput);
         } else if (userInput[0].equals("delete")) {
-            return new commandDeleteTask(userInput[1]);
+            return new DeleteCommand(userInput);
         } else if (userInput[0].equals("list")) {
-            return new commandDisplayTaskList();
+            return new ListCommand();
         } else if (userInput[0].equals("remind")) {
-            return new commandDisplayReminders();
+            return new RemindCommand();
         } else if (userInput[0].equals("done")) {
-            return new commandMarkTaskAsDone(userInput[1]);
+            return new DoneCommand(userInput);
         } else if (userInput[0].equals("show")) {
-            return new commandDisplaySchedule(userInput[1]);
+            return new ScheduleCommand(userInput);
         } else if (userInput[0].equals("snooze")) {
-            return new commandSnoozeTask(userInput[1]);
+            return new SnoozeCommand(userInput);
+        } else if (Arrays.binarySearch(taskCommands, userInput[0]) >= 0) {
+            return new AddCommand(userInput);
         } else {
-            // add task to list
-            if (userInput[0].equals("todo") || userInput[0].equals("deadline") || userInput[0].equals("event") ||
-                userInput[0].equals("range") || userInput[0].equals("doafter")) {
-                return new commandAddTask(userInput[0], userInput[1]);
-            } else if (!userInput[0].equals("bye")) {
-                return new commandInputNotRecognised();
-            }
+            return new InvalidCommand();
         }
     }
 }
