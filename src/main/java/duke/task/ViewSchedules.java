4779df447c134bd3dca4cbc5c73ac37837da0998
CEGLincoln <e0311003@u.nus.edu>
2019-09-22 18:18:25 +0800

    something went wrong

--- src/main/java/duke/task/ViewSchedules.java
+++ src/main/java/duke/task/ViewSchedules.java
@@ -2,9 +2,6 @@ package duke.task;
 
 import duke.parser.Parser;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.Date;
 
 //import static duke.task.Task.getDate;
@@ -13,7 +10,7 @@ public class ViewSchedules extends TaskList
 {
     private Date when;
     public ViewSchedules(String when) {
-        this.when = Parser.getDate(when);
+        this.when = Parser.stringToDate(when);
     }
 
     public Date getCurrentDate() {
