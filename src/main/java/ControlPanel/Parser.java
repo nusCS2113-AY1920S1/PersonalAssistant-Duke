a4b606d3763c338b3cda22cc031027efd4af481e
therealnickcheong <nicholas1cheong@gmail.com>
2019-09-22 13:21:00 +0800

    JavaFX tutorial 1-3 done

--- src/main/java/ControlPanel/Parser.java
+++ src/main/java/ControlPanel/Parser.java
@@ -16,52 +16,41 @@ public class Parser {
      * @throws DukeException if any exception is caught
      */
     public static Command parse(String cmd) throws DukeException {
-        Command command = null;
-        if (cmd.equals("bye")){
+        Command command;
+        if (cmd.equals("bye")) {
             command = new ExitCommand();
-        }
-        else if (cmd.equals("list")){
+        } else if (cmd.equals("list")) {
             command = new ViewCommand();
-        }
-        else if (cmd.contains("find")){
-            if (cmd.equals("find"))
+        } else if (cmd.contains("find")) {
+            if (cmd.equals("find")) {
                 throw new DukeException("OOPS!!! The description of a find cannot be empty.");
+            }
             String keyword = cmd.split(" ")[1];
             command = new SearchCommand(keyword);
-        }
-        else if(cmd.startsWith("done")) {
+        } else if (cmd.startsWith("done")) {
             String temp = cmd.replaceAll("[^0-9]", "");
             int serialNo = Integer.parseInt(temp);
             command = new DoneCommand(serialNo);
-        }
-        else if (cmd.contains("delete")){
+        } else if (cmd.contains("delete")) {
             String temp = cmd.replaceAll("[^0-9]", "");
             int serialNo = Integer.parseInt(temp);
             command = new DeleteCommand(serialNo);
-
-        }else if(cmd.contains("reminders")) {
+        } else if (cmd.contains("reminders")) {
             String keyword = cmd.split(" ")[0];
             command = new RemindersCommand(keyword);
-
-        }else if(cmd.startsWith("schedule")){
+        } else if (cmd.startsWith("schedule")) {
             command  = new ViewScheduleCommand(cmd);
-
-        }else if(cmd.startsWith("reschedule")){
+        } else if (cmd.startsWith("reschedule")) {
             command = new RescheduleCommand(cmd);
-
-
-        } else if(cmd.contains("choose")) {
+        } else if (cmd.contains("choose")) {
             command = new ChooseEventTime(cmd);
-        }
-        else {
+        } else {
             String keyword = cmd.split(" ")[0];
-            if (!(keyword.equals("deadline") || keyword.equals("event") || keyword.equals("todo") || keyword.equals("period") ||keyword.equals("duration") || keyword.equals("multiEvent"))){
+            if (!(keyword.equals("deadline") || keyword.equals("event") || keyword.equals("todo") || keyword.equals("period") ||keyword.equals("duration") || keyword.equals("multiEvent"))) {
                 throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means");
             }
             command = new AddCommand(keyword, cmd);
         }
-
-
         return command;
     }
 }
