90e51e0645147954c3d6beac8657370fdced756b
ZhangYihanNus <zhangyh0002@gmail.com>
2019-09-23 23:16:34 +0800

    edit to Event class

--- src/main/java/Events/EventTypes/EventSubclasses/Assessment.java
+++ src/main/java/Events/EventTypes/EventSubclasses/Assessment.java
@@ -1,14 +1,21 @@
-package Events.EventTypes.EventSubclasses;
+package Events.EventTypes.EventSubClasses;
 
 import Events.EventTypes.Event;
 
-public class Assessment extends Event {
-    public Assessment(String description) {
-        super(description);
+public abstract class Assessment extends Event {
+    /**
+     * Creates assessment event with isDone boolean for reading from files
+     * @param eventType type of event denoted by character
+     */
+    public Assessment(String description, boolean isDone, String startDateAndTime, String endDateAndTime, char eventType) {
+        super(description, isDone, startDateAndTime, endDateAndTime, eventType);
     }
 
-    @Override
-    public String getDate() {
-        return this.;
+    /**
+     * Creates assessment without isDone boolean for user input (assumes task entered is incomplete)
+     * @param eventType type of event denoted by character
+     */
+    public Assessment(String description, String startDateAndTime, String endDateAndTime, char eventType) {
+        super(description, false, startDateAndTime, endDateAndTime, eventType);
     }
 }
