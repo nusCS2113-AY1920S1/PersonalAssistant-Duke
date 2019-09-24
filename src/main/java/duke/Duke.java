1297f45aee3a548f7b966c5b6e7cc768646bb85b
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2019-09-24 11:37:16 +0800

    Completely change the look of the GUI

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -4,37 +4,23 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import duke.Data.Storage;
-import duke.GUI.DialogBox;
 import duke.Task.*;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.scene.layout.Region;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 public class Duke extends Application {
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
     private Ui ui;
     private Storage storage;
     private TaskList tasks;
+    Button button1;
+    Button button2;
+    Button button3;
 
-    //
     public Duke() {
 
     }
@@ -74,107 +60,47 @@ public class Duke extends Application {
     public void start(Stage stage) {
         //Step 1. Setting up required components
 
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
+        Pane layout = new Pane();    //create a Pane
 
-        userInput = new TextField();
-        sendButton = new Button("Send");
+        button1 = new Button();                //create a button for View Schedule
+        button1.setText("View Schedule");
 
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        button2 = new Button();                //create a button for Manage Students
+        button2.setText("Manage Students");
 
-        scene = new Scene(mainLayout);
+        button3 = new Button();                //create a button for Manage Training Programmes
+        button3.setText("Manage Training Programmes");
 
+        layout.getChildren().addAll(button1, button2, button3);   //add all buttons to Pane
+
+        Scene scene = new Scene(layout, 1000,600);   //create a scene, set, and show
         stage.setScene(scene);
         stage.show();
 
         //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
+
+        stage.setTitle("Main menu");
         stage.setResizable(false);
-        //stage.setMinHeight(600.0);
-        //stage.setMinWidth(400.0);
-        stage.setMinHeight(700.0);
-        stage.setMinWidth(1000.0);
+        stage.setHeight(600.0);
+        stage.setWidth(1000.0);
 
-        mainLayout.setPrefSize(400.0, 600.0);
+        //Move the buttons to their correct positions
+        button1.setLayoutX(100);
+        button1.setLayoutY(100);
 
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        button2.setLayoutX(100);
+        button2.setLayoutY(250);
 
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
+        button3.setLayoutX(100);
+        button3.setLayoutY(400);
 
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        //Change the look of the buttons
+        button1.setStyle("-fx-pref-height: 50px; -fx-pref-width: 180px; -fx-background-color: orange;");
 
-        userInput.setPrefWidth(325.0);
+        button2.setStyle("-fx-pref-height: 50px; -fx-pref-width: 180px; -fx-background-color: orange;");
 
-        sendButton.setPrefWidth(55.0);
+        button3.setStyle("-fx-pref-height: 50px; -fx-pref-width: 180px; -fx-background-color: orange;");
 
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Part 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
-    /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     *
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    private Label getDialogLabel(String text) {
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
-
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        if (userInput.getText().equals("bye")) {     //Make the GUI automatically close by itself.
-            Platform.exit();
-            System.exit(0);
-        } else {
-            Label userText = new Label(userInput.getText());
-            Label dukeText = new Label(getResponse(userInput.getText()));
-            dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-            );
-            userInput.clear();
-        }
-    }
-
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    private String getResponse(String input) {
-        return "Duke heard: " + input;
     }
 
 }
