ea21744815e7db698c57d7ec8e40b1403953e350
Deculsion <danasanma@gmail.com>
2019-09-25 14:36:14 +0800

    Changed to use new enums package and task constructors.

--- src/main/java/duke/commands/AddCommand.java
+++ src/main/java/duke/commands/AddCommand.java
@@ -3,8 +3,10 @@ package duke.commands;
 import duke.TaskList;
 import duke.Ui;
 import duke.Storage;
+import duke.items.DateTime;
 import duke.exceptions.BadInputException;
 import duke.enums.CommandType;
+import duke.enums.TaskType;
 
 /**
  * Command objects for adding Todos, Events, and Deadlines.
@@ -14,17 +16,41 @@ public class AddCommand extends Command {
     private String description;
     private String details;
     private int afterIndex;
+    private DateTime[] dateTimes = new DateTime[2];
 
     /**
      * Why does this require a javadoc comment.
      */
-    public AddCommand(CommandType type, String description, String details, int afterIndex) {
+    public AddCommand(CommandType type, String description, String details, int afterIndex) throws BadInputException {
         super(type);
         this.description = description;
         this.details = details;
         this.afterIndex = afterIndex;
-    }
 
+        switch (type) {
+            case TODO:
+                if (!details.equals("")) {
+                    dateTimes[0] = new DateTime(details);
+                }
+            case DEADLINE:
+                dateTimes[0] = new DateTime(details);
+                break;
+
+            case EVENT:
+                try {
+                    String[] startEndTime = details.split(" to ", 2);
+                    dateTimes[0] = new DateTime(startEndTime[0]);
+                    dateTimes[1] = new DateTime(startEndTime[1]);
+                } catch (Exception e) {
+                    throw new BadInputException("Improper datetime. "
+                            + "Correct format: event <event name> /at <event start time> to <event end time>");
+                }
+
+                break;
+
+        }
+
+    }
 
     @Override
     public void execute(TaskList list, Ui ui, Storage storage) throws BadInputException {
@@ -42,14 +68,14 @@ public class AddCommand extends Command {
 
         if (super.type == CommandType.TODO) {
             if (!details.equals("")) {
-                list.addTodoItem(description, doAfterWhat, Integer.parseInt(details));
+                list.addItem(TaskType.TODO, description, Integer.parseInt(details));
             } else {
-                list.addTodoItem(description, doAfterWhat);
+                list.addItem(TaskType.TODO,description);
             }
         } else if (super.type == CommandType.DEADLINE) {
-            list.addDeadlineItem(description, details, doAfterWhat);
+            list.addItem(TaskType.DEADLINE, description, dateTimes[0]);
         } else { //Type is event
-            list.addEventItem(description, details, doAfterWhat);
+            list.addItem(TaskType.EVENT, description, dateTimes[1], dateTimes[1]);
         }
     }
 }
