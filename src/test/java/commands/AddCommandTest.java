3da33569946dededa1fb2198bc1eda941e5eecda
Rishab Patwari <e0311251@u.nus.edu>
2019-09-25 18:12:40 +0800

    - Fixed CheckStyle error where EscapeLiteral was used

--- src/test/java/commands/AddCommandTest.java
+++ src/test/java/commands/AddCommandTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddCommandTest {
     TaskList newList = new TaskList();
+
     @Test
     public void execute_addTodoTask_success() throws BadInputException {
         new AddCommand(CommandType.TODO, "Test TODO", "").execute(newList);
@@ -19,14 +20,15 @@ public class AddCommandTest {
     @Test
     public void execute_addDeadlineTask_success() throws BadInputException {
         new AddCommand(CommandType.DEADLINE, "Test DEADLINE", "15/12/2019 1500").execute(newList);
-        assertEquals( "[D] [\u2718] Test DEADLINE " +"(by: Sun Dec 15 15:00:00 SGT 2019)"
-                , newList.getTask(0).toString());
+        assertEquals("[D] " + newList.getTask(0).getStatusIcon() + " Test DEADLINE (by: Sun Dec 15 15:00:00 SGT 2019)",
+                newList.getTask(0).toString());
     }
 
     @Test
     public void execute_addEventTask_success() throws BadInputException {
         new AddCommand(CommandType.EVENT, "Test EVENT", "15/12/2019 1500 to 17/12/2019 1500").execute(newList);
-        assertEquals( "[E] [\u2718] Test EVENT " + "(at: Sun Dec 15 15:00:00 SGT 2019 "
+
+        assertEquals("[E] " + newList.getTask(0).getStatusIcon() + " Test EVENT (at: Sun Dec 15 15:00:00 SGT 2019 "
                 + "to Tue Dec 17 15:00:00 SGT 2019)", newList.getTask(0).toString());
     }
 }
