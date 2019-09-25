d86ad166e9d2454251b7388365d11ef11b0f316d
Rishab Patwari <e0311251@u.nus.edu>
2019-09-25 20:00:30 +0800

    - Trying to get tests to pass

--- src/test/java/commands/AddCommandTest.java
+++ src/test/java/commands/AddCommandTest.java
@@ -10,16 +10,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddCommandTest {
-    TaskList newList = new TaskList();
+
 
     @Test
     public void execute_addTodoTask_success() throws BadInputException {
+        TaskList newList = new TaskList();
         new AddCommand(CommandType.TODO, "Test TODO", "").execute(newList);
         assertEquals("Test TODO", newList.getTask(0).getDescription());
     }
 
     @Test
     public void execute_addDeadlineTask_success() throws BadInputException {
+        TaskList newList = new TaskList();
         new AddCommand(CommandType.DEADLINE, "Test DEADLINE", "15/12/2019 1500").execute(newList);
         assertEquals("Test DEADLINE", newList.getTask(0).getDescription());
         assertEquals("Sun Dec 15 15:00:00 SGT 2019", newList.getTask(0).getDate().toString());
@@ -27,9 +29,10 @@ public class AddCommandTest {
 
     @Test
     public void execute_addEventTask_success() throws BadInputException {
+        TaskList newList = new TaskList();
         new AddCommand(CommandType.EVENT, "Test EVENT", "15/12/2019 1500 to 17/12/2019 1500").execute(newList);
         assertEquals("Test EVENT", newList.getTask(0).getDescription());
         assertEquals("Sun Dec 15 15:00:00 SGT 2019", newList.getTask(0).getDate().toString());
-        assertEquals("Tue Dec 17 15:00:00 SGT 2019", ((Event) newList.getTask(0)).getEventEndTime().toString());
+        assertEquals("Tue Dec 17 15:00:00 SGT 2019", ((Event) newList.getTask(0)).getEventEndTime());
     }
 }
