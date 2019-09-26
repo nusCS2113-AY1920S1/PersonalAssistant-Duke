0535acee6e24d2d9afbcd58abb36282d238608b4
AmosChan97 <amoszzchan@outlook.com>
2019-09-26 22:28:03 +0800

    some refactoring

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -10,32 +10,32 @@ public class Parser{
     public static Command parse(String input) throws DukeException, ParseException {
         String[] splitStr = input.split(" ");
         switch (splitStr[0]) {
-            case "list":
-                return new ListCommand();
-            case "bye":
-                return new ByeCommand();
-            case "done":
-                return new DoneCommand(splitStr);
-            case "deadline":
-                return new DeadlineCommand(input, splitStr);
-            case "todo":
-                return new TodoCommand(input, splitStr);
-            case "event":
-                return new EventCommand(input, splitStr);
-            case "delete":
-                return new DeleteCommand(splitStr);
-            case "find":
-                return new FindCommand(input, splitStr);
-            case "upcoming":
-                return new UpcomingCommand();
-            case "viewschedule":
-                return new ViewScheduleCommand();
-            case "snooze":
-                return new SnoozeCommand(splitStr);
-            case "unsnooze":
-                return new UnSnoozeCommand(splitStr);  
-            default:
-                throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        case "list":
+            return new ListCommand();
+        case "bye":
+            return new ByeCommand();
+        case "done":
+            return new DoneCommand(splitStr);
+        case "deadline":
+            return new DeadlineCommand(input, splitStr);
+        case "todo":
+            return new TodoCommand(input, splitStr);
+        case "event":
+            return new EventCommand(input, splitStr);
+        case "delete":
+            return new DeleteCommand(splitStr);
+        case "find":
+            return new FindCommand(input, splitStr);
+        case "upcoming":
+            return new UpcomingCommand();
+        case "viewschedule":
+            return new ViewScheduleCommand();
+        case "snooze":
+            return new SnoozeCommand(splitStr);
+        case "unsnooze":
+            return new UnSnoozeCommand(splitStr);
+        default:
+            throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
 
         }
     }
