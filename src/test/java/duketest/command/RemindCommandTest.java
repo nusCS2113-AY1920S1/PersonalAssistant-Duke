52240376a3e2fe618426efab0b8fa8da50ad1675
Zhubo225 <42376196+Zhubo225@users.noreply.github.com>
2019-09-27 15:08:53 +0800

    Update RemindCommandTest and SnoozeCommandTest (#19)

--- src/test/java/duketest/command/RemindCommandTest.java
+++ src/test/java/duketest/command/RemindCommandTest.java
@@ -1,21 +1,14 @@
 package duketest.command;
 
-import duke.command.ByeCommand;
 import duke.command.Command;
 import duke.command.RemindCommand;
-import duke.exceptions.DukeException;
-import duke.storage.FileHandling;
-import duke.tasks.TaskList;
-import duke.ui.Ui;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RemindCommandTest {
     @Test
-    void checkRemind() throws DukeException {
+    void checkRemind() {
         Command c = new RemindCommand();
         assertFalse(c.isExit);
     }
-
 }
