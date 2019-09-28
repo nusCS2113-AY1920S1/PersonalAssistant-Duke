904c88f86fc183f18236c7d6688e85b8f813fac0
yueyeah <yuejunyi@u.nus.edu>
2019-09-28 22:09:09 +0800

    Error-Checking for rep, flexible freq

--- src/main/java/compal/compal/Messages.java
+++ src/main/java/compal/compal/Messages.java
@@ -27,8 +27,11 @@ public class Messages {
     public static final String MESSAGE_MISSING_TIME = "MissingTimeError: Time field cannot be empty."
             + " Please enter a valid time.";
     public static final String MESSAGE_MISSING_PRIORITY = "MissingPriorityError: Priority "
-            + "field cannot be empty";
+            + "field cannot be empty.";
+    public static final String MESSAGE_MISSING_REP = "MissingRepError: Rep field cannot be empty."
+            + "Please enter a valid number of repetitions.";
 
+    public static final String MESSAGE_MISSING_REP_ARG = "ArgumentError: Missing /rep";
     public static final String MESSAGE_MISSING_DATE_ARG = "ArgumentError: Missing /date";
     public static final String MESSAGE_MISSING_TIME_ARG = "ArgumentError: Missing /time";
     public static final String MESSAGE_MISSING_HOUR_ARG = "ArgumentError: Missing /hour";
--- src/main/java/compal/logic/commands/RecurTaskCommand.java
+++ src/main/java/compal/logic/commands/RecurTaskCommand.java
@@ -6,20 +6,22 @@ import compal.tasks.RecurringTask;
 import compal.tasks.Task;
 import compal.tasks.TaskList;
 
-import static compal.compal.Messages.MESSAGE_MISSING_COMMAND_ARG;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
+import static compal.compal.Messages.*;
+
 /**
  * Executes user command "recurtask".
  */
 public class RecurTaskCommand extends Command implements CommandParser {
-
+    // Todo: new keyword /freq, determines whether weekly, monthly, daily
     private static final String TOKEN_REP = "/rep";
+    private static final String TOKEN_FREQ = "/freq";
+    private static final int DEFAULT_FREQ_NUM = 7;
     private TaskList taskList;
 
     /**
@@ -34,28 +36,62 @@ public class RecurTaskCommand extends Command implements CommandParser {
 
     /**
      * Returns the number of repetitions of the recurring task in an integer form.
-     * Used in recurTaskPacker.
      *
      * @param restOfUserInput User input string.
      * @return Number of repetitions of the recurring task.
+     * @throws Compal.DukeException
      */
-    public static int getRep(String restOfUserInput) {
-        int splitPoint = restOfUserInput.indexOf(TOKEN_REP);
-        String repPart = restOfUserInput.substring(splitPoint);
-        Scanner sc = new Scanner(repPart);
-        sc.next();
-        int repNum = sc.nextInt();
-        return repNum;
+    public int getRep(String restOfUserInput) throws Compal.DukeException {
+        if (restOfUserInput.contains(TOKEN_REP)) {
+            int splitPoint = restOfUserInput.indexOf(TOKEN_REP);
+            String repPart = restOfUserInput.substring(splitPoint);
+            Scanner scanner = new Scanner(repPart);
+            scanner.next();
+            if (!scanner.hasNext()) {
+                compal.ui.printg(MESSAGE_MISSING_REP);
+                throw new Compal.DukeException(MESSAGE_MISSING_REP);
+            }
+            int repNum = scanner.nextInt();
+            return repNum;
+        } else {
+            compal.ui.printg(MESSAGE_MISSING_REP_ARG);
+            throw new Compal.DukeException(MESSAGE_MISSING_REP_ARG);
+        }
     }
 
     /**
-     * Increases the date by one week.
-     * Used in recurTaskPacker.
+     * Returns the number of days between the recurring task in an integer form.
+     *
+     * @param restOfUserInput
+     * @return Number of days between each recurring task.
+     * Will default to DEFAULT_FREQ_NUM.
+     */
+    public static int getFreq(String restOfUserInput) {
+        int freqNum = DEFAULT_FREQ_NUM;
+        if (restOfUserInput.contains(TOKEN_FREQ)) {
+            int splitPoint = restOfUserInput.indexOf(TOKEN_FREQ);
+            String freqPart = restOfUserInput.substring(splitPoint);
+            Scanner scanner = new Scanner(freqPart);
+            scanner.next();
+            if (!scanner.hasNext()) {
+                return freqNum;
+            } else {
+                freqNum = scanner.nextInt();
+                return freqNum;
+            }
+        } else {
+            return freqNum;
+        }
+    }
+
+    /**
+     * Increases the date by the number of days specified by the user.
      *
      * @param dateString The date to increment.
+     * @param freqNum The number of days between each task.
      * @return Final incremented date.
      */
-    public static String incrementDateByWeek(String dateString) {
+    public static String incrementDate(String dateString, int freqNum) {
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
         Date date = null;
         try {
@@ -65,7 +101,7 @@ public class RecurTaskCommand extends Command implements CommandParser {
         }
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
-        calendar.add(Calendar.DATE, 7);
+        calendar.add(Calendar.DATE, freqNum);
         return format.format(calendar.getTime());
     }
 
@@ -86,6 +122,7 @@ public class RecurTaskCommand extends Command implements CommandParser {
             String date = getDate(restOfInput);
             String time = getTime(restOfInput);
             int rep = getRep(restOfInput);
+            int freq = getFreq(restOfInput);
             String dateStr = date;
             for (int count = 0; count < rep; count++) {
                 RecurringTask newRecurTask = new RecurringTask(description, priority, dateStr, time);
@@ -93,7 +130,7 @@ public class RecurTaskCommand extends Command implements CommandParser {
                 int arrSize = taskList.arrlist.size() - 1;
                 String descToPrint = taskList.arrlist.get(arrSize).toString();
                 compal.ui.printg(descToPrint);
-                dateStr = incrementDateByWeek(dateStr);
+                dateStr = incrementDate(dateStr, freq);
             }
         } else {
             compal.ui.printg(MESSAGE_MISSING_COMMAND_ARG);
