68644460e3422ed3dac0e8b9ba48c361d121b94c
danisheddie <danisheddie1405@gmail.com>
2019-09-29 00:58:18 +0800

    Added class for sub-Menu

---
+++ src/main/java/duke/Task/MySubMenu.java
@@ -0,0 +1,15 @@
+package duke.Task;
+
+import java.util.ArrayList;
+
+public class MySubMenu extends MyMenu {
+
+
+    public MySubMenu(String subMenuDescription, String menu) {
+        super(subMenuDescription, menu);
+    }
+
+
+
+
+}
