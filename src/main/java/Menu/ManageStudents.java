1562ab84207919383be5dbb5568047f85fef880c
danisheddie <danisheddie1405@gmail.com>
2019-09-29 17:02:36 +0800

    Added class for managing Students

---
+++ src/main/java/Menu/ManageStudents.java
@@ -0,0 +1,13 @@
+package Menu;
+
+/**
+ * This class is called when option 2 is chosen
+ */
+public class ManageStudents extends MyMenu {
+
+    public ManageStudents(String myCategory, String menu) {
+        super(myCategory, menu);
+    }
+
+
+}
