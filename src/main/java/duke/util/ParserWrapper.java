c37e08f8627c3fd403c5b22cefcb4435f92d7855
namiwa <khairul.nasri895@gmail.com>
2019-09-29 19:58:56 +0800

    Added frame of checker in ParserWrapper

--- src/main/java/duke/util/ParserWrapper.java
+++ src/main/java/duke/util/ParserWrapper.java
@@ -2,20 +2,37 @@ package duke.util;
 
 import duke.command.Command;
 import duke.exceptions.DukeException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 
 public class ParserWrapper {
 
     // Testing Natty Wrapper
     private NattyWrapper natty;
+    private HashSet<String> dateCommandKeys;
+    private final String[] values = {"event", "deadline", "fixedDuration"};
 
     /**
      * Constructor for parser wrapper class.
      */
     public ParserWrapper() {
         natty = new NattyWrapper();
+        dateCommandKeys = setDateCommandKeys();
     }
 
-    public Command parse(String input) throws DukeException {
+    private HashSet<String> setDateCommandKeys() {
+        return new HashSet<>(Arrays.asList(values));
+    }
+
+    /**
+     * Parsing date arguments.
+     * @param input User input.
+     * @return Command class based on user input.
+     * @throws DukeException error based on user input.
+     */
+    public Command parseDateArgs(String input) throws DukeException {
+
         Command ret = DukeParser.parse(input);
         return ret;
     }
