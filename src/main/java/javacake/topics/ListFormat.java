8fbb0f2ba7be66001741a2671fa2e7d194d3dd2a
GlenWong97 <shuzewong@yahoo.com.sg>
2019-09-29 22:57:23 +0800

    v1.1-Created MainList class that contains the available topics shown in the main list of application

---
+++ src/main/java/javacake/topics/ListFormat.java
@@ -0,0 +1,14 @@
+package javacake.topics;
+
+import java.util.ArrayList;
+
+public abstract class ListFormat {
+
+    public ArrayList<String> al;
+
+    /**
+     * Prints all the strings in the ArrayList al
+     */
+    public abstract void printList();
+
+}
