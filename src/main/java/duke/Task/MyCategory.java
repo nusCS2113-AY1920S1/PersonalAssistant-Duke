44244149f79c43399d5b9fbb4f21f9c5790f8783
danisheddie <danisheddie1405@gmail.com>
2019-09-29 15:56:34 +0800

    Swap name to Category

---
+++ src/main/java/duke/Task/MyCategory.java
@@ -0,0 +1,19 @@
+package duke.Task;
+
+import java.util.ArrayList;
+
+/**
+ * This class is to add categories for the respective menus
+ */
+
+public class MyCategory extends MyMenu {
+
+
+    public MyCategory(String subMenuDescription, String menu) {
+        super(subMenuDescription, menu);
+    }
+
+
+
+
+}
