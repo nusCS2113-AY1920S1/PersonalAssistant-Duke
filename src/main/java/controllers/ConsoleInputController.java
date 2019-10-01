6a3784fb16958bf6bfe2be7c94ba96063c3d5e5c
iamabhishek98 <saiabhishek.karnati@u.nus.edu>
2019-10-01 20:34:03 +0800

    DoAfter tasks initial commit

--- src/main/java/controllers/ConsoleInputController.java
+++ src/main/java/controllers/ConsoleInputController.java
@@ -186,6 +186,7 @@ public class ConsoleInputController implements IViewController {
         case "event":
         case "todo":
         case "deadline":
+        case "doafter":
             try {
                 ITask newTask = taskFactory.createTask(input);
                 boolean anomaly = taskList.addToList(newTask);
