9d929457a3b24711b9d4d63ce2f6b9211797f561
Sfloydzy <e0310657@u.nus.edu>
2019-10-01 20:44:50 +0800

    Added files and changed parser

---
+++ src/main/java/duke/Module/TimeSlot.java
@@ -0,0 +1,43 @@
+package duke.Module;
+
+import java.util.Date;
+
+/**
+ * Function contains all the details of a time slot that needs to be scheduled.
+ */
+public final class TimeSlot {
+    /**
+     * Start of the event.
+     */
+    private Date startTime;
+    /**
+     * End of the event.
+     */
+    private Date endTime;
+    /**
+     * Location of event.
+     */
+    private String location;
+    /**
+     * Name of the Being taken.
+     * e.g: Soccer 1
+     */
+    private String className;
+
+    /**
+     * Constructor.
+     *
+     * @param start the specified start time
+     * @param end   the specified end time
+     * @param loc   the specified location of the event
+     * @param name  the specified name of the event
+     */
+    private TimeSlot(final Date start, final Date end,
+                     final String loc, final String name) {
+        startTime = start;
+        endTime = end;
+        location = loc;
+        className = name;
+    }
+
+}
---
+++ src/main/java/duke/Module/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Package contains all the modules that will interact to duke.
+ * Each Class is a feature of Duke.
+ * List of features.
+ * 1. Menu
+ * 2. Schedule
+ * 3. Managing Students
+ * 4. Training Programs
+ */
+
+package duke.Module;
