7e0c85a21a77e2d676130905a0c072b83f571a6f
Sukrut1881 <e0311151@u.nus.edu>
2019-10-01 22:44:32 +0800

    Added RecurringTaskTest

---
+++ src/test/java/duke/tasks/RecurringTaskTest.java
@@ -0,0 +1,19 @@
+package duke.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RecurringTaskTest {
+
+    @Test
+    void testToString() {
+        LocalDateTime startDate = LocalDateTime.of(2019, 10, 2, 8, 8);
+        RecurringTask recurringTask = new RecurringTask("Homework", startDate, 2);
+        recurringTask.updateRecurringTask();
+        assertEquals(recurringTask.toString(), "[R][✘] Homework by "
+                        + startDate.toString().replace("T", " ") + " (every 2 days)");
+    }
+}
