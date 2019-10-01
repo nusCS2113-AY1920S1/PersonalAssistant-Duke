e700b1d9204353c1370d5c059495926572c301d8
Sukrut1881 <e0311151@u.nus.edu>
2019-10-01 22:09:39 +0800

    Added MarkDoneCommand Test

---
+++ src/test/java/duke/commands/MarkDoneCommandTest.java
@@ -0,0 +1,29 @@
+package duke.commands;
+
+import duke.UiStub;
+import duke.commons.DukeException;
+import duke.storage.Storage;
+import duke.tasks.Task;
+import duke.tasks.Todo;
+import javafx.scene.layout.VBox;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class MarkDoneCommandTest {
+
+    @Test
+    void execute() throws DukeException {
+        UiStub ui = new UiStub(new VBox());
+        Storage storage = new Storage("tasks.txt", ui);
+        Task task = new Todo("homework");
+        AddCommand addCommand = new AddCommand(task);
+        addCommand.execute(ui, storage);
+        MarkDoneCommand markDoneCommand = new MarkDoneCommand(0);
+        markDoneCommand.execute(ui, storage);
+        assertTrue(storage.getTasks().get(0).isDone());
+        DeleteCommand deleteCommand = new DeleteCommand(0);
+        deleteCommand.execute(ui, storage);
+
+    }
+}
