5f569edb5a6357d980cf207d4fe2e524b73a67fb
JustinChia1997 <justinchiajj@hotmail.com>
2019-10-02 02:14:01 +0800

    Fixed test case part two

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,6 +1,7 @@
+
 import org.junit.jupiter.api.Test;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DukeTest {
     @Test
