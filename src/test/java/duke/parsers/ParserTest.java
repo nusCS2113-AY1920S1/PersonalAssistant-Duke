a39f16bca882fcc7e97ab0bb539e05192599b406
Fractalisk <e0309823@u.nus.edu>
2019-10-01 15:07:31 +0800

    Remove obsolete Junit tests

--- src/test/java/duke/parsers/ParserTest.java
+++ src/test/java/duke/parsers/ParserTest.java
@@ -12,17 +12,17 @@ class ParserTest {
     void parse() {
         try {
             assertTrue(Parser.parse("bye") instanceof ExitCommand);
-            assertTrue(Parser.parse("todo hackathon") instanceof addCommand);
-            assertTrue(Parser.parse("todo hackathon /needs 3 hours") instanceof addCommand);
-            assertTrue(Parser.parse("todo hackathon /between 1/1/2019 1800, 2/1/2019 1900") instanceof addCommand);
-            assertTrue(Parser.parse("deadline homework /by sunday") instanceof addCommand);
-            assertTrue(Parser.parse("event exam /at classroom") instanceof addCommand);
+            assertTrue(Parser.parse("breakfast burger") instanceof AddCommand);
+            assertTrue(Parser.parse("breakfast burger /calories 100") instanceof AddCommand);
+            assertTrue(Parser.parse("breakfast burger /calories 100 /date 1/1/2019") instanceof AddCommand);
+            assertTrue(Parser.parse("lunch burger") instanceof AddCommand);
+            assertTrue(Parser.parse("lunch burger /calories 100") instanceof AddCommand);
+            assertTrue(Parser.parse("lunch burger /calories 100 /date 1/1/2019") instanceof AddCommand);
+            assertTrue(Parser.parse("dinner burger") instanceof AddCommand);
+            assertTrue(Parser.parse("dinner burger /calories 100") instanceof AddCommand);
+            assertTrue(Parser.parse("dinner burger /calories 100 /date 1/1/2019") instanceof AddCommand);
             assertTrue(Parser.parse("list") instanceof ListCommand);
             assertTrue(Parser.parse("done 1") instanceof MarkDoneCommand);
-            assertTrue(Parser.parse("schedule 12/12/2012 1212") instanceof ScheduleCommand);
-            assertTrue(Parser.parse("remindme 4") instanceof RemindCommand);
-            assertTrue(Parser.parse("findfreetime 4") instanceof FindFreeTimeCommand);
-            assertTrue(Parser.parse("snooze 17 19") instanceof SnoozeCommand);
         } catch (DukeException e) {
             System.out.println("Something is wrong with the parser");
         }
