11a3b5cd44fd2e116d94c5072e147a78044c45c5
namiwa <khairul.nasri895@gmail.com>
2019-10-01 23:29:12 +0800

    testing with request library

---
+++ src/main/java/duke/util/RequestsData.java
@@ -0,0 +1,22 @@
+package duke.util;
+
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class RequestsData {
+
+    public void setRequest() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://openjdk.java.net/"))
+                .build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+    }
+}
