1f132aea97bc04a05e6f5c755500cd24d8ea154c
Hong Chuan <hongchuan.nus@gmail.com>
2019-10-01 18:02:40 +0800

    Add image and Add Dofixed

--- src/main/java/duke/parsers/ParserStorageUtil.java
+++ src/main/java/duke/parsers/ParserStorageUtil.java
@@ -6,6 +6,7 @@ import duke.commons.MessageUtil;
 import duke.tasks.Deadline;
 import duke.tasks.DoWithin;
 import duke.tasks.Event;
+import duke.tasks.Fixed;
 import duke.tasks.RecurringTask;
 import duke.tasks.Task;
 import duke.tasks.Todo;
@@ -47,6 +48,10 @@ public class ParserStorageUtil {
         } else if ("R".equals(type)) {
             task = new RecurringTask(description, ParserTimeUtil.parseStringToDate(taskParts[3].strip()),
                     Integer.parseInt(taskParts[4].strip()));
+        } else if ("F".equals(type)) {
+            int hour = Integer.parseInt(taskParts[3].strip());
+            int min = Integer.parseInt(taskParts[4].strip());
+            task = new Fixed(description, hour, min);
         } else {
             task = new Todo(description);
         }
@@ -72,6 +77,8 @@ public class ParserStorageUtil {
         } else if (task instanceof RecurringTask) {
             return ("R | " + task.isDone() + " | " + task.getDescription() + " | "
                     + ((RecurringTask) task).getStartDate() + " | " +  ((RecurringTask) task).getRepeatInterval());
+        } else if (task instanceof Fixed) {
+            return "F | " + task.isDone() + " | " + task.getDescription() + " | " + ((Fixed) task).getFixed();
         }
         throw new DukeException(MessageUtil.CORRUPTED_TASK);
     }
