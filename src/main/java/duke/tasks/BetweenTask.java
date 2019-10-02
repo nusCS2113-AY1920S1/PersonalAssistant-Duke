c491ea930c9f06e3b9ebad08f4d6764f0a9a6212
jwyf <e0311252@u.nus.edu>
2019-10-02 21:26:17 +0800

    Complete A-CheckStyle for tasks package

--- src/main/java/duke/tasks/BetweenTask.java
+++ src/main/java/duke/tasks/BetweenTask.java
@@ -1,37 +1,32 @@
 package duke.tasks;
 
-/**
- * @author Suther David Samuel (A0182488N)
- * A class inheriting from duke.tasks.Task used to represent tasks that have both a description and
- * has to be done within a period
- */
 public class BetweenTask extends Task {
-    private String start_date;
-    private String end_date;
+    private String startDate;
+    private String endDate;
 
     /**
      * Constructor for a duke.tasks.BetweenTask task, which consists of the description of the task and the period
      * associated with it.
      *
      * @param description refers to the description of the task
-     * @param start_date refers to the start of the period
-     * @param end_date refers to the end of the period
+     * @param startDate refers to the start of the period
+     * @param endDate refers to the end of the period
      */
-    public BetweenTask(String description, String start_date, String end_date){
+    public BetweenTask(String description, String startDate, String endDate) {
         super(description);
-        this.start_date = start_date;
-        this.end_date = end_date;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 
     /**
-     * Returns a String representation of the duke.tasks.BetweenTask object, displaying its type (duke.tasks.BetweenTask),
-     * description and the period associated with it.
+     * Returns a String representation of the duke.tasks.BetweenTask object, displaying its type
+     * (duke.tasks.BetweenTask), description and the period associated with it.
      *
      * @return a String representation of the duke.tasks.BetweenTask object
      */
     @Override
-    public String toString(){
-        return "[B]" + super.toString() + " (between: " + start_date + " and " + end_date +")";
+    public String toString() {
+        return "[B]" + super.toString() + " (between: " + startDate + " and " + endDate + ")";
     }
 
 }
