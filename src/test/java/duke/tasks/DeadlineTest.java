660ac63c92f028ebd015a1a86f05128c9d3ba1c4
jwyf <e0311252@u.nus.edu>
2019-10-02 18:24:04 +0800

    Complete A-CheckStyle for test code

--- src/test/java/duke/tasks/DeadlineTest.java
+++ src/test/java/duke/tasks/DeadlineTest.java
@@ -7,11 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DeadlineTest {
 
     @Test
-    public void testToString(){
-        assertEquals("[D][x] Read book (by: whenever)", new Deadline("Read book", "whenever").toString());
-        assertEquals("[D][x] Celebrate My Birthday (by: 12/12/2019 11:59 PM)", new Deadline("Celebrate My Birthday", "12/12/2019 2359").toString());
-        assertEquals("[D][x] Celebrate My Birthday (by: 12/12/2019 12:20 PM)", new Deadline("Celebrate My Birthday", "12/12/2019 12:20 PM").toString());
-        assertEquals("[D][x] Read book (by: 12/09/2019 11:59 PM)", new Deadline("Read book", "12/09/2019").toString());
+    public void testToString() {
+        assertEquals("[D][x] Read book (by: whenever)", new Deadline("Read book",
+                "whenever").toString());
+        assertEquals("[D][x] Celebrate My Birthday (by: 12/12/2019 11:59 PM)", new Deadline("Celebrate My Birthday",
+                "12/12/2019 2359").toString());
+        assertEquals("[D][x] Celebrate My Birthday (by: 12/12/2019 12:20 PM)", new Deadline("Celebrate My Birthday",
+                "12/12/2019 12:20 PM").toString());
+        assertEquals("[D][x] Read book (by: 12/09/2019 11:59 PM)", new Deadline("Read book",
+                "12/09/2019").toString());
     }
 
 }
--- src/test/java/duke/tasks/DoAfterTest.java
+++ src/test/java/duke/tasks/DoAfterTest.java
@@ -6,8 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DoAfterTest {
     @Test
-    public void toString_DoAfterTask_DoAfterString(){
-        assertEquals("[A][x] Read book (after: Task 1)", new DoAfter("Read book",1, 0).toString());
-        assertEquals("[A][x] Celebrate My Birthday (after: Task 2)", new DoAfter("Celebrate My Birthday", 2, 1).toString());
+    public void toString_DoAfterTask_DoAfterString() {
+        assertEquals("[A][x] Read book (after: Task 1)", new DoAfter("Read book",
+                1, 0).toString());
+        assertEquals("[A][x] Celebrate My Birthday (after: Task 2)", new DoAfter("Celebrate My Birthday",
+                2, 1).toString());
     }
 }
