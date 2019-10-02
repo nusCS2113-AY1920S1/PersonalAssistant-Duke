f070c1be9603e9bdc3cd07e7fe9d2a4a1f074ecb
samuel787 <suther.david.samuel@gmail.com>
2019-10-02 17:04:52 +0800

    Checkstyle errors for BetweenTaskTest and RecurringTaskTest have been resolved

--- src/test/java/duke/tasks/RecurringTaskTest.java
+++ src/test/java/duke/tasks/RecurringTaskTest.java
@@ -4,20 +4,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * @author Suther David Samuel (A0182488N)
- * JUnit testing for RecurringTask
- */
-
 public class RecurringTaskTest {
 
     @Test
-    public void toString_testingToString_success(){
-        assertEquals("[R][x] eatmacs (daily on: 19/12/2012 at evenings)", new RecurringTask("eatmacs", "19/12/2012", "evenings", "daily").toString());
+    public void toString_testingToString_success() {
+        assertEquals("[R][x] eatmacs (daily on: 19/12/2012 at evenings)",
+                new RecurringTask("eatmacs", "19/12/2012", "evenings", "daily")
+                        .toString());
     }
 
     @Test
-    public void isDateValid_invalidDate_false(){
-        assertEquals(false, RecurringTask.isDateVaid(99,99,1999));
+    public void isDateValid_invalidDate_false() {
+        assertEquals(false,
+                RecurringTask.isDateVaid(99,99,1999));
     }
 }
