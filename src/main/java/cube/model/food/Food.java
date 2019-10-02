a1481ec47f55d2ea2cc9a42386d40f92166ba51a
parvathi14 <e0376585@u.nus.edu>
2019-10-02 17:35:36 +0800

    Milestone v1.1 first edit

--- src/main/java/cube/model/food/Food.java
+++ src/main/java/cube/model/food/Food.java
@@ -1,29 +1,98 @@
 package cube.model.food;
 
-// for testing only.
-public class Food {
-	public Food() {
-		
+import java.io.Serializable;
+import java.util.Date;
+
+public class Food implements Serializable {
+	protected String name;
+	protected String type;
+	protected static double revenue;
+	protected double price;
+	protected int stock;
+	protected Date expiryDate;
+
+	/**
+	 * Constructor with four arguments.
+	 *
+	 * @param name Name of the food product.
+	 * @param type Type of food product.
+	 * @param price Price of the product.
+	 * @param expiryDate Date on which the product expires.
+	 */
+
+	public Food(String name, String type, double price, Date expiryDate) {
+		this.name = name;
+		this.type = type;
+		this.price = price;
+		this.expiryDate = expiryDate;
+		this.stock = 0;
+		this.revenue = 0;
 	}
 
-	public static int getRevenue() {
-		return 0;
+	/**
+	 * Gets the revenue earned from selling the product.
+	 *
+	 * @return the revenue earned from selling the product.
+	 */
+
+	public static double getRevenue() {
+		return revenue;
 	}
 
-	public static void updateRevenue(int a) {
-		
+	/**
+	 * Updates the total revenue made from selling the product.
+	 *
+	 * @param newRevenue New total revenue made from selling the product.
+	 */
+
+	public static void updateRevenue(double newRevenue) {
+		revenue = newRevenue;
 	}
 
-	public int getPrice() {
-		return 0;
+	/**
+	 * Gets the price of the product.
+	 *
+	 * @return the price of the product.
+	 */
+
+	public double getPrice() {
+		return price;
 	}
 
-	public void updateStock(int a) {
+	/**
+	 * Updates the quantity of the product available in stock.
+	 *
+	 * @param newStock New quantity of product available.
+	 */
+	public void updateStock(int newStock) {
+		stock = newStock;
 	}
+
+	/**
+	 * Gets the quantity of the product available in stock.
+	 *
+	 * @return Quantity of product in stock.
+	 */
 
 	public int getStock() {
-		return 0;
+		return stock;
 	}
+
+	/**
+	 * Function called when price of the product needs to be changed.
+	 *
+	 * @param newPrice New price of the product.
+	 */
+
+	public void updatePrice (double newPrice) {
+		price = newPrice;
+	}
+
+	/**
+	 * Shows that product already exists in the inventory.
+	 *
+	 * @return true
+	 */
 
 	public boolean exists() {
 		return true;
