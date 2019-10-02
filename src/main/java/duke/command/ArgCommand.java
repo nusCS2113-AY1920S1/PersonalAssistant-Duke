a343253073772babad8a8bc4431fb13180cc2181
John Khoo <john_khoo@u.nus.edu>
2019-10-02 23:13:56 +0800

    Add auxiliary parser functions

--- src/main/java/duke/command/ArgCommand.java
+++ src/main/java/duke/command/ArgCommand.java
@@ -9,10 +9,20 @@ import java.util.HashMap;
  * Abstract class for commands that involve an argument.
  */
 public abstract class ArgCommand extends Command {
-    protected String arg; //argument supplied to the command
+
+    protected String arg = null; //argument supplied to the command
     protected String emptyArgMsg; //error message if the argument is empty
-    protected String[] switches; //list of recognised switches
-    protected HashMap<String, String> switchVals; //hashmap of parameters
+    protected HashMap<String, String> switchVals; //hashmap of switch parameters
+    protected HashMap<String, ArgLevel> switches; //list of recognised switches
+
+    /**
+     *  Inner class to hold indices of tokens particular tokens when parsing an input string.
+     */
+    static class Tokens {
+        //man I miss structs
+        static int spaceIdx, dashIdx, quoteIdx, endQuoteIdx;
+        static String currSwitch;
+    }
 
     /**
      * Parses the user's input and loads the parameters for this Command from it.
@@ -22,17 +32,17 @@ public abstract class ArgCommand extends Command {
      *                       parsed.
      */
     public void parse(String inputStr) throws DukeException {
-        arg = inputStr.strip();
-        if (arg.length() == 0) {
+        if (inputStr.length() == 0) {
             throw new DukeException(emptyArgMsg);
         }
-        int spaceIdx = inputStr.indexOf(" ");
-        int dashIdx = inputStr.indexOf("-");
-        int quoteIdx = inputStr.indexOf("\"");
-        while (spaceIdx >= 0) {
-            spaceIdx = inputStr.indexOf(" ", spaceIdx + 1);
-            dashIdx = inputStr.indexOf("-", spaceIdx);
-            quoteIdx = inputStr.indexOf("\"", spaceIdx);
+
+        //find initial tokens
+
+
+        while (Tokens.spaceIdx >= 0) {
+            if (Tokens.quoteIdx < Tokens.spaceIdx) {
+
+            }
         }
     }
 
@@ -40,6 +50,30 @@ public abstract class ArgCommand extends Command {
     public void execute(DukeContext ctx) throws DukeException, DukeException {
         if (arg == null) {
             throw new DukeException("Command needs to parse argument first!");
+        }
+    }
+
+    protected void complain(String complaint, String ...issues) throws DukeException {
+        StringBuilder complaintBuilder = new StringBuilder(complaint).append(": ");
+        for (String issue : issues) {
+            complaintBuilder.append(issue)
+        }
+       throw new DukeException(complaint + ": ")
+    }
+
+    private void initTokens(String inputStr) {
+        Tokens.spaceIdx = inputStr.indexOf(" ");
+        Tokens.dashIdx = inputStr.indexOf("-");
+        Tokens.quoteIdx = inputStr.indexOf("\"");
+        Tokens.endQuoteIdx = inputStr.indexOf("\"", Tokens.quoteIdx + 1);
+        Tokens.currSwitch = null;
+
+        if (Tokens.spaceIdx == -1 && switches.size() != 0) {
+            //search pairs instead and complain with names
+            for (ArgLevel level : switches.values()) {
+                if (level != ArgLevel.NONE) {
+                }
+            }
         }
     }
 }
