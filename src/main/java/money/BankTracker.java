6fc70f17b6b3b4471717b349bb74f1d6d7bd7234
cctt1014 <e0261888@u.nus.edu>
2019-10-06 00:15:47 +0800

    fix the bugs in creating a new bank-tracker and add the javadoc

--- src/main/java/money/BankTracker.java
+++ src/main/java/money/BankTracker.java
@@ -2,27 +2,37 @@ package money;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
+/**
+ * This class represents a bank account tracker which can be used to
+ * store the related information of a bank account
+ * (account description, balance, the latest update date and the interest rate)
+ */
 public class BankTracker {
 
     private String description;
     private int amt;
     private LocalDate latestDate;
     private double rate;
-    private SimpleDateFormat simpleDateFormat;
-
+    private DateTimeFormatter dateTimeFormatter;
 
     public BankTracker(String accountDescription, int initialAmt, LocalDate initialDate, double interestRate) {
         description = accountDescription;
         amt = initialAmt;
         latestDate =  initialDate;
         rate = interestRate;
-        simpleDateFormat = new SimpleDateFormat("d/M/yyyy");
+        dateTimeFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
     }
 
+    /**
+     * The method is a getter to get a formatted string which contains
+     * all the information of a bank account tracker
+     * @return A string which contains all the information of a bank account tracker
+     */
     public String getBankAccountInfo() {
         return "  Name: " + description + "\n  Balance: " + amt + "\n  Initial Date: "
-                + simpleDateFormat.format(latestDate) + "\n  Interest Rate: " + rate;
+                + dateTimeFormatter.format(latestDate) + "\n  Interest Rate: " + rate;
     }
 }
--- src/main/java/moneycommands/CreateBankAccountCommand.java
+++ src/main/java/moneycommands/CreateBankAccountCommand.java
@@ -1,22 +1,30 @@
 package moneycommands;
 
 import controlpanel.MoneyStorage;
+import controlpanel.Parser;
+import controlpanel.Ui;
 import money.Account;
 import money.BankTracker;
-import controlpanel.Parser;
-import controlpanel.Storage;
-import controlpanel.Ui;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
+/**
+ * This class create a bank account tracker for the user to track their
+ * different bank account.
+ */
 public class CreateBankAccountCommand extends MoneyCommand {
 
     private BankTracker newTracker;
 
+    /**
+     * The constructor analyzes the input command and get the information
+     * (account description, initial amount, initial date and interest rate)
+     * of the new bank account tracker and pack it as a new bank-tracker.
+     * @param inputString The command line typed in by the user
+     * @throws ParseException The exception for parsing the date
+     */
     public CreateBankAccountCommand(String inputString) throws ParseException {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
         String desc = inputString.split(" /amt ")[0];
@@ -28,11 +36,23 @@ public class CreateBankAccountCommand extends MoneyCommand {
                 initialDate, Double.parseDouble(words[4]));
     }
 
+    /**
+     * This method labels whether this command means ceasing the overall program.
+     * @return Whether this command means ceasing the overall program.
+     */
     @Override
     public boolean isExit() {
         return false;
     }
 
+    /**
+     * Since the new bank account tracker has been packed in the constructor,
+     * in this method, Financial Ghost will append the information of the new
+     * tracker to the list in the Account class and print a confirm message.
+     * @param account The class record all the financial information of the user
+     * @param ui The user interface
+     * @param storage The class used to store the information to the local disk
+     */
     @Override
     public void execute(Account account, Ui ui, MoneyStorage storage) {
         account.getBankTrackerList().add(newTracker);
