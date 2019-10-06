8023f5f64ec8df6916429d36e8940ff9febaa0b8
claysmilesoil <e0323492.u.nus.edu>
2019-10-06 15:08:28 +0800

    Add nonsense questions to list

--- src/main/java/javacake/quiz/QuestionList.java
+++ src/main/java/javacake/quiz/QuestionList.java
@@ -5,9 +5,6 @@ import java.util.Random;
 
 public class QuestionList {
     private ArrayList<Question> chosenQuestions;
-    private int basicQuestionsTotal;
-    private int oopQuestionsTotal;
-    private int extensionsQuestionsTotal;
     public static final int MAX_QUESTIONS = 1;
 
     public QuestionList() {
@@ -19,8 +16,14 @@ public class QuestionList {
 
         String q1 = "How many members are in this team? Answer in int.";
         String a1 = "4";
+        String q2 = "wake me up\n a. before you go go\n b. when september ends\n c. WAKE ME UP INSIDE";
+        String a2 = "c";
+        String q3 = "The Scanner class allows a program to read from file. T/F?";
+        String a3 = "t";
 
         basicQuestionList.add(new BasicQuestion(q1, a1));
+        basicQuestionList.add(new BasicQuestion(q2, a2));
+        basicQuestionList.add(new BasicQuestion(q3, a3));
 
         return basicQuestionList;
     }
@@ -28,11 +31,17 @@ public class QuestionList {
     private ArrayList<OOPQuestion> initOOPList() {
         ArrayList<OOPQuestion> OOPQuestionList = new ArrayList<>();
 
-        String q1 = "What does OOP stand for in software engineering context? Enter the corresponding alphabet.\n" +
+        String q1 = "What does OOP stand for in software engineering context?\n" +
                 " a. Out Of Print\n b. Object-Oriented Programming\n c. Ogre Onion Paradigm";
         String a1 = "b";
+        String q2 = "Name a class that does not inherit from any other class.";
+        String a2 = "object";
+        String q3 = "An interface implements methods that can be inherited by its subclasses. T/F?";
+        String a3 = "f";
 
         OOPQuestionList.add(new OOPQuestion(q1, a1));
+        OOPQuestionList.add(new OOPQuestion(q2, a2));
+        OOPQuestionList.add(new OOPQuestion(q3, a3));
 
         return OOPQuestionList;
     }
@@ -40,11 +49,20 @@ public class QuestionList {
     private ArrayList<ExtensionQuestion> initExtensionList() {
         ArrayList<ExtensionQuestion> ExtensionQuestionList = new ArrayList<>();
 
-        String q1 = "What should you NOT do to handle an exception in main? Enter the corresponding alphabet.\n" +
+        String q1 = "What should you NOT do to handle an exception in main?\n" +
                 " a. Rethrow the exception\n b. Print error message \n c. Call backup method";
         String a1 = "a";
+        String q2 = "What does the [finally] block do? Choose the best answer.\n" +
+                " a. specify code that is guaranteed to execute with or without exception.\n" +
+                " b. specify code that is guaranteed to execute at the end of a program.\n" +
+                " c. specify code that is guaranteed to execute after an assertion returns false.";
+        String a2 = "a";
+        String q3 = "What's the actual best programming language?";
+        String a3 = "assembly";
 
-        ExtensionQuestionList.add(new ExtensionQuestion(q1,a1));
+        ExtensionQuestionList.add(new ExtensionQuestion(q1, a1));
+        ExtensionQuestionList.add(new ExtensionQuestion(q2, a2));
+        ExtensionQuestionList.add(new ExtensionQuestion(q3, a3));
 
         return ExtensionQuestionList;
     }
