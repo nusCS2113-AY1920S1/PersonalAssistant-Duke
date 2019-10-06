453cebdd9ab7165113e1b80970faec23425b4192
sinteary <leewengyancynthia@gmail.com>
2019-10-07 02:14:36 +0800

    Edit checkstyle and documentation

--- src/main/java/controllers/ProjectInputController.java
+++ src/main/java/controllers/ProjectInputController.java
@@ -10,12 +10,21 @@ public class ProjectInputController {
     private ProjectRepository projectRepository;
     private CLIView consoleView;
 
+    /**
+     * Class responsible for handling user input when user chooses to manage a project.
+     * @param consoleView The main UI of ArchDuke.
+     * @param projectRepository The list of all projects.
+     */
     public ProjectInputController(CLIView consoleView, ProjectRepository projectRepository) {
         this.manageProjectInput = new Scanner(System.in);
         this.projectRepository = projectRepository;
         this.consoleView = consoleView;
     }
 
+    /**
+     * Allows the user to manage the project by branching into the project of their choice.
+     * @param input User input containing project index number (to add to project class).
+     */
     public void manageProject(String input) {
         int projectNumber = Integer.parseInt(input);
         IProject projectToManage = projectRepository.getProject(projectNumber);
@@ -25,33 +34,31 @@ public class ProjectInputController {
             if (manageProjectInput.hasNextLine()) {
                 String projectCommand = manageProjectInput.nextLine();
                 switch (projectCommand) {
-                    case "exit":
-                        continueManaging = false;
-                        consoleView.exitProject(projectToManage.getDescription());
-                        break;
-                    case "add member":
-                        consoleView.consolePrint("Enter member details: n/NAME p/PHONE e/EMAIL");
-                        String memberDetails = manageProjectInput.nextLine();
-                        MemberFactoryUtil memberFactory = new MemberFactoryUtil();
-                        if (memberFactory.memberIsCreated(memberDetails)) {
-                            consoleView.addMember(projectToManage, memberFactory.getNewMember());
-                        } else {
-                            consoleView.consolePrint("Failed to add member. Please ensure you have entered "
-                                + "at least the name of the new member.");
-                        }
-                        break;
-                    case "view members":
-                        consoleView.viewAllMembers(projectToManage);
-                        break;
-                    default:
-                        consoleView.consolePrint("Invalid command. Try again!");
-                        break;
+                case "exit":
+                    continueManaging = false;
+                    consoleView.exitProject(projectToManage.getDescription());
+                    break;
+                case "add member":
+                    consoleView.consolePrint("Enter member details: n/NAME p/PHONE e/EMAIL");
+                    String memberDetails = manageProjectInput.nextLine();
+                    MemberFactoryUtil memberFactory = new MemberFactoryUtil();
+                    if (memberFactory.memberIsCreated(memberDetails)) {
+                        consoleView.addMember(projectToManage, memberFactory.getNewMember());
+                    } else {
+                        consoleView.consolePrint("Failed to add member. Please ensure you have entered "
+                            + "at least the name of the new member.");
+                    }
+                    break;
+                case "view members":
+                    consoleView.viewAllMembers(projectToManage);
+                    break;
+                default:
+                    consoleView.consolePrint("Invalid command. Try again!");
+                    break;
                 }
             } else {
                 consoleView.consolePrint("Please enter a command.");
             }
         }
     }
-
-
 }
