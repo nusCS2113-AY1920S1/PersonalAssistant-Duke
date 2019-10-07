77805adef11eb2bf568d67b28fd1a3fd45cf5591
JeremyKwok <jeremykke@gmail.com>
2019-10-08 03:11:16 +0800

    Evidence: Abstraction of Observation and Result

---
+++ src/main/java/duke/task/Evidence.java
@@ -0,0 +1,30 @@
+package duke.task;
+
+import duke.exception.DukeException;
+
+/*
+ * Abstraction of evidence constituting a medical diagnosis.
+ * An Evidence object corresponds to the notes of the doctor
+ * on a piece of medical evidence as well as
+ * an integer between 1-4 representing the priority or significance of the evidence.
+ *
+ * Attributes:
+ * - name: information on the evidence
+ * - impression: the impression object the evidence is tagged to
+ * - priority: the priority level of the evidence
+ */
+public abstract class Evidence extends DukeData {
+    public Evidence(String name, Impression impression, int priority) {
+        super(name, impression, priority);
+    }
+
+    @Override
+    public int updatePriority(int priorityVal) throws DukeException {
+        if (priorityVal >= 0 && priorityVal < 5) {
+            return super.setPriority(priorityVal);
+        } else {
+            throw new DukeException("The priority must be within 0 to 4!");
+        }
+    }
+
+}
