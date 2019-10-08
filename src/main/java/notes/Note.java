0fd9c0d9041c0fae65c4c0e643e2b45203b566b9
yueyuu <yueyu623@gmail.com>
2019-10-08 18:48:45 +0800

    Add some comments for Note class

--- src/main/java/notes/Note.java
+++ src/main/java/notes/Note.java
@@ -4,12 +4,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-public class NoteDaily {
+public class Note {
+    /** Stores the date of the note
+     *  If the note is for a particular day, it will store that day's date.
+     *  If the note is for a particular week, it will store the date of that week's Monday.
+     *  If the note is for a particular month, it will store the first date of that month.
+     *  */
     protected LocalDate noteDate; //yyyy-MM-dd
     protected ArrayList<String> notes;
     public static DateTimeFormatter noteFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-    public NoteDaily(String dateOfNote) {
+    public Note(String dateOfNote) {
         noteDate = LocalDate.parse(dateOfNote, noteFormatter);
         notes = new ArrayList<String>();
     }
