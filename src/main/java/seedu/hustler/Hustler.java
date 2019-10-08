b54bd27ae528309709d63d2f70711fc419a84c8d
ngjiewu <ngjiewu@gmail.com>
2019-10-08 09:29:21 +0800

    Added buttons to basic GUI

--- src/main/java/seedu/hustler/Hustler.java
+++ src/main/java/seedu/hustler/Hustler.java
@@ -18,16 +18,12 @@ import seedu.hustler.parser.CommandParser;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.layout.Region;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 /**
  * A personal assitant that takes in user input and gives and performs
