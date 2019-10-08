e4e5515fbb4dbc2dbca477fa8dcba6ea090a54fc
leowyh <leowyh4@gmail.com>
2019-10-08 21:14:02 +0800

    Added payment Package, Payments and PaymentManager Classes Removed old PaymentDetails and PaymentForm Classes

---
+++ src/main/java/payment/PaymentManager.java
@@ -0,0 +1,47 @@
+package payment;
+
+import java.util.ArrayList;
+
+/**
+ * PaymentManager for managing Payments objects and PaymentForms from the PaymentsList
+ */
+public class PaymentManager {
+
+
+    /**
+     * Finds the Payments objects containing a payee name and returns a list of Payments.
+     * @param payee Payee of the item.
+     * @return Payments object with payee name.
+     */
+    public void findPayee(String payee, ArrayList<Payments> paymentslist){
+        //TODO
+    }
+
+    /**
+     * Edits the Payments object details, may overload string to take different ways of inputs.
+     */
+    public void editPayments(){
+        //TODO
+    }
+
+    /**
+     * List the Payments object details, may extend to generate statement of accounts
+     */
+    public void listPayments(){
+        //TODO
+    }
+
+    /**
+     * Deletes the Payments object details
+     */
+    public void deletePayments(){
+        //TODO
+    }
+
+    /**
+     * Add the Payments object details to PaymentsList
+     */
+    public void addPayments(){
+        //TODO
+    }
+}
