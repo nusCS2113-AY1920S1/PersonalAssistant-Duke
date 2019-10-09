e26183765b8867c38831784f991520e6cc724213
Nathan <nystera@gmail.com>
2019-10-09 16:30:35 +0800

    +Added tagging system. +find <keyword> now includes tag. +Changed difficulty to parse through /d. +Better parsing for adding Tasks. +Refactored files with proper folders.

--- src/main/java/seedu/hustler/task/variables/Difficulty.java
+++ src/main/java/seedu/hustler/task/variables/Difficulty.java
@@ -1,34 +1,50 @@
 package seedu.hustler.task.variables;
 
+/**
+ * The class that stores the difficulty of the task.
+ */
 public class Difficulty {
 
-     enum type {
+    /**
+     * Enumerator that separates the level of the difficulty.
+     */
+     enum Level {
         L,
         M,
         H
     }
 
-    private type difficulty;
+    /**
+     * The level of the difficulty.
+     */
+    private Level level;
 
+    /**
+     * Constructs a difficulty level based on user input.
+     * Will automatically set default difficulty as M (medium)
+     * if user input cannot be parsed.
+     * @param difficulty the level of the difficulty.
+     */
     public Difficulty(String difficulty) {
          switch (difficulty.toLowerCase()) {
              case "l":
              case "low":
-             this.difficulty = type.L;
+             this.level = Level.L;
              break;
              case "m":
              case "medium":
              default:
-             this.difficulty = type.M;
+             this.level = Level.M;
              break;
              case "h":
              case "high":
-             this.difficulty = type.H;
+             this.level = Level.H;
         }
     }
 
+    @Override
     public String toString() {
-        return "[" + this.difficulty.toString() + "]";
+        return "[" + this.level.toString() + "]";
     }
 
 }
