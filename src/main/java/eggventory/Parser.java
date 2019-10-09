ea59282f032330379621fd809091a508ff324624
Rebecca Oei <e0322808@u.nus.edu>
2019-10-09 19:15:18 +0800

    Parser fix for addcommand to handle wrong input more accurately

--- src/main/java/eggventory/Parser.java
+++ src/main/java/eggventory/Parser.java
@@ -34,6 +34,7 @@ public class Parser {
     /**
      * Processes the contents of an add command (everything after the word "add").
      * Splits up the input string into an array containing the various attributes of the stock being added.
+     * Strips leading/trailing whitespace from
      *
      * @param input String containing the attributes of the stock.
      * @return an array consisting of StockType, StockCode, Quantity and Description.
@@ -41,12 +42,20 @@ public class Parser {
      */
     private String[] processAdd(String input) throws InsufficientInfoException {
 
-        String[] addInput = input.split(" ", 4); //There are 4 attributes for now.
+        String[] addInput = input.split(" +", 4); //There are 4 attributes for now.
 
-        if (addInput.length < 4 | addInput[0].isBlank() | addInput[1].isBlank()
-                | addInput[2].isBlank() | addInput[3].isBlank()) {
+        if (addInput.length < 4) {
             throw new InsufficientInfoException("Please enter stock information after the 'add' command in"
                     + " this format:\nadd <StockType> <StockCode> <Quantity> <Description>");
+        } else {
+            addInput[0] = addInput[0].strip();
+            addInput[1] = addInput[1].strip();
+            addInput[2] = addInput[2].strip();
+            addInput[3] = addInput[3].strip();
+            if (addInput[0].isBlank() | addInput[1].isBlank() | addInput[2].isBlank() | addInput[3].isBlank()) {
+                throw new InsufficientInfoException("Please enter stock information after the 'add' command in"
+                        + " this format:\nadd <StockType> <StockCode> <Quantity> <Description>");
+            }
         }
 
         return addInput;
@@ -80,7 +89,7 @@ public class Parser {
         //Extract the first word.
         //inputArr[0] is the main command word.
         //inputArr[1] is the subsequent command string, and may also be empty.
-        String[] inputArr = listInput.split(" ", 2);
+        String[] inputArr = listInput.split(" +", 2);
         Command command;
 
         switch (inputArr[0]) {
