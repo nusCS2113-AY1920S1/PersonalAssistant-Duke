19787c38c20004a77f6b87895dd691c8db19e20c
lpeize <e0261957@u.nus.edu>
2019-10-09 15:52:47 +0800

    Junit 1.2.1.2

--- src/test/java/compal/logic/commands/ByeCommand.java
+++ src/test/java/compal/logic/commands/ByeCommand.java
@@ -1,8 +1,15 @@
 package compal.logic.commands;
 
 import compal.commons.Compal;
+import org.junit.jupiter.api.BeforeEach;
 
 public class ByeCommand {
     private ByeCommand byeCommand;
-    private Compal d;
+    private Compal compal;
+
+    @BeforeEach
+    public void setup() {
+        compal = new Compal();
+        byeCommand = new ByeCommand();
+    }
 }
