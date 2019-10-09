57921bae33d83537433285c813981b9d7fb107a5
yuyanglin28 <annielin9908@gmail.com>
2019-10-09 22:39:05 +0800

    test class for period and last task

---
+++ src/test/java/tasks/PeriodTest.java
@@ -0,0 +1,53 @@
+package tasks;
+
+import jdk.jfr.StackTrace;
+import org.junit.jupiter.api.Test;
+import utils.DukeException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PeriodTest {
+    @Test
+    public void checkToString() throws DukeException {
+        //add period This is a test Period /from 10/12/2019 1120 /to 12/12/2019 1122
+        String description = "This is a test Period";
+        String start = "10/12/2019 1120";
+        String end = "12/12/2019 1122";
+        SimpleDateFormat ft = new SimpleDateFormat("dd/MM/yyyy hhmm");
+        String result = "[P][✘] This is a test Period (from: Tue Dec 10 11:20:00 SGT 2019 to: Thu Dec 12 11:22:00 SGT 2019)";
+        try {
+
+            Date tempStart = ft.parse(start);
+            Date tempEnd = ft.parse(end);
+            Period temp = new Period(description, tempStart, tempEnd);
+            assertEquals(result, temp.toString());
+        } catch (ParseException e) {
+            throw new DukeException("Invalid date format, the correct format is: dd/MM/yyyy");
+        }
+
+    }
+
+    @Test
+    public void checkDataString() throws DukeException {
+        //add period This is a test Period /from 10/12/2019 1120 /to 12/12/2019 1122
+        String description = "This is a test Period";
+        String start = "10/12/2019 1120";
+        String end = "12/12/2019 1122";
+        SimpleDateFormat ft = new SimpleDateFormat("dd/MM/yyyy hhmm");
+        String result = "P | 0 | This is a test Period | 10/12/2019 1120 | 12/12/2019 1122";
+        try {
+
+            Date tempStart = ft.parse(start);
+            Date tempEnd = ft.parse(end);
+            Period temp = new Period(description, tempStart, tempEnd);
+            assertEquals(result, temp.dataString());
+        } catch (ParseException e) {
+            throw new DukeException("Invalid date format, the correct format is: dd/MM/yyyy");
+        }
+
+    }
+}
