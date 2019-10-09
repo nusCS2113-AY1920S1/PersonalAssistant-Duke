d9bd51fd8753022510da608d8d44d1504b3ef226
JacobToresson <>
2019-10-09 23:32:28 +0800

    Creating Gson files

---
+++ src/main/java/duke/task/CreateGSONFile.java
@@ -0,0 +1,8 @@
+package duke.task;
+
+import com.google.gson.Gson;
+
+public class CreateGSONFile {
+    Gson gson = new Gson();
+
+}
---
+++ src/main/java/duke/task/ReadGSONFile.java
@@ -0,0 +1,7 @@
+package duke.task;
+
+import com.google.gson.Gson;
+
+public class ReadGSONFile {
+    Gson gson = new Gson();
+}
