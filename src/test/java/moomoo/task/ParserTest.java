a2a687cd30b8a02e8d9f67f685986c236ea36bba
Lindon Ng <lindonng@hotmail.com>
2019-10-10 17:16:47 +0800

    Changed JUnit Tests

--- src/test/java/moomoo/task/ParserTest.java
+++ src/test/java/moomoo/task/ParserTest.java
@@ -1,14 +1,6 @@
-package duke.task;
+package moomoo.task;
 
-import duke.command.AddDeadLineCommand;
-import duke.command.AddEventCommand;
-import duke.command.AddToDoCommand;
-import duke.command.DeleteTaskCommand;
-import duke.command.ExitCommand;
-import duke.command.FindTaskCommand;
-import duke.command.ListTaskCommand;
-import duke.command.MarkTaskAsDoneCommand;
-import duke.command.AddDoWithinPeriodCommand;
+import moomoo.command.ExitCommand;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,18 +11,10 @@ public class ParserTest {
     public void testParser() {
         try {
             assertTrue(Parser.parse("bye") instanceof ExitCommand);
-            assertTrue(Parser.parse("list") instanceof ListTaskCommand);
-            assertTrue(Parser.parse("todo work") instanceof AddToDoCommand);
-            assertTrue(Parser.parse("event to do work /at 02/12/2019 1830") instanceof AddEventCommand);
-            assertTrue(Parser.parse("deadline to do stuff /by 12/12/2020 1830") instanceof AddDeadLineCommand);
-            assertTrue(Parser.parse("done 1") instanceof MarkTaskAsDoneCommand);
-            assertTrue(Parser.parse("delete 1") instanceof DeleteTaskCommand);
-            assertTrue(Parser.parse("find hello") instanceof FindTaskCommand);
-            assertTrue(Parser.parse("dowithin read book /from 3/2/2019 1830 /to 3/5/2019 1830")
-                    instanceof AddDoWithinPeriodCommand);
+
             Parser.parse("invalid input");
-        } catch (DukeException e) {
-            assertEquals("duke.task.DukeException: OOPS!!! I'm sorry, but I don't know what that means :-(",
+        } catch (MooMooException e) {
+            assertEquals("moomoo.task.MooMooException: OOPS!!! I'm sorry, but I don't know what that means :-(",
                     e.toString());
         }
     }
--- src/test/java/moomoo/task/TaskListTest.java
+++ src/test/java/moomoo/task/TaskListTest.java
@@ -1,19 +1,10 @@
-package duke.task;
+package moomoo.task;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskListTest {
     @Test
     public void testAddToTaskList() {
         TaskList newTaskList = new TaskList();
-        newTaskList.addToArrayList(new ToDo("Hello World"));
-
-        assertEquals(newTaskList.getSize(), 1);
-        assertEquals(newTaskList.getTask(0).toString(), "[T][NOT DONE] Hello World");
-
-        newTaskList.deleteFromArrayList(0);
-        assertEquals(newTaskList.getSize(), 0);
     }
 }
