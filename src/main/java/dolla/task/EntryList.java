c9b04816fd4823d50da466a52d015f385c0c26c3
Penguin <quek.aik.peng@gmail.com>
2019-10-10 16:36:19 +0800

    Updated EntryList.

--- src/main/java/dolla/task/EntryList.java
+++ src/main/java/dolla/task/EntryList.java
@@ -4,6 +4,9 @@ import dolla.Log;
 
 import java.util.ArrayList;
 
+/**
+ * Holds all the entries that have been added to Dolla.
+ */
 public class EntryList extends LogList {
 
     public EntryList(ArrayList<Log> importEntryList) {
