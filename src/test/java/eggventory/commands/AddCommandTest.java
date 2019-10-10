7d1de099e062f410637e825270696066d0228f8b
Raghav-B <raghavbhardwaj1998@gmail.com>
2019-10-10 23:45:25 +0800

    Remove ToDo, Deadline & Event JUnit tests

--- src/test/java/eggventory/commands/AddCommandTest.java
+++ src/test/java/eggventory/commands/AddCommandTest.java
@@ -42,35 +42,4 @@ public class AddCommandTest {
                 "testDescription").execute(testList);
         assertEquals("testDescription",testList.getStock(0).getDescription());
     }
-
-    /*
-    @Test
-    public void execute_addTodoTask_success() throws BadInputException {
-        StockType newList = new StockType();
-        new AddCommand(CommandType.TODO, "Test TODO", "").execute(newList);
-        assertEquals("Test TODO", newList.getStock(0).getDescription());
-    }
-
-    @Test
-    public void execute_addDeadlineTask_success() throws BadInputException {
-        StockType newList = new StockType();
-        new AddCommand(CommandType.DEADLINE, "Test DEADLINE", "15/12/2019 1500").execute(newList);
-        SimpleDateFormat formatted = new SimpleDateFormat("EEE MMM d yyyy K:mm a");
-        String startDate = formatted.format(newList.getStock(0).getDate().getTime());
-        assertEquals("Test DEADLINE", newList.getStock(0).getDescription());
-        assertEquals("Sun Dec 15 2019 3:00 PM", startDate);
-    }
-
-    @Test
-    public void execute_addEventTask_success() throws BadInputException {
-        StockType newList = new StockType();
-        new AddCommand(CommandType.EVENT, "Test EVENT", "15/12/2019 1500 to 17/12/2019 1500").execute(newList);
-        SimpleDateFormat formatted = new SimpleDateFormat("EEE MMM d yyyy K:mm a");
-        String startDate = formatted.format(newList.getStock(0).getDate().getTime());
-        String endDate = formatted.format(newList.getStock(0).getEndDate().getTime());
-        assertEquals("Test EVENT", newList.getStock(0).getDescription());
-        assertEquals("Sun Dec 15 2019 3:00 PM", startDate);
-        assertEquals("Tue Dec 17 2019 3:00 PM", endDate);
-    }
-    */
 }
