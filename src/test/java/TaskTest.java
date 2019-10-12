8587ff82026570f8ba941218369306bba1c2d533
Dillen <dillenwong@gmail.com>
2019-10-12 23:40:28 +0800

    [Test] Add testAddTask function

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,6 +1,11 @@
 import controllers.TaskFactory;
+import models.task.Task;
 import org.junit.jupiter.api.Test;
-import views.CLIView;
+import models.task.TaskState;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,6 +17,15 @@ public class TaskTest {
         assertEquals(2, 2);
     }
 
-    public void testAddTask(){
+    public void testAddTask() throws ParseException {
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            Date dueDate = formatter.parse("19/10/2019");
+            Task dummyTask = new Task("task1",1,dueDate,10,TaskState.TODO);
+            Task task = consoleInputFactory.createTask("t/task1 p/1 d/19/10/2019 c/10 s/todo");
+            assertEquals(dummyTask,task);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
