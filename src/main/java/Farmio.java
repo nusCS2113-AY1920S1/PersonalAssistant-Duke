5a822ca5acea5491eba7e4f723d3d485ea5beac3
tzw0 <e0310410@u.nus.edu>
2019-10-13 17:01:17 +0800

    implemented typewriter

--- src/main/java/Farmio.java
+++ src/main/java/Farmio.java
@@ -73,7 +73,7 @@ public class Farmio {
         ui.clearScreen();
         displayArt("welcome");
         ui.show("Press ENTER to continue.");
-//        ui.typeWriter("Press ENTER to continue."); //does not work in console, only terminal, uncomment to test
+//        ui.typeWriter("Typewriter test message here"); //does not work in console, only terminal, uncomment to test
         ui.getEnter();
     }
 
