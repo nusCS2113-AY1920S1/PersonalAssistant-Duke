09cf67bd5afeaaa562ce4830e490398d08527993
Shaun Teo <xshaun97@gmail.com>
2019-10-15 01:03:12 +0800

    added list day command

--- src/main/java/duke/command/ListCommand.java
+++ src/main/java/duke/command/ListCommand.java
@@ -12,40 +12,44 @@ import java.util.ArrayList;
  * duke.command.ListCommand class which executes the command of displaying the duke.tasklist.TaskList to the user
  */
 public class ListCommand extends Command {
-	private String mode;
-	private final String defaultMode = "DEFAULT";
-	public ListCommand() {
-		mode = defaultMode;
-	}
+    private String mode;
+    private final String defaultMode = "DEFAULT";
 
-	public ListCommand(String modeInformation) {
-		mode = modeInformation;
-	}
+    public ListCommand() {
+        mode = defaultMode;
+    }
 
-	@Override
-	public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
-		switch (mode) {
-			case "DEFAULT":
-				if (tasks.size() > 0) {
-					ui.showLine("Here are the tasks in your list:");
-					for (int i = 0; i < tasks.size(); i++) {
-						ui.showEntry(i + 1, tasks.get(i));
-					}
-				} else {
-					ui.showLine("There are no tasks in your list.");
-				}
-				break;
-			case "priority":
-				tasks.priorityPrint(ui);
-				break;
-			default:
-				throw new DukeException("The description of list is invalid");
-		}
+    public ListCommand(String modeInformation) {
+        mode = modeInformation;
+    }
 
-	}
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        switch (mode) {
+            case "DEFAULT":
+                if (tasks.size() > 0) {
+                    ui.showLine("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        ui.showEntry(i + 1, tasks.get(i));
+                    }
+                } else {
+                    ui.showLine("There are no tasks in your list.");
+                }
+                break;
+            case "priority":
+                tasks.priorityPrint(ui);
+                break;
+            case "day":
+                tasks.dayViewPrint(ui);
+                break;
+            default:
+                throw new DukeException("The description of list is invalid");
+        }
 
-	@Override
-	public boolean isExit() {
-		return false;
-	}
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
