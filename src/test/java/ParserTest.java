1314186820cae96759aa5fea50d3b0857246e76c
John Khoo <john_khoo@u.nus.edu>
2019-10-15 22:19:44 +0800

    Revert bad merge

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,19 +1,28 @@
 public class ParserTest {
 
-    // TODO check whether every task can be detected
-    // TODO check if exception are thrown for incorrect input formats
+    // TODO test failure modes
+    // this is going to take a lot of typing
 
     //private Parser uut = new Parser();
 
-    /*@Test
+    @Test
     public void parseCommands_validCommands_correctCommandsReturned() {
         try {
             assertEquals(uut.parse("list").getClass(), ListCommand.class);
             assertEquals(uut.parse("todo JUnit tests").getClass(), NewToDoCommand.class);
             assertEquals(uut.parse("event tutorial /at 12/09/2019 1400 /to 12/10/2019 1200").getClass(),
                         NewEventCommand.class);
-       } catch (DukeException excp) {
+            assertEquals(uut.parse("deadline submission /by 12/09/2019 1400").getClass(),
+                    NewDeadlineCommand.class);
+            assertEquals(uut.parse("done 1").getClass(), DoneCommand.class);
+            assertEquals(uut.parse("bye").getClass(), ByeCommand.class);
+            assertEquals(uut.parse("delete 1").getClass(), DeleteCommand.class);
+            assertEquals(uut.parse("find u").getClass(), FindCommand.class);
+            assertEquals(uut.parse("recurring deadline submission /by 18/09/2019 2359 /repeats weekly "
+                    + "/until 23/11/2019 1300").getClass(), NewRecurringTaskCommand.class);
+            assertEquals(uut.parse("snooze 1 23/11/2019 1300").getClass(), SnoozeCommand.class);
+        } catch (DukeException excp) {
             fail("Exception thrown while extracting commands!");
         }
-    }*/
+    }
 }
