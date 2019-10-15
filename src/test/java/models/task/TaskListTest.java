0f6af02c1b0282fa2e022a2aa8e99ae9ce20d543
Dillen <dillenwong@gmail.com>
2019-10-15 16:47:06 +0800

    [Test] Added additional test cases.

--- src/test/java/models/task/TaskListTest.java
+++ src/test/java/models/task/TaskListTest.java
@@ -36,6 +36,29 @@ public class TaskListTest {
 
     @Test
     public void testGetTaskList() {
-       
+        TaskList taskList = new TaskList();
+        ArrayList<String> taskRequirements = new ArrayList<>();
+
+        Task task1 = new Task("task1", 1, null,100, TaskState.OPEN,taskRequirements);
+        Task task2 = new Task("task2", 2, null,100, TaskState.OPEN,taskRequirements);
+        Task task3 = new Task("task3", 3, null,100, TaskState.OPEN,taskRequirements);
+        Task task4 = new Task("task4", 4, null,100, TaskState.OPEN,taskRequirements);
+        taskList.addTask(task1);
+        taskList.addTask(task2);
+        taskList.addTask(task3);
+        taskList.addTask(task4);
+        assertNotNull(taskList.getTaskList());
+
+        String testTaskListString = "[1. task4 | Priority: 4 | Due: -- | Credit: 100 | State: OPEN, "
+                + "2. task3 | Priority: 3 | Due: -- | Credit: 100 | State: OPEN, "
+                + "3. task2 | Priority: 2 | Due: -- | Credit: 100 | State: OPEN, "
+                + "4. task1 | Priority: 1 | Due: -- | Credit: 100 | State: OPEN]";
+        assertEquals(taskList.getAllTaskDetails().toString(), testTaskListString);
+
+        String testRemoveTaskListString = "[1. task4 | Priority: 4 | Due: -- | Credit: 100 | State: OPEN, "
+                + "2. task2 | Priority: 2 | Due: -- | Credit: 100 | State: OPEN, "
+                + "3. task1 | Priority: 1 | Due: -- | Credit: 100 | State: OPEN]";
+        taskList.removeTask(2);
+        assertEquals(taskList.getAllTaskDetails().toString(), testRemoveTaskListString);
     }
 }
