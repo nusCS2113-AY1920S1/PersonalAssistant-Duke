a2a7064580ed4e3920576d1f9d6d71942df99591
Penguin <quek.aik.peng@gmail.com>
2019-10-15 23:10:25 +0800

    Converted entryList in Dolla to private. Functions affected also updated.

--- src/main/java/dolla/DollaData.java
+++ src/main/java/dolla/DollaData.java
@@ -1,21 +1,31 @@
 package dolla;
 
 import dolla.task.EntryList;
+import dolla.task.LogList;
 
 import java.util.ArrayList;
 
 public class DollaData {
 
     private String mode = "dolla";
-    public EntryList entryList; // TODO: Find out alternatives to using a public variable
+    private EntryList entryList;
 
     public DollaData() {
         this.entryList = new EntryList(new ArrayList<Log>());
         // this.entryList = new EntryList(importEntryList()); TODO: Import from save file
     }
 
-    public EntryList getEntryList() {
-        return entryList;
+    public LogList getLogList(String mode) {
+        if (mode.equals("entries")) {
+            return entryList;
+        }
+        return null; // placeholder so that Dolla can compile
+    }
+
+    public void addToLogList(String mode, Log newLog) {
+        if (mode.equals("entries")) {
+            entryList.add(newLog);
+        }
     }
 
     public String getMode() {
