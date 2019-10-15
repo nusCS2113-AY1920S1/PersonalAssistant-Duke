7dcccb51657416869c30847c2cc5caf63385e0b7
Exprescode <jinghui.yew@gmail.com>
2019-10-15 15:31:01 +0800

    Farmio/Storage.java: Fix load level issue.

--- src/main/java/Farmio/Storage.java
+++ src/main/java/Farmio/Storage.java
@@ -11,19 +11,20 @@ public class Storage {
 
     private static String appDir;
 
-    public Storage(){
+    public Storage() {
         appDir = System.getProperty("user.dir");
     }
 
-    public boolean getFarmerExist(){
+    public boolean getFarmerExist() {
         return new File(appDir.concat("\\data\\save.json")).exists();
     }
 
     /**
      * Finds and return previously saved game data.
+     *
      * @return Last saved farmer object.
      * @throws ParseException "save.json" does not contain json data.
-     * @throws IOException "save.json" does not exist.
+     * @throws IOException    "save.json" does not exist.
      */
     public JSONObject loadFarmer() throws ParseException, IOException {
         Reader reader = new FileReader(appDir.concat("\\data\\save.json"));
@@ -33,6 +34,7 @@ public class Storage {
 
     /**
      * Stores farmer object into "save.json".
+     *
      * @param farmer Farmio.Farmer object to be stored in to save file.
      * @throws IOException Fail to save farmer object into save file.
      */
@@ -43,7 +45,7 @@ public class Storage {
 
     public String getAsciiArt(String name) throws IOException {
         StringBuilder art = new StringBuilder();
-        FileReader fileReader = new FileReader(getResourceFile("asciiArt/" + name+".txt"));
+        FileReader fileReader = new FileReader(getResourceFile("asciiArt/" + name + ".txt"));
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String line;
         while ((line = bufferedReader.readLine()) != null) {
@@ -53,15 +55,15 @@ public class Storage {
     }
 
     public JSONObject getLevel(int level) throws IOException, ParseException {
-        Reader reader = new FileReader(getResourceFile("level"+level+".json"));
+        Reader reader = new FileReader(getResourceFile("levels/" + level + ".json"));
         JSONParser parser = new JSONParser();
         return (JSONObject) parser.parse(reader);
     }
 
-    private File getResourceFile(String name) throws IOException {
+    private File getResourceFile(String path) throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
-        URL resource = classLoader.getResource(name);
-        if(resource == null){
+        URL resource = classLoader.getResource(path);
+        if (resource == null) {
             throw new IOException("Game is corrupted!");
         }
         return new File(resource.getFile());
