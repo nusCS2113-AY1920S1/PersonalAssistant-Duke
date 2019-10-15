ae2ec0d6086261febba8fad389ead356bedeb841
maxxyx96 <maxxyx96@gmail.com>
2019-10-15 21:41:50 +0800

    Added javadocs for AddBudgetCommand, UpdateBudgetCommand and ViewBudgetCommand.

--- src/main/java/duke/command/UpdateBudgetCommand.java
+++ src/main/java/duke/command/UpdateBudgetCommand.java
@@ -27,16 +27,41 @@ public class UpdateBudgetCommand extends Command {
         }
     }
 
+
+    /**
+     * Executes a command with task list and ui.
+     * (not used)
+     *
+     * @param items The task list that contains a list of tasks.
+     * @param ui    To tell the user that it is executed successfully.
+     */
     @Override
     public void execute(TaskList items, Ui ui) {
 
     }
 
+    /**
+     * Executes a command with task list and ui (GUI).
+     * (not used)
+     *
+     * @param items The task list that contains a list of tasks.
+     * @param ui    To tell the user that it is executed successfully.
+     * @return String to be outputted to the user.
+     */
     @Override
     public String executeGui(TaskList items, Ui ui) {
         return null;
     }
 
+    /**
+     * Executes a command that overwrites existing storage with the updated task list.
+     * (not used)
+     *
+     * @param items   The task list that contains a list of tasks.
+     * @param ui      To tell the user that it is executed successfully.
+     * @param storage The storage to be overwritten.
+     * @throws IOException If there is an error reading the file.
+     */
     @Override
     public void executeStorage(TaskList items, Ui ui, Storage storage) throws IOException {
 
