c957b44ae279bcdb850b86ac7165ce7976be96f5
AugGust <piggy013130@yahoo.com.sg>
2019-10-16 19:57:13 +0800

    Linked initial output text Area and fixed some file names

--- src/main/java/gui/Window.java
+++ src/main/java/gui/Window.java
@@ -1,21 +1,18 @@
 package gui;
 
-import javax.swing.JFrame;
-
-import java.awt.Dimension;
+import javax.swing.*;
+import java.awt.*;
 
 public class Window {
-    public Window() {
+    public static JTextArea outputArea;
 
-    }
-
-    /**
-     * Creates a new JFrame instance
-     */
-    public void newForm()   {
+    public Window(){
         JFrame frame = new JFrame("Duke Pro");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setMinimumSize(new Dimension(800, 480));
+        frame.setMinimumSize(new Dimension(900, 600 ));
+        frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+        MainWindow mw = new MainWindow();
+        outputArea = mw.outputText;
+        frame.setContentPane(mw.rootPane);
         frame.setVisible(true);
     }
 }
