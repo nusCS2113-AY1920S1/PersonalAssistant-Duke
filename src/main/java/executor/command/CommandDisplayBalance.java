fdb6d445b125bef42c11c61228a5404e1dc4eb29
Mudaafi <SleepyUndergrad@gmail.com>
2019-10-16 18:26:04 +0800

    Added functionality that shows User's Balance upon start up.
    Added CommandDisplayBalance which prints the Users Balance.
    
    -- JUnit Testing included
    
    Signed-off-by: Mudaafi <SleepyUndergrad@gmail.com>

---
+++ src/main/java/executor/command/CommandDisplayBalance.java
@@ -0,0 +1,27 @@
+package executor.command;
+
+import executor.task.TaskList;
+import ui.Ui;
+import ui.Wallet;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class CommandDisplayBalance extends Command {
+
+    // Constructor
+    public CommandDisplayBalance() {
+    }
+
+    @Override
+    public void execute(TaskList taskList) {
+    }
+
+    @Override
+    public void execute(Wallet wallet) {
+        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+        Ui.dukeSays("Your Balance: $"
+                + decimalFormat.format(wallet.getBalance())
+        );
+    }
+}
