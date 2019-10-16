84885af99c7122a83fe7d057a9b27a13573934b3
LJX0006 <liujxcindy@gmail.com>
2019-10-16 23:18:05 +0800

    Improve PrepStep in Recipe List

--- src/main/java/duke/command/recipecommands/AddPrepStepCommand.java
+++ src/main/java/duke/command/recipecommands/AddPrepStepCommand.java
@@ -19,21 +19,19 @@ public class AddPrepStepCommand extends CommandPrepStep {
     @Override
     public ArrayList<String> execute(PrepStepList prepStepList, Ui ui, PrepStepStorage prepStepStorage) throws ParseException {
         ArrayList<String> arrayList = new ArrayList<>();
+        String stepDescription = "";
         if (userInput.trim().equals(COMMAND_ADD_PREPSTEP)) {
             arrayList.add(ERROR_MESSAGE_GENERAL + MESSAGE_FOLLOWUP_NUll);
-            System.out.println("stuck here");
-        } else if (userInput.trim().charAt(11) == ' ') {
-            if (userInput.trim().contains("n/")) {
-                String prepStep = userInput.split("n/", 2)[1].trim();
-                prepStepList.addPrepStep(prepStep);
-                System.out.println("added prepStep");
-                prepStepStorage.saveFile(prepStepList);
-                arrayList.add(MESSAGE_ADDED + "       " + prepStepList.getPrepStepList().get(prepStepList.getSize() - 1) + "\n" + "You have added the prep steps.");
+            System.out.println("stuck here1");
+        }
+        else if (userInput.trim().charAt(19) == ' ') {
+            String description = userInput.split("\\s", 2)[1].trim();
+            if (description.contains("q/")) {
+                String temp = description.split("q/", 2)[0].trim();
+                stepDescription = temp.split("\\s", 2)[0].trim();
             } else {
                 arrayList.add(ERROR_MESSAGE_RANDOM);
             }
-        } else {
-            arrayList.add(ERROR_MESSAGE_RANDOM);
         }
         return arrayList;
     }
--- src/main/java/duke/common/RecipeMessages.java
+++ src/main/java/duke/common/RecipeMessages.java
@@ -7,12 +7,14 @@ public class RecipeMessages {
     public static final String COMMAND_LIST_RECIPE_INGREDIENT = "listingredient";
     public static final String COMMAND_DELETE_RECIPE_INGREDIENT = "delri";
     public static final String COMMAND_DELETE_RECIPE_TITLE = "delrt";
+    public static final String COMMAND_DELETE_PREPSTEP = "delp";
 
     public static final String COMMAND_ADD_FEEDBACK = "addfeedback";
     public static final String COMMAND_ADD_RATING = "addrating";
     public static final String COMMAND_ADD_PREPSTEP = "addprepstep";
 
     public static final String MESSAGE_DELETE_RECIPE = "     Noted. I've removed this ingredient:\n";
+    public static final String MESSAGE_DELETE_PREPSTEP = "     Noted. I've removed this step:\n";
 
     public static final String ERROR_MESSAGE_INVALID_RECIPE_QUANTITY = "Please input a valid value for the quantity";
     public static final String ERROR_MESSAGE_INVALID_RECIPE_INDEX = "Please input a valid value for the index";
