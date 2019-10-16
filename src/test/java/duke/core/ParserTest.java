425f8fd22950fa2a87a0b41813243939c73536c3
lmtaek <ltaekman@gmail.com>
2019-10-16 18:54:04 +0800

    Added test methods for 'delete patient' and 'delete task' instances within the Parser class.

--- src/test/java/duke/core/ParserTest.java
+++ src/test/java/duke/core/ParserTest.java
@@ -1,7 +1,6 @@
 package duke.core;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParserTest {
@@ -12,7 +11,10 @@ public class ParserTest {
     String assignPatientIDToTaskS = "assign by id: S 1 2 02/02/2002 2222";
     String assignPatientIDToTaskE = "assign by id: E 2 1 02/02/2002 2222 to 03/02/2002 1234";
 
-    String deletePatientInput = "delete patient #123";
+    String deletePatientInputWithID = "delete patient #123";
+    String deletePatientInputWithName = "delete patient billy joe";
+    String deleteTaskInputWithID = "delete task 10";
+    String deleteTaskInputWithName = "delete task Take medicine";
 
     @Test
     public void parseAddPatientTest() throws DukeException {
@@ -55,8 +57,27 @@ public class ParserTest {
     }
 
     @Test
-    public void parseDeletePatient() {
+    public void parseDeletePatient() throws DukeException {
+        Parser testParserID = new Parser(deletePatientInputWithID);
+        Parser testParserName = new Parser(deletePatientInputWithName);
 
+        String testOutputID = testParserID.parseDeletePatient();
+        String testOutputName = testParserName.parseDeletePatient();
+
+        assertTrue(testOutputID.charAt(0) == '#' && testOutputID.equals("#123"), "Delete patient by ID parsing failed. Expected '#123' but got: " + testOutputID);
+        assertTrue(testOutputName.equals("billy joe"), "Delete patient by name parsing failed. Expected 'billy joe' but got: " + testOutputName);
+    }
+
+    @Test
+    public void parseDeleteTask() throws DukeException {
+        Parser testParserID = new Parser(deleteTaskInputWithID);
+        Parser testParserName = new Parser(deleteTaskInputWithName);
+
+        String testOutputID = testParserID.parseDeleteTask();
+        String testOutputName = testParserName.parseDeleteTask();
+
+        assertTrue(testOutputID.charAt(0) == '#' && testOutputID.equals("#10"), "Delete task by ID parsing failed. Expected '#10' but got: " + testOutputID);
+        assertTrue(testOutputName.equals("Take medicine"), "Delete task by name parsing failed. Expected 'Take medicine' but got: " + testOutputName);
     }
 
 
