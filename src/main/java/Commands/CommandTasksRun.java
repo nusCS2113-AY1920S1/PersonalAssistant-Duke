ce6e29d8a2c43e760b798d96d691ac1f1da8c160
jing-xuan <tayjxn@gmail.com>
2019-10-16 23:55:17 +0800

    Fixed inability to exit bug, implemented check objective stage and command

--- src/main/java/Commands/CommandTasksRun.java
+++ src/main/java/Commands/CommandTasksRun.java
@@ -7,5 +7,7 @@ public class CommandTasksRun extends Command {
     @Override
     public void execute(Farmio farmio) throws FarmioException {
         farmio.getFarmer().startDay(farmio);
+        farmio.getUi().show("End of Day!");
+        farmio.setStage(Farmio.Stage.CHECK_OBJECTIVES);
     }
 }
