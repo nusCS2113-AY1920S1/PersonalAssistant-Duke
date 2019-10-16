b11e021a9d11f3da19e4755efc8beed8a92035e8
John Khoo <john_khoo@u.nus.edu>
2019-10-16 15:58:19 +0800

    Begin implementing aliases vararg for autocorrect

--- src/main/java/duke/command/ArgCommand.java
+++ src/main/java/duke/command/ArgCommand.java
@@ -3,6 +3,7 @@ package duke.command;
 import duke.DukeCore;
 import duke.exception.DukeException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeSet;
@@ -14,7 +15,8 @@ public abstract class ArgCommand extends Command {
 
     protected static Map<String, ArgLevel> switches; //list of recognised switches, and their argument requirements
     protected static TreeSet switchTree; //tree of switches, used for fast lookup for autocomplete
-    protected static HashMap switchAliases;
+    protected static Map<String, String> switchAliases; //map of all possible words that can be identified as switches,
+    //and the switches they correspond to
     private String arg = null; //argument supplied to the command
     private HashMap<String, String> switchVals = new HashMap<String, String>(); //hashmap of switch parameters
 
@@ -56,20 +58,24 @@ public abstract class ArgCommand extends Command {
      * @param switchNameArr The names of the switches for this command.
      * @param argLevelArr The argument requirement level for each command.
      * @param switchRootArr The shortest recognised truncation of each switch.
+     * @param aliases An array containing an even number of strings. Each pair of strings is added as a key-value
+     * pair to the switchAliases map.
      */
-    protected static void switchInit(String[] switchNameArr, ArgLevel[] argLevelArr, String[] switchRootArr) {
+    protected static void switchInit(String[] switchNameArr, ArgLevel[] argLevelArr, String[] switchRootArr,
+            String... aliases) {
         assert(switchNameArr.length == argLevelArr.length);
         assert(switchRootArr.length == switchNameArr.length);
+        assert(aliases.length % 2 == 0);
         int switchCnt = switchNameArr.length;
         Map.Entry[] argLevelEntries = new Map.Entry[switchCnt];
-        Map.Entry[] switchRootEntries = new Map.Entry[switchCnt];
+        ArrayList<Map.Entry> entryArrList = new ArrayList<Map.Entry>;
         for (int i = 0; i < switchCnt; ++i) {
             argLevelEntries[i] = Map.entry(switchNameArr[i], argLevelArr[i]);
-            switchRootEntries[i] = Map.entry(switchNameArr[i], switchRootArr[i]);
+            //TODO: generate switch aliases via loop
         }
+        //TODO: add extra aliases and create switchAlias Map
         switches = Map.ofEntries(argLevelEntries);
         switchTree = new TreeSet<String>(switches.keySet());
-        //TODO: generate switchAliases, and figure out how to map extra switch aliases from each command
     }
 
     // Override these methods to specify parameters of child classes
