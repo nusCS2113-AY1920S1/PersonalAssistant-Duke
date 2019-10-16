a93a7e748c1c29cdf0fb61b903f1d6faaa3a4a93
John Khoo <john_khoo@u.nus.edu>
2019-10-16 20:29:56 +0800

    Adapt some old commands to new system

--- src/main/java/duke/command/DoneCommand.java
+++ src/main/java/duke/command/DoneCommand.java
@@ -3,12 +3,38 @@ package duke.command;
 import duke.DukeCore;
 import duke.exception.DukeException;
 
+import java.util.Map;
+
 public class DoneCommand extends ArgCommand {
+
+    private static Map<String, ArgLevel> switches = Map.of();
+    private static Map<String, String> switchAliases = Map.of();
+
     @Override
     public void execute(DukeCore core) throws DukeException {
-        String taskStr = core.taskList.markDone(arg);
+        String taskStr = core.taskList.markDone(getArg());
         taskStr = "Nice! I've marked this task as done:" + System.lineSeparator() + "  " + taskStr;
         core.storage.writeTaskFile(core.taskList.getFileStr());
         core.ui.print(taskStr);
     }
+
+    @Override
+    ArgLevel getCmdArgLevel() {
+        return ArgLevel.REQUIRED;
+    }
+
+    @Override
+    String getEmptyArgMsg() {
+        return "You didn't tell me which task you did!";
+    }
+
+    @Override
+    Map<String, ArgLevel> getSwitches() {
+        return switches;
+    };
+
+    @Override
+    Map<String, String> getSwitchAliases() {
+        return switchAliases;
+    };
 }
--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -2,17 +2,50 @@ package duke.command;
 
 import duke.DukeCore;
 import duke.exception.DukeException;
+import javafx.util.Pair;
+
+import java.util.Map;
 
 public class FindCommand extends ArgCommand {
-    public FindCommand() {
-        emptyArgMsg = "You didn't tell me what to look for!";
+    private static Map<String, ArgLevel> switches;
+    private static Map<String, String> switchAliases;
+
+    static {
+        // TODO: Update these
+        String[] switchNameArr = new String[]{"switch"};
+        ArgLevel[] argLevelArr = new ArgLevel[]{ArgLevel.OPTIONAL};
+        String[] switchRootArr = new String[]{"s"};
+        Pair<Map<String, ArgLevel>, Map<String, String>> switchData =
+                switchInit(switchNameArr, argLevelArr, switchRootArr);
+        switches = switchData.getKey();
+        switchAliases = switchData.getValue();
     }
 
     @Override
     public void execute(DukeCore core) throws DukeException {
-        String findStr = "Here are the tasks that contain '" + arg + "':";
-        findStr = findStr + core.taskList.find(arg).replace(System.lineSeparator(),
+        String findStr = "Here are the tasks that contain '" + getArg()+ "':";
+        findStr = findStr + core.taskList.find(getArg()).replace(System.lineSeparator(),
                 System.lineSeparator() + "  ");
         core.ui.print(findStr);
     }
+
+    @Override
+    ArgLevel getCmdArgLevel() {
+        return ArgLevel.REQUIRED;
+    }
+
+    @Override
+    String getEmptyArgMsg() {
+        return "You didn't tell me what to search for!";
+    }
+
+    @Override
+    Map<String, ArgLevel> getSwitches() {
+        return switches;
+    };
+
+    @Override
+    Map<String, String> getSwitchAliases() {
+        return switchAliases;
+    };
 }
