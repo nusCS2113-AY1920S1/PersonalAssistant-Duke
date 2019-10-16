61361548a5eb7836b4a773b20fa414f1fea9c1ae
HUANG XUAN KUN <hxk123123123@gmail.com>
2019-10-17 02:25:59 +0800

    Added Junit test for TypoCorrector

---
+++ src/test/java/duke/core/TypoCorrectorTest.java
@@ -0,0 +1,39 @@
+package duke.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Junit test for TypoCorrector class
+ * @author  HUANG XUAN KUN
+ * @version 1.2
+ */
+public class TypoCorrectorTest {
+
+    @Test
+    void StringMatchTest(){
+        ArrayList<String[]> testCases = new ArrayList<String[]>(
+                Arrays.asList(new String[]{"beyee", "bye"},
+                        new String[]{"bqe", "bye"},
+                        new String[]{"lsit patant", "list patients"},
+                        new String[]{"lsfvs takss", "list tasks"},
+                        new String[]{"deete Ptients #12", "delete patient #12"},
+                        new String[]{"deleot tasksa task description", "delete task task description"},
+                        new String[]{"addd patents name nric room remark", "add patient name nric room remark"},
+                        new String[]{"dad tsak a very long task name", "add task a very long task name"}
+                        ));
+        for (String[] testPair : testCases){
+            String correctedOutput = TypoCorrector.CommandCorrection(testPair[0]);
+            System.out.println("Input Command: "+ testPair[0]);
+            System.out.println("Expected Command: " + testPair[1]);
+            System.out.println("Corrected Command: "+ correctedOutput);
+            System.out.println();
+//            assertEquals(testPair[1], correctedOutput);
+        }
+
+    }
+}
