ccefa48d72b0fdce11900d2884f6a65b91fe9ad1
Dillen <dillenwong@gmail.com>
2019-10-16 22:19:41 +0800

    [Docs] Added Java docs for DateTimeHelper

--- src/main/java/util/date/DateTimeHelper.java
+++ src/main/java/util/date/DateTimeHelper.java
@@ -5,12 +5,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateTimeHelper {
-
+    /**
+     * This method takes in the date in String and return is as a Date object.
+     * @param date String of the date
+     * @return a Date object for consumption.
+     * @throws ParseException
+     */
     public Date formatDate(String date) throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
         return formatter.parse(date);
     }
 
+    /**
+     * This method format the Date object and return as a String date.
+     * @param date takes in the Date object.
+     * @return a date String format for consumption.
+     */
     public String formatDateForDisplay(Date date) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
         return formatter.format(date);
