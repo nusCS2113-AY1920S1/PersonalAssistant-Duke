e6b969ca2bfb2af9230aa98998bafd4a437f069e
John Khoo <john_khoo@u.nus.edu>
2019-10-17 01:21:48 +0800

    Update switch class

--- src/main/java/duke/command/Switch.java
+++ src/main/java/duke/command/Switch.java
@@ -1,16 +1,25 @@
 package duke.command;
 
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class Switch {
 
-   public String name;
-   public Class type;
-   public boolean isOptional;
-   public ArgLevel argLevel;
+   public final String name;
+   public final Class type;
+   public final boolean isOptional;
+   public final ArgLevel argLevel;
+   public final String root;
+   public final List<String> aliases;
 
-   public Switch(String name, Class type, boolean isOptional, ArgLevel argLevel) {
+   public Switch(String name, Class type, boolean isOptional, ArgLevel argLevel, String root, String... aliases) {
       this.name = name;
       this.type = type;
       this.isOptional = isOptional;
       this.argLevel = argLevel;
+      this.root = root;
+      this.aliases = List.of(aliases);
    }
 }
