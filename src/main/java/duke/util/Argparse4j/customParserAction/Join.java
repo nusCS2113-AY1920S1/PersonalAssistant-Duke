54ea6da1354e22d939ae41fb262605f0177b9290
LongLeCE <siromansiroas@gmail.com>
2019-10-17 00:29:29 +0800

    Added custom parser action to read multiple strings

---
+++ src/main/java/duke/util/Argparse4j/customParserAction/Join.java
@@ -0,0 +1,25 @@
+package duke.util.Argparse4j.customParserAction;
+
+import java.util.Map;
+import net.sourceforge.argparse4j.inf.Argument;
+import net.sourceforge.argparse4j.inf.ArgumentAction;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+
+public class Join implements ArgumentAction {
+
+    @Override
+    public void run(ArgumentParser parser, Argument arg, Map<String, Object> attrs, String flag, Object value) {
+        if (value instanceof Iterable) {
+            attrs.put(arg.getDest(), String.join(" ", (Iterable) value));
+        }
+    }
+
+    @Override
+    public void onAttach(Argument arg) {
+    }
+
+    @Override
+    public boolean consumeArgument() {
+        return true;
+    }
+}
