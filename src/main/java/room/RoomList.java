ba69396bbac02a68a3cba91a4e7e4fb8080267a8
zkchang97 <e0309954@u.nus.edu>
2019-10-16 23:54:12 +0800

    Edits RoomCommand.java, adds comment lines for clarity

--- src/main/java/room/RoomList.java
+++ src/main/java/room/RoomList.java
@@ -6,17 +6,11 @@ import java.util.ArrayList;
 
 public class RoomList extends ArrayList<Room> {
 
-    public RoomList(ArrayList<String> loader) throws DukeException {
+    public RoomList(ArrayList<String> loader) {
         for (String line : loader) {
-            String[] splitStr = line.split("\\|");
-            switch(splitStr[Constants.TYPE]) {
-                case "RM":
-                    this.add(new AddRoom(splitStr[Constants.ISBOOKED], splitStr[Constants.ROOMCODE],
-                            splitStr[Constants.ROOMDATE], splitStr[Constants.ROOMTIMESLOT]));
-                    break;
-                default:
-                    throw new DukeException("File format incorrect.");
+            String[] splitStr = line.split("\\|", 4);
+            this.add(new AddRoom(splitStr[Constants.ISBOOKED], splitStr[Constants.ROOMCODE],
+                    splitStr[Constants.ROOMDATE], splitStr[Constants.ROOMTIMESLOT]));
             }
         }
-    }
 }
