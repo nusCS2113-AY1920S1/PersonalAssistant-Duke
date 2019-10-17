ba060fc0624cd95d09300d43b8eee47aa1c99f3d
danisheddie <42911431+danisheddie@users.noreply.github.com>
2019-10-17 13:42:45 +0800

    [Refactoring] Created an ebstract class to handle ManageStudents commands

--- src/main/java/duke/command/Command.java
+++ src/main/java/duke/command/Command.java
@@ -1,4 +1,11 @@
 package duke.command;
 
-public class Command {
+import duke.Ui;
+import duke.sports.ManageStudents;
+
+/**
+ * An abstract class to handle commands
+ */
+public abstract class Command {
+    abstract public String execute(ManageStudents manageStudents, Ui ui);
 }
