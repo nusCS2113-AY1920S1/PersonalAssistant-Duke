3fb4f9564a25208445050e6df81998be51c1f7ee
Rebecca Oei <42566283+cyanoei@users.noreply.github.com>
2019-10-17 12:10:01 +0800

    Add deleteStockTypeCommand (#212)
    * Modified parser to handle the first command word only, and only split subsequent words based on that command.
    Modified add function to accept multi-word description.
    
    * Improved parser's javadoc comments
    
    * Updated javadoc comments for several classes
    Find command now only searches description instead of the entire print string.
    
    * deleted wrong images
    
    * added back photos with the right names
    
    * is more descriptive
    
    * Updated name of Ui file
    
    thank you raghav
    
    * Updated photo names
    
    thanks raghav
    
    * Save at every add/delete
    
    * item -> stock and some other small edits
    
    * draft submissions of ug/dg
    
    * updated UG/DG links
    
    THANKS RAGHAV AHH
    
    * Tentative work on CollectiveStock. Committing to revert to another version to fix a bug
    
    * Parser fix for addcommand to handle wrong input more accurately
    
    * Added uniquestock, edited documentation for stock/collectivestock
    
    * Minimum attribute for stocks introduced. Item class introduced for UniqueStock to keep track of.
    
    * Stock update continued. Basic methods for Item and UniqueStock classes completed.
    
    * Small fix to parser update
    
    * print/save in item and uniquestock
    
    * Remove trailing spaces on last input of add commands
    
    * Implementing parser command whitespace strip the correct way
    
    * Fixed checkstyle violations
    
    * updated StockTest to include the new save string for minimum required stock
    
    * Removed references to doAfter code
    
    * Moved add functionalities out of main parser
    
    * ParseAdd basics completed
    
    * edited exit message to be better
    
    * Changed stockList's getQuantity to take the size of the arraylist (of stocktypes) rather than keeping a quantity attribute. Edited save formats to work with storage.
    
    * Fully removed references to quantity.
    
    * Minor refactoring of the saving/printing processes to become more OOP and parallel with each other. (NO .get.get.get)
    
    * edit to please travis
    
    * Updated stockType test because a method name changed
    
    * Implement deleteStockType
    
    * Fix typo error on deletestocktypecommand
    
    * Add deleteStockTypeTest

---
+++ src/main/java/eggventory/commands/delete/DeleteStockTypeCommand.java
@@ -0,0 +1,46 @@
+package eggventory.commands.delete;
+
+import eggventory.StockList;
+import eggventory.Storage;
+import eggventory.Ui;
+import eggventory.commands.Command;
+import eggventory.enums.CommandType;
+import eggventory.items.StockType;
+
+public class DeleteStockTypeCommand extends Command {
+
+    private String stockTypeName;
+
+    public DeleteStockTypeCommand(CommandType type, String stockTypeName) {
+        super(type);
+        this.stockTypeName = stockTypeName;
+    }
+
+    @Override
+    public String execute(StockList list, Ui ui, Storage storage) {
+
+        String output;
+
+        if (stockTypeName.equals("Uncategorised")) {
+            output = "Sorry, Uncategorised is the default category, and cannot be deleted.";
+            ui.print(output);
+            return output;
+        }
+
+        StockType deleted = list.deleteStockType(stockTypeName);
+
+        if (deleted == null) {
+            output = String.format("Sorry, I cannot find the stock type \"%s\" refers to. "
+                    + "Please try again.", stockTypeName);
+            ui.print(output);
+        } else {
+            output = String.format("I deleted the following stockType: %s. "
+                    + "I also deleted the following stocks of that type: \n"
+                    + deleted.toString(), stockTypeName);
+            storage.save(list);
+            ui.print(output);
+        }
+
+        return output;
+    }
+}
