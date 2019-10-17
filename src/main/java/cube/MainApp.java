9be2a53a73691ac0cb91c71482cf17a355042252
kuromono <clouderext@gmail.com>
2019-10-17 23:48:45 +0800

    Add some test logic to GUI structure

--- src/main/java/cube/MainApp.java
+++ src/main/java/cube/MainApp.java
@@ -1,15 +1,38 @@
 package cube;
 
+import cube.exception.CubeException;
+import cube.model.food.FoodList;
+import cube.storage.StorageManager;
+import cube.util.FileUtilJson;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import cube.ui.MainWindow;
 
 public class MainApp extends Application {
+
+    private StorageManager storageManager;
+    private FileUtilJson storage;
+    private FoodList foodList;
+
+    public void init(String filePath) {
+        storage = new FileUtilJson(filePath);
+
+        try {
+            storageManager = storage.load();
+            foodList = storageManager.getFoodList();
+        } catch (CubeException e) {
+            foodList = new FoodList();
+            storageManager = new StorageManager();
+        }
+    }
+
     @Override
     public void start(Stage stage) {
-        MainWindow mwc = new MainWindow(stage);
+        //MainWindow mwc = new MainWindow(stage);
+        init("data");
+        MainWindow mwc = new MainWindow(stage, storageManager, storage, foodList);
+
         mwc.initComponents();
         mwc.show();
     }
--- src/main/java/cube/ui/MainWindow.java
+++ src/main/java/cube/ui/MainWindow.java
@@ -1,5 +1,11 @@
 package cube.ui;
 
+import cube.exception.CubeException;
+import cube.logic.command.Command;
+import cube.logic.parser.Parser;
+import cube.model.food.FoodList;
+import cube.storage.StorageManager;
+import cube.util.FileUtilJson;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 import javafx.fxml.FXML;
@@ -8,9 +14,17 @@ public class MainWindow extends UiManager<Stage> {
     public static final String FXML = "MainWindow.fxml";
 
     private Stage primaryStage;
+    private ResultDisplay resultDisplay;
 
     @FXML
     private StackPane commandBoxPlaceholder;
+
+    @FXML
+    private StackPane resultDisplayPlaceholder;
+
+    private StorageManager storageManager;
+    private FileUtilJson storage;
+    private FoodList foodList;
 
     public MainWindow (Stage primaryStage) {
         super(FXML, primaryStage);
@@ -18,13 +32,37 @@ public class MainWindow extends UiManager<Stage> {
         this.primaryStage = primaryStage;
     }
 
+    public MainWindow (Stage primaryStage, StorageManager storageManager, FileUtilJson storage, FoodList foodList) {
+        super(FXML, primaryStage);
+
+        this.primaryStage = primaryStage;
+        this.storageManager = storageManager;
+        this.storage = storage;
+        this.foodList = foodList;
+    }
+
     public void show() {
         primaryStage.show();
     }
 
     public void initComponents() {
-        CommandBox commandBox = new CommandBox();
+        //CommandBox commandBox = new CommandBox();
+        CommandBox commandBox = new CommandBox(storageManager, storage, foodList);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
+
+        resultDisplay = new ResultDisplay();
+        resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
+    }
+
+    private String cmdExecutor(String input) {
+        try {
+            Command c = Parser.parse(input);
+            c.execute(foodList, new Ui(), storageManager);
+            storage.save(storageManager);
+        } catch (CubeException e) {
+            e.printStackTrace();
+        }
+        return input;
     }
 
     /**
