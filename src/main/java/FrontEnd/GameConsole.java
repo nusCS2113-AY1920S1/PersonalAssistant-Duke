b4fd22827ed04dd1eef88f37d08d5222e97894b4
tzw0 <e0310410@u.nus.edu>
2019-10-18 16:22:25 +0800

    formatted level, day, gold

--- src/main/java/FrontEnd/GameConsole.java
+++ src/main/java/FrontEnd/GameConsole.java
@@ -11,15 +11,16 @@ import java.util.ArrayList;
 public class GameConsole {
     private static final String TOP_BORDER = "._______________________________________________________________________________________________________.\n";
     private static final String BOTTOM_FULL_BORDER = "|_______________________________________________________________________________________________________|\n";
-    private static final String MENU_PROMPT_AND_CODE_TITLE = "|-----------------"+ AsciiColours.CYAN + "<MENU>" + AsciiColours.SANE + " for instruction list or settings---------------|---------"+ AsciiColours.CYAN + "<USER CODE>" + AsciiColours.SANE + "-----------|\n";
+    private static final String MENU_PROMPT_AND_CODE_TITLE = "|-----------------"+ AsciiColours.CYAN + "<MENU>" + AsciiColours.SANE + " for instruction list or settings---------------|------------"+ AsciiColours.CYAN + "<CODE>" + AsciiColours.SANE + "-------------|\n";
     private static final String BOX_BOTTOM_BORDER = "|_______________________________________________________________________|_______________________________|\n";
     private static final String BOX_TOP_BORDER = "|_______________________________________________________________________|_______________________________|\n"; //"|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|\n";
     private static final String CODE_TITLE_FILLER = "|                               |\n";
-    private static final String EMPTY_STAGE_LINE = "                                                       ";
+    private static final String CODE_BOTTOM_BORDER = "|                               "+ AsciiColours.SANE +"|\n";
     private static final String LEFT_PANEL_BOTTOM_BORDER = "|_______________";
     private static final String LEFT_PANEL_TOP_BORDER = "|_______________";//"|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾";
     private static final String ASSETS_TITLE = "|---" + AsciiColours.CYAN + "<ASSETS>" + AsciiColours.SANE + "----";
     private static final String ASSETS_BODY_FILLER = "|               ";
+    private static final String ASSETS_BOTTOM_BORDER = "|" + AsciiColours.UNDERLINE + "               ";
     private static final String BOTTOM_BORDER = "|_______________|_______________________________________________________|_______________________________|\n";
     private static String horizontalPanel(String title, String content, int totalSpace) {
         String blankspace = "";
@@ -47,6 +48,13 @@ public class GameConsole {
         }
         return userCodeOutput;
     }
+    private ArrayList<Pair<String, Integer>> formatAssets(ArrayList<Pair<String, Integer>> assets) {
+        ArrayList<Pair<String, Integer>> formattedAssets = new ArrayList<>();
+        if (assets.size() < 11) {
+//            assets.add(ASSETS_BODY_FILLER);
+        }
+        return assets;
+    }
     static String content(ArrayList<String> stage, Farmio farmio) { //does not include story
         StringBuilder output = new StringBuilder();
         String objective = "";// farmio.getLevel().getNarratives().get(0);
@@ -58,15 +66,15 @@ public class GameConsole {
         ArrayList<Pair<String, Integer>> assets = farmio.getFarmer().getAssets();
         userCode = formatAndHighlightCode(userCode, farmio.getFarmer().getCurrentTask(), farmio.getFarmer().isHasfailedCurrentTask());
         output.append(AsciiColours.SANE + TOP_BORDER);
-        output.append("|" + AsciiColours.RED + horizontalPanel("OBJECTIVE:", objective, 71) + AsciiColours.SANE).append(CODE_TITLE_FILLER);
+        output.append("|" + AsciiColours.RED).append(horizontalPanel("OBJECTIVE:", objective, 71)).append(AsciiColours.SANE).append(CODE_TITLE_FILLER);
         output.append(BOX_BOTTOM_BORDER);
         output.append(MENU_PROMPT_AND_CODE_TITLE);
         output.append(BOX_TOP_BORDER);
-        output.append("|" + AsciiColours.BLUE + horizontalPanel("Level: ", Integer.toString(level), 15) + AsciiColours.SANE).append(stage.get(0)).append(userCode.get(0)).append("\n");
-        output.append("|" + AsciiColours.MAGENTA + horizontalPanel("Day:   ", Integer.toString(day), 15) + AsciiColours.SANE).append(stage.get(1)).append(userCode.get(1)).append("\n");
-        output.append("|" + AsciiColours.YELLOW + horizontalPanel("Gold:", Integer.toString(gold), 15) + AsciiColours.SANE).append(stage.get(2)).append(userCode.get(2)).append("\n");
+        output.append("|" + AsciiColours.BLUE).append(horizontalPanel("Level: ", "", 14 -Integer.toString(level).length())).append(level).append(" ").append(AsciiColours.SANE).append(stage.get(0)).append(userCode.get(0)).append("\n");
+        output.append("|" + AsciiColours.MAGENTA).append(horizontalPanel("Day:   ", "", 14 - Integer.toString(day).length())).append(day).append(" ").append(AsciiColours.SANE).append(stage.get(1)).append(userCode.get(1)).append("\n");
+        output.append("|" + AsciiColours.YELLOW).append(horizontalPanel("Gold:", "", 14 - Integer.toString(gold).length())).append(gold).append(" ").append(AsciiColours.SANE).append(stage.get(2)).append(userCode.get(2)).append("\n");
         output.append(LEFT_PANEL_TOP_BORDER).append(stage.get(3)).append(userCode.get(3)).append("\n");
-        output.append(AsciiColours.GREEN + horizontalPanel("|@", location, 16)+ AsciiColours.SANE).append(stage.get(4)).append(userCode.get(4)).append("\n");
+        output.append(AsciiColours.GREEN).append(horizontalPanel("|@", location, 16)).append(AsciiColours.SANE).append(stage.get(4)).append(userCode.get(4)).append("\n");
         output.append(LEFT_PANEL_BOTTOM_BORDER).append(stage.get(5)).append(userCode.get(5)).append("\n");
         output.append(ASSETS_TITLE).append(stage.get(6)).append(userCode.get(6)).append("\n");
         for (int i = 7; i < 18; i ++) {
@@ -93,7 +101,7 @@ public class GameConsole {
         for (int i = 0; i < 20; i ++) {
             output.append(horizontalPanel("", stage.get(i), 101)).append("\n");
         }
-        output.append(BOTTOM_FULL_BORDER);
+//        output.append(BOTTOM_FULL_BORDER);
         StringBuilder output2 = new StringBuilder();
         for (int i = 0; i < output.length(); i ++) {
             if (output.charAt(i) == '\n') {
