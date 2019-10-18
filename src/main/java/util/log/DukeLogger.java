7d143ef9df85e337c63e6bc9bdb9c600c88c43cb
Dillen <dillenwong@gmail.com>
2019-10-18 21:35:24 +0800

    [Feat] Added Dukelogger class to logs actions.

--- src/main/java/util/log/DukeLogger.java
+++ src/main/java/util/log/DukeLogger.java
@@ -1,4 +1,23 @@
 package util.log;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class DukeLogger {
+    private static Logger _logger;
+
+    public static void logDebug(Class className, String message) {
+        _logger = LogManager.getLogger(className);
+        _logger.debug(message);
+    }
+
+    public static void logInfo(Class className, String message) {
+        _logger = LogManager.getLogger(className);
+        _logger.info(message);
+    }
+
+    public static void logError(Class className, String message) {
+        _logger = LogManager.getLogger(className);
+        _logger.error(message);
+    }
 }
