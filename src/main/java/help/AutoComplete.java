6630161eef195b95a99ddd3e90677ddf32137fbd
ChenChao19 <chenchao19@live.com>
2019-10-20 00:37:43 +0800

    Fix Bug for AutoComplete

--- src/main/java/help/AutoComplete.java
+++ src/main/java/help/AutoComplete.java
@@ -1,13 +1,10 @@
 package help;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class AutoComplete {
-    private List<String> PopUpList;
-    private List<String> MatchList;
     private final String[] List = {
             "add income",
             "add instalment",
@@ -50,49 +47,11 @@ public class AutoComplete {
     };
     private List<String> CommandList = Arrays.asList(List);
 
-    public AutoComplete()  {
+    public AutoComplete() {
         Collections.sort(CommandList);
-        this.PopUpList = new ArrayList<String>();
-        this.MatchList = new ArrayList<String>();
     }
 
-    public List<String> Populate(String prefix) {
-        populatePopup(prefix);
-        return PopUpList;
-    }
-
-    public void populatePopup(String prefix) {
-        int maxEntries = 5;
-        allMatches(prefix);
-        int count = Math.min(MatchList.size(), maxEntries);
-        for (int i = 0; i < count; i++) {
-            PopUpList.add(MatchList.get(i));
-        }
-    }
-
-    public void allMatches(String prefix) {
-        int firstIndex = -1;
-        for (int i = 0; i < CommandList.size(); i++) {
-            if (CommandList.get(i).startsWith(prefix)) {
-                firstIndex = i;
-                break;
-            }
-        }
-
-        if (firstIndex == -1) {
-            return;
-        }
-
-        int lastIndex = firstIndex;
-        for (int i = firstIndex + 1; i < CommandList.size(); i++) {
-            if (!CommandList.get(i).startsWith(prefix)) {
-                lastIndex = i - 1;
-                break;
-            }
-        }
-
-        for(int i = firstIndex; i <= lastIndex; i++) {
-            MatchList.add(CommandList.get(i));
-        }
+    public java.util.List<String> getCommandList() {
+        return CommandList;
     }
 }
