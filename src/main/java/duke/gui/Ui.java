8226f90cad86c1d2d1de8586627608eb987b1170
gowgos5 <gowgos5@gmail.com>
2019-10-19 16:39:52 +0800

    Refactor Ui.java

--- src/main/java/duke/gui/Ui.java
+++ src/main/java/duke/gui/Ui.java
@@ -3,16 +3,20 @@ package duke.gui;
 import javafx.stage.Stage;
 
 /**
- * API of UI component.
+ * Application Programming Interface (API) of the UI component of the application.
  */
 public interface Ui {
     /**
-     * Starts the UI (and the application).
+     * Starts the UI (and the JavaFX application).
+     *
+     * @param primaryStage Stage created by the JavaFX system when the application first starts up.
      */
-    void start(Stage stage);
+    void start(Stage primaryStage);
 
     /**
-     * Print message: TODO.
+     * Print message on the command window.
+     *
+     * @param message Output message.
      */
-    void print(String output);
+    void print(String message);
 }
