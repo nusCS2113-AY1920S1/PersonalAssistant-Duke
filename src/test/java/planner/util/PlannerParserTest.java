6e9dadd53d3eeff8a5f906564eb87118cbeb1547
namiwa <khairul.nasri895@gmail.com>
2019-10-21 00:12:51 +0800

    Removed outdate duke tests and rename parserTest

--- src/test/java/planner/util/PlannerParserTest.java
+++ src/test/java/planner/util/PlannerParserTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class DukeParserTest {
+public class PlannerParserTest {
 
     @Test
     public void addCommandTest() throws ModException {
