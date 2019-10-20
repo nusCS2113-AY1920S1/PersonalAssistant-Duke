53be2648d18e2637e9ec2bdcfd47e98a5709778f
kuromono <clouderext@gmail.com>
2019-10-21 00:40:55 +0800

    Fix AddCommandParserTest not compiling

--- src/test/java/cube/logic/parser/AddCommandParserTest.java
+++ src/test/java/cube/logic/parser/AddCommandParserTest.java
@@ -7,9 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class AddCommandParserTest {
     @Test
     public void findFullStringTest () {
-        String test1 = "add -n super banana";
-        String test2 = "add -n super banana -t this one -p 12.30";
-        AddCommandParser test = new AddCommandParser();
+        String test1 = "add super banana";
+        String test2 = "add super banana -t this one -p 12.30";
+        ParserUtil test = new ParserUtil();
 
         assertEquals(test.findFullString(test1.split(" "),1),"super banana");
         assertEquals(test.findFullString(test2.split(" "),4),"this one");
