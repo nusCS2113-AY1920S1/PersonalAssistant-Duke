420249e2f23251a2488f899a6cb852c96ee34cb3
Lucria <jerryzhang28eclipse@gmail.com>
2019-10-20 16:19:12 +0800

    [Refactor] Removed unused Exception

--- src/main/java/exceptions/InvalidInputException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class InvalidInputException extends DukeException {
-
-    public InvalidInputException(String error) {
-        super("Sorry! I don't understand what you mean by: " + error);
-    }
-}
