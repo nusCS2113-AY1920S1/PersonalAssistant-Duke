ada8485eb2374be6c8f87024a1950de64fb8acb2
LL-Pengfei <42534460+LL-Pengfei@users.noreply.github.com>
2019-10-21 17:54:28 +0800

    no message

--- src/main/java/cube/logic/command/ExitCommand.java
+++ src/main/java/cube/logic/command/ExitCommand.java
@@ -16,7 +16,7 @@ public class ExitCommand extends Command{
 	private final String MESSAGE_SUCCESS = "Bye. Hope to see you again soon!";
 
 	/**
-	 * The class returns true. When called, the user wishes to exit the programme.
+	 * This method returns true. When called, the user wishes to exit the programme.
 	 *
 	 * @return True to signal exit programme.
 	 */
@@ -24,7 +24,14 @@ public class ExitCommand extends Command{
 	public boolean isExit() {
 		return true;
 	}
-	
+
+	/**
+	 * This method provides feedback to user message for Programme Exit.
+	 *
+	 * @param list The food list.
+	 * @param storage The current Storage.
+	 * @return The message feedback to user before Programme Exit.
+	 */
 	@Override
 	public CommandResult execute(FoodList list, StorageManager storage) {
 		return new CommandResult(MESSAGE_SUCCESS, false, true);
