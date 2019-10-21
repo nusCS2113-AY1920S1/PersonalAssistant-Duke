888adb6e354101869768af12d4f03aa0af95262f
Jy <piggy013130@yahoo.com.sg>
2019-10-21 21:13:53 +0800

    Fixed style errors

--- src/main/java/core/Duke.java
+++ src/main/java/core/Duke.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
  * @author T14-4 team
  */
 public class Duke {
-	
+
     /**
      * deals with loading tasks from the file and saving tasks in the file
      */
@@ -31,14 +31,14 @@ public class Duke {
     private ArrayList<Task> tasks;
 
     private ArrayList<Member> members;
-    
+
     public static Duke instance;
 
 
     /**
      * A constructor which applies the file path to load previous data
      *
-     * @param taskFilePath the file path of task list
+     * @param taskFilePath   the file path of task list
      * @param memberFilePath the file path of member list
      */
     public Duke(String taskFilePath, String memberFilePath) {
@@ -68,32 +68,32 @@ public class Duke {
             }
         }
     }
-    
+
     /**
      * Attempts to parse and execute given input
-     * 
+     *
      * @param command input line from user
      */
-    public void doCommand(String command)	{
-    	Command c;
-		try {
-			c = Parser.commandLine(command);
-	        c.execute(tasks, members, storage);
-	        if (c.isExit())
-	        	System.exit(0);
-		} catch (DukeException e) {
-			 Ui.print(e.getMessage());
-		}
+    public void doCommand(String command) {
+        Command c;
+        try {
+            c = Parser.commandLine(command);
+            c.execute(tasks, members, storage);
+            if (c.isExit())
+                System.exit(0);
+        } catch (DukeException e) {
+            Ui.print(e.getMessage());
+        }
     }
-    
-    
-   /**
-    * Static version of doCommand. For window access
-    * 
-    * @param command input line from user
-    */
-    public static void processCommand(String command)	{
-    	instance.doCommand(command);
+
+
+    /**
+     * Static version of doCommand. For window access
+     *
+     * @param command input line from user
+     */
+    public static void processCommand(String command) {
+        instance.doCommand(command);
     }
 
     /**
--- src/main/java/gui/Window.java
+++ src/main/java/gui/Window.java
@@ -23,80 +23,80 @@ import javax.swing.JTextField;
 
 public class Window {
 
-	private JFrame frame;
-	private JTextField inputField;
+    private JFrame frame;
+    private JTextField inputField;
 
-	public static JTextArea outputArea;
+    public static JTextArea outputArea;
 
-	/**
-	 * Create the application.
-	 */
-	public Window() {
-		initialize();
-		this.frame.setVisible(true);
-	}
+    /**
+     * Create the application.
+     */
+    public Window() {
+        initialize();
+        this.frame.setVisible(true);
+    }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(120, 168, 219));
-		frame.getContentPane().setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(22, 397, 363, 151);
-		panel.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
-		panel.setBackground(SystemColor.window);
-		frame.getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBorder(BorderFactory.createEmptyBorder());
-		scrollPane.setBounds(10, 10, 343, 131);
-		panel.add(scrollPane);
-		
-		JTextArea outputArea = new JTextArea();
-		outputArea.setEditable(false);
-		scrollPane.setViewportView(outputArea);
-		outputArea.setFont(new Font("Constantia", Font.PLAIN, 15));
-		outputArea.setWrapStyleWord(true);
-		outputArea.setLineWrap(true);
-		DefaultCaret caret = (DefaultCaret)outputArea.getCaret();
-		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setLayout(null);
-		panel_1.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
-		panel_1.setBackground(Color.WHITE);
-		panel_1.setBounds(408, 508, 363, 40);
-		frame.getContentPane().add(panel_1);
-		
-		inputField = new JTextField();
-		inputField.setFont(new Font("Constantia", Font.PLAIN, 15));
-		inputField.setBounds(10, 10, 343, 19);
-		panel_1.add(inputField);
-		inputField.setColumns(10);
-		inputField.setBorder(BorderFactory.createEmptyBorder());
-		frame.setBounds(100, 100, 800, 600);
-		frame.setResizable(false);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.addWindowListener(new WindowAdapter()	{
-			public void windowOpened(WindowEvent e) {
-				inputField.requestFocus();
-			}
-		});
-		Window.outputArea = outputArea;
-		
-		Action enterPressed = new AbstractAction()	{
-			@Override
-			public void actionPerformed(ActionEvent e)	{
-				Duke.processCommand(inputField.getText());
-				inputField.setText("");
-			}
-		};
-		inputField.addActionListener(enterPressed);
-	}
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.getContentPane().setBackground(new Color(120, 168, 219));
+        frame.getContentPane().setLayout(null);
+
+        JPanel panel = new JPanel();
+        panel.setBounds(22, 397, 363, 151);
+        panel.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+        panel.setBackground(SystemColor.window);
+        frame.getContentPane().add(panel);
+        panel.setLayout(null);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        scrollPane.setBounds(10, 10, 343, 131);
+        panel.add(scrollPane);
+
+        JTextArea outputArea = new JTextArea();
+        outputArea.setEditable(false);
+        scrollPane.setViewportView(outputArea);
+        outputArea.setFont(new Font("Constantia", Font.PLAIN, 15));
+        outputArea.setWrapStyleWord(true);
+        outputArea.setLineWrap(true);
+        DefaultCaret caret = (DefaultCaret) outputArea.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+
+        JPanel panel_1 = new JPanel();
+        panel_1.setLayout(null);
+        panel_1.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+        panel_1.setBackground(Color.WHITE);
+        panel_1.setBounds(408, 508, 363, 40);
+        frame.getContentPane().add(panel_1);
+
+        inputField = new JTextField();
+        inputField.setFont(new Font("Constantia", Font.PLAIN, 15));
+        inputField.setBounds(10, 10, 343, 19);
+        panel_1.add(inputField);
+        inputField.setColumns(10);
+        inputField.setBorder(BorderFactory.createEmptyBorder());
+        frame.setBounds(100, 100, 800, 600);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowOpened(WindowEvent e) {
+                inputField.requestFocus();
+            }
+        });
+        Window.outputArea = outputArea;
+
+        Action enterPressed = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Duke.processCommand(inputField.getText());
+                inputField.setText("");
+            }
+        };
+        inputField.addActionListener(enterPressed);
+    }
 }
