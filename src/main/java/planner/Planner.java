57a72cb3b6485856a1478d33f92312906156ec84
namiwa <khairul.nasri895@gmail.com>
2019-10-22 00:17:55 +0800

    Removed old planner from main Planner class

--- src/main/java/planner/Planner.java
+++ src/main/java/planner/Planner.java
@@ -4,10 +4,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.HashMap;
 
-import planner.command.Command;
 import planner.command.logic.ModuleCommand;
 import planner.exceptions.ModException;
-import planner.exceptions.ModTimeIntervalTooCloseException;
 import planner.exceptions.planner.ModBadRequestStatus;
 import planner.exceptions.planner.ModFailedJsonException;
 import planner.modules.data.ModuleInfoDetailed;
@@ -94,67 +92,6 @@ public class Planner {
         }
     }
 
-    private void modRun() {
-        modUi.helloMsg();
-        modSetup();
-        while (true) {
-            try {
-                String fullCommand = modUi.readCommand();
-                modUi.showLine();
-                ModuleCommand c = parser.parse(fullCommand, false);
-                c.execute(modDetailedMap, modTasks, ccas, modUi, store, jsonWrapper);
-            } catch (ModException e) {
-                System.out.println(e.getMessage());
-            } finally {
-                modUi.showLine();
-            }
-        }
-    }
-
-    /**
-     * Main setup function to start threads in reminder and fill module data on startup.
-     */
-    private void setup() {
-        try {
-            // Starting reminder threads and pulling data from API
-            reminder = new Reminder(tasks.getTasks());
-            reminder.run();
-            // This pulls data once and stores in the data files.
-            jsonWrapper.runRequests(store);
-            modDetailedMap = jsonWrapper.getModuleDetailedMap();
-        } catch (ModTimeIntervalTooCloseException e) {
-            System.out.println(e.getMessage());
-        } catch (ModBadRequestStatus er) {
-            er.printStackTrace();
-        } catch (ModFailedJsonException ej) {
-            System.out.println(ej.getLocalizedMessage());
-        }
-    }
-
-    /**
-     * The main run loop for Duke, requesting for user input
-     * and running valid commands. Invalid commands will be
-     * alerted to users.
-     */
-    private void run() {
-        ui.helloMsg();
-        setup();
-        boolean isExit = false;
-        while (!isExit) {
-            try {
-                String fullCommand = ui.readCommand();
-                ui.showLine();
-                Command c = parser.parse(fullCommand);
-                c.execute(tasks, ui, store, reminder);
-                isExit = c.isExit();
-            } catch (ModException e) {
-                e.printStackTrace();
-            } finally {
-                ui.showLine();
-            }
-        }
-    }
-
     private void handleInput(String input) {
         try {
             modUi.showLine();
@@ -189,8 +126,6 @@ public class Planner {
     public static void main(String[] args) {
         //TODO: args flag could be passed into program for optional runs
         Planner planner = new Planner();
-        //duke.run();
-        //duke.modRun();
         planner.modRunArgparse4j();
     }
 }
