1ab91efeb3fbe949acdb7f43348b25517dd6b441
LL-Pengfei <42534460+LL-Pengfei@users.noreply.github.com>
2019-10-22 02:24:24 +0800

    no message

--- src/test/java/cube/ui/UiTest.java
+++ src/test/java/cube/ui/UiTest.java
@@ -1,8 +1,11 @@
+//@@author LL-Pengfei
+package cube.ui;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CubeTest {
+public class UiTest {
     @Test
     public void dummyTest() {
         assertEquals(2, 2);
