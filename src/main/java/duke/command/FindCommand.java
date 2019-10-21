6c0b60306d5b1e2a66cf9710b02791a5c3fd2179
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2019-10-21 10:37:58 +0800

    Checkstyle for FindCommand

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -3,15 +3,23 @@ package duke.command;
 import duke.Ui;
 import duke.sports.ManageStudents;
 
-public class FindCommand extends Command {
+public final class FindCommand extends Command {
+
+    /**
+     * Represents a name of the student to find.
+     */
     private String findStudent;
 
-    public FindCommand(String findStudent) {
-        this.findStudent = findStudent;
+    /**
+     * Setter method to set the name of the student.
+     * @param myFindStudent The name of the student to set.
+     */
+    public FindCommand(final String myFindStudent) {
+        this.findStudent = myFindStudent;
     }
 
     @Override
-    public String execute(ManageStudents manageStudents, Ui ui) {
+    public String execute(final ManageStudents manageStudents, final Ui ui) {
         return manageStudents.findName(findStudent);
     }
 }
