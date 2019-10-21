12177dae024a8efbc3d132c5f3df5628e42d0726
chekjun <limchekjun@gmail.com>
2019-10-21 11:40:12 +0800

    Reverted spelling change to FIndOrderCommand because merge conflict.

--- src/main/java/duke/logic/command/order/FIndOrderCommand.java
+++ src/main/java/duke/logic/command/order/FIndOrderCommand.java
@@ -4,7 +4,7 @@ import duke.logic.command.CommandResult;
 import duke.logic.command.exceptions.CommandException;
 import duke.model.Model;
 
-public class FindOrderCommand extends OrderCommand {
+public class FIndOrderCommand extends OrderCommand {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
