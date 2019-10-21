5a91c86c2d419df544fd51d4ab211c1eb0b12bc8
HUANG XUAN KUN <hxk123123123@gmail.com>
2019-10-22 02:32:52 +0800

    Created CSVStorage, Storage and StorageManager classes

---
+++ src/main/java/duke/storage/CSVStorage.java
@@ -0,0 +1,78 @@
+package duke.storage;
+
+import duke.core.DukeException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class CSVStorage extends Storage {
+
+    private String filePath;
+
+    /**
+     * Constructs a Storage object with a specific file path.
+     *
+     * @param filePath A string that represents the path of the file to read or
+     *                 write.
+     */
+    public CSVStorage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public void write(ArrayList<ArrayList<String>> infoList, String[] headers) throws DukeException {
+        try {
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get(this.filePath));
+            // Set up csv printer with headers given
+            CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader(headers));
+            for (ArrayList<String> row : infoList) {
+                csvPrinter.printRecord(row);
+            }
+            csvPrinter.flush();
+        } catch (IOException e) {
+            throw new DukeException(e.getMessage());
+        }
+    }
+
+
+    /**
+     * Read data from the file and store into a ArrayList of task.
+     *
+     * @return A ArrayList of Map<Header, value> from the file.
+     * @throws DukeException If file is not found.
+     */
+    public ArrayList<Map<String, String>> read() throws DukeException {
+        // Initialize capacity for 3000 rows of records.
+        ArrayList<Map<String,String>> infoList = new ArrayList<>(3000);
+        File csvFile = new File(filePath);
+        try {
+            if (csvFile.createNewFile()) {
+                System.out.println("File " + filePath + " is created.");
+            } else {
+                Reader in = new FileReader(filePath);
+                Iterable<CSVRecord> records = CSVFormat.EXCEL.withFirstRecordAsHeader().parse(in);
+                for (CSVRecord record : records) {
+                    // There are maximum 9 columns per row
+                    Map<String,String> headerValueMap = record.toMap();
+                    infoList.add(headerValueMap);
+                }
+            }
+            return infoList;
+        } catch (Exception e) {
+            throw new DukeException("Loading of "
+                    + filePath
+                    + " is unsuccessful.\n"
+                    + "e.getMessage()");
+        }
+    }
+}
---
+++ src/main/java/duke/storage/Storage.java
@@ -0,0 +1,26 @@
+package duke.storage;
+
+import duke.core.DukeException;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public abstract class Storage {
+
+    /**
+     * Write info to local csv files.
+     *
+     * @param infoList A list of records to be to be written in rows to csv file
+     * @throws DukeException throw exception with error message when i/o fails
+     */
+    public abstract void write(ArrayList<ArrayList<String>> infoList, String[] headers) throws DukeException;
+
+
+    /**
+     * Load the patients' info from local csv files.
+     *
+     * @return A arrayList of Map<Header, Value> to be saved
+     * @throws DukeException throw a dukeException with error message for debugging
+     */
+    public abstract ArrayList<Map<String, String>> read() throws DukeException;
+}
