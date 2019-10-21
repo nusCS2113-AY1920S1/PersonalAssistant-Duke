3a508500d202f14f8b2d6682bff7af1433d5e9a5
Yu Jiahan <116010274@link.cuhk.edu.cn>
2019-10-22 03:16:58 +0800

    fix conflicts w/ dishesCommand classes

--- src/main/java/duke/command/dishesCommand/AddIngredient.java
+++ src/main/java/duke/command/dishesCommand/AddIngredient.java
@@ -20,7 +20,7 @@ public class AddIngredient extends Cmd<DishList> {
     public void execute(DishList dish1, Ui ui, Storage storage) throws DukeException {
         try {
             dish1.getDish(Nb - 1).addIngredients(ingredient);
-            System.out.println("\t added ingredient: " + ingredient + "\n\t to dish: " + dish1.getDish(Nb - 1).toString());
+            System.out.println("\t added ingredient: " + ingredient + "\n\t to dish: " + dish1.getDish(Nb - 1).getDishname());
         } catch (Exception e) {
             throw new DukeException("cannot add ingredient");
         }
--- src/main/java/duke/command/dishesCommand/ListDishCommand.java
+++ src/main/java/duke/command/dishesCommand/ListDishCommand.java
@@ -16,10 +16,9 @@ public class ListDishCommand extends Cmd<DishList> {
         } else {
             System.out.println("\t Here are the dishes in your list:");
             for (int i = 1; i <= dish1.getSize(); i++) { // looping to print all the saved tasks
-                ui.showDishes("\t " + i + "." + dish1.getDish(i - 1).toString()
+                ui.showDishes("\t " + i + "." + dish1.getDish(i - 1).getDishname()
                         , dish1.getDish(i - 1).getTotalNumberOfOrders());
             }
         }
     }
-
 }
