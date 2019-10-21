71b23933fcae93eb55fdc0b24f7be427555cc030
danisheddie <42911431+danisheddie@users.noreply.github.com>
2019-10-21 22:19:12 +0800

    Added missing parameter

--- src/main/java/duke/task/TaskList.java
+++ src/main/java/duke/task/TaskList.java
@@ -55,8 +55,9 @@ public class TaskList {
      *
      * @param i This is the first parameter
      *          it takes the newly created Deadline/ToDo/Event
+     * @param c
      */
-    public void addTask(final Item i) {
+    public void addTask(final Item i, String c) {
         list.add(i);
         System.out.println("Got it. I've added this task:\n "
             + list.get(getList().size() - 1).toString() + "\n"
