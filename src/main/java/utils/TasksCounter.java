637823c13e2825ed5fb93430a7d35e98c5ecb3a4
AugGust <piggy013130@yahoo.com.sg>
2019-10-22 14:32:04 +0800

    Fixed indentations

--- src/main/java/utils/TasksCounter.java
+++ src/main/java/utils/TasksCounter.java
@@ -7,26 +7,27 @@ import tasks.Task;
 public class TasksCounter {
     private ArrayList<Task> tasks;
 
-	/**
-	 * Initilises a new TaskCounter with given arraylist of tasks.
-	 * For counting number of completed tasks in given set
-	 *
-	 * @param tasks task set to be counted
-	 */
-	public TasksCounter(ArrayList<Task> tasks) {
+    /**
+     * Initilises a new TaskCounter with given arraylist of tasks.
+     * For counting number of completed tasks in given set
+     *
+     * @param tasks task set to be counted
+     */
+    public TasksCounter(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
-	/**
-	 * Gets percentage of tasks marked "done" in given set of tasks
-	 *
-	 * @return integer of percentage marked done
-	 */
-	public int getPercCompleted() {
+    /**
+     * Gets percentage of tasks marked "done" in given set of tasks
+     *
+     * @return integer of percentage marked done
+     */
+    public int getPercCompleted() {
         float totalCompleted = 0;
         for (int i = 0; i < tasks.size(); i++) {
-            if (tasks.get(i).getIsDone())
+            if (tasks.get(i).getIsDone()) {
                 totalCompleted += 1;
+            }
         }
         return (int) (totalCompleted / (float) tasks.size() * 100f);
     }
