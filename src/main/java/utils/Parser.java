33118fe80606f4172a4f4c09de7771b951a6ab6f
Jy <piggy013130@yahoo.com.sg>
2019-10-22 23:39:43 +0800

    Attempt to fix style error

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -295,7 +295,8 @@ public class Parser {
      * if the method cannot recognize the word, return the original word.
      */
     public static String commandCorrector(String command) {
-        String[] dict = {"HELP", "ADD", "LIST", "DONE", "BYE", "DELETE", "FIND", "RECURRING", "SNOOZE", "SCHEDULE", "CHECK", "LINK", "UNLINK", "REMOVE"};
+        String[] dict = {"HELP", "ADD", "LIST", "DONE", "BYE", "DELETE", "FIND", "RECURRING",
+                "SNOOZE", "SCHEDULE", "CHECK", "LINK", "UNLINK", "REMOVE"};
         double[] similarity = new double[dict.length];
         double maxSimilarity = 0;
         int maxSimilarityCommandIndex = -1;
