57f22be821d15d3c4d7e1e638d7fd55ed69155e2
AmosChan97 <amoszzchan@outlook.com>
2019-10-22 22:20:31 +0800

    List Bookings

--- src/main/java/command/ListCommand.java
+++ src/main/java/command/ListCommand.java
@@ -7,12 +7,20 @@ import storage.Storage;
 import ui.Ui;
 import user.User;
 
-import java.io.IOException;
-import java.text.ParseException;
-
-public class ListCommand extends Command{
+public class ListCommand extends Command {
+    /**
+     * Prints to the UI a list containing all bookings made.
+     * @param roomList list of rooms
+     * @param bookingList bookings list
+     * @param ui user interface
+     * @param bookingStorage handles read write of bookings list file
+     * @param roomStorage handles read write of room list file
+     * @param user Current user
+     * @throws DukeException if there are no bookings
+     */
     @Override
-    public void execute(RoomList roomList, BookingList bookingList, Ui ui, Storage bookingStorage, Storage roomStorage, User user) throws DukeException, IOException, ParseException {
+    public void execute(RoomList roomList, BookingList bookingList, Ui ui, Storage bookingStorage,
+                        Storage roomStorage, User user) throws DukeException {
         if (bookingList.isEmpty()) {
             throw new DukeException("OOPS! There are no bookings in your list");
         }
