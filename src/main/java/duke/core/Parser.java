026ca9d51c386aaa5981ae8156a9bc3ff2e6bb16
lmtaek <ltaekman@gmail.com>
2019-10-22 22:28:09 +0800

    Parser class alterations done to work with ':' parsing.

--- src/main/java/duke/core/Parser.java
+++ src/main/java/duke/core/Parser.java
@@ -1,8 +1,10 @@
+//@@lmtaek
 package duke.core;
 
 public class Parser {
 
     String userInput;
+    String[] parsedInput;
 
     /**
      * Constructor for the Parser class.
@@ -10,83 +12,109 @@ public class Parser {
      * @param userInput Takes in user's raw input and stores it to use in its methods, parsing it
      *                  into a format that is appropriate for the command it invokes.
      */
-    public Parser(String userInput) {
-        this.userInput = userInput;
+    public Parser(String userInput) throws DukeException {
+        this.userInput = userInput.trim();
+        try {
+            parsedInput = userInput.split(":");
+        } catch (Exception e) {
+            throw new DukeException("Could not parse user input!");
+        }
     }
 
     /**
-     * Parses user input so that it is compatible with Add commands.
+     * Parses user input so that it is compatible with `add patient` command.
+     * <p>
+     * `add patient` output: patient_name, patient_NRIC, patient_room, patient_remark
      *
-     * @return A formatted string that will work for the available 'add' commands.
+     * @return A formatted string that will work for the available `add patient` command.
      * @throws DukeException If the method is unable to parse the userInput correctly, it will throw
      *                       a DukeException.
      */
-    public String[] parseAdd() throws DukeException {
-        String[] parsedCommand = userInput.toLowerCase().split("\\s+", 3);
+    public String[] parseAddPatient() throws DukeException {
+        String[] formattedOutput = new String[4];
         try {
-            if (parsedCommand[1].equals("patient")) {
-                String[] patientInfo = userInput.replaceAll(
-                        "(?i)add patient ", "").trim().split("\\s+", 4);
-                return patientInfo;
-            } else if (parsedCommand[1].equals("task")) {
-                String[] taskInfo = new String[1];
-                taskInfo[0] = userInput.replaceAll("(?i)add task ", "").trim();
-                return taskInfo;
+            for (int i = 1; i <= formattedOutput.length; i++) {
+                formattedOutput[i - 1] = parsedInput[i].trim();
             }
+            return formattedOutput;
         } catch (Exception e) {
-            throw new DukeException("Please change the format for your 'add' command.");
-        }
-        throw new DukeException("Invalid 'add' command.");
-    }
-
-    /**
-     * Takes the user input and formats it so it is compatible with 'Assign' commands.
-     *
-     * @return A string of formatted output to be used by 'Assign' commands.
-     * @throws DukeException Thrown when the user input cannot be parsed in the desired manner.
-     */
-    public String[] parseAssign() throws DukeException {
-        String[] formattedInput = new String[5];
-        try {
-            String[] tempInput = userInput.trim().replaceAll(
-                    "(?i)assign ", "").split("\\s+", 4);
-            if (tempInput[0].equals("E")) {
-                String[] parsedTimes = tempInput[3].split(" to ", 2);
-
-                for (int i = 0; i < 3; i++) {
-                    formattedInput[i] = tempInput[i];
-                }
-                formattedInput[3] = parsedTimes[0];
-                formattedInput[4] = parsedTimes[1];
-            } else {
-                for (int i = 0; i < tempInput.length; i++) {
-                    formattedInput[i] = tempInput[i];
-                }
-            }
-            return formattedInput;
-        } catch (Exception e) {
-            throw new DukeException("Invalid 'assign' command.");
+            throw new DukeException("Please follow the  "
+                    + "`add patient :<patient name> :<NRIC> :<patient room> :<patient_remark>` "
+                    + "format.");
         }
     }
 
     /**
-     * Takes the user input and formats it so it is compatible with the 'list' case in CommandManager.
+     * Parses user input so that it is compatible with `add task` command.
+     * <p>
+     * `add task` output: task_description
      *
-     * @return A string of formatted output to be used by 'list' case in CommandManager.
+     * @return A formatted string that will work for the available `add task` command.
+     * @throws DukeException If the method is unable to parse the userInput correctly, it will throw
+     *                       a DukeException.
+     */
+    public String parseAddTask() throws DukeException {
+        String formattedOutput;
+        try {
+            formattedOutput = parsedInput[1].trim();
+        } catch (Exception e) {
+            throw new DukeException("Please follow the `add task :<task description>`.");
+        }
+        return formattedOutput;
+    }
+
+    /**
+     * Takes the user input and formats it so it is compatible with `assign standard task` commands.
+     * <p>
+     * `assign standard task` output: patient_name or #patient_id, task_name or #task_id, dateTime
+     *
+     * @return A string of formatted output to be used by `assign standard task` command.
      * @throws DukeException Thrown when the user input cannot be parsed in the desired manner.
      */
-    public String[] parseList() throws DukeException {
+    public String[] parseAssignStandardTask() throws DukeException {
+        String[] formattedInput = new String[3];
         try {
-            String[] formattedInput;
-            formattedInput = userInput.replaceAll("(?i)list ", "").trim().split("\\s+");
+            for (int i = 1; i <= formattedInput.length; i++) {
+                formattedInput[i - 1] = parsedInput[i].trim();
+            }
             return formattedInput;
         } catch (Exception e) {
-            throw new DukeException("Invalid 'list' command.");
+            throw new DukeException("Please follow the "
+                    + "`assign standard task :<patient name> or #<patient id> :#<task id> or <task name> "
+                    + ":<dd/MM/YYYY HHmm>` format.");
+        }
+    }
+
+    /**
+     * Takes the user input and formats it so it is compatible with `assign event task` command.
+     * <p>
+     * `assign event task` output: patient_name or #patient_id, task_name or #task_id, start_time, end_time
+     *
+     * @return A string of formatted output to be used by `assign event task` command.
+     * @throws DukeException Thrown when the user input cannot be parsed in the desired manner.
+     */
+    public String[] parseAssignEventTask() throws DukeException {
+        String[] formattedInput = new String[4];
+        try {
+            String[] parsedTimes = parsedInput[4].split(" to ");
+            for (int i = 1; i < (formattedInput.length - 1); i++) {
+                formattedInput[i - 1] = parsedInput[i].trim();
+            }
+
+            formattedInput[3] = parsedTimes[0];
+            formattedInput[4] = parsedTimes[1];
+            return formattedInput;
+        } catch (Exception e) {
+            throw new DukeException("Please follow the "
+                    + "`assign event task :<patient name> or #<patient id> :#<task ID> or <task name> "
+                    + ":<dd/MM/YYYY HHmm> to <dd/MM/YYYY HHmm>` format.");
         }
     }
 
     /**
      * Takes the user input and formats it so it is compatible with 'delete patient' command.
+     * <p>
+     * `delete patient` output: patient_name or #patient_id
      *
      * @return A string of formatted output to be used by 'delete patient' commands.
      * @throws DukeException Thrown when the user input cannot be parsed in the desired manner.
@@ -94,16 +122,17 @@ public class Parser {
     public String parseDeletePatient() throws DukeException {
         try {
             String formattedInput;
-            String inputToParse = userInput.replaceAll("(?i)delete patient ", "").trim();
-            formattedInput = inputToParse;
+            formattedInput = parsedInput[1].trim();
             return formattedInput;
         } catch (Exception e) {
-            throw new DukeException("Invalid 'delete' command format.");
+            throw new DukeException("Please follow the `delete patient :<patient name> or #<patient id>` format");
         }
     }
 
     /**
      * Takes the user input and formats it so it is compatible with 'delete task' command.
+     * <p>
+     * `delete task` output: task_id or task_name
      *
      * @return A string of formatted output to be used by 'delete task' commands.
      * @throws DukeException Thrown when the user input cannot be parsed in the desired manner.
@@ -111,79 +140,108 @@ public class Parser {
     public String parseDeleteTask() throws DukeException {
         try {
             String formattedInput;
-            String inputToParse = userInput.replaceAll("(?i)delete task ", "").trim();
-            formattedInput = inputToParse;
+            formattedInput = parsedInput[1].trim();
             return formattedInput;
         } catch (Exception e) {
-            throw new DukeException("Invalid 'delete task' command.");
+            throw new DukeException("Please follow the `delete task :<task name> or #<task id>` format");
         }
     }
 
     /**
      * Takes user input and formats it so it is compatible with 'delete patient task' command.
+     * <p>
+     * `delete patient task` output: patient_name or #patient_id, task_name or #task_id
      *
      * @return Array of strings to be used by 'delete patient task' command.
      * @throws DukeException when user input cannot be parsed properly.
      */
     public String[] parseDeletePatientTask() throws DukeException {
         try {
-            String[] formattedInput;
-            String inputToParse = userInput.replaceAll("(?i)delete patient task ", "").trim();
-            formattedInput = inputToParse.split("\\s+", 2);
-
+            String[] formattedInput = new String[2];
+            for (int i = 1; i <= formattedInput.length; i++) {
+                formattedInput[i - 1] = parsedInput[i].trim();
+            }
             return formattedInput;
-
         } catch (Exception e) {
-            throw new DukeException("Invalid 'delete patient task' command.");
+            throw new DukeException("Please follow the `delete patient task :<patient name> or #<patient id>"
+                    + " :<task name> or #<task id>` format.");
         }
     }
 
     /**
      * Takes the user input and formats it so it is compatible with 'update patient' command.
+     * <p>
+     * `update patient` output: patient_name or #patient_id, edited_field, updated_info
      *
      * @return A string of formatted output to be used by 'update patient' commands.
      * @throws DukeException Thrown when the user input cannot be parsed in the desired manner.
      */
-    public String parseUpdatePatient() throws DukeException {
-        String formattedInput;
-        String inputToParse = userInput.replaceAll("(?i)update patient ", "").trim();
-        formattedInput = inputToParse;
-        return formattedInput;
-    }
-
-    /**
-     * Takes the user input and formats it so it is compatible with 'find' commands.
-     *
-     * @return A string of formatted output to be used by 'find' commands.
-     * @throws DukeException Thrown when the user input cannot be parsed in the desired manner.
-     */
-    public String parseFind() throws DukeException {
+    public String[] parseUpdatePatient() throws DukeException {
         try {
-            String formattedInput;
-            String[] inputToParse = userInput.replaceAll("(?i)find patient ", "").trim().split("\\s+");
+            String[] formattedInput = new String[3];
 
-            if (inputToParse[0].equalsIgnoreCase("task")) {
-                formattedInput = userInput.replaceAll("(?i)find patient task ", "").trim();
-            } else {
-                formattedInput = userInput.replaceAll("(?i)find patient ", "").trim();
+            for (int i = 1; i <= formattedInput.length; i++) {
+                formattedInput[i - 1] = parsedInput[i].trim();
             }
             return formattedInput;
         } catch (Exception e) {
-            throw new DukeException("Invalid 'find patient' command.");
+            throw new DukeException("Please use the `update patient :<patient name> or #<patient id>"
+                    + "/<edited info type> :<updated info>` format.");
         }
     }
 
-
     /**
      * Takes the user input and formats it so it is compatible with 'update task' command.
+     * <p>
+     * `update task` output: task_name or #task_id, updated_description
      *
      * @return A string of formatted output to be used by 'update task' commands.
      * @throws DukeException Thrown when the user input cannot be parsed in the desired manner.
      */
-    public String parseUpdateTask() throws DukeException {
-        String formattedInput;
-        String inputToParse = userInput.replaceAll("(?i)update task ", "").trim();
-        formattedInput = inputToParse;
-        return formattedInput;
+    public String[] parseUpdateTask() throws DukeException {
+        try {
+            String[] formattedInput = new String[2];
+            for (int i = 1; i <= formattedInput.length; i++) {
+                formattedInput[i - 1] = parsedInput[i].trim();
+            }
+            return formattedInput;
+        } catch (Exception e) {
+            throw new DukeException("Please use the `update patient :<task name> or #<task id>"
+                    + " :<updated description>` format.");
+        }
+    }
+
+    /**
+     * Takes the user input and formats it so it is compatible with 'find patient' command.
+     * <p>
+     * `find patient` output: patient_name or #patient_id
+     *
+     * @return A string of formatted output to be used by 'find patient' command.
+     * @throws DukeException Thrown when the user input cannot be parsed in the desired manner.
+     */
+    public String parseFindPatient() throws DukeException {
+        try {
+            String formattedInput = parsedInput[1].trim();
+            return formattedInput;
+        } catch (Exception e) {
+            throw new DukeException("Please use the `find patient :<patient name> or #<patient id>` format.");
+        }
+    }
+
+    /**
+     * Takes the user input and formats it so it is compatible with 'find patient task' command.
+     * <p>
+     * `find patient tasks` output: patient_name or #patient_id
+     *
+     * @return A string of formatted output to be used by 'find patient task' command.
+     * @throws DukeException Thrown when the user input cannot be parsed in the desired manner.
+     */
+    public String parseFindPatientTasks() throws DukeException {
+        try {
+            String formattedInput = parsedInput[1].trim();
+            return formattedInput;
+        } catch (Exception e) {
+            throw new DukeException("Please use the `find patient tasks :<patient name> or #<patient id>` format.");
+        }
     }
 }
