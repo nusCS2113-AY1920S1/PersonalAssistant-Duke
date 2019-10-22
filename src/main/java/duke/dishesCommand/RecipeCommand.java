609b3015e86e2b804de967ad62f8d09076f28807
Yu Jiahan <116010274@link.cuhk.edu.cn>
2019-10-22 19:20:33 +0800

    check out to match version in group repo

---
+++ src/main/java/duke/dishesCommand/RecipeCommand.java
@@ -0,0 +1,24 @@
+package duke.dishesCommand;
+
+import duke.exception.DukeException;
+import duke.Dishes.DishList;
+import duke.storage.Storage;
+import duke.task.TaskList;
+import duke.ui.Ui;
+
+/**
+ * Represents  an abstract Command that could be an add, delete, exit, done, find or list.
+ */
+public abstract class RecipeCommand {
+
+    public abstract void execute(DishList dish1, TaskList taskList, Ui ui, Storage storage) throws DukeException;
+
+    /**
+     * Returns the boolean indicating that it is( not) an {@link ExitCommand}.
+     *
+     * @return false by default
+     */
+    public boolean isExit() {
+        return false;
+    }
+}
