cf05b2c3e1ac70484d6a9648e8b005f37af6fd50
andrewleow97 <e0323960@u.nus.edu>
2019-10-22 23:09:06 +0800

    Added gradedMsg

--- src/main/java/planner/util/commons/PlannerUi.java
+++ src/main/java/planner/util/commons/PlannerUi.java
@@ -213,7 +213,7 @@ public class PlannerUi {
 
     public void gradedMsg(String moduleCode, String letterGrade) {
         showLine();
-        System.out.println("Got it, graded " + moduleCode + " with grade : " + letterGrade);
+        System.out.println("Got it, graded " + moduleCode + " with grade: " + letterGrade);
     }
 
 
