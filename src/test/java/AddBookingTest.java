0fd3e318b260a4e1bbe7a5467b0595e61f75938a
Alex-Teo <alex-teo@comp.nus.edu.sg>
2019-10-22 21:54:48 +0800

    Add Javadoc comments to booking related classes

--- src/test/java/AddBookingTest.java
+++ src/test/java/AddBookingTest.java
@@ -7,12 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AddBookingTest {
     @Test
     void testAddBooking() {
+        String user = "Bob";
         String room = "room4";
         String description = " study";
         String dateTimeStart = "22/12/2019 1100";
         String timeEnd = "22/12/2019 1200";
         User temp = new User("dummy");
-        Booking newBooking = new Booking(room, description, dateTimeStart, timeEnd, temp);
-        assertEquals(newBooking.toString(), "room4 22/12/2019 1100 22/12/2019 1200");
+        Booking newBooking = new Booking(user, room, description, dateTimeStart, timeEnd);
+        assertEquals(newBooking.toString(), "Bob room4 22/12/2019 1100 to 22/12/2019 1200 P");
     }
 }
