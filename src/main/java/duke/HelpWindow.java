379b89d8cbc813cd5218f5a0c6098f8bdb9ce710
Gervaise <e0191550@u.nus.edu>
2019-10-22 16:42:36 +0800

    Added help commands

--- src/main/java/duke/HelpWindow.java
+++ src/main/java/duke/HelpWindow.java
@@ -64,11 +64,11 @@ public class HelpWindow extends AnchorPane {
                 } else if (cbCommands.getSelectionModel().getSelectedItem().equals("fixedduration")) {
                     getDescription.setText(cbCommands.getValue() + " <task description> /for <duration> <unit>");
                     getDescription.getScene().getWindow();
-                } else if (cbCommands.getSelectionModel().getSelectedItem().equals("repeat")){
+                } else if (cbCommands.getSelectionModel().getSelectedItem().equals("repeat")) {
                     getDescription.setText(cbCommands.getValue()
                             + " <task> /from <date and time> /for <duration> <day/week/month>");
                     getDescription.getScene().getWindow();
-                } else if (cbCommands.getSelectionModel().getSelectedItem().equals("doafter")){
+                } else if (cbCommands.getSelectionModel().getSelectedItem().equals("doafter")) {
                     getDescription.setText(cbCommands.getValue()
                             + " <task description> /after <existing task description>");
                     getDescription.getScene().getWindow();
