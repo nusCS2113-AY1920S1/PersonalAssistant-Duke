90c72742ed79108f79af1ce42db4d46e1cc65700
maxxyx96 <maxxyx96@gmail.com>
2019-10-22 10:37:34 +0800

    Optimised saving of states for backup and exit commands.

--- src/main/java/duke/command/ExitCommand.java
+++ src/main/java/duke/command/ExitCommand.java
@@ -1,6 +1,12 @@
 package duke.command;
 
+import duke.storage.BudgetStorage;
+import duke.storage.ContactStorage;
+import duke.storage.PriorityStorage;
 import duke.storage.Storage;
+import duke.task.BudgetList;
+import duke.task.ContactList;
+import duke.task.PriorityList;
 import duke.task.TaskList;
 import duke.ui.Ui;
 
@@ -45,7 +51,30 @@ public class ExitCommand extends Command {
      */
     @Override
     public void executeStorage(TaskList items, Ui ui, Storage storage) throws IOException {
-        ui.showBye();
+    }
+
+    /**
+     * Executes a command that overwrites existing storage with the all task list.
+     *
+     * @param items Task list that contains a list of tasks.
+     * @param ui To tell user that it is executed successfully.
+     * @param storage Storage that stores and handles taskLists.
+     * @param budgetStorage Storage that stores and handles budgets.
+     * @param budgetList Budget list that contains the budget information.
+     * @param contactStorage Storage that stores and handles contact list.
+     * @param contactList Contact list that contains a list of Contacts.
+     * @param priorityStorage Storage that stores and handles priority list.
+     * @param priorityList Priority list that contains a list of Priorities.
+     * @throws IOException If there is an error reading the file.
+     */
+    public void executeStorage(TaskList items, Ui ui, Storage storage,
+                               BudgetStorage budgetStorage, BudgetList budgetList,
+                               ContactStorage contactStorage, ContactList contactList,
+                               PriorityStorage priorityStorage, PriorityList priorityList) throws IOException {
+        priorityStorage.write(priorityList);
+        budgetStorage.write(budgetList);
+        contactStorage.write(contactList);
         storage.write(items);
+        ui.showBye();
     }
 }
