88bd108937feb328b3a231dbf36e4ae155cfc4eb
Gervaise <e0191550@u.nus.edu>
2019-10-22 16:35:23 +0800

    Added help commands

--- src/main/java/duke/HelpWindow.java
+++ src/main/java/duke/HelpWindow.java
@@ -64,15 +64,13 @@ public class HelpWindow extends AnchorPane {
                 } else if (cbCommands.getSelectionModel().getSelectedItem().equals("fixedduration")) {
                     getDescription.setText(cbCommands.getValue() + " <task description> /for <duration> <unit>");
                     getDescription.getScene().getWindow();
-                }
-                else if (cbCommands.getSelectionModel().getSelectedItem().equals("repeat")){
-                    getDescription.setText(cbCommands.getValue() +
-                            " <task> /from <date and time> /for <duration> <day/week/month>");
+                } else if (cbCommands.getSelectionModel().getSelectedItem().equals("repeat")){
+                    getDescription.setText(cbCommands.getValue()
+                            + " <task> /from <date and time> /for <duration> <day/week/month>");
                     getDescription.getScene().getWindow();
-                }
-                else if (cbCommands.getSelectionModel().getSelectedItem().equals("doafter")){
-                    getDescription.setText(cbCommands.getValue() +
-                            " <task description> /after <existing task description>");
+                } else if (cbCommands.getSelectionModel().getSelectedItem().equals("doafter")){
+                    getDescription.setText(cbCommands.getValue()
+                            + " <task description> /after <existing task description>");
                     getDescription.getScene().getWindow();
                 }
             }
