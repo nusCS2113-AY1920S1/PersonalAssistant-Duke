05939c6fa74f0f8610d5c6bed247fb0912c12e72
andrewleow97 <e0323960@u.nus.edu>
2019-10-22 23:07:12 +0800

    JavaDoc for GradeCommand

--- src/main/java/planner/command/GradeCommand.java
+++ src/main/java/planner/command/GradeCommand.java
@@ -1,3 +1,4 @@
+//@@author andrewleow97
 package planner.command;
 
 import planner.exceptions.original.ModException;
@@ -13,10 +14,17 @@ import planner.util.periods.CcaList;
 import java.util.HashMap;
 
 public class GradeCommand extends ModuleCommand {
+
+    /**
+     * Constructor for GradeCommand
+     */
     public GradeCommand(Arguments args) {
         super(args);
     }
 
+    /**
+     * Allows users to grade modules in their task list, or add modules with a grade attached if it is not in their list
+     */
     @Override
     public void execute(HashMap<String, ModuleInfoDetailed> detailedMap,
                         ModuleTasksList tasks,
@@ -24,18 +32,17 @@ public class GradeCommand extends ModuleCommand {
                         PlannerUi plannerUi,
                         Storage store,
                         JsonWrapper jsonWrapper) throws ModException {
-        //grade moduleCode letterGrade
         String moduleCode = arg("moduleCode").toUpperCase();
         String letterGrade = arg("letterGrade").toUpperCase();
         ModuleInfoDetailed mod = detailedMap.get(moduleCode);
         ModuleTask temp = new ModuleTask(moduleCode, mod);
-        if (!tasks.getTasks().contains(temp)) {
+        if (!tasks.getTasks().contains(temp)) { // if list does not have module requested, add it with a grade
             mod.setGrade(letterGrade);
             ModuleTask temp2 = new ModuleTask(moduleCode, mod);
             tasks.getTasks().add(temp2);
             plannerUi.addedMsg(temp2);
             jsonWrapper.storeTaskListAsJson(tasks.getTasks(), store);
-        } else if (tasks.getTasks().contains(temp)) {
+        } else if (tasks.getTasks().contains(temp)) { // otherwise set grade
             int location = tasks.getTasks().indexOf(temp);
             tasks.getTasks().get(location).setGrade(letterGrade);
         } else {
