a24ecce26de0f3caaee8d0497244645d84638ab6
Jy <piggy013130@yahoo.com.sg>
2019-10-23 23:20:55 +0800

    Renamed GUI to Team Manager and packaged new JAR

---
+++ src/main/java/commands/RemindCommand.java
@@ -0,0 +1,23 @@
+package commands;
+
+import java.util.ArrayList;
+
+import members.Member;
+import tasks.Task;
+import utils.DukeException;
+import utils.Storage;
+
+public class RemindCommand extends Command {
+
+	@Override
+	public void execute(ArrayList<Task> tasks, ArrayList<Member> members, Storage storage) throws DukeException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isExit() {
+		return false;
+	}
+
+}
