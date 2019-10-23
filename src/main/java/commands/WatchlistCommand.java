498e16e7b5603fd923e828924121626cc9f7ed30
hotspur1997 <hotspur1997@gmail.com>
2019-10-23 19:16:13 +0800

    refractored code for watchlist

--- src/main/java/commands/WatchlistCommand.java
+++ src/main/java/commands/WatchlistCommand.java
@@ -25,13 +25,7 @@ public class WatchlistCommand  extends CommandSuper {
                 executeTaskDone();
                 break;
             case delete:
-                String mov = getPayload();
-                System.out.println(mov);
-                if (WatchlistHandler.removeFromWatchlist(mov, (MovieHandler)(this.getUIController()))) {
-                    ((MovieHandler) getUIController()).setFeedbackText("Successfully removed the movie from WatchList: " + mov);
-                } else {
-                    ((MovieHandler) getUIController()).setFeedbackText("Such a movie does not exist in your WatchList. Check your spelling?");
-                }
+                deleteFromWatchlist();
                 break;
             default:
                 break;
@@ -48,12 +42,12 @@ public class WatchlistCommand  extends CommandSuper {
             movie = movie.toLowerCase();
             String type = this.getFlagMap().get("-d").get(0);
             switch (type) {
-                case " d ":
+                case "d":
                     String endDate = this.getFlagMap().get("-e").get(0);
                     Deadline deadline = new Deadline(movie, "D", endDate);
                     WatchlistHandler.add(deadline);
                     break;
-                case " p ":
+                case "p":
                     String stDate = this.getFlagMap().get("-s").get(0);
                     String enDate = this.getFlagMap().get("-e").get(0);
                     Period period = new Period(movie, "P", stDate, enDate);
@@ -65,7 +59,7 @@ public class WatchlistCommand  extends CommandSuper {
             WatchlistHandler.print_list((MovieHandler)(this.getUIController()));
         } catch(NullPointerException | IndexOutOfBoundsException e) {
             ((MovieHandler)(this.getUIController())).setFeedbackText("Please enter a valid command in the form of: \n" +
-                    "add watchlist <name of movie> -d <type of task> -s <start date only for task> -e <end date for task>");
+                    "watchlist add <name of movie> -d <type of task> -s <start date only for task> -e <end date for task>");
         }
     }
 
@@ -88,4 +82,16 @@ public class WatchlistCommand  extends CommandSuper {
         }
     }
 
+    /**
+     * removes an item from the watchlist
+     */
+    private void deleteFromWatchlist() {
+        String mov = getPayload();
+        System.out.println(mov);
+        if (WatchlistHandler.removeFromWatchlist(mov, (MovieHandler)(this.getUIController()))) {
+            ((MovieHandler) getUIController()).setFeedbackText("Successfully removed the movie from WatchList: " + mov);
+        } else {
+            ((MovieHandler) getUIController()).setFeedbackText("Such a movie does not exist in your WatchList. Check your spelling?");
+        }
+    }
 }
