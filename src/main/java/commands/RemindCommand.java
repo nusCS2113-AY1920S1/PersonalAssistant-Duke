6238c1b3878262cd48d732aea9282237ab281359
Jy <piggy013130@yahoo.com.sg>
2019-10-23 23:23:54 +0800

    Fixed styles

--- src/main/java/commands/RemindCommand.java
+++ src/main/java/commands/RemindCommand.java
@@ -9,15 +9,15 @@ import utils.Storage;
 
 public class RemindCommand extends Command {
 
-	@Override
-	public void execute(ArrayList<Task> tasks, ArrayList<Member> members, Storage storage) throws DukeException {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void execute(ArrayList<Task> tasks, ArrayList<Member> members, Storage storage) throws DukeException {
+        // TODO Auto-generated method stub
 
-	@Override
-	public boolean isExit() {
-		return false;
-	}
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 
 }
