b0830b0d979f797ed07ce059622227554af1f19c
saradj <43466740+saradj@users.noreply.github.com>
2019-10-23 15:25:57 +0800

    Update FindIngredientCommand.java

--- src/main/java/duke/command/ingredientCommand/FindIngredientCommand.java
+++ src/main/java/duke/command/ingredientCommand/FindIngredientCommand.java
@@ -1,15 +1,17 @@
 package duke.command.ingredientCommand;
 
-        import duke.command.Cmd;
-        import duke.exception.DukeException;
-        import duke.storage.Storage;
-        import duke.task.TaskList;
-        import duke.ui.Ui;
+import duke.command.Cmd;
+import duke.exception.DukeException;
+import duke.ingredient.Ingredient;
+import duke.list.GenericList;
+import duke.storage.Storage;
+import duke.task.TaskList;
+import duke.ui.Ui;
 
 /**
  * Represents a specific {@link Cmd} used to find a String occurring in the {@link TaskList}.
  */
-public class FindIngredientCommand extends Cmd<TaskList> {
+public class FindIngredientCommand extends Cmd<Ingredient> {
 
     private String toFind;
 
@@ -19,7 +21,7 @@ public class FindIngredientCommand extends Cmd<TaskList> {
 
 
     @Override
-    public void execute(TaskList taskList, Ui ui, Storage storage) throws DukeException {
+    public void execute(GenericList<Ingredient> ingList, Ui ui, Storage storage) throws DukeException {
 
     }
 }
