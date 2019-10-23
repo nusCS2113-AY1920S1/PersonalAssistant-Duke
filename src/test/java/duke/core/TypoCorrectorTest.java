00826808f35227960e62ad33758172cebbffe682
HUANG XUAN KUN <hxk123123123@gmail.com>
2019-10-23 19:58:58 +0800

    Update TypoCorrect and its Junit test for the new parsing format

--- src/test/java/duke/core/TypoCorrectorTest.java
+++ src/test/java/duke/core/TypoCorrectorTest.java
@@ -1,11 +1,10 @@
 package duke.core;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Junit test for TypoCorrector class.
@@ -22,15 +21,15 @@ public class TypoCorrectorTest {
                 new String[]{"bqe", "bye"},
                 new String[]{"lsit patant", "list patients"},
                 new String[]{"lsfvs takss", "list tasks"},
-                new String[]{"deelte Ptients #12", "delete patient #12"},
-                new String[]{"deleot tasksa task description", "delete task task description"},
-                new String[]{"delette tasks #1", "delete task #1"},
-                new String[]{"addd patents name nric room remark", "add patient name nric room remark"},
-                new String[]{"dad tsak a very long task name", "add task a very long task name"},
-                new String[]{"addd task abc", "add task abc"},
-                new String[]{"asgn standrdtask #1 12 12/12/2019 1645", "assign standardtask #1 12 12/12/2019 1645"},
-                new String[]{"assgn eventask #1 12 12/12/2019 1645 to 12/12/2020 1645",
-                    "assign eventtask #1 12 12/12/2019 1645 to 12/12/2020 1645"}
+                new String[]{"deelte Ptients:#12", "delete patient :#12"},
+                new String[]{"deleot tasksa :task description", "delete task :task description"},
+                new String[]{"delette tasks :#1", "delete task :#1"},
+                new String[]{"addd patents:name :nric :room :remark", "add patient :name :nric :room :remark"},
+                new String[]{"dad tsak:a very long task name", "add task :a very long task name"},
+                new String[]{"addd task :abc", "add task :abc"},
+                new String[]{"asgn deadli task: #1 :12 :12/12/2019 1645", "assign deadline task : #1 :12 :12/12/2019 1645"},
+                new String[]{"assgn even tk :#1 :12 :12/12/2019 1645 :12/12/2020 1645",
+                    "assign event task :#1 :12 :12/12/2019 1645 :12/12/2020 1645"}
             ));
         for (String[] testPair : testCases) {
             String correctedOutput = TypoCorrector.commandCorrection(testPair[0]);
