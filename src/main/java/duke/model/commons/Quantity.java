588f422d0260656c01c7d2c1260b646ee4ce2f2c
Lim Wen Han Eugene <e0309924@u.nus.edu>
2019-10-24 20:09:37 +0800

    quantity to 1dp and item totalcost to 2dp

--- src/main/java/duke/model/commons/Quantity.java
+++ src/main/java/duke/model/commons/Quantity.java
@@ -1,16 +1,20 @@
 package duke.model.commons;
 
+import java.text.DecimalFormat;
+
 import static duke.commons.util.AppUtil.checkNegativeDouble;
 
 public class Quantity {
+    private static DecimalFormat df1 = new DecimalFormat("#.#");
     private static final Double DEFAULT_NUMBER = 0.0;
-    private Double number;
 
     private static final String MESSAGE_CONSTRAINTS_QUANTITY = "Quantity must be a valid non-negative number";
 
+    private Double number;
+
     public Quantity(double number) {
         checkNegativeDouble(number, MESSAGE_CONSTRAINTS_QUANTITY);
-        this.number = number;
+        this.number = Double.parseDouble(df1.format(number));
     }
 
     public Quantity(Quantity toCopy) {
