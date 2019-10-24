728bd1e1489032152938de26d90ab5ff615da787
KevinAlvarez7 <33540851+KevinAlvarez7@users.noreply.github.com>
2019-10-24 22:31:21 +0800

    Dev Guide Changes (#129)

---
+++ src/main/java/duke/logic/commands/FindCommand.java
@@ -0,0 +1,13 @@
+package duke.logic.commands;
+
+import duke.exceptions.DukeException;
+import duke.models.LockerList;
+import duke.storage.FileHandling;
+import duke.ui.Ui;
+
+public class FindCommand extends Command {
+    @Override
+    public void execute(LockerList lockerList, Ui ui, FileHandling storage) throws DukeException {
+
+    }
+}
---
+++ src/main/java/duke/logic/commands/SortCommand.java
@@ -0,0 +1,13 @@
+package duke.logic.commands;
+
+import duke.exceptions.DukeException;
+import duke.models.LockerList;
+import duke.storage.FileHandling;
+import duke.ui.Ui;
+
+public class SortCommand extends Command {
+    @Override
+    public void execute(LockerList lockerList, Ui ui, FileHandling storage) throws DukeException {
+
+    }
+}
