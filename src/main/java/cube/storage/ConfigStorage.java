7b3bc593a0c16787b6d0e9513565c87c5239750f
kuromono <clouderext@gmail.com>
2019-10-24 23:40:21 +0800

    Add some javadocs for configstorage

--- src/main/java/cube/storage/ConfigStorage.java
+++ src/main/java/cube/storage/ConfigStorage.java
@@ -1,23 +1,44 @@
+/**
+ * Object to store user configurable options.
+ *
+ * @author kuromono
+ */
 package cube.storage;
 
 public class ConfigStorage {
     private double windowHeight;
     private double windowWidth;
 
+    /**
+     * Default constructor.
+     * Creates a new instance of ConfigStorage class with default settings.
+     */
     public ConfigStorage() {
         this.windowHeight = 600;
         this.windowWidth = 450;
     }
 
+    /**
+     * Constructor that takes in 2 arguments.
+     * Creates a new instance of ConfigStorage class with supplied settings.
+     */
     public ConfigStorage(int windowHeight, int windowWidth) {
         this.windowHeight = windowHeight;
         this.windowWidth = windowWidth;
     }
 
+    /**
+     * Retrieves the user configured window height for JavaFX window.
+     * @return Window Height.
+     */
     public double getWindowHeight() {
         return windowHeight;
     }
 
+    /**
+     * Retrieves the user configured window width for JavaFX window.
+     * @return Window Width.
+     */
     public double getWindowWidth() {
         return windowWidth;
     }
