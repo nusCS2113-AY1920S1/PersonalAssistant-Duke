f6efc965f67d657fddf15cbfbba0993ef488e08c
KaneQuah <42487366+KaneQuah@users.noreply.github.com>
2019-10-24 10:33:12 +0800

    updated parser

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -21,6 +21,8 @@ public class Parser {
     public static String dateSeparator = "\\s*\\&\\s*";
     public static String postpone = "\\s*/to\\s*";
     public static String newLine = "\n";
+    public static int windowWidth = 80;
+    public static String acceptedExtensions = "txt|csv";
     private static String addCommand;
     private static String helpCommand;
     private static String modCommand;
