6736dcc07c00ed25a322d04bf17a55a7711dfe11
Jacob Toresson <>
2019-10-25 23:20:55 +0800

    checkstyle fix

--- src/main/java/duke/command/PatientDischargeSpec.java
+++ src/main/java/duke/command/PatientDischargeSpec.java
@@ -1,15 +1,15 @@
 package duke.command;
 
 public class PatientDischargeSpec extends ArgSpec {
-        private static final duke.command.PatientDischargeSpec spec = new duke.command.PatientDischargeSpec();
+    private static final PatientDischargeSpec spec = new PatientDischargeSpec();
 
-        public static duke.command.PatientDischargeSpec getSpec() {
-            return spec;
-        }
-
-        private PatientDischargeSpec() {
-            emptyArgMsg = "";
-            cmdArgLevel = ArgLevel.NONE;
-            initSwitches(new Switch("summary", String.class, true, ArgLevel.OPTIONAL, "sum"));
-        }
+    public static PatientDischargeSpec getSpec() {
+        return spec;
     }
+
+    private PatientDischargeSpec() {
+        emptyArgMsg = "";
+        cmdArgLevel = ArgLevel.NONE;
+        initSwitches(new Switch("summary", String.class, true, ArgLevel.OPTIONAL, "sum"));
+    }
+}
