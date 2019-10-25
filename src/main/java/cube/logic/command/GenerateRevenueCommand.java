cd64c52286ca993465662227ad4af1800e76f0f2
LL-Pengfei <42534460+LL-Pengfei@users.noreply.github.com>
2019-10-25 09:17:28 +0800

    tutorial comment added for genrevenue

--- src/main/java/cube/logic/command/GenerateRevenueCommand.java
+++ src/main/java/cube/logic/command/GenerateRevenueCommand.java
@@ -8,6 +8,8 @@ package cube.logic.command;
 // need to add support for adding revenue at the start and update revenue later (cmd)
 // need to change the name to revenue for user friendliness
 // updated by LL-Pengfei
+// need to store revenue in persistent storage
+// need to make the revenue (individual rev) updated when selling specific food
 
 import cube.logic.command.exception.CommandErrorMessage;
 import cube.logic.command.exception.CommandException;
