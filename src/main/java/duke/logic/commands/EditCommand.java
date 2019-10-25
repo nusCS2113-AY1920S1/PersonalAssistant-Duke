63659b495861154d61b23bfb6dc870c1f6010bfa
Jefferson111 <e0309385@u.nus.edu>
2019-10-25 22:54:06 +0800

    Placeholder edit method
    Need to rework on model

--- src/main/java/duke/logic/commands/EditCommand.java
+++ src/main/java/duke/logic/commands/EditCommand.java
@@ -1,4 +1,31 @@
 package duke.logic.commands;
 
-public class EditCommand {
+import duke.logic.commands.results.CommandResultText;
+import duke.model.Event;
+import duke.model.Model;
+import duke.model.lists.EventList;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EditCommand extends Command {
+    private static final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private EventList events;
+
+    public EditCommand(EventList events) {
+        this.events = events;
+    }
+
+    /**
+     * Executes this command on the given task list and user interface.
+     *
+     * @param model The model object containing information about the user.
+     */
+    @Override
+    public CommandResultText execute(Model model) {
+        for (Event e : events) {
+            logger.log(Level.INFO, e.toString());
+        }
+        return new CommandResultText(model.getTasks());
+    }
 }
