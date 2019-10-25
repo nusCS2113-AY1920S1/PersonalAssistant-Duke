a5970711c14fabb6be4703db0565a1a9d2b2d9d7
JeremyKwok <jeremykke@gmail.com>
2019-10-26 01:50:18 +0800

    PatientFindCommand: new find command in patient context

--- src/main/java/duke/command/PatientFindCommand.java
+++ src/main/java/duke/command/PatientFindCommand.java
@@ -1,7 +1,12 @@
 package duke.command;
 
 import duke.DukeCore;
+import duke.data.DukeObject;
+import duke.data.Impression;
+import duke.data.Patient;
 import duke.exception.DukeException;
+
+import java.util.ArrayList;
 
 public class PatientFindCommand extends ArgCommand {
 
@@ -12,10 +17,34 @@ public class PatientFindCommand extends ArgCommand {
 
     @Override
     public void execute(DukeCore core) throws DukeException {
-        // TODO: change code to find DukeObjects
-        String findStr = "Here are the tasks that contain '" + getArg() + "':";
-        findStr = findStr + core.taskList.find(getArg()).replace(System.lineSeparator(),
-                System.lineSeparator() + "  ");
-        core.ui.print(findStr);
+        super.execute(core);
+        String searchTerm = getArg();
+        String findStr = "Here are the objects that contain '" + getArg() + "':\n";
+        Patient patient = (Patient) core.uiContext.getObject();
+        ArrayList<DukeObject> searchResult = new ArrayList<>();
+        ArrayList<Impression> impressionResult = new ArrayList<>();
+        if (getSwitchVals().isEmpty()) {
+            searchResult = patient.find(searchTerm);
+        } else {
+            impressionResult = patient.findImpression(searchTerm);
+            for (Impression imp : impressionResult) {
+                if (getSwitchVals().containsKey("impression")) {
+                    searchResult.add(imp);
+                }
+                if (getSwitchVals().containsKey("evidence")) {
+                    searchResult.addAll(imp.findEvidence(searchTerm));
+                }
+                if (getSwitchVals().containsKey("treatment")) {
+                    searchResult.addAll(imp.findEvidence(searchTerm));
+                }
+            }
+        }
+
+        String information = "";
+
+        for (int i = 0; i < searchResult.size(); i++) {
+            information += (i + 1) + ". " + searchResult.get(i).getName() + "\n";
+        }
+        core.ui.print(findStr + information);
     }
 }
