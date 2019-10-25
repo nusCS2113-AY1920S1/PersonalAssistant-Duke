723cbb93559a7423850e4444fe2678ec80727397
Jefferson111 <e0309385@u.nus.edu>
2019-10-25 22:55:40 +0800

    Add prompt parser

--- src/main/java/duke/logic/parsers/PromptParser.java
+++ src/main/java/duke/logic/parsers/PromptParser.java
@@ -1,4 +1,10 @@
 package duke.logic.parsers;
 
+import duke.logic.commands.Command;
+import duke.logic.commands.PromptCommand;
+
 public class PromptParser {
+    public static Command parseCommand(String prompt) {
+        return new PromptCommand(prompt);
+    }
 }
