1a9442c289b1af0b6603c7aff7c173c1e6ac94d7
Jefferson111 <e0309385@u.nus.edu>
2019-10-25 22:53:42 +0800

    Add EditorCommand; turns on editor

--- src/main/java/duke/logic/commands/EditorCommand.java
+++ src/main/java/duke/logic/commands/EditorCommand.java
@@ -1,4 +1,20 @@
 package duke.logic.commands;
 
-public class EditorCommand {
+import duke.commons.exceptions.EmptyVenueException;
+import duke.logic.EditorManager;
+import duke.logic.commands.results.CommandResultText;
+import duke.model.Model;
+
+public class EditorCommand extends Command {
+    private static final String MESSAGE_EDITOR = "Editor mode is turned on.";
+    /**
+     * Executes this command on the given task list and user interface.
+     *
+     * @param model The model object containing information about the user.
+     */
+    @Override
+    public CommandResultText execute(Model model) throws EmptyVenueException {
+        EditorManager.activate(model.getEventList(), model.getEventVenues());
+        return new CommandResultText(MESSAGE_EDITOR);
+    }
 }
