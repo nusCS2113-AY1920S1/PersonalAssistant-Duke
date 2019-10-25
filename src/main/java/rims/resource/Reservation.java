b02558d3a31ceffd642aeb4ff371602d128be832
rabhijit <abhijit.ravichandran@gmail.com>
2019-10-25 12:27:27 +0800

    major refactoring of code; add, delete, list, loan, reserve, return re-implemented

--- src/main/java/rims/resource/Reservation.java
+++ src/main/java/rims/resource/Reservation.java
@@ -6,135 +6,95 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Reservation {
-    private int reservation_id;
-    private int user_id;
-    private int resource_id;
-    private Date date_from;
-    private Date date_until;
+    private int reservationId;
+    private int userId;
+    private int resourceId;
+    private Date dateFrom;
+    private Date dateTill;
 
     /**
      * This builds an reservation from given user input
      * 
-     * @param reservation_id
-     * @param resource_id
-     * @param user_id
+     * @param reservationId
+     * @param resourceId
+     * @param userId
      * @param from
      * @param until
      * @throws ParseException
      */
-    public Reservation(int reservation_id, int resource_id, int user_id, String from, String until) throws ParseException {
-        this.reservation_id = reservation_id;
-        this.resource_id = resource_id;
-        this.user_id = user_id;
-        this.date_from = stringToDate(from);
-        this.date_until = stringToDate(until);
+
+    // for creating brand new Reservation
+    public Reservation(int reservationId, int resourceId, int userId, Date dateFrom, Date dateTill) throws ParseException {
+        this.reservationId = reservationId;
+        this.resourceId = resourceId;
+        this.userId = userId;
+        this.dateFrom = dateFrom;
+        this.dateTill = dateTill;
     }
 
-    /**
-     * This overloaded constructor is built for data retrieval, where all data are
-     * in the form of strings. The string parameters are later type castedd into
-     * suitable data types like integer.
-     */
-    public Reservation(String reservation_id, String resource_id, String user_id, String from, String until)
-            throws ParseException {
-        this.reservation_id = Integer.parseInt(reservation_id);
-        this.resource_id = Integer.parseInt(resource_id);
-        this.user_id = Integer.parseInt(user_id);
-        this.date_from = stringToDate(from);
-        this.date_until = stringToDate(until);
+    // for loading from Storage
+    public Reservation(int reservationId, int resourceId, int userId, String dateFrom, String dateTill) throws ParseException {
+        this.reservationId = reservationId;
+        this.resourceId = resourceId;
+        this.userId = userId;
+        this.dateFrom = stringToDate(dateFrom);
+        this.dateTill = stringToDate(dateTill);
     }
 
-    /**
-     * get method to return the user id who made this reservation
-     * 
-     * @return this.user_id
-     */
-    public int getUid() {
-        return this.user_id;
-    }
-
-    /**
-     * get method to return the user id who made this reservation
-     * 
-     * @return this.user_id
-     */
     public int getReservationId() {
-        return this.reservation_id;
+        return reservationId;
     }
 
-    /**
-     * get method to return the user id who made this reservation
-     * 
-     * @return this.user_id
-     */
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
     public Date getStartDate() {
-        return this.date_from;
+        return dateFrom;
     }
 
-    /**
-     * get method to return the user id who made this reservation
-     * 
-     * @return this.user_id
-     */
     public Date getEndDate() {
-        return this.date_until;
+        return dateTill;
     }
 
-    /**
-     * This method returns the current reserve detail, formatted in a user friendly
-     * string
-     * 
-     * @return String
-     */
-    public String toString() {
-        String s;
-        s = "Reservation ID[" + reservation_id + "]" + " Borrowed By User: " + user_id + "\n" + "\n\tFrom " + date_from
-                + " to " + date_until + '\n';
-        return s;
-    }
-
-    /**
-     * This toString method formats the string output slightly differently for
-     * better UI.
-     * 
-     * @return String
-     */
-    public String toString(String input) {
-        String s = "";
-        if (input.equals("return")) {
-            s = "Resource ID[" + resource_id + "] From " + date_from + " to " + date_until + '\n';
+    public boolean isStillValid() {
+        Date currentDate = new Date(System.currentTimeMillis());
+        if (currentDate.after(dateFrom) && currentDate.after(dateTill)) {
+            return false;
         }
-        return s;
+        return true;
     }
 
-    /**
-     * This method returns the current reserve detail, formatted in a string usable
-     * by storage class
-     * 
-     * @return String
-     */
-    public String toDataString() {
-        String s;
-        s = reservation_id + "," + resource_id + "," + user_id + "," + dateToString(date_from) + ","
-                + dateToString(date_until);
-        return s;
+    // to change
+    public String toString() {
+        return "[" + reservationId + "]" + " borrowed by user: " + userId + " from " + getDateToPrint(dateFrom) + " till " + getDateToPrint(dateTill);
     }
 
-    /**
-     * This method casts strings to date, used for data retreival from txt file.
-     */
+    public String toDataFormat() {
+        return reservationId + "," + resourceId + "," + userId + "," + dateToString(dateFrom) + "," + dateToString(dateTill);
+    }
+
     public Date stringToDate(String stringDate) throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HHmm");
         Date dateValue = formatter.parse(stringDate);
         return dateValue;
     }
 
-    /**
-     * This method casts dates to string
-     */
     public String dateToString(Date thisDate) {
         DateFormat format = new SimpleDateFormat("dd/MM/yyyy HHmm");
         String stringDate = format.format(thisDate);
+        return stringDate;
+    }
+
+    public String getDateToPrint(Date date) {
+        DateFormat dayFormat = new SimpleDateFormat("d");
+        int day = Integer.parseInt(dayFormat.format(date)) % 10;
+        String suffix = day == 1 ? "st" : (day == 2 ? "nd" : (day == 3 ? "rd" : "th"));
+        String stringDate = (new SimpleDateFormat("EEEEE, ")).format(date) + (dayFormat.format(date)) + suffix + " of " + (new SimpleDateFormat("MMMMM yyyy, hh:mm aaa")).format(date);
         return stringDate;
     }
 }
