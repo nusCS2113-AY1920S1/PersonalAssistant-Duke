c94baf888d44f752766c5aeee04771b694723022
John Khoo <john_khoo@u.nus.edu>
2019-10-27 00:30:33 +0800

    Convert back to normal command

--- src/main/java/duke/command/ImpressionNewCommand.java
+++ src/main/java/duke/command/ImpressionNewCommand.java
@@ -91,4 +91,6 @@ public class ImpressionNewCommand extends DukeDataCommand {
             }
         }
     }
+
+
 }
--- src/main/java/duke/command/UpCommand.java
+++ src/main/java/duke/command/UpCommand.java
@@ -3,15 +3,10 @@ package duke.command;
 import duke.DukeCore;
 import duke.exception.DukeException;
 
-public class UpCommand extends ArgCommand {
-    @Override
-    protected ArgSpec getSpec() {
-        return UpSpec.getSpec();
-    }
+public class UpCommand extends Command {
 
     @Override
     public void execute(DukeCore core) throws DukeException {
-        super.execute(core);
         core.uiContext.moveUpOneContext();
     }
 }
