5f9b6a95e5f0c7eebcbf96047a162e4c267096eb
SholihinK <sholihin.b.k@gmail.com>
2019-10-26 15:40:30 +0800

    Added Authorship

--- src/test/java/compal/logic/command/ViewCommandTest.java
+++ src/test/java/compal/logic/command/ViewCommandTest.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-
+//@@author SholihinK
 class ViewCommandTest {
 
     private ArrayList<Task> taskArrListMain = new ArrayList<>();
