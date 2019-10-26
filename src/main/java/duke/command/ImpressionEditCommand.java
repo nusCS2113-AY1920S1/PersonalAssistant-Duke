ccd4d6c19339d5f840a6c9755b15e905666f1088
John Khoo <john_khoo@u.nus.edu>
2019-10-27 03:21:49 +0800

    write code to find specific file in edit command

--- src/main/java/duke/command/ImpressionEditCommand.java
+++ src/main/java/duke/command/ImpressionEditCommand.java
@@ -1,14 +1,18 @@
 package duke.command;
 
 import duke.DukeCore;
+import duke.data.DukeData;
 import duke.data.Impression;
 import duke.data.Investigation;
+import duke.data.Medicine;
 import duke.data.Observation;
 import duke.data.Plan;
 import duke.data.Result;
 import duke.exception.DukeException;
 import duke.exception.DukeHelpException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ImpressionEditCommand extends DukeDataCommand {
@@ -22,6 +26,7 @@ public class ImpressionEditCommand extends DukeDataCommand {
     public void execute(DukeCore core) throws DukeException {
         super.execute(core);
         String editType = uniqueDataType();
+        DukeData editData;
         boolean isAppending = false;
         if (isSwitchSet("append")) {
             isAppending = true;
@@ -34,8 +39,35 @@ public class ImpressionEditCommand extends DukeDataCommand {
                 throw new DukeHelpException("Descriptions are only for impressions!", this);
             }
 
-            // find DukeData
-            // ...
+            // find DukeData of editType
+            // TODO: proper search with disambiguation
+            // TODO: select by index
+
+            String editDataName = getSwitchVal(editType);
+            List<DukeData> searchResults = getImpression(core).find(editDataName);
+            switch (editType) {
+            case "plan":
+                editData = findDataOfClass(searchResults, Plan.class);
+                break;
+            case "medicine":
+                editData = findDataOfClass(searchResults, Medicine.class);
+                break;
+            case "investigation":
+                editData = findDataOfClass(searchResults, Investigation.class);
+                break;
+            case "result":
+                editData = findDataOfClass(searchResults, Result.class);
+                break;
+            case "observation":
+                editData = findDataOfClass(searchResults, Observation.class);
+                break;
+            default:
+                throw new DukeException("Invalid data type!");
+            }
+
+            if (editData == null) {
+                throw new DukeHelpException("I can't find a " + editType + " item named '" + editDataName + "'!", this);
+            }
 
             // process each switch entered
             for (Map.Entry<String, String> entry : getSwitchVals().entrySet()) {
@@ -49,6 +81,7 @@ public class ImpressionEditCommand extends DukeDataCommand {
                     continue;
                 }
 
+                // TODO
                 switch (editType) {
                 case "plan":
                 case "medicine":
@@ -71,5 +104,15 @@ public class ImpressionEditCommand extends DukeDataCommand {
         if (newDesc != null) {
             impression.setDescription((isAppending) ? impression.getDescription() + newDesc : newDesc);
         }
+    }
+
+    // this is a terrible method, kill it as soon as possible
+    private DukeData findDataOfClass(List<DukeData> searchResults, Class dataClass) {
+        for (DukeData result : searchResults) {
+            if (result.getClass() == dataClass) {
+                return result;
+            }
+        }
+        return null;
     }
 }
