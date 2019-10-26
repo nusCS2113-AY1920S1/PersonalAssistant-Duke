965afd2bd08e88a852d0a9df664b47e395f46ca0
Hashir Zahir <hashirsahad@gmail.com>
2019-10-27 02:46:09 +0800

    Remove unused functions and code

--- src/main/java/duke/commons/fileIO/FilePaths.java
+++ src/main/java/duke/commons/fileIO/FilePaths.java
@@ -1,28 +1,15 @@
 package duke.commons.fileIO;
-
 import com.google.gson.Gson;
-import com.google.gson.annotations.SerializedName;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.BufferedReader;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 
 /**
- * This class file defines all the filepaths that will be used in the storage component.
+ * This class file defines all the resource config file path helper functions that
+ * will be used in the storage component.
  */
 public class FilePaths {
-//    public static final String sep = System.getProperty("file.separator");
-//    public static final File DATA_FILE = new File("src" + sep + "main" + sep + "java" + sep + "duke"
-//            + sep + "Data" + sep + "duke.txt");
-//    public static final File DEFAULTS_FILE = new File("src" + sep + "main" + sep + "java" + sep + "duke"
-//            + sep + "Data" + sep + "defaultItems.txt");
-//    public static final File GOAL_FILE = new File("src" + sep + "main" + sep + "java" + sep + "duke"
-//            + sep + "Data" + sep + "goal.txt");
-//    public static final File USER_FILE = new File("src" + sep + "main" + sep + "java" + sep + "duke"
-//            + sep + "Data" + sep + "user.txt");
-//    public static final File AUTOCORRECT_FILE = new File("src" + sep + "main" + sep + "java" + sep + "duke"
-//            + sep + "Data" + sep + "word.txt");
 
     private static String filePathsMasterConfigStr = "duke/Data/filepaths.json";
     private static Gson gson = new Gson();
@@ -41,10 +28,6 @@ public class FilePaths {
     public FilePaths() {
         filePathsMasterConfigStr = FileUtil.getSystemFilePathStr(filePathsMasterConfigStr);
         setFilePathsConfigMap(FileUtil.readResourceFile(filePathsMasterConfigStr));
-    }
-
-    public static String getFilePathsMasterConfigStr() {
-        return filePathsMasterConfigStr;
     }
 
     public static void setFilePathsConfigMap(BufferedReader bufferedReader) {
