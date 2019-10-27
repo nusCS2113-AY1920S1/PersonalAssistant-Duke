36667a2733b32027f20f7b59b85017658d98cac2
JeremyKwok <jeremykke@gmail.com>
2019-10-27 14:00:01 +0800

    HomeFindCommand: initial command

---
+++ src/main/java/duke/command/HomeFindCommand.java
@@ -0,0 +1,62 @@
+package duke.command;
+
+import duke.DukeCore;
+import duke.data.DukeObject;
+import duke.data.Impression;
+import duke.data.Patient;
+import duke.exception.DukeException;
+
+import java.util.ArrayList;
+
+public class HomeFindCommand extends ArgCommand {
+
+    @Override
+    protected ArgSpec getSpec() {
+        return HomeFindSpec.getSpec();
+    }
+
+    @Override
+    public void execute(DukeCore core) throws DukeException {
+        super.execute(core);
+        String searchTerm = getArg();
+        String findStr = "Here are the objects that contain '" + getArg() + "':\n";
+        ArrayList<DukeObject> searchResult = new ArrayList<>();
+        if (getSwitchVals().isEmpty()) {
+            searchResult = core.patientMap.find(searchTerm);
+        } else {
+            ArrayList<Patient> filteredPatients = core.patientMap.findPatient(searchTerm);
+            for (Patient patient : filteredPatients) {
+                if (getSwitchVals().containsKey("patient")) {
+                    searchResult.add(patient);
+                }
+                ArrayList<Impression> impressionResult = patient.findImpression(searchTerm);
+                for (Impression imp : impressionResult) {
+                    if (getSwitchVals().containsKey("impression")) {
+                        searchResult.add(imp);
+                    }
+                    if (getSwitchVals().containsKey("evidence")) {
+                        searchResult.addAll(imp.findEvidence(searchTerm));
+                    }
+                    if (getSwitchVals().containsKey("treatment")) {
+                        searchResult.addAll(imp.findEvidence(searchTerm));
+                    }
+                }
+            }
+        }
+
+        StringBuilder information = new StringBuilder("");
+
+        for (int i = 0; i < searchResult.size(); i++) {
+            information.append(i + 1).append(". ");
+            DukeObject item = searchResult.get(i);
+            if (item.getParent() != null) {
+                if (item.getParent().getParent() != null) {
+                    information.append(item.getParent().getParent().getName() + " - ");
+                }
+                information.append(item.getParent().getName() + " - ");
+            }
+            information.append(searchResult.get(i).getName()).append(System.lineSeparator());
+        }
+        core.ui.print(findStr + information.toString());
+    }
+}
