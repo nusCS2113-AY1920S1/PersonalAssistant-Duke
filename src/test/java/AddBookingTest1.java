63c91817d58c26ebd6fea382056170e7b870a181
Alex-Teo <alex-teo@comp.nus.edu.sg>
2019-10-28 00:24:45 +0800

    Add test cases

--- src/test/java/AddBookingTest1.java
+++ src/test/java/AddBookingTest1.java
@@ -4,16 +4,16 @@ import user.User;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AddBookingTest {
+public class AddBookingTest1 {
     @Test
     void testAddBooking() {
         String user = "Bob";
         String room = "room4";
         String description = " study";
         String dateTimeStart = "22/12/2019 1100";
-        String timeEnd = "22/12/2019 1200";
-        User temp = new User("dummy");
+        String timeEnd = "1200";
+        //User temp = new User("dummy");
         Booking newBooking = new Booking(user, room, description, dateTimeStart, timeEnd);
-        assertEquals(newBooking.toString(), "Bob room4 22/12/2019 1100 to 22/12/2019 1200 P");
+        assertEquals(newBooking.toString(), "Bob room4 22/12/2019 1100 to 1200 P");
     }
 }
