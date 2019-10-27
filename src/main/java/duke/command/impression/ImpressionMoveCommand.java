7c07bb2b9e11596baca378b2c001c5c380c6adcd
John Khoo <john_khoo@u.nus.edu>
2019-10-28 03:58:16 +0800

    Create ImpressionMove command and spec

---
+++ src/main/java/duke/command/impression/ImpressionMoveCommand.java
@@ -0,0 +1,30 @@
+package duke.command.impression;
+
+import duke.DukeCore;
+import duke.command.ArgSpec;
+import duke.exception.DukeException;
+import duke.exception.DukeHelpException;
+
+public class ImpressionMoveCommand extends ImpressionCommand {
+
+    @Override
+    protected ArgSpec getSpec() {
+        return ImpressionMoveSpec.getSpec();
+    }
+
+    @Override
+    public void execute(DukeCore core) throws DukeException {
+        // TODO: query user for correct impression if no impression is given
+        String evArg = getSwitchVal("evidence");
+        String treatArg = getSwitchVal("treatment");
+        if (getArg() != null && evArg == null && treatArg == null) {
+
+        } else if (getArg() == null && evArg != null && treatArg == null) {
+
+        } else if (getArg() == null && evArg == null && treatArg != null) {
+
+        } else {
+            throw new DukeHelpException("I don't know what you want me to look for!", this);
+        }
+    }
+}
