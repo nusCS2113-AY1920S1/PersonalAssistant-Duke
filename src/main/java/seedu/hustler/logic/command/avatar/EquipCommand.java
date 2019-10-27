079c553a4497c37ebb4deb42db954da6f43d2a27
Jing Kang <ngjingkang97@gmail.com>
2019-10-27 18:14:17 +0800

    save

--- src/main/java/seedu/hustler/logic/command/avatar/EquipCommand.java
+++ src/main/java/seedu/hustler/logic/command/avatar/EquipCommand.java
@@ -1,21 +1,32 @@
-package seedu.hustler.command.avatar;
+package seedu.hustler.logic.command.avatar;
 
 import seedu.hustler.Hustler;
+import seedu.hustler.logic.CommandLineException;
 import seedu.hustler.logic.command.Command;
-import seedu.hustler.data.AvatarStorage;
+import seedu.hustler.logic.parser.anomaly.InventoryAnomaly;
+import seedu.hustler.ui.Ui;
 
 import java.io.IOException;
 
 public class EquipCommand extends Command {
 
-    private final int index;
+    private final String[] index;
+    private InventoryAnomaly inventoryAnomaly = new InventoryAnomaly();
 
-    public EquipCommand(int index) {
+    public EquipCommand(String[] index) {
         this.index = index;
     }
 
     @Override
     public void execute() throws IOException {
-        Hustler.inventory.getToEquip(index - 1);
+        Ui ui = new Ui();
+        int parsedInt = Integer.parseInt(index[1]);
+        try {
+            inventoryAnomaly.detect(this.index);
+        } catch (CommandLineException e) {
+            ui.show_message(e.getMessage());
+        }
+
+        Hustler.inventory.getToEquip(parsedInt - 1);
     }
 }
