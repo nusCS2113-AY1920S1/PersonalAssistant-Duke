201ee275906ec279020eeed8a3443ec83512455d
Nathan <nystera@gmail.com>
2019-10-28 02:42:56 +0800

    +Fixed InventoryAnomaly to handle empty parser handling. +Changed messages to have clearer prompts.

--- src/main/java/seedu/hustler/logic/parser/anomaly/InventoryAnomaly.java
+++ src/main/java/seedu/hustler/logic/parser/anomaly/InventoryAnomaly.java
@@ -5,17 +5,24 @@ import seedu.hustler.logic.CommandLineException;
 
 public class InventoryAnomaly extends DetectAnomaly{
 
+    private final String NO_INDEX_MESSAGE = "Please do not leave the index field empty. Retype \"/equip <index>\" instead.";
+    private final String NOT_NUMBER_MESSAGE = "Please input a proper number. Retype \"equip <index>\" instead.";
+    private final String OUT_OF_BOUNDS_MESSAGE = "Please input an index that exists inside your inventory. Check your items by typing \"/inventory\".";
+
     @Override
     public void detect(String[] userInput) throws CommandLineException {
         int index = 0;
+        if (userInput.length == 1) {
+            throw new CommandLineException(NO_INDEX_MESSAGE);
+        }
         try {
             index = Integer.parseInt(userInput[1]);
             index--;
             if(index >= Hustler.inventory.getSize() || index < 0) {
-                throw new CommandLineException("Please input a index that exist inside the shop list!");
+                throw new CommandLineException(OUT_OF_BOUNDS_MESSAGE);
             }
         } catch (NumberFormatException e) {
-            throw new CommandLineException("Please input a number here!");
+            throw new CommandLineException(NOT_NUMBER_MESSAGE);
         }
     }
 }
