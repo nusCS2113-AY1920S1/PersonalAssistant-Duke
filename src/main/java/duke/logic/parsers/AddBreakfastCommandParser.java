d3047ca2e5981ab79a398225f2b61e289fdf3644
Hashir Zahir <hashirsahad@gmail.com>
2019-10-27 21:14:13 +0800

    Fix stray tabs in source file for checkstyle

--- src/main/java/duke/logic/parsers/AddBreakfastCommandParser.java
+++ src/main/java/duke/logic/parsers/AddBreakfastCommandParser.java
@@ -16,13 +16,13 @@ public class AddBreakfastCommandParser implements ParserInterface<AddCommand> {
      */
     @Override
     public AddCommand parse(String userInput) {
-	try {
-	    InputValidator.validate(userInput);
-	    String[] mealNameAndInfo = ArgumentSplitter.splitMealArguments(userInput);
-	    return new AddCommand(new Breakfast(mealNameAndInfo[0], mealNameAndInfo[1]),0);
-	} catch (DukeException e) {
-	    return new AddCommand(false,e.getMessage());
-	}
+        try {
+            InputValidator.validate(userInput);
+            String[] mealNameAndInfo = ArgumentSplitter.splitMealArguments(userInput);
+            return new AddCommand(new Breakfast(mealNameAndInfo[0], mealNameAndInfo[1]),0);
+        } catch (DukeException e) {
+            return new AddCommand(false,e.getMessage());
+        }
         //todo: handle trailing userInput without "/"
     }
 }
