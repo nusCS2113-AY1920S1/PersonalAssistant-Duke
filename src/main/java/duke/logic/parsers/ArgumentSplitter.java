3956a74a65a223534a3e5449482bd6be0170248f
Ivan Andika <ivanandika@u.nus.edu>
2019-10-27 12:09:02 +0800

    Add javadocs comments

--- src/main/java/duke/logic/parsers/ArgumentSplitter.java
+++ src/main/java/duke/logic/parsers/ArgumentSplitter.java
@@ -1,7 +1,15 @@
 package duke.logic.parsers;
 
+/**
+ * ArgumentSplitter is a public class that handles the splitting of command arguments.
+ */
 public class ArgumentSplitter {
 
+    /**
+     * split multiple meal related command arguments (nutritional values).
+     * @param userInput String input by user.
+     * @return <code>String[]{mealName, mealInfo}</code> Name and info of the meal.
+     */
     public static String[] splitMealArguments(String userInput) {
 
         String[] splitStrings = userInput.split("/", 2);
@@ -14,6 +22,12 @@ public class ArgumentSplitter {
         return new String[]{mealName, mealInfo};
     }
 
+    /**
+     * split command arguments based on specific delimiter
+     * @param userInput String input by user.
+     * @param delimiter the delimiter that the userInput will be split around.
+     * @return <code>String[] {splitStrings[0], splitStrings[1]}</code> Split arguments of the command.
+     */
     public static String[] splitArguments(String userInput, String delimiter) {
         String[] splitStrings = userInput.split(delimiter, 2);
         splitStrings[0] = splitStrings[0].trim();
