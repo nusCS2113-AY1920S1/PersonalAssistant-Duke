2a80555c8de92aa29df741c9b32444091907bcc3
JeremyKwok <jeremykke@gmail.com>
2019-10-29 01:12:28 +0800

    Find: Modify Find commands to use new search context

--- src/main/java/duke/command/impression/ImpressionFindCommand.java
+++ src/main/java/duke/command/impression/ImpressionFindCommand.java
@@ -4,9 +4,11 @@ package duke.command.impression;
 import duke.DukeCore;
 import duke.command.ArgCommand;
 import duke.command.ArgSpec;
-import duke.data.DukeData;
+import duke.data.DukeObject;
 import duke.data.Impression;
+import duke.data.SearchResult;
 import duke.exception.DukeException;
+import duke.ui.context.Context;
 
 import java.util.ArrayList;
 
@@ -22,10 +24,10 @@ public class ImpressionFindCommand extends ArgCommand {
         super.execute(core);
         String searchTerm = getArg();
         String findStr = "Here are the objects that contain '" + getArg() + "':\n";
-        ArrayList<DukeData> searchResult = new ArrayList<>();
+        ArrayList<DukeObject> searchResult = new ArrayList<>();
         Impression impression = (Impression) core.uiContext.getObject();
         if (getSwitchVals().isEmpty()) {
-            searchResult = impression.find(searchTerm);
+            searchResult.addAll(impression.find(searchTerm));
         } else {
             if (getSwitchVals().containsKey("evidence")) {
                 searchResult.addAll(impression.findEvidences(searchTerm));
@@ -35,12 +37,15 @@ public class ImpressionFindCommand extends ArgCommand {
             }
         }
 
-        StringBuilder information = new StringBuilder("");
+        /*StringBuilder information = new StringBuilder("");
 
         for (int i = 0; i < searchResult.size(); i++) {
             information.append(i + 1).append(". ");
             information.append(searchResult.get(i).getName()).append(System.lineSeparator());
         }
-        core.ui.print(findStr + information.toString());
+        core.ui.print(findStr + information.toString());*/
+        SearchResult search = new SearchResult(searchTerm, searchResult, impression);
+        core.uiContext.setContext(Context.SEARCH, search);
+        core.ui.print("Returning result of search of " + searchTerm);
     }
 }
