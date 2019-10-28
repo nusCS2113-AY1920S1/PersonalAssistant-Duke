927bfb34495ddbc6891cdd5d58855c1c308a54b5
Hashir Zahir <hashirsahad@gmail.com>
2019-10-28 18:55:10 +0800

    Add generic argument splitter for slash separated arguments for suggest command

--- src/main/java/duke/logic/commands/SuggestCommand.java
+++ src/main/java/duke/logic/commands/SuggestCommand.java
@@ -1,17 +1,38 @@
 package duke.logic.commands;
 
-import duke.commons.exceptions.DukeException;
 import duke.logic.suggestion.MealSuggestionAnalytics;
 import duke.model.meal.MealList;
-import duke.model.wallet.TransactionList;
 import duke.model.user.User;
 import duke.model.wallet.Wallet;
 import duke.storage.Storage;
-import duke.ui.Ui;
+import java.util.Calendar;
 
-import java.util.Scanner;
 
+/**
+ * Class to handle suggestion command arguments from the user and pass them to the analytic module.
+ */
 public class SuggestCommand extends Command {
+
+    private int maxMealsToSuggest;
+
+    // TODO: Support for meal types (eg: lunch, dinner) from user so that only relevant meals suggested.
+
+    /**
+     * Constructor of suggestion command.
+     * @param suggestionDate Date on which meal suggestion is required.
+     * @param maxMealsToSuggest Maximum number of suggested meals to be shown to the user.
+     */
+    public SuggestCommand(Calendar suggestionDate, int maxMealsToSuggest) {
+        this.calendarDate = suggestionDate;
+        this.maxMealsToSuggest = maxMealsToSuggest;
+    }
+
+    // Constructor called when parser fails to parse arguments
+    public SuggestCommand(boolean flag, String messageStr) {
+        this.isFail = flag;
+        this.errorStr = messageStr;
+    }
+
     @Override
     public void execute(MealList meals, Storage storage, User user, Wallet wallet) {
         /*
@@ -19,9 +40,13 @@ public class SuggestCommand extends Command {
               user, the date provided and the user meal parameters provided to get the best meal
               suggestion.
         */
+        // Allow followup user action after meals are suggested.
+        // isDone = false;
         MealSuggestionAnalytics mealSuggestionAnalytics = new MealSuggestionAnalytics();
     }
 
+    @Override
     public void execute2(MealList meals, Storage storage, User user, Wallet wallet) {
+
     }
 }
--- src/main/java/duke/logic/parsers/SuggestCommandParser.java
+++ src/main/java/duke/logic/parsers/SuggestCommandParser.java
@@ -3,21 +3,48 @@ package duke.logic.parsers;
 import duke.commons.exceptions.DukeException;
 import duke.logic.commands.SuggestCommand;
 
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.HashMap;
+
 /**
  * Parser class to handle suggestion of meals.
  */
 public class SuggestCommandParser implements ParserInterface<SuggestCommand> {
 
+    private static final String dateArgStr = "date";
+    private static final String displayArgStr = "display";
+    private int maxMealsToSuggest = 5;
+
     /**
      * Parse user input and return SuggestCommand.
-     * @param userInput String input by user
-     * @return <code>SuggestCommand</code> to be updated
+     * @param userInputStr String input by user
+     * @return <code>SuggestCommand</code> Command object that will process meal suggestions.
      * @throws DukeException If the userInput cannot be parsed
      */
     @Override
-    public SuggestCommand parse(String userInput) throws DukeException {
-        InputValidator.validate(userInput);
-        // TODO: Finalize suggest command input format and update UG/DG
-        return new SuggestCommand();
+    public SuggestCommand parse(String userInputStr) throws DukeException {
+        InputValidator.validate(userInputStr);
+        HashMap<String, String> argumentsMap = ArgumentSplitter.splitForwardSlashArguments(userInputStr);
+        Calendar suggestionDate = Calendar.getInstance();
+        if (argumentsMap.containsKey("date")) {
+            try {
+                suggestionDate.setTime(dateFormat.parse(argumentsMap.get(dateArgStr)));
+            } catch (ParseException e) {
+                return new SuggestCommand(true, "Unable to parse" + dateArgStr + " as a date. " +
+                        "Please follow DD/MM/YYYY format.");
+            }
+        }
+
+        if (argumentsMap.containsKey(displayArgStr)) {
+            try {
+                maxMealsToSuggest = Integer.parseInt(argumentsMap.get(displayArgStr));
+            } catch (NumberFormatException e) {
+                return new SuggestCommand(true, "Unable to parse display as integer. Please " +
+                        "input as integer. eg: /display 5");
+            }
+        }
+
+        return new SuggestCommand(suggestionDate, maxMealsToSuggest);
     }
 }
