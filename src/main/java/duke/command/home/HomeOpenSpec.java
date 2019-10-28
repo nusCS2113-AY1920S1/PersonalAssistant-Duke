43babc234c10ba8616711dca4d0593281c2731e5
gowgos5 <gowgos5@gmail.com>
2019-10-28 12:18:04 +0800

    Added indexing support for open command in Home context

--- src/main/java/duke/command/home/HomeOpenSpec.java
+++ src/main/java/duke/command/home/HomeOpenSpec.java
@@ -15,7 +15,8 @@ public class HomeOpenSpec extends ArgSpec {
         emptyArgMsg = "You didn't tell me anything about the patient!";
         cmdArgLevel = ArgLevel.NONE;
         initSwitches(
-                new Switch("bed", String.class, false, ArgLevel.REQUIRED, "b")
+                new Switch("bed", String.class, true, ArgLevel.REQUIRED, "b"),
+                new Switch("index", Integer.class, true, ArgLevel.REQUIRED, "i")
         );
     }
 }
