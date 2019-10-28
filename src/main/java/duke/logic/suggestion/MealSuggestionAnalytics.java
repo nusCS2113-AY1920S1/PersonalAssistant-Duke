753eb62396b48de8b7f1dae3c472fb684330f60c
Hashir Zahir <hashirsahad@gmail.com>
2019-10-29 02:27:23 +0800

    Update analytics constructor and add sorting of suggested meals for ranked meal preference

--- src/main/java/duke/logic/suggestion/MealSuggestionAnalytics.java
+++ src/main/java/duke/logic/suggestion/MealSuggestionAnalytics.java
@@ -3,8 +3,11 @@ package duke.logic.suggestion;
 import duke.model.meal.Meal;
 import duke.model.meal.MealList;
 
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * This class handles all the data analytics aspect of the meal suggestion feature.
@@ -13,13 +16,33 @@ import java.util.Calendar;
  */
 public class MealSuggestionAnalytics {
 
+    private HashMap<String, HashMap<String, Integer>> defaultMealSuggestionList;
+
     public MealSuggestionAnalytics() {
 
     }
 
-    public ArrayList<Meal> getMealSuggestions(ArrayList<Meal> defaultMealList, MealList mealList,
-                                              Calendar suggestionDate, int maxMealsToSuggest) {
-        // TODO: Add logic to return filtered and sorted suggested meals
-        return defaultMealList;
+    public ArrayList<Meal> getMealSuggestions(MealList meals, Calendar suggestionDate, int maxMealsToSuggest) {
+        defaultMealSuggestionList = meals.getStoredList();
+        return new ArrayList<Meal>();
+        // return getMealListWithinCalories(calorieLimit);
+    }
+
+    private ArrayList<Meal> getMealListWithinCalories(int calorieLimit) {
+        ArrayList<Meal> suggestedMealList = new ArrayList<>();
+        for (String mealNameStr : defaultMealSuggestionList.keySet()) {
+            HashMap<String, Integer> mealDetailsMap = defaultMealSuggestionList.get(mealNameStr);
+            if (mealDetailsMap.containsKey("calorie") && mealDetailsMap.get("calorie") < calorieLimit) {
+                //suggestedMealList.add(meal);
+            }
+        }
+
+        sortMealList(suggestedMealList, "calorie");
+
+        return suggestedMealList;
+    }
+
+    private void sortMealList(ArrayList<Meal> mealList, String parameterStr) {
+        Collections.sort(mealList, Comparator.comparing(meal -> meal.getNutritionalValue().get(parameterStr)));
     }
 }
