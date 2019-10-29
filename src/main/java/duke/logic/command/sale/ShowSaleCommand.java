318d9aeee585e85c719d1a5e3cc3dc13360247c2
chekjun <limchekjun@gmail.com>
2019-10-29 22:55:11 +0800

    Add show sale command.

---
+++ src/main/java/duke/logic/command/sale/ShowSaleCommand.java
@@ -0,0 +1,22 @@
+package duke.logic.command.sale;
+
+import duke.logic.command.CommandResult;
+import duke.logic.command.exceptions.CommandException;
+import duke.model.Model;
+
+public class ShowSaleCommand extends SaleCommand {
+    public static final String COMMAND_WORD = "";
+    private static final String MESSAGE_LIST_SUCCESS = "Showing all sales.";
+
+    /**
+     * Creates a ShowOrderCommand that displays all Sales.
+     */
+    public ShowSaleCommand() {
+
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return new CommandResult(String.format(MESSAGE_LIST_SUCCESS), CommandResult.DisplayedPage.SALE);
+    }
+}
