770c0048bbe5e01cfa783d39c0d37e90f5bb7f1b
jasonchanwq <jasonchanwq@gmail.com>
2019-10-29 16:46:46 +0800

    Add DeleteCommandTest

---
+++ src/test/java/command/DeleteCommandTest.java
@@ -0,0 +1,47 @@
+package command;
+
+import commands.DeleteCommand;
+import members.Member;
+import org.junit.jupiter.api.Test;
+import tasks.Task;
+import tasks.ToDo;
+import utils.DukeException;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class DeleteCommandTest {
+
+    //@@author: Jason
+    @Test
+    public void checkDelete() throws DukeException {
+
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(new ToDo("test"));
+        tasks.add(new ToDo("test2"));
+        tasks.remove(0);
+        assertEquals("test2", tasks.get(0).getDescription());
+    }
+
+    //@@author: Jason
+    @Test
+    public void execute_negativeIntegerInput_throwsDukeException() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(new ToDo("test"));
+        assertThrows(DukeException.class, () -> new DeleteCommand("-1")
+                .execute(tasks, new ArrayList<Member>(), null));
+    }
+
+    //@@author: Jason
+    @Test
+    public void execute_stringAfterIntegerInput_throwsDukeException() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(new ToDo("test"));
+        assertThrows(DukeException.class, () -> new DeleteCommand("1 string")
+                .execute(tasks, new ArrayList<Member>(), null));
+    }
+
+}
