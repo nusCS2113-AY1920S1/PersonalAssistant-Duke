2ad2d79df78522600adac77cfa6c2f43bce42f33
laurenthg <laurenthuang@hotmail.fr>
2019-10-30 02:08:45 +0800

    add edit to help command

--- src/main/java/leduc/Ui.java
+++ src/main/java/leduc/Ui.java
@@ -195,7 +195,12 @@ public class Ui {
         System.out.println("\t reschedule INDEX /at DATE - DATE : reschedule an event task");
         System.out.println("\t remind : remind the first three task");
         System.out.println("\t setwelcome WELCOME : customize the welcome message");
-        System.out.println("\t edit : edit a task (then, you have to follow the instructions)");
+        System.out.println("\t edit :\n\t\t For multi-step command : 'edit' and then follow the instructions" +
+                           "\n\t\t For one shot command:" +
+                           "\n\t\t\t edit the description: 'edit INDEX description DESCRIPTION' " +
+                           "\n\t\t\t edit the date of an homework task: 'edit INDEX /by DATE' " +
+                           "\n\t\t\t edit the period of an event task: 'edit INDEX /at DATE - DATE' " +
+                           "\n\t\t\t edit the priority: 'edit INDEX prio INDEX' ");
         System.out.println("\t show DATEOPTION DATE: show task by day/dayofweek/month/year ( day format is DD/MM/YYYY; " +
                 "dayofweek format is monday,tuesday...; month format is MM/YYYY; year format is YYYY)");
         System.out.println("\t prioritize INDEX prio INDEX : give priority to task");
