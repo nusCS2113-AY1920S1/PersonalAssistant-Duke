f79a94fde6bd061bf40cfa8576059543e4a63c13
John Khoo <john_khoo@u.nus.edu>
2019-10-29 12:16:38 +0800

    Setup Impression command tests

--- src/test/java/ImpressionCommandTest.java
+++ src/test/java/ImpressionCommandTest.java
@@ -1,17 +1,51 @@
 import duke.command.impression.ImpressionNewCommand;
+import duke.data.Impression;
+import duke.data.Medicine;
+import duke.data.Patient;
+import duke.exception.DukeException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ImpressionCommandTest extends CommandTest {
 
+    private Impression impression;
+    private Patient patient;
+
+    @BeforeEach
+    public void setupPatientAndImpression() {
+        patient = new Patient("name", "bedNo", "allergies", 0, 0,
+                0, 0, "", "");
+        impression = new Impression("name", "description", patient);
+        patient.addNewImpression(impression);
+        core.patientMap.addPatient(patient);
+    }
+
     @Test
-    public void ImpressionNewCommand_validCommand_correctDataCreated() {
-        //TODO test other commands
+    public void ImpressionNewCommand_fullCommand_correctDataCreated() {
+        //TODO test other DukeData
         ImpressionNewCommand cmd = new ImpressionNewCommand();
         Map<String, String> medVals = Map.ofEntries(
-                Map.entry("name", "test")
+                Map.entry("name", "test"),
+                Map.entry("priority", "2"),
+                Map.entry("status", "1"),
+                Map.entry("dose", "test dose"),
+                Map.entry("startDate", "today"),
+                Map.entry("duration", "next two weeks")
         );
         cmd.setSwitchValsMap(medVals);
+        Medicine testMed = new Medicine("test", impression, 2, 1, "test dose",
+                "today", "next two weeks");
+
+        try {
+            cmd.execute(core);
+            assertEquals(testMed, impression.getTreatment("test"));
+        } catch (DukeException excp) {
+            fail("Exception thrown while executing valid command!");
+        }
     }
 }
