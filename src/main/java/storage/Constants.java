0f90e47be9477436b95d8cbef8f69113ceb99a15
zkchang97 <e0309954@u.nus.edu>
2019-10-29 21:09:27 +0800

    Changed formatting to add rooms

--- src/main/java/storage/Constants.java
+++ src/main/java/storage/Constants.java
@@ -8,14 +8,12 @@ public class Constants {
     public static final String ROOMFILENAME = "data\\roomlist.txt";
     public static final int TYPE = 0;
     public static final int ISDONE = 1;
-    public static final int ISBOOKED = 0;
     public static final int DESCRIPTION = 2;
     public static final int ROOMCODE = 0;
     public static final int TIME = 3;
-    public static final int NEEDS = 3;
     public static final int TIMESTART = 3;
     public static final int TIMEEND = 4;
-    public static final int ROOMDATE = 1;
-    public static final int ROOMTIMESLOT = 2;
+    public static final int ROOMDATETIMESTART = 1;
+    public static final int ROOMDATETIMEEND = 2;
     public static final String UNHAPPY = "☹";
 }
