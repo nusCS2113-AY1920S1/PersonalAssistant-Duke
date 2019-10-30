58a48ad6001292aab839a6d1f1c66b572b36f2ab
Jy <piggy013130@yahoo.com.sg>
2019-10-30 20:59:20 +0800

    More progress on reminder system

--- src/main/java/logic/command/ReminderCommand.java
+++ src/main/java/logic/command/ReminderCommand.java
@@ -11,11 +11,12 @@ public class ReminderCommand extends Command {
 
     /**
      * Creates new ReminderCommand
-     * @param taskIndex index of task to be reminded about
-     * @param beforeInt numeric value of time before
+     *
+     * @param taskIndex  index of task to be reminded about
+     * @param beforeInt  numeric value of time before
      * @param beforeMult multiplier of time before, minutes, hours or days
      */
-    public ReminderCommand(int taskIndex, int beforeInt, char beforeMult)   {
+    public ReminderCommand(int taskIndex, int beforeInt, char beforeMult) {
         this.taskIndex = taskIndex;
         this.beforeInt = beforeInt;
         this.beforeMult = beforeMult;
@@ -26,6 +27,23 @@ public class ReminderCommand extends Command {
      */
     @Override
     public CommandOutput execute(Model model) {
-        return new CommandOutput("Reminder todo");
+        if (taskIndex > model.getTaskListSize()) {
+            return new CommandOutput("Please provide a valid task index");
+        } else if (model.getTasksManager().getTaskById(taskIndex - 1).getTime() == null) {
+            return new CommandOutput("Please set a time for the task first");
+        } else {
+            String feedback = "Alright! I'll remind you about:\n"
+                    + model.getTasksManager().getTaskById(taskIndex - 1) + "\n" + beforeInt;
+            if (beforeMult == 'm') {
+                feedback += " minutes ";
+            } else if (beforeMult == 'h') {
+                feedback += " hours ";
+            } else {
+                feedback += " days ";
+            }
+            feedback += "before it's due";
+            return new CommandOutput(feedback);
+        }
+
     }
 }
