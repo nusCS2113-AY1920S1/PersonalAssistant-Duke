d1aa1ced200a032761b0739e67dd633b0466c204
kuromono <clouderext@gmail.com>
2019-10-30 10:38:11 +0800

    Added some batchcommand javadocs

--- src/main/java/cube/logic/command/BatchCommand.java
+++ src/main/java/cube/logic/command/BatchCommand.java
@@ -1,7 +1,12 @@
+/**
+ * BatchCommand.java
+ * Support commands related to batch file imports & exports.
+ *
+ * @author kuromono
+ */
 package cube.logic.command;
 
 import cube.exception.CubeException;
-import cube.logic.command.exception.CommandException;
 import cube.logic.command.util.CommandResult;
 import cube.model.ModelManager;
 import cube.model.food.Food;
@@ -28,12 +33,23 @@ public class BatchCommand extends Command {
     private final String MESSAGE_EXPORT = "exported";
     private final String MESSAGE_IMPORT = "imported";
 
+    /**
+     * Default constructor.
+     * @param fileName Sets the filename of the CSV file to be loaded/saved from.
+     * @param operationType Specifies to either IMPORT or EXPORT operation.
+     */
     public BatchCommand(String fileName, String operationType) {
         this.fileName = fileName;
         this.operationType = OperationType.valueOf(operationType);
         this.batchUtil = new FileUtilCSV<>("data", fileName, new Food());
     }
 
+    /**
+     * Calls & updates the required functions for CSV batch import operations.
+     * @param model ModelManager object to update the FoodList object from the import.
+     * @param storage StorageManager object to update the FoodList object from the import.
+     * @throws CubeException Throws an exception if error occured during file handling.
+     */
     private void batchImport(ModelManager model, StorageManager storage) throws CubeException {
         ArrayList<Food> foodList = batchUtil.load();
         FoodList importedFoodList = new FoodList(foodList);
@@ -41,10 +57,18 @@ public class BatchCommand extends Command {
         storage.storeFoodList(importedFoodList);
     }
 
+    /**
+     * Calls & updates the required functions for CSV batch export operations.
+     * @param storage StorageManager object that contains the FoodList object to be saved.
+     * @throws CubeException Throws an exception if error occured during file handling.
+     */
     private void batchExport(StorageManager storage) throws CubeException {
         batchUtil.save(storage.getFoodList().getFoodList());
     }
 
+    /**
+     * Constructs the command result output to be shown to the user.
+     */
     @Override
     public CommandResult execute(ModelManager model, StorageManager storage) {
         try {
