47ef0333bcea35795825ccfe6a4f44c6c40d59e1
njw95 <e0176911@u.nus.edu>
2019-10-30 19:14:46 +0800

    Fix style

--- src/main/java/dictionary/SynonymBank.java
+++ src/main/java/dictionary/SynonymBank.java
@@ -22,7 +22,7 @@ public class SynonymBank {
         for (String synonym : synonyms) {
             if (synonymBank.containsKey(synonym)) {
                 synonymBank.get(synonym).add(wordDescription);
-            } else { //if not exist, make an entry for this current synonym, insert wordDescription to the HashSet<String>
+            } else {
                 synonymBank.put(synonym, new HashSet<>(Collections.singletonList(wordDescription)));
             }
         }
@@ -32,8 +32,8 @@ public class SynonymBank {
      * @param word word to be deleted
      */
     public void deleteWordAllSynonyms(Word word) {
-        for (String synonym : word.getSynonyms()) { //if word is drink , synonyms are beverage , alcohol
-            synonymBank.get(synonym).remove(word.getWordString()); //for each synonym beverage,alcohol etc, remove drink from their syno list
+        for (String synonym : word.getSynonyms()) {
+            synonymBank.get(synonym).remove(word.getWordString());
         }
     }
 
@@ -41,11 +41,10 @@ public class SynonymBank {
      * Deletes a word from some synonyms of that word.
      * @param deletedSynonyms list of synonyms to delete the word
      * @param deletedWord word to be deleted
-     * Using the previous example where drink has synonyms of beverage, alcohol and some other synonyms
      * We want to delete drink from beverage and alcohol only, leaving the rest stationary
      */
     public void deleteWordSomeSynonyms(ArrayList<String> deletedSynonyms, String deletedWord) {
-        for (String synonym : deletedSynonyms) { //deletedSynonyms contains beverage and alcohol
+        for (String synonym : deletedSynonyms) {
             synonymBank.get(synonym).remove(deletedWord); //look up these synonyms and remove the deletedWord
         }
     }
@@ -53,7 +52,6 @@ public class SynonymBank {
     /**
      * Adds a word to all synonyms in SynonymBank of that the word has.
      * @param word word to add synonym
-     * This feature basically cross adds, if A is a synonym of B, then B must be a synonym of A
      * Ensures that the synonymBank is updated
      */
     public void addWordAllSynonyms(Word word) {
--- src/main/java/dictionary/TagBank.java
+++ src/main/java/dictionary/TagBank.java
@@ -32,7 +32,7 @@ public class TagBank {
      * @param word word to be deleted
      */
     public void deleteWordAllTags(Word word) {
-        for (String tag : word.getTags()) { //say word is happy , tags are emotion, happiness,
+        for (String tag : word.getTags()) {
             tagBank.get(tag).remove(word.getWordString());
         }
     }
