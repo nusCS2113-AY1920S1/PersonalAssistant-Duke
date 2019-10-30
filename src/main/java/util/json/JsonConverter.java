ae58645450881b3e589839818719de4966e1613f
Lucria <jerryzhang28eclipse@gmail.com>
2019-10-31 00:51:48 +0800

    [Feat] Edited loading all projects

--- src/main/java/util/json/JsonConverter.java
+++ src/main/java/util/json/JsonConverter.java
@@ -8,14 +8,11 @@ import exceptions.DukeException;
 import models.project.Project;
 import util.log.DukeLogger;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class JsonConverter {
-    private String userDirectory = System.getProperty("user.dir") + "/savedProjects.json";
+    private String userDirectory = System.getProperty("user.dir");
 
     /**
      * Method that is responsible for saving Projects Data by using GSON library to convert to a human editable JSON
@@ -51,17 +48,19 @@ public class JsonConverter {
      */
     public ArrayList<Project> loadAllProjectsData() {
         Gson gson = new Gson();
-        ArrayList<Project> allProjects;
-        try (FileReader fileReader = new FileReader(userDirectory)) {
-            DukeLogger.logDebug(ConsoleInputController.class, "Loading saved file.");
-            allProjects = gson.fromJson(fileReader, new TypeToken<ArrayList<Project>>(){}.getType());
-            if (allProjects == null) {
-                return new ArrayList<>();
+        ArrayList<Project> allProjects = new ArrayList<>();
+        File directory = new File(userDirectory);
+        File[] allProjectJson = directory.listFiles((file, name) -> name.endsWith(".json"));
+        for (File projectJson : allProjectJson) {
+            try (FileReader fileReader = new FileReader(projectJson)) {
+                DukeLogger.logDebug(ConsoleInputController.class, "Loading saved file.");
+                Project newProject = gson.fromJson(fileReader, new TypeToken<Project>(){}.getType());
+                allProjects.add(newProject);
+                DukeLogger.logDebug(ConsoleInputController.class, "Saved file loaded.");
+            } catch (IOException err) {
+                DukeLogger.logError(ConsoleInputController.class, "Saved file not loaded");
+                return allProjects;
             }
-            DukeLogger.logDebug(ConsoleInputController.class, "Saved file loaded.");
-        } catch (IOException err) {
-            DukeLogger.logError(ConsoleInputController.class, "Saved file not loaded");
-            return new ArrayList<>();
         }
         return allProjects;
     }
