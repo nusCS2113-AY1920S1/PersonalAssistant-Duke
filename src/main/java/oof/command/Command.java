19e19aa197e4526568d6b60c0bd3b6dbadeedee1
jasperosy <jasper2g@gmail.com>
2019-10-31 15:30:10 +0800

    Update constant case

--- src/main/java/oof/command/Command.java
+++ src/main/java/oof/command/Command.java
@@ -23,7 +23,7 @@ import oof.model.task.Todo;
  */
 public abstract class Command {
 
-    protected static final int description_length_max = 20;
+    protected static final int DESCRIPTION_LENGTH_MAX = 20;
 
     /**
      * Invokes other Command subclasses based on the input given by the user.
@@ -44,7 +44,7 @@ public abstract class Command {
      * @return True if maximum description length is exceeded, false otherwise.
      */
     public boolean exceedsMaxLength(String description) {
-        return description.length() >= description_length_max;
+        return description.length() >= DESCRIPTION_LENGTH_MAX;
     }
 
     /**
