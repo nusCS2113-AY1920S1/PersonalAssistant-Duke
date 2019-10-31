777448fdb72ce192b8b1463b7fc2303ee7212bc1
yetong <e0310555@u.nus.edu>
2019-10-31 22:56:47 +0800

    fix style

--- src/main/java/dolla/parser/Parser.java
+++ src/main/java/dolla/parser/Parser.java
@@ -150,7 +150,7 @@ public abstract class Parser implements ParserStringList, ModeStringList {
      */
     protected boolean verifySort() {
         try {
-            if(inputArray[1] != null) {
+            if (inputArray[1] != null) {
                 return true;
             } else {
                 return false;
