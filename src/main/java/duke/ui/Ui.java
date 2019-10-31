8396278b7c4c9f0f29cd6a2e81681a7cd76909ae
Yu Jiahan <116010274@link.cuhk.edu.cn>
2019-11-01 01:11:42 +0800

    update Ui class

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -94,6 +94,14 @@ public class Ui {
         System.out.println("                     `-._______.-                                      ");
         System.out.println("\n");
     }
+    public void orderDrawing() {
+        System.out.println( "\t ╭━━━╮╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╱\n" +
+                "\t ┃╭━╮┃╱╱╱╱╱╱╱╱╱┃┃╱╱╱╱╱╱╱╱╱╱╱\n" +
+                "\t ┃┃╱┃┃╱╱╭━╮╱╱╭━╯┃╱╱╭━━╮╱╱╭━╮\n" +
+                "\t ┃┃╱┃┃╱╱┃╭╯╱╱┃╭╮┃╱╱┃┃━┫╱╱┃╭╯\n" +
+                "\t ┃╰━╯┃╱╱┃┃╱╱╱┃╰╯┃╱╱┃┃━┫╱╱┃┃╱\n" +
+                "\t ╰━━━╯╱╱╰╯╱╱╱╰━━╯╱╱╰━━╯╱╱╰╯╱\n");
+    }
 
     /**
      * Used to print the greeting message from {@link Duke}.
@@ -167,17 +175,23 @@ public class Ui {
         showLine();
     }
     public void showOrderTemplate() {
+        orderDrawing();
         showLine();
         System.out.println("\t Continue by adding, removing, listing, altering, marking done order \n\t Template: ");
         showLine();
-        System.out.println("\t add <order content>");
-        System.out.println("\t remove <order index>");
-        System.out.println("\t list <list type: all, undone, today, or undoneToday>");
-        System.out.println("\t list <filtering dish name>");
-        System.out.println("\t list <filtering order serving date: DD/MM/YYYY>");
-        System.out.println("\t alter <order index> <order serving date: DD/MM/YYYY>");
-        System.out.println("\t initialize (REMOVES all entries in the order list)");
+        System.out.println("\t add <dish1 name>*<dish1 amount>, <dish2 name>*<dish2 amount>, ... , <dishN name>*<dishN amount>" +
+                " /on <order date: DD/MM/YYYY>");
+        System.out.println("\t list <list type: all, undone, today, undoneToday>");
+        System.out.println("\t list dish <filtering dish name>");
+        System.out.println("\t list date <filtering order serving date: DD/MM/YYYY>");
+
+        System.out.println("\t alter <order index> <order date: DD/MM/YYYY>");
         System.out.println("\t done <order index>");
+        System.out.println("\t remove <order index>");
+
+
+        System.out.println("\t initialize (REMOVES all entries in the order list)");
+        System.out.println("\t ***** Note: order index starts from 1 *****");
         showLine();
     }
     public void showIngredientsInFridge(IngredientsList ingredientsList) {
@@ -221,22 +235,11 @@ public class Ui {
     /**
      * Show that this task is marked.
      *
-     * @param doneTask The task that is marked as done
+     * @param doneOrder The description of the order that is marked as done
      */
-    public void showMarkDone(String doneTask) {
-        System.out.println("\t Nice! I've marked this task as done:");
-        System.out.println("\t " + doneTask);
-    }
-
-    /**
-     * Show the task that has been snoozed.
-     *
-     * @param date        the date
-     * @param changedTask the task that has been changed
-     */
-    public void showChangedDate(String date, String changedTask) {
-        System.out.println("\t Nice! I've snoozed this task as until " + date + ":");
-        System.out.println("\t " + changedTask);
+    public void showMarkDoneOrder(String doneOrder) {
+        System.out.println("\t Nice! I've marked this order as done:");
+        System.out.println("\t " + doneOrder);
     }
 
     /**
@@ -295,12 +298,12 @@ public class Ui {
     /**
      * Shows that a order has been added.
      *
-     * @param command ay
-     * @param size    ya
+     * @param description information of the order be added
+     * @param size  current size of the whole order list
      */
-    public void showAddOrder(String command, int size) {
+    public void showAddOrder(String description, int size) {
         System.out.println("\t Got it. I've added this order: ");
-        System.out.println("\t " + command);
+        System.out.println("\t " + description);
         showOrderListSize(size);
     }
 
