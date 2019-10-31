47b30aac6e16e6745c624059a666594691f658a0
JustinChia1997 <justinchiajj@hotmail.com>
2019-10-31 23:38:32 +0800

    Fixed bug where tag is empty

--- src/main/java/logic/command/MatchCommand.java
+++ src/main/java/logic/command/MatchCommand.java
@@ -13,6 +13,7 @@ import java.util.logging.Logger;
 
 public class MatchCommand extends Command {
     private String taskName;
+
     public MatchCommand(String name) {
         taskName = name;
     }
@@ -25,16 +26,31 @@ public class MatchCommand extends Command {
         Task task = tasksManager.getTaskByName(taskName);
         ArrayList<String> reqSkills = task.getReqSkills();
         ArrayList<String> matchedMembers = new ArrayList<>();
-        for(int i=0; i< memberList.size(); i+=1){
-            if(memberList.get(i).hasSkill("fly")){
-                LoggerController.logDebug(this.getClass(), memberList.get(i).getName());
-                matchedMembers.add(memberList.get(i).getName());
+        for (int i = 0; i < memberList.size(); i += 1) {
+            String matchedMemberName = "";
+            matchedMemberName = getMemberWithSkill(memberList.get(i), reqSkills);
+            if (matchedMemberName != null) {
+                matchedMembers.add(matchedMemberName);
+                LoggerController.logDebug(this.getClass(), matchedMemberName);
             }
         }
-        String userOutput = "";
-        for(int i=0; i < matchedMembers.size(); i+=1){
-            userOutput +=String.valueOf(i+1) +": " + matchedMembers.get(i) + "\n";
+
+        String userOutput = "These are the members with the relevant skills: \n";
+        for (int i = 0; i < matchedMembers.size(); i += 1) {
+            userOutput += (i + 1) + ": " + matchedMembers.get(i) + "\n";
+        }
+        if(matchedMembers.size()==0){
+            userOutput = "No matching members who have the skills or task has required no skills assigned";
         }
         return new CommandOutput(userOutput);
+    }
+
+    private String getMemberWithSkill(Member member, ArrayList<String> reqSkills) {
+        for (int j = 0; j < reqSkills.size(); j += 1) {
+            if (member.hasSkill(reqSkills.get(j))) {
+                return member.getName();
+            }
+        }
+        return null;
     }
 }
