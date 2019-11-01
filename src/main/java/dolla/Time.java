dfa0de9cc1c2a620d6771577c9d298b6a0c62032
weng-kexin <weng.kexin98@gmail.com>
2019-11-01 14:06:42 +0800

    optimized unused imports

--- src/main/java/dolla/Time.java
+++ src/main/java/dolla/Time.java
@@ -3,7 +3,6 @@ package dolla;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Locale;
 
 public class Time {
