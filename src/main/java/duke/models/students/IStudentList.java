8d9170aa419fd70cd059201b61725e5cefe46fdb
danisheddie <42911431+danisheddie@users.noreply.github.com>
2019-11-01 11:55:55 +0800

    Refactored student classes

--- src/main/java/duke/models/students/IStudentList.java
+++ src/main/java/duke/models/students/IStudentList.java
@@ -1,4 +1,15 @@
 package duke.models.students;
 
-public class IStudentList {
+import java.util.ArrayList;
+
+public interface IStudentList {
+    void addStudent(Student newStudent);
+
+    void deleteStudent(int index);
+
+    ArrayList<String> getAllStudent();
+
+    Student getStudent(int i);
+
+    int getStudentListSize();
 }
