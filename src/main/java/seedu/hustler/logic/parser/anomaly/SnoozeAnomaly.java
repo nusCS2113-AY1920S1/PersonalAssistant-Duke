d38b9599feaf36706ec726a8fe6477fa7577922d
ngjiewu <ngjiewu@gmail.com>
2019-11-01 22:12:25 +0800

    Fixed #267

--- src/main/java/seedu/hustler/logic/parser/anomaly/SnoozeAnomaly.java
+++ src/main/java/seedu/hustler/logic/parser/anomaly/SnoozeAnomaly.java
@@ -2,8 +2,11 @@ package seedu.hustler.logic.parser.anomaly;
 
 import seedu.hustler.Hustler;
 import seedu.hustler.logic.CommandLineException;
+import seedu.hustler.logic.command.Command;
+import seedu.hustler.task.ToDo;
 
 import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 
 import static seedu.hustler.logic.parser.DateTimeParser.getDateTime;
 
@@ -12,12 +15,14 @@ import static seedu.hustler.logic.parser.DateTimeParser.getDateTime;
  */
 public class SnoozeAnomaly extends DetectAnomaly {
 
-    private static final String MESSAGE_INVALID_COMMAND_FORMAT = "Snooze format should be: 'snooze <index> <date> <time>' OR\n\t"
-            + "'snooze <index> <integer> <period>'";
-    private static final String MESSAGE_INVALID_TASK_DISPLAYED_INDEX = "The task index provided is invalid!";
+    private static final String MESSAGE_INVALID_COMMAND_FORMAT = "Snooze format should be: '/snooze <index> <date> <time>' OR\n\t"
+            + "'/snooze <index> <integer> <unit>'";
+    private static final String MESSAGE_INVALID_INDEX = "The task index provided is invalid!";
     private static final String MESSAGE_INVALID_DATE_TIME = "Date Time should follow the format DD/MM/YY HHmm.";
-    private static final String MESSAGE_INVALID_PERIOD = "The period provided is invalid!\n\t"
-            + "Valid periods are minutes/hours/days/weeks/months";
+    private static final String MESSAGE_INVALID_PERIOD = "The <unit> provided is invalid!\n\t"
+            + "Valid <unit> are minutes/hours/days/weeks/months.";
+    private static final String MESSAGE_INVALID_TASK_TYPE = "You cannot snooze a ToDo Task!";
+    private static final String MESSAGE_INVALID_INTEGER = "Please enter an integer for <index>!";
 
     /**
      * Detects anomalies in snooze command input.
@@ -40,12 +45,16 @@ public class SnoozeAnomaly extends DetectAnomaly {
         try {
             snoozeIndex = Integer.parseInt(parsedInput[0]);
         } catch (NumberFormatException e) {
-            throw new CommandLineException("Please enter an integer for the index.");
+            throw new CommandLineException(MESSAGE_INVALID_INTEGER);
         }
 
         snoozeIndex--;
         if (snoozeIndex >= Hustler.list.size()) {
-            throw new CommandLineException(MESSAGE_INVALID_TASK_DISPLAYED_INDEX);
+            throw new CommandLineException(MESSAGE_INVALID_INDEX);
+        }
+
+        if (Hustler.list.get(snoozeIndex) instanceof ToDo) {
+            throw new CommandLineException((MESSAGE_INVALID_TASK_TYPE));
         }
 
         // 1st method to snooze
@@ -61,17 +70,14 @@ public class SnoozeAnomaly extends DetectAnomaly {
             // 2nd method to snooze
             try {
                 Integer.parseInt(parsedInput[1]);
-                String period = parsedInput[2];
-                String[] periods = {"minutes", "hours", "days", "weeks", "months"};
-                for (String validPeriod : periods) {
-                    if (period.equals(validPeriod)) {
-                        return;
-                    }
+                String unit = parsedInput[2];
+                String[] validUnits = {"minutes", "hours", "days", "weeks", "months"};
+                if (!Arrays.asList(validUnits).contains(unit)) {
+                    throw new CommandLineException(MESSAGE_INVALID_PERIOD);
                 }
             } catch (NumberFormatException e) {
-                throw new CommandLineException(MESSAGE_INVALID_COMMAND_FORMAT);
+                throw new CommandLineException(MESSAGE_INVALID_INTEGER);
             }
-            throw new CommandLineException(MESSAGE_INVALID_PERIOD);
         }
     }
 }
