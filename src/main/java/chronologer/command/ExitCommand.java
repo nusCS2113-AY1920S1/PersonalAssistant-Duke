153e867c52d15cb7d0faa322a54f3df3035148e5
Daryl Tan <tanyixiang16@yahoo.com>
2019-11-01 23:47:55 +0800

    Fixed bye command, now it terminates.

--- src/main/java/chronologer/command/ExitCommand.java
+++ src/main/java/chronologer/command/ExitCommand.java
@@ -1,19 +1,15 @@
 package chronologer.command;
 
-import chronologer.exception.ChronologerException;
 import chronologer.storage.Storage;
 import chronologer.task.TaskList;
-import chronologer.ui.UiTemporary;
 
 /**
  * Terminates the program.
  *
  * @author Sai Ganesh Suresh
- * @version v1.3
+ * @version v1.4
  */
 public class ExitCommand extends Command {
-
-    private static final String GOODBYE = "Goodbye";
 
     public ExitCommand() {
     }
@@ -21,10 +17,8 @@ public class ExitCommand extends Command {
     /**
      * Terminates the program by updating isExit to true.
      */
-    public void execute(TaskList tasks, Storage storage) throws ChronologerException {
-        super.commandOut();
-        UiTemporary.printGoodbye();
-        UiTemporary.printOutput(GOODBYE);
+    public void execute(TaskList tasks, Storage storage)  {
+        System.exit(0);
     }
 
 }
