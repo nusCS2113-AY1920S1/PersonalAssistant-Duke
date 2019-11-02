dad883fd8bfc294eee5c1b646b741e4cd4116888
kkeejjuunn <kejun0529@gmail.com>
2019-11-03 02:53:10 +0800

    Implement the check of name, NRIC, room number while adding a new patient.

--- src/main/java/duke/commands/patient/AddPatientCommand.java
+++ src/main/java/duke/commands/patient/AddPatientCommand.java
@@ -16,10 +16,10 @@ public class AddPatientCommand implements Command {
     private Patient newPatient;
 
     /**
-     * .
+     * It extracts the patient information.
      *
-     * @param patientInfo .
-     * @throws DukeException .
+     * @param patientInfo contains the patient information to be added.
+     * @throws DukeException if there ia any information is missing.
      */
     public AddPatientCommand(String[] patientInfo) throws DukeException {
         super();
@@ -28,19 +28,38 @@ public class AddPatientCommand implements Command {
 
         } catch (Exception e) {
             throw new DukeException(AddPatientCommand.class,
-                    "Please follow the format 'add patient <name> <NRIC> <Room> <remark>'. ");
+                    "Please follow the format 'add patient :<Name> :<NRIC> :<Room> :<remark>'. ");
         }
     }
 
+    /**
+     * It adds a new patient to Dukepital.
+     *
+     * @param assignedTaskManager contains the information between all the tasks and patients.
+     * @param taskManager         contains information of all the tasks.
+     * @param patientManager      contains information of all the patients.
+     * @param dukeUi                  interacts with user.
+     * @param storageManager      save the changes in csv file.
+     * @throws DukeException if the patient is not added successfully.
+     */
     @Override
-    public void execute(AssignedTaskManager patientTask, TaskManager tasks, PatientManager patientList, DukeUi dukeUi,
-                        StorageManager storageManager) throws DukeException {
+    public void execute(AssignedTaskManager assignedTaskManager, TaskManager taskManager, PatientManager patientManager,
+                        DukeUi dukeUi, StorageManager storageManager) throws DukeException {
 
-        patientList.addPatient(newPatient);
-        storageManager.savePatients(patientList.getPatientList());
-        dukeUi.patientAdded(newPatient);
+        try {
+            patientManager.addPatient(newPatient);
+            storageManager.savePatients(patientManager.getPatientList());
+            dukeUi.patientAdded(newPatient);
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
+    /**
+     * It terminates the Dukepital.
+     *
+     * @return false.
+     */
     @Override
     public boolean isExit() {
         return false;
