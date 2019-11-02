71c93ebfea9eb988fc6930929c9aab3f0bdc2532
liujiajun <liujiajun1999@gmail.com>
2019-11-02 21:08:52 +0800

    Update test

--- src/test/java/duke/logic/parser/order/AddOrderCommandParserTest.java
+++ src/test/java/duke/logic/parser/order/AddOrderCommandParserTest.java
@@ -1,17 +1,34 @@
 package duke.logic.parser.order;
 
+import duke.logic.command.exceptions.CommandException;
+import duke.logic.command.order.AddOrderCommand;
 import duke.logic.parser.exceptions.ParseException;
+import duke.model.order.Order;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class AddOrderCommandParserTest {
     private AddOrderCommandParser parser = new AddOrderCommandParser();
 
     @Test
+    public void addOrder_activeStatus_success() {
+        OrderModelStub modelStub = new OrderModelStub();
+        AddOrderCommand command = parser.parse("-status active");
+        try {
+            command.execute(modelStub);
+            Assertions.assertEquals(Order.Status.ACTIVE, modelStub.getOrder(0).getStatus());
+        } catch (CommandException e) {
+            fail();
+        }
+    }
+
+    @Test
     public void addOrder_invalidStatus_failure() {
         Assertions.assertThrows(
             ParseException.class,
-            () -> parser.parse("-status INVALID")
+            () -> parser.parse("-status RANDOM")
         );
     }
 
@@ -23,16 +40,21 @@ public class AddOrderCommandParserTest {
         );
     }
 
+    @Test
+    public void addOrder_noArgument_success() {
+        Assertions.assertAll(() -> parser.parse(""));
+    }
+
     /**
-     * Invalid equivalence partitions for quantity of item:
+     * Invalid equivalence partition for quantity of item:
      * - negative quantity and quantity more than 50000
-     * Valid equivalence partitions for quantity:
+     * Valid equivalence partition for quantity:
      * - quantities in (0, 50000)
      * Boundary values:
-     * - 0: valid input
-     * - 50000: valid input
+     * - 0, 50000, 0.1, 49999.9 (valid input)
+     * - 50000.1, -0.01 (invalid input)
      * <p>
-     * The four test cases below tests EP at a time.
+     * The four test cases below tests EP one at a time.
      */
 
     @Test
@@ -54,19 +76,17 @@ public class AddOrderCommandParserTest {
     }
 
     @Test
-    public void addOrder_zeroQuantity_success() {
+    public void addOrder_boundaryQuantity_success() {
         Assertions.assertAll(() -> parser.parse("-item a, 0"));
-    }
-
-    @Test
-    public void addOrder_maxQuantity_success() {
         Assertions.assertAll(() -> parser.parse("-item a, 50000.0"));
+        Assertions.assertAll(() -> parser.parse("-item a, 49999.9"));
+        Assertions.assertAll(() -> parser.parse("-item a, 0.1"));
     }
-
-
 
     @Test
-    public void addOrder_noArgument_success() {
-        Assertions.assertAll(() -> parser.parse(""));
+    public void addOrder_boundaryQuantity_outOfRange() {
+        Assertions.assertThrows(ParseException.class, () -> parser.parse("-item a, -0.01"));
+        Assertions.assertThrows(ParseException.class, () -> parser.parse("-item a, 50000.1"));
     }
+
 }
