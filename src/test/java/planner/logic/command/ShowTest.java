729f9137d3f0a371aa9136ecaf77217f4bb11012
e0313687 <e0313687@u.nus.edu>
2019-11-03 00:32:38 +0800

    Added ShowTest back, accidental deletion in the previous commit.

---
+++ src/test/java/planner/logic/command/ShowTest.java
@@ -0,0 +1,27 @@
+package planner.logic.command;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import planner.InputTest;
+import planner.main.CliLauncher;
+
+public class ShowTest extends InputTest {
+
+    /**
+     * For testing, the following format of command to be tested, followed by the bye command must be followed.
+     * This enforces that the scanner object is closed and prevents test cases from causing infinite loops.
+     */
+    @DisplayName("Show module test")
+    @Test
+    public void testShow() {
+        final String test = "show module\n";
+        final String bye = "bye";
+        provideInput(test + bye);
+        final String[] hold = {"a"};
+        CliLauncher.main(hold);
+        assertEquals(outContent, outContent);
+    }
+}
