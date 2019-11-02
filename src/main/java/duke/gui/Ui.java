1afd4b0c7ba3d8094c22cc9115645989379e7568
HUANG XUAN KUN <hxk123123123@gmail.com>
2019-11-02 15:57:03 +0800

    Update GUI framework

---
+++ src/main/java/duke/gui/Ui.java
@@ -0,0 +1,14 @@
+package duke.ui;
+
+import javafx.stage.Stage;
+
+/**
+ * API of UI component.
+ */
+public interface Ui {
+
+    /**
+     * Starts the UI (and the App).
+     */
+    void start(Stage primaryStage);
+}
---
+++ src/main/java/duke/gui/UiManager.java
@@ -0,0 +1,85 @@
+package duke.ui;
+
+import duke.Launcher;
+import duke.commons.core.LogsCenter;
+import duke.commons.util.StringUtil;
+import duke.logic.Logic;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.logging.Logger;
+
+/**
+ * The manager of the UI component.
+ */
+public class UiManager implements Ui {
+
+    public static final String ALERT_DIALOG_PANE_FIELD_ID = "alertDialogPane";
+
+    private static final Logger logger = LogsCenter.getLogger(UiManager.class);
+    private static final String ICON_APPLICATION = "/images/bakinghome_icon.png";
+
+    private Logic logic;
+    private MainWindow mainWindow;
+
+    public UiManager(Logic logic) {
+        super();
+        this.logic = logic;
+    }
+
+    /**
+     * Shows an alert dialog on {@code owner} with the given parameters.
+     * This method only returns after the user has closed the alert dialog.
+     */
+    private static void showAlertDialogAndWait(Stage owner, AlertType type, String title, String headerText,
+                                               String contentText) {
+        final Alert alert = new Alert(type);
+        alert.getDialogPane().getStylesheets().add("view/duke.css");
+        alert.initOwner(owner);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        alert.getDialogPane().setId(ALERT_DIALOG_PANE_FIELD_ID);
+        alert.showAndWait();
+    }
+
+    private Image getImage(String imagePath) {
+        return new Image(Launcher.class.getResourceAsStream(imagePath));
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        logger.info("Starting UI...");
+
+        //Set the application icon.
+        primaryStage.getIcons().add(getImage(ICON_APPLICATION));
+
+        try {
+            mainWindow = new MainWindow(primaryStage, logic);
+            mainWindow.show(); //This should be called before creating other UI parts
+            mainWindow.fillInnerParts();
+        } catch (Throwable e) {
+            logger.severe(StringUtil.getDetails(e));
+            showFatalErrorDialogAndShutdown("Fatal error during initializing", e);
+        }
+    }
+
+    private void showAlertDialogAndWait(Alert.AlertType type, String title, String headerText, String contentText) {
+        showAlertDialogAndWait(mainWindow.getPrimaryStage(), type, title, headerText, contentText);
+    }
+
+    /**
+     * Shows an error alert dialog with {@code title} and error message, {@code e},
+     * and exits the application after the user has closed the alert dialog.
+     */
+    private void showFatalErrorDialogAndShutdown(String title, Throwable e) {
+        logger.severe(title + " " + e.getMessage() + StringUtil.getDetails(e));
+        showAlertDialogAndWait(Alert.AlertType.ERROR, title, e.getMessage(), e.toString());
+        Platform.exit();
+        System.exit(1);
+    }
+
+}
