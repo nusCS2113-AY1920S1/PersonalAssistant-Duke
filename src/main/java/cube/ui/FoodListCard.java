1edd1df139aa570524b109b9e357e73ff710b57a
kuromono <clouderext@gmail.com>
2019-11-02 22:49:04 +0800

    Modifed FoodList views

--- src/main/java/cube/ui/FoodListCard.java
+++ src/main/java/cube/ui/FoodListCard.java
@@ -4,21 +4,26 @@ import cube.model.food.Food;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 
 public class FoodListCard extends UiManager<HBox> {
     private static final String FXML = "FoodListCard.fxml";
 
     private Food food;
+    private int index;
+
+    private final EditExecutor editExecutor;
+    private final DeleteExecutor deleteExecutor;
 
     @FXML
     private HBox cardPane;
     @FXML
     private Label name;
     @FXML
-    private Label id;
+    private FlowPane tags;
     @FXML
-    private Label type;
+    private Label id;
     @FXML
     private Label price;
     @FXML
@@ -31,16 +36,53 @@ public class FoodListCard extends UiManager<HBox> {
     @FXML
     private Button delete;
 
-    public FoodListCard(Food food, int displayedIndex) {
+    public FoodListCard(Food food, int displayedIndex, EditExecutor editExecutor, DeleteExecutor deleteExecutor) {
         super(FXML);
-
-
         this.food = food;
+        this.index = displayedIndex;
+
         name.setText(food.getName());
         id.setText(displayedIndex + ". ");
-        type.setText("Type: " + food.getType());
+        tags.getChildren().add(new Label(food.getType()));
         price.setText("Price: $" + food.getPrice());
         stock.setText("Stock: " + food.getStock());
         expiry.setText("Expiry Date: " + food.getExpiryDate().toString());
+
+        this.editExecutor = editExecutor;
+        this.deleteExecutor = deleteExecutor;
+    }
+
+    @FXML
+    private void handleEdit() {
+        System.out.println("EDIT");
+        editExecutor.execute(index);
+    }
+
+    @FXML
+    private void handleDelete() {
+        System.out.println("DELETE");
+        deleteExecutor.execute(index);
+    }
+
+    /**
+     * Represents a function that can execute edit commands.
+     */
+    @FunctionalInterface
+    public interface EditExecutor {
+        /**
+         * Executes the command and returns the result.
+         */
+        void execute(int index);
+    }
+
+    /**
+     * Represents a function that can execute delete commands.
+     */
+    @FunctionalInterface
+    public interface DeleteExecutor {
+        /**
+         * Executes the command and returns the result.
+         */
+        void execute(int index);
     }
 }
