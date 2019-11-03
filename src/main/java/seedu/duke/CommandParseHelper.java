467a5f6d88531dc5cfabb7236054ffb41e05c3d4
ziyun99 <ziyun99@users.noreply.github.com>
2019-11-03 14:51:06 +0800

    add email-filter design constraints in DG

--- src/main/java/seedu/duke/CommandParseHelper.java
+++ src/main/java/seedu/duke/CommandParseHelper.java
@@ -95,7 +95,6 @@ public class CommandParseHelper {
             userInput = userInput.replaceAll("\\s*(?<key>-[\\w]+)\\s+(?<value>[\\w]+[\\s|\\w/]*)\\s*$", "");
             optionMatcher = optionPattern.matcher(userInput);
         }
-        System.out.println(optionList);
         return optionList;
     }
 
