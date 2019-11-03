2d2573dd6346d765624dd3d589d4ef812b464987
jasonchanwq <jasonchanwq@gmail.com>
2019-11-04 03:10:59 +0800

    Edit EditTaskDateTime Command now checks against current time

--- src/main/java/logic/command/edit/EditTaskDateTimeCommand.java
+++ src/main/java/logic/command/edit/EditTaskDateTimeCommand.java
@@ -13,6 +13,7 @@ public class EditTaskDateTimeCommand extends Command {
 
     private static final String SUCCESS_MESSAGE = "The deadline has been changed to: ";
     public static final String INDEX_NOT_IN_TASKlIST_MESSAGE = "Index is not within the task list";
+    public static final String NEW_DATETIME_IS_BEHIND_CURRENT_TIME_MESSAGE = "The input DateTime has already passed!";
     public int taskIndex;
     public Date newDate;
 
@@ -23,8 +24,11 @@ public class EditTaskDateTimeCommand extends Command {
 
     @Override
     public CommandOutput execute(Model model) throws DukeException {
+        Date currentDate = new Date();
         if (taskIndex < 1 || taskIndex > model.getTaskListSize()) {
             return new CommandOutput(INDEX_NOT_IN_TASKlIST_MESSAGE);
+        } else if (newDate.compareTo(currentDate) < 0) {
+            return new CommandOutput(NEW_DATETIME_IS_BEHIND_CURRENT_TIME_MESSAGE);
         } else {
             model.getTaskList().get(taskIndex - 1).setTime(newDate);
             model.getTaskList().get(taskIndex - 1).setReminder(null);
