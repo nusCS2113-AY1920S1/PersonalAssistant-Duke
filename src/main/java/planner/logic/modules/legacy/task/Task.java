c2cad5b4b94218b2dc4798a069349c2ac9728019
andrewleow97 <e0323960@u.nus.edu>
2019-11-04 02:47:41 +0800

    Checkstyle

--- src/main/java/planner/logic/modules/legacy/task/Task.java
+++ src/main/java/planner/logic/modules/legacy/task/Task.java
@@ -41,7 +41,7 @@ public class Task {
 
     @Override
     public String toString() {
-        String completed = (done) ? "[\u2713] " : "[\u2717] ";
+        String completed = (done) ? "[✓] " : "[✗] ";
         return completed + task;
     }
 
