d24d4851b9ffa59af56937644d847e34a9838628
AjeyAshok <a5000shiok@gmail.com>
2019-11-04 05:43:30 +0800

    Added exceptions to expenditure

--- src/main/java/executor/command/CommandGetSpendingByYear.java
+++ src/main/java/executor/command/CommandGetSpendingByYear.java
@@ -6,6 +6,9 @@ import ui.ReceiptTracker;
 import ui.Ui;
 import ui.Wallet;
 
+import java.time.LocalDate;
+import java.time.Year;
+
 public class CommandGetSpendingByYear extends Command {
     private String userInput;
 
@@ -24,10 +27,37 @@ public class CommandGetSpendingByYear extends Command {
     public void execute(Wallet wallet) {
         ReceiptTracker receiptsInYear = new ReceiptTracker();
         String yearStr = Parser.parseForPrimaryInput(CommandType.EXPENDEDYEAR, userInput);
-        int year = Integer.parseInt(yearStr);
-        receiptsInYear = wallet.getReceipts().findReceiptByYear(year);
-        Double totalMoney = receiptsInYear.getTotalCashSpent();
-        Ui.dukeSays("The total amount of money spent in " + year + " : " + totalMoney);
+
+        if (yearStr.isEmpty()) {
+            Ui.dukeSays("No year input detected. FORMAT : expendedyear 2019");
+            return;
+        }
+
+        if (yearStr.length() != 4) {
+            Ui.dukeSays("Year input contains extra number of variables. FORMAT : expendedyear 2019");
+            return;
+        }
+
+        try {
+            int year = Integer.parseInt(yearStr);
+
+            if (year > Year.now().getValue()) {
+                Ui.dukeSays("Future year entered is invalid!");
+                return;
+            }
+
+            if (year < 1000) {
+                Ui.dukeSays("Year is too far back into the past");
+                return;
+            }
+            receiptsInYear = wallet.getReceipts().findReceiptByYear(year);
+            Double totalMoney = receiptsInYear.getTotalCashSpent();
+            Ui.dukeSays("The total amount of money spent in " + year + " : " + totalMoney);
+
+        } catch (Exception e) {
+            Ui.dukeSays("Year input is either a double or contains String values. FORMAT : expendedyear 2019");
+        }
+
     }
 
     @Override
