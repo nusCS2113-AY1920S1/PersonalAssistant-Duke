b70a69920cefbad5b40b1e12b0419297a3c542a7
kuromono <clouderext@gmail.com>
2019-11-03 12:17:58 +0800

    Fix CubeLauncher & upgraded gradlew to 5.6.4

--- src/main/java/cube/CubeLauncher.java
+++ src/main/java/cube/CubeLauncher.java
@@ -1,4 +1,14 @@
 package cube;
 
+import javafx.application.Application;
+
 public class CubeLauncher {
+    public static void main(String[] args) {
+
+        if (args.length == 1 && args[0].equals("cli")) {
+            Cube.main(args);
+        } else {
+            Application.launch(CubeApp.class, args);
+        }
+    }
 }
