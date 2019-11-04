2e77f39fb64a07fa2f5093af09e0fc456c8cf95f
lee761 <vivianleeboongeok@gmail.com>
2019-11-04 10:57:33 +0800

    priority can take in med

--- src/main/java/seedu/duke/task/entity/Task.java
+++ src/main/java/seedu/duke/task/entity/Task.java
@@ -18,7 +18,7 @@ public class Task {
      * The enumeration of priority level.
      */
     public enum Priority {
-        HIGH, MEDIUM, LOW
+        HIGH, MEDIUM, MED, LOW
     }
 
     /**
--- src/main/java/seedu/duke/task/parser/TaskCommandParseHelper.java
+++ src/main/java/seedu/duke/task/parser/TaskCommandParseHelper.java
@@ -375,7 +375,7 @@ public class TaskCommandParseHelper {
         Task.Priority level = null;
         if (level.HIGH.name().equals(input)) {
             return level.HIGH;
-        } else if (level.MEDIUM.name().equals(input)) {
+        } else if (level.MEDIUM.name().equals(input) || level.MED.name().equals(input)) {
             return level.MEDIUM;
         } else if (level.LOW.name().equals(input)) {
             return level.LOW;
