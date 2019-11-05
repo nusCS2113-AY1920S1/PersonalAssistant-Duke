90e7ebf34c45b206bf74ccccb03b04e5a9f87a93
jing-xuan <tayjxn@gmail.com>
2019-11-05 21:34:48 +0800

    Wrote tests for Parser

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -8,25 +8,36 @@ public class ParserTest {
     @Test
     public void doTest(){
         try {
-            Parser.parse("do", farmio.Stage.TASK_ADD);
+            Parser.parse("do", Farmio.Stage.TASK_ADD);
             assert false;
         } catch (FarmioException e) {
             assert true;
         }
         try {
-            Parser.parse("do gotomarketa", farmio.Stage.TASK_ADD);
+            Parser.parse("do ", Farmio.Stage.TASK_ADD);
             assert false;
         } catch (FarmioException e) {
             assert true;
         }
         try {
-            Parser.parse("do gotomarket", farmio.Stage.TASK_ADD);
-            assert true;
-        } catch (FarmioException e) {
+            Parser.parse("do go to market", Farmio.Stage.TASK_ADD);
             assert false;
+        } catch (FarmioException e) {
+            assert true;
         }
         try {
-            Parser.parse("do buyseeds", farmio.Stage.TASK_ADD);
+            Parser.parse("do risaen", Farmio.Stage.TASK_ADD);
+            assert false;
+        } catch (FarmioException e) {
+            assert true;
+        }
+        try {
+            Parser.parse("do gotomarket", Farmio.Stage.TASK_ADD);
+            Parser.parse("do gotowheatfarm", Farmio.Stage.TASK_ADD);
+            Parser.parse("do buyseeds", Farmio.Stage.TASK_ADD);
+            Parser.parse("do plantseeds", Farmio.Stage.TASK_ADD);
+            Parser.parse("do harvestwheat", Farmio.Stage.TASK_ADD);
+            Parser.parse("do sellgrain", Farmio.Stage.TASK_ADD);
             assert true;
         } catch (FarmioException e) {
             assert false;
@@ -36,28 +47,62 @@ public class ParserTest {
     @Test
     public void ifTest() {
         try {
-            Parser.parse("if", farmio.Stage.TASK_ADD);
+            Parser.parse("if", Farmio.Stage.TASK_ADD);
             assert false;
         } catch (FarmioException e) {
             assert true;
         }
         try {
-            Parser.parse("if hasseeds do plantseeds", farmio.Stage.TASK_ADD);
-            assert true;
-        } catch (FarmioException e) {
-            assert false;
-        }
-        try {
-            Parser.parse("if hasseed do plantseeds", farmio.Stage.TASK_ADD);
+            Parser.parse("if ", Farmio.Stage.TASK_ADD);
             assert false;
         } catch (FarmioException e) {
             assert true;
         }
         try {
-            Parser.parse("if hasseeds do plantseed", farmio.Stage.TASK_ADD);
+            Parser.parse("if hasseeds", Farmio.Stage.TASK_ADD);
             assert false;
         } catch (FarmioException e) {
             assert true;
+        }
+        try {
+            Parser.parse("if hasseeds do", Farmio.Stage.TASK_ADD);
+            assert false;
+        } catch (FarmioException e) {
+            assert true;
+        }
+        try {
+            Parser.parse("if hasseeds do ", Farmio.Stage.TASK_ADD);
+            assert false;
+        } catch (FarmioException e) {
+            assert true;
+        }
+        try {
+            Parser.parse("if riant do rian", Farmio.Stage.TASK_ADD);
+            assert false;
+        } catch (FarmioException e) {
+            assert true;
+        }
+        try {
+            Parser.parse("if gold greater than or equals 10", Farmio.Stage.TASK_ADD);
+            assert false;
+        } catch (FarmioException e) {
+            assert true;
+        }
+        try {
+            Parser.parse("if gold greater than -10 do buyseeds", Farmio.Stage.TASK_ADD);
+            assert false;
+        } catch (FarmioException e) {
+            assert true;
+        }
+        try {
+            Parser.parse("if gold greater than 10 do buyseeds", Farmio.Stage.TASK_ADD);
+            Parser.parse("if hasseeds do plantseeds", Farmio.Stage.TASK_ADD);
+            Parser.parse("if gold lessthanorequals 10 do buyseeds", Farmio.Stage.TASK_ADD);
+            Parser.parse("if gold LESS THAN 20 do buyseeds", Farmio.Stage.TASK_ADD);
+            Parser.parse("if gold greater thanorequals 100 do buyseeds", Farmio.Stage.TASK_ADD);
+            assert true;
+        } catch (FarmioException e) {
+            assert false;
         }
     }
 }
