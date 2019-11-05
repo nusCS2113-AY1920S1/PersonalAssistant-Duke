cd875d5e2f15b1e02b9909e2a4599b102a52a449
Jefferson111 <e0309385@u.nus.edu>
2019-11-05 19:37:58 +0800

    Fix imports

--- src/test/java/duke/logic/commands/HelpCommandTest.java
+++ src/test/java/duke/logic/commands/HelpCommandTest.java
@@ -2,14 +2,12 @@ package duke.logic.commands;
 
 import duke.ModelStub;
 import duke.commons.exceptions.DukeException;
-import duke.logic.commands.results.CommandResultText;
 
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
+import java.awt.HeadlessException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HelpCommandTest {
 
