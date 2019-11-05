cdd7d5595690926030b7449ab5a1dcc27cdcb565
GlenWong97 <shuzewong@yahoo.com.sg>
2019-11-05 22:21:26 +0800

    Minor changes

--- src/main/java/javacake/ui/Handler.java
+++ src/main/java/javacake/ui/Handler.java
@@ -9,16 +9,21 @@ import java.util.logging.Level;
 public class Handler {
 
     private static JavaCake javaCake;
-    /*public void handleExit() {
+    private static MainWindow mw = new MainWindow();
+
+    /*void handleExit() {
         System.out.println("EXIT");
         JavaCake.logger.log(Level.INFO, "EXITING PROGRAM!");
         // find out if exit condition
-        MainWindow.setExitToTrue();
-        MainWindow.setResponse(javaCake.getResponse(MainWindow.getInput()));
-        MainWindow.showContentContainer();
+        AvatarScreen.avatarMode = AvatarScreen.AvatarMode.SAD;
+        mw.setIsExitToTrue();
+        String input = mw.getInput();
+        mw.setResponse(input);
+        mw.getShowContentContainer();
         PauseTransition pause = new PauseTransition(Duration.seconds(2));
-        pause.setOnFinished(e -> MainWindow.primaryStage.hide());
+        pause.setOnFinished(e -> mw.hidePrimaryStage());
         pause.play();
     }*/
+
 
 }
