c117f34196bf7f4d80f4ce43ee9100a4b3df65c1
priyan-coder <rajamohanpriyan@gmail.com>
2019-11-05 14:09:25 +0800

    refactored command schedule

--- src/main/java/executor/command/CommandSchedule.java
+++ src/main/java/executor/command/CommandSchedule.java
@@ -6,19 +6,15 @@ import ui.Ui;
 import ui.Wallet;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public class CommandSchedule extends Command {
     protected String userInput;
 
-
-    //constructor
     /**
      * Constructor for CommandSchedule subCommand Class.
-     * @param userInput The user input from the CLI
+     * @param userInput String the user input from the CLI
      */
     public CommandSchedule(String userInput) {
         this.userInput = userInput;
@@ -34,37 +30,24 @@ public class CommandSchedule extends Command {
     @Override
     public void execute(TaskList taskList) {
         String dateInput = Parser.removeStr(this.commandType.toString(), this.userInput);
-        Ui.dukeSays("Here is your schedule for the following date '"
-                + dateInput
-                + "'."
-        );
-        try {
-            printSchedule(dateInput, taskList);
-        } catch (Exception e) {
-            System.out.println("Read invalid input");
-        }
+        printSchedule(dateInput, taskList);
     }
 
-    private void printSchedule(String dateInput, TaskList taskList) throws ParseException {
-        LocalDate date = LocalDate.now();
-        //insert code to convert string to date type
-        String dateStr_3="28/09/16";
-        DateTimeFormatter formatter_3 = DateTimeFormatter.ofPattern("dd/MM/yy");
-        LocalDate userDate = LocalDate.parse(dateInput,formatter_3);
-        for (int index = 0; index < taskList.getSize(); ++index) {
-            try {
+    private void printSchedule(String dateInput, TaskList taskList) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+        try {
+            LocalDate userDate = LocalDate.parse(dateInput, formatter);
+            Ui.dukeSays("Here is your schedule for the following date \n"
+                    + dateInput
+                    + ":\n");
+            for (int index = 0; index < taskList.getSize(); ++index) {
                 LocalDate taskDate = taskList.getList().get(index).getDate();//creates of copy of datetime in the task
-                if (taskDate == null) {
-                    return;
-                }
-                taskDate.setTime(0);//this sets the time to zero
-                if (taskDate.equals(userDate)) {
+                if (taskDate != null && taskDate.equals(userDate)) {
                     taskList.printTaskByIndex(index);
                 }
-            } catch (Exception e) {
-                System.out.println("invalid");
             }
+        } catch(Exception e){
+            Ui.dukeSays("Please kindly type help to see the format for using Command Schedule");
         }
-
     }
 }
