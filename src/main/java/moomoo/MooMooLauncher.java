38fd9da96e11dbb3771938b121da9c9dad1ac89b
Lindon Ng <lindonng@hotmail.com>
2019-11-06 21:14:54 +0800

    run process to change cmd prompt to windows.

--- src/main/java/moomoo/MooMooLauncher.java
+++ src/main/java/moomoo/MooMooLauncher.java
@@ -1,16 +1,30 @@
 package moomoo;
 
+import moomoo.task.MooMooException;
+
 import java.io.IOException;
 
 /**
  * Launches the application.
  */
 public class MooMooLauncher {
+
     /**
-     * Runs the program either in GUI or CLI mode.
+     * Runs the program in CLI mode.
      * @param args If appropriate argument is given, GUI will be launched.
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws MooMooException {
+        String operatingSystem = System.getProperty("os.name").toLowerCase();
+        if (operatingSystem.contains("win")) {
+            try {
+                ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "chcp", "65001", ">", "nul", "2>&1").inheritIO();
+                pb.start();
+            } catch (IOException e) {
+                throw new MooMooException("An error has occurred. Some output may be corrupted");
+            }
+        }
         MooMoo.main(args);
     }
 }
+
+
