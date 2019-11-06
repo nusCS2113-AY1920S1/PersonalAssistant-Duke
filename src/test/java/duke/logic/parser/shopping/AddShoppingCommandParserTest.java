ebb562119254cc0c48abfdbc8cf369961fe4fe7c
Lim Wen Han Eugene <e0309924@u.nus.edu>
2019-11-06 23:44:15 +0800

    codacy

--- src/test/java/duke/logic/parser/shopping/AddShoppingCommandParserTest.java
+++ src/test/java/duke/logic/parser/shopping/AddShoppingCommandParserTest.java
@@ -8,14 +8,11 @@ class AddShoppingCommandParserTest {
     private AddShoppingCommandParser parser = new AddShoppingCommandParser();
 
     @Test
-    public void parse_quantityWithinValidRange_success() {
+    public void parseQuantity() {
         Assertions.assertAll(() -> parser.parse("-name egg -qty 1000"));
         Assertions.assertAll(() -> parser.parse("-name bread -qty 50000"));
         Assertions.assertAll(() -> parser.parse("-name fish -qty 0"));
-    }
 
-    @Test
-    public void parse_invalid_quantity_throwsIllegalArgumentException() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parse("-name egg -qty 50001"));
     }
 }
