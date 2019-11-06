977a1713c3fdba3de1f0bef74d7d89f37433f9d7
Xdecosee <finalfantasy_1998@hotmail.com>
2019-11-07 00:00:30 +0800

    remove exception import

--- src/main/java/wallet/storage/HelpStorage.java
+++ src/main/java/wallet/storage/HelpStorage.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.InputStream;
 import java.io.IOException;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 /**
