8e786c3b27046c32b676b5944b9e03241d64f688
mohideenik <mohideenimran@outlook.com>
2019-11-06 12:10:46 +0800

    Add tests for TimeFrame

--- src/test/java/duchess/model/TimeFrameTest.java
+++ src/test/java/duchess/model/TimeFrameTest.java
@@ -10,14 +10,27 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TimeFrameTest {
-    private final LocalDateTime dateTime = Util.parseDateTime("12/12/2019 1200");
-    private final LocalDateTime dateTime2 = Util.parseDateTime("12/12/2019 1400");
+    private final TimeFrame instantFrame;
+    private final TimeFrame indefiniteFrame;
+    private final TimeFrame normalFrame;
+    private final TimeFrame normalFrame2;
 
-    private final TimeFrame instantFrame = TimeFrame.ofInstantaneousTask(dateTime);
-    private final TimeFrame indefiniteFrame = TimeFrame.ofTimelessTask();
-    private final TimeFrame normalFrame = new TimeFrame(dateTime, dateTime2);
-
+    /**
+     * Initializes few TimeFrames for testing.
+     *
+     * @throws DuchessException if unable to create a TimeFrame
+     */
     public TimeFrameTest() throws DuchessException {
+        LocalDateTime dateTime = Util.parseDateTime("12/12/2019 1200");
+        LocalDateTime dateTime2 = Util.parseDateTime("12/12/2019 1400");
+        normalFrame = new TimeFrame(dateTime, dateTime2);
+
+        LocalDateTime dateTime3 = Util.parseDateTime("12/12/2019 1300");
+        LocalDateTime dateTime4 = Util.parseDateTime("12/12/2019 1500");
+        normalFrame2 = new TimeFrame(dateTime3, dateTime4);
+
+        instantFrame = TimeFrame.ofInstantaneousTask(dateTime);
+        indefiniteFrame = TimeFrame.ofTimelessTask();
     }
 
     @Test
@@ -26,15 +39,22 @@ public class TimeFrameTest {
         assertTrue(indefiniteFrame.compareTo(instantFrame) < 0);
         assertTrue(instantFrame.compareTo(indefiniteFrame) > 0);
         assertTrue(normalFrame.compareTo(normalFrame) == 0);
+        assertTrue(normalFrame.compareTo(normalFrame2) < 0);
     }
 
     @Test
-    public void fallsWithin_indefiniteTasks() {
+    public void fallsWithin_indefiniteTimeFrame() {
         assertFalse(normalFrame.fallsWithin(indefiniteFrame));
     }
 
     @Test
-    public void clashesWith_instantaneousTask() {
+    public void clashesWith_instantaneousTimeFrame() {
         assertFalse(normalFrame.clashesWith(instantFrame));
+    }
+
+    @Test
+    public void clashesWith_normalTimeFrame() {
+        assertTrue(normalFrame.clashesWith(normalFrame2));
+        assertTrue(normalFrame2.clashesWith(normalFrame));
     }
 }
