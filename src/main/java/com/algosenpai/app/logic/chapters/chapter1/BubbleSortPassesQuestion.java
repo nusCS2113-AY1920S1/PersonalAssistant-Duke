42a7fdc85083662040e06970c2f061dd8e846004
therealcs1010 <e0310402@u.nus.edu>
2019-11-06 21:52:33 +0800

    Implemented space separated instead of comma

--- src/main/java/com/algosenpai/app/logic/chapters/chapter1/BubbleSortPassesQuestion.java
+++ src/main/java/com/algosenpai/app/logic/chapters/chapter1/BubbleSortPassesQuestion.java
@@ -52,7 +52,7 @@ public class BubbleSortPassesQuestion extends Question {
         question = "An array of " + arraySize + " elements underwent the following Bubble Sort Algorithm : "
                 + initialArray + "\n";
         question += "What would be the new configuration of the elements after " + passes + " passes?\n"
-                + "Please provide your answer in comma-separated format. e.g. x, y, z, ...\n\n";
+                + "Please provide your answer in space-separated format. e.g. x y z ...\n\n";
         question += "for (int i = 0; i < passes; i++) {\n" + "   for (int j = 0; j < arr.size - 1 - i; j ++) {\n"
                 + "       if (arr[j] > arr[j + 1]) {\n" + "            swap (arr[j], arr[j+1]);\n" + "       }\n"
                 + "   }\n" + "}\n";
