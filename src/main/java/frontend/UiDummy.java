976485bca1a79d87ab6ca9934312b95d88a00e1e
tzw0 <e0310410@u.nus.edu>
2019-11-06 11:43:13 +0800

    fixed if task not green when checking

--- src/main/java/frontend/UiDummy.java
+++ src/main/java/frontend/UiDummy.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class UiDummy implements Ui {
     public int testValue;
-    private Scanner scanner;
-    private final String CLEAR_SCREEN = "\033c" + "\033[2J";
 
     /**
      * Creates a user interface object.
@@ -26,7 +24,7 @@ public class UiDummy implements Ui {
      * Prints the exit message.
      */
     public void showExit() {
-        typeWriter("Goodbye.", false);
+
     }
 
     /**
@@ -34,7 +32,6 @@ public class UiDummy implements Ui {
      * @param message to be printed as an error.
      */
     public void showError(String message) {
-        show("Error: " + message);
     }
 
     /**
@@ -42,14 +39,14 @@ public class UiDummy implements Ui {
      * @param message as the warning message.
      */
     public void showWarning(String message) {
-        show(AsciiColours.RED + "Warning: " + message + AsciiColours.SANE);
+
     }
 
     /**
      * Clears the screen.
      */
     public void clearScreen() {
-        System.out.println(CLEAR_SCREEN);
+
     }
 
     /**
@@ -57,7 +54,7 @@ public class UiDummy implements Ui {
      * @param message as the info message.
      */
     public void showInfo(String message) {
-        show(AsciiColours.CYAN + "Info: " + AsciiColours.SANE + message);
+
     }
 
     /**
@@ -65,8 +62,7 @@ public class UiDummy implements Ui {
      * @return the user input.
      */
     public String getInput() {
-        show("\nInput: ");
-        return scanner.nextLine().replace("[","").replace("]","");
+        return "";
     }
 
     /**
@@ -74,19 +70,11 @@ public class UiDummy implements Ui {
      * @param delay time in milliseconds.
      */
     public void sleep(int delay) {
-        try {
-            Thread.sleep(delay);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-            clearScreen();
-            showWarning("Simulator refersh interrupted! Interface may not display correctly.");
-        }
+
     }
 
     public void showHint(String text) {
-        show(AsciiColours.YELLOW + "Hint:" + AsciiColours.SANE);
-        show(text);
-        show("~.Enter [Start] when you are ready to complete the objective");
+
     }
 
     /**
@@ -94,39 +82,7 @@ public class UiDummy implements Ui {
      * @param text to be printed.
      * @param hasPressEnter if 'Press ENTER' should be added to the print.
      */
-    public void typeWriter(String text, boolean hasPressEnter) { //TODO clean this method up
-        final char LEVEL_BEGIN_PLACEHOLDER = '~';
-        boolean isNewline = false;
-        int lineLength = 0;
-        if (!text.isBlank()) System.out.print(">>> ");
-        sleep(150);
-        for (int i = 0; i < text.length(); i++) {
-            lineLength++;
-            if (lineLength > GameConsole.FULL_CONSOLE_WIDTH - 10 && text.charAt(i) == ' ') {
-                System.out.print("\n   ");
-                lineLength = 0;
-            } else if (text.charAt(i) == '\n') {
-                isNewline = true;
-                lineLength = 0;
-            } else if (text.charAt(i) == LEVEL_BEGIN_PLACEHOLDER) {
-                System.out.println("\n" + " ".repeat(GameConsole.FULL_CONSOLE_WIDTH / 2 - 8) + AsciiColours.GREEN
-                        + AsciiColours.UNDERLINE + "[LEVEL BEGIN]" + AsciiColours.SANE + "\n");
-                show("       Enter [Start] if you are ready to complete the objective or Enter [HINT] if you get stuck!");
-                return;
-            }
-            else {
-                System.out.printf("%c", text.charAt(i));
-            }
-            if (isNewline) {
-                System.out.print("\n    ");
-                isNewline = false;
-            }
-            sleep(10);
-        }
-        if (hasPressEnter) {
-            show("\n\n" + " ".repeat(GameConsole.FULL_CONSOLE_WIDTH - GameConsole.USER_CODE_SECTION_WIDTH)
-                    + "Press [ENTER] to continue..");
-        }
-        show("");
+    public void typeWriter(String text, boolean hasPressEnter) {
+
     }
 }
---
+++ src/test/java/FrontEndTest.java
@@ -0,0 +1,16 @@
+import exceptions.FarmioFatalException;
+import farmio.Farmer;
+import farmio.Farmio;
+import farmio.Level;
+import farmio.Storage;
+import frontend.Simulation;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FrontEndTest {
+    @Test
+    void test() {
+
+    }
+}
