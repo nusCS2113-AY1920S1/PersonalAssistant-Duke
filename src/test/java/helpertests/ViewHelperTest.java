9a477fd7413a872142a6a018fbee3ace20ad7f49
seanlimhx <42381169+seanlimhx@users.noreply.github.com>
2019-11-07 23:05:57 +0800

    [Test] Added test cases for ViewHelperTest

--- src/test/java/helpertests/ViewHelperTest.java
+++ src/test/java/helpertests/ViewHelperTest.java
@@ -94,4 +94,104 @@ public class ViewHelperTest {
         assertArrayEquals(expectedOutput3, actualOutput3);
     }
 
+    @Test
+    void consolePrintMultipleTablesTest_oneColumn() {
+        ArrayList<String> sampleTableContent1 = new ArrayList<>();
+        sampleTableContent1.add("Project 1: Test");
+        sampleTableContent1.add("Members:");
+        sampleTableContent1.add(" --");
+        sampleTableContent1.add("Next Deadline:");
+        sampleTableContent1.add(" - No deadlines left -");
+        sampleTableContent1.add("");
+        sampleTableContent1.add("Overall Progress:");
+        sampleTableContent1.add(" - Completed: 0%");
+        sampleTableContent1.add(" - In Progress: 0%");
+        sampleTableContent1.add(" - Not Done: 100%");
+        ArrayList<String> sampleTableContent2 = new ArrayList<>();
+        sampleTableContent2.add("Project 2: Another Test");
+        sampleTableContent2.add("Members:");
+        sampleTableContent2.add(" --");
+        sampleTableContent2.add("Next Deadline:");
+        sampleTableContent2.add(" --");
+        sampleTableContent2.add("Overall Progress:");
+        sampleTableContent2.add(" --");
+        ArrayList<ArrayList<String>> input = new ArrayList<>();
+        input.add(sampleTableContent1);
+        input.add(sampleTableContent2);
+        String[] actualOutput = viewHelper.consolePrintMultipleTables(input, DEFAULT_HORI_BORDER_LENGTH_70_FOR_TEST,
+                1, "Here are all the Projects you are managing:");
+        String[] expectedOutput = new String[] {
+                "+----------------------------------------------------------------------+",
+                "|Here are all the Projects you are managing:                           |",
+                "+----------------------------------------------------------------------+",
+                "| +------------------------------------------------------------------+ |",
+                "| |Project 1: Test                                                   | |",
+                "| +------------------------------------------------------------------+ |",
+                "| |Members:                                                          | |",
+                "| | --                                                               | |",
+                "| |Next Deadline:                                                    | |",
+                "| | - No deadlines left -                                            | |",
+                "| |                                                                  | |",
+                "| |Overall Progress:                                                 | |",
+                "| | - Completed: 0%                                                  | |",
+                "| | - In Progress: 0%                                                | |",
+                "| | - Not Done: 100%                                                 | |",
+                "| +------------------------------------------------------------------+ |",
+                "| +------------------------------------------------------------------+ |",
+                "| |Project 2: Another Test                                           | |",
+                "| +------------------------------------------------------------------+ |",
+                "| |Members:                                                          | |",
+                "| | --                                                               | |",
+                "| |Next Deadline:                                                    | |",
+                "| | --                                                               | |",
+                "| |Overall Progress:                                                 | |",
+                "| | --                                                               | |",
+                "| +------------------------------------------------------------------+ |",
+                "+----------------------------------------------------------------------+"};
+        assertArrayEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void consolePrintMultipleTablesTest_twoColumns() {
+        ArrayList<String> sampleTableContent1 = new ArrayList<>();
+        sampleTableContent1.add("test task (P: 1, D: --, C: 2, S: OPEN)");
+        sampleTableContent1.add("1. Cynthia");
+        sampleTableContent1.add("2. Dillen");
+        sampleTableContent1.add("3. Jerry");
+        ArrayList<String> sampleTableContent2 = new ArrayList<>();
+        sampleTableContent2.add("Watch show (P: 5, D: 01 Nov 2019, C: 3, S: DOING)");
+        sampleTableContent2.add("1. Dillen");
+        sampleTableContent2.add("2. Jerry");
+        ArrayList<String> sampleTableContent3 = new ArrayList<>();
+        sampleTableContent3.add("Eat food (P: 1, D: 03 Nov 2019, C: 3, S: OPEN)");
+        sampleTableContent3.add("1. Cynthia");
+        sampleTableContent3.add("2. Jerry");
+        ArrayList<ArrayList<String>> input = new ArrayList<>();
+        input.add(sampleTableContent1);
+        input.add(sampleTableContent2);
+        input.add(sampleTableContent3);
+        String[] actualOutput = viewHelper.consolePrintMultipleTables(input, DEFAULT_HORI_BORDER_LENGTH_70_FOR_TEST,
+                2, "Here are the members assigned to each task:");
+        String[] expectedOutput = new String[] {
+                "+----------------------------------------------------------------------+",
+                "|Here are the members assigned to each task:                           |",
+                "+----------------------------------------------------------------------+",
+                "| +-------------------------------+ +-------------------------------+  |",
+                "| |test task (P: 1, D: --, C: 2,  | |Watch show (P: 5, D: 01 Nov    |  |",
+                "| |S: OPEN)                       | |2019, C: 3, S: DOING)          |  |",
+                "| +-------------------------------+ +-------------------------------+  |",
+                "| |1. Cynthia                     | |1. Dillen                      |  |",
+                "| |2. Dillen                      | |2. Jerry                       |  |",
+                "| |3. Jerry                       | +-------------------------------+  |",
+                "| +-------------------------------+ +-------------------------------+  |",
+                "|                                   |Eat food (P: 1, D: 03 Nov      |  |",
+                "|                                   |2019, C: 3, S: OPEN)           |  |",
+                "|                                   +-------------------------------+  |",
+                "|                                   |1. Cynthia                     |  |",
+                "|                                   |2. Jerry                       |  |",
+                "|                                   +-------------------------------+  |",
+                "+----------------------------------------------------------------------+"};
+        assertArrayEquals(expectedOutput, actualOutput);
+    }
+
 }
