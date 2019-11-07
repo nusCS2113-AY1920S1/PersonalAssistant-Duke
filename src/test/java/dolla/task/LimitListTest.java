4948266bb92d39c426cc760525621e602fea2ff8
weng-kexin <weng.kexin98@gmail.com>
2019-11-07 12:01:57 +0800

    added test for limitList

---
+++ src/test/java/dolla/task/LimitListTest.java
@@ -0,0 +1,35 @@
+package dolla.task;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static dolla.parser.ParserStringList.LIMIT_DURATION_D;
+import static dolla.parser.ParserStringList.LIMIT_DURATION_W;
+import static dolla.parser.ParserStringList.LIMIT_TYPE_B;
+import static dolla.parser.ParserStringList.LIMIT_TYPE_S;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LimitListTest {
+
+    private Limit newBudget = createNewLimit(LIMIT_TYPE_B, 5, LIMIT_DURATION_D);
+
+    private Limit newSaving = createNewLimit(LIMIT_TYPE_S, 50, LIMIT_DURATION_W);
+
+    private Limit createNewLimit(String limitType, double amount, String limitDuration) {
+        return new Limit(limitType, amount, limitDuration);
+    }
+
+    private ArrayList<Record> createNewLimitList() {
+        ArrayList<Record> LimitList = new ArrayList<Record>();
+        return LimitList;
+    }
+
+    @Test
+    public void addTest1() {
+        ArrayList<Record> newLimitList = createNewLimitList();
+        newLimitList.add(newBudget);
+        newLimitList.add(newSaving);
+        assertEquals(2, newLimitList.size());
+    }
+}
