5bcd9a3e1b4ee5d1a6ec36c9ee9bfa6a0be015c6
priyan-coder <rajamohanpriyan@gmail.com>
2019-11-07 21:20:43 +0800

    adding percentage features to budget command

--- src/main/java/executor/command/CommandBudget.java
+++ src/main/java/executor/command/CommandBudget.java
@@ -4,6 +4,8 @@ import duke.exception.DukeException;
 import interpreter.Parser;
 import storage.StorageManager;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class CommandBudget extends Command {
@@ -29,8 +31,11 @@ public class CommandBudget extends Command {
         }
         DecimalFormat decimalFormat = new DecimalFormat("#0.00");
         storageManager.getWallet().getReceipts().setBudget(this.budgetAmount);
+        Double percentage = percentageOfBudgetExceeded(storageManager.getWalletExpenses());
         this.infoCapsule.setCodeToast();
-        this.infoCapsule.setOutputStr("Budget updated to: $" + decimalFormat.format(this.budgetAmount) + "\n");
+        this.infoCapsule.setOutputStr("Budget updated to: $" + decimalFormat.format(this.budgetAmount)
+                + ""
+                + "\n");
 
     }
 
@@ -47,5 +52,36 @@ public class CommandBudget extends Command {
             throw new DukeException("Please kindly follow the format : budget $<amount> \n"
                     + "Please enter an amount greater than zero!\n");
         }
+    }
+
+    private String percentageOfBudgetExceeded(Double amountSpent) throws DukeException {
+        try {
+            if (amountSpent <= getBudgetAmount()) {
+                return "You are still good and safe as you did not overspend your budget";
+            }
+
+            Double percent = ((amountSpent - getBudgetAmount()) / getBudgetAmount()) * 100;
+            return percent.toString();
+        } catch (Exception e) {
+            throw new DukeException("Unable to calculate percentage overspent!");
+        }
+    }
+
+    private String percentageOfBudgetUsedUp(Double amountSpent) {
+        
+    }
+
+
+    private double roundByDecimalPlace(double value, int places) {
+        if (places < 0) {
+            throw new IllegalArgumentException();
+        }
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    private Double getBudgetAmount() {
+        return budgetAmount;
     }
 }
