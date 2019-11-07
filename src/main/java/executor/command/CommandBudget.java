4890e20eb01f806d87dd86c248cbaf8e3479825f
priyan-coder <rajamohanpriyan@gmail.com>
2019-11-07 21:00:07 +0800

    budget feature implemented successfully

--- src/main/java/executor/command/CommandBudget.java
+++ src/main/java/executor/command/CommandBudget.java
@@ -28,24 +28,24 @@ public class CommandBudget extends Command {
             return;
         }
         DecimalFormat decimalFormat = new DecimalFormat("#0.00");
-        storageManager.setWalletBalance(this.budgetAmount);
+        storageManager.getWallet().getReceipts().setBudget(this.budgetAmount);
         this.infoCapsule.setCodeToast();
-        this.infoCapsule.setOutputStr("Balance updated to: $" + decimalFormat.format(this.budgetAmount) + "\n");
+        this.infoCapsule.setOutputStr("Budget updated to: $" + decimalFormat.format(this.budgetAmount) + "\n");
 
     }
 
     private Double extractAmount() throws DukeException {
-        String incomeStr = Parser.parseForPrimaryInput(this.commandType, this.userInput);
+        String userBudget = Parser.parseForPrimaryInput(this.commandType, this.userInput);
         try {
-            incomeStr = incomeStr.trim().replace("$", "");
-            Double amount = Double.parseDouble(incomeStr);
+            userBudget = userBudget.trim().replace("$", "");
+            Double amount = Double.parseDouble(userBudget);
             if (amount <= 0) {
                 throw new Exception();
             }
             return amount;
         } catch (Exception e) {
             throw new DukeException("Please kindly follow the format : budget $<amount> \n"
-                    + "Please enter an amount greater than zero in your wallet !\n");
+                    + "Please enter an amount greater than zero!\n");
         }
     }
 }
