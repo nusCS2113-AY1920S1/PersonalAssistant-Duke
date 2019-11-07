c99b3d25c9ff57c5b33226da715ad888788dca25
John Khoo <john_khoo@u.nus.edu>
2019-11-08 03:36:47 +0800

    Complete ImpressionResultSpec

--- src/main/java/duke/command/impression/ImpressionResultSpec.java
+++ src/main/java/duke/command/impression/ImpressionResultSpec.java
@@ -2,11 +2,14 @@ package duke.command.impression;
 
 import duke.DukeCore;
 import duke.command.ArgLevel;
-import duke.command.ArgSpec;
+import duke.command.ObjSpec;
 import duke.command.Switch;
+import duke.data.DukeObject;
+import duke.data.Impression;
+import duke.data.Investigation;
 import duke.exception.DukeException;
 
-public class ImpressionResultSpec extends ArgSpec {
+public class ImpressionResultSpec extends ObjSpec {
     private static final ImpressionResultSpec spec = new ImpressionResultSpec();
 
     public static ImpressionResultSpec getSpec() {
@@ -23,7 +26,24 @@ public class ImpressionResultSpec extends ArgSpec {
     @Override
     protected void execute(DukeCore core) throws DukeException {
         super.execute(core);
-        //Impression impression = ImpressionUtils.getImpression(core);
-        // TODO: find by name or index
+        Impression impression = ImpressionUtils.getImpression(core);
+        DukeObject obj = ImpressionUtils.getData(null, null, cmd.getArg(), impression);
+        if (obj == null) {
+            processResults(core, ImpressionUtils.searchData(null, null, cmd.getArg(), impression));
+        } else if (!(obj instanceof Investigation)) {
+            throw new DukeException("Only investigations can be converted into results!");
+        } else {
+            executeWithObj(core, obj);
+        }
+    }
+
+
+    @Override
+    protected void executeWithObj(DukeCore core, DukeObject obj) throws DukeException {
+        Impression impression = ImpressionUtils.getImpression(core);
+        Investigation invx = (Investigation) obj;
+        impression.addNewEvidence(invx.toResult(cmd.getSwitchVal("summary")));
+        impression.deleteTreatment(obj.getName());
+        core.ui.updateUi("Converted the '" + obj.getName() + "' investigation to a report!");
     }
 }
