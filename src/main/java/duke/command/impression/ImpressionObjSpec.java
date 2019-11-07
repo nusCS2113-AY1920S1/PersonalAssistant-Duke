f89215880ae580640b04b1f0fa84d85aca893267
John Khoo <john_khoo@u.nus.edu>
2019-11-08 03:14:40 +0800

    Complete ImpressionObjSpec

--- src/main/java/duke/command/impression/ImpressionObjSpec.java
+++ src/main/java/duke/command/impression/ImpressionObjSpec.java
@@ -2,6 +2,7 @@ package duke.command.impression;
 
 import duke.DukeCore;
 import duke.command.ObjSpec;
+import duke.data.DukeData;
 import duke.data.Impression;
 import duke.data.SearchResults;
 import duke.exception.DukeException;
@@ -12,8 +13,14 @@ public abstract class ImpressionObjSpec extends ObjSpec {
     protected void execute(DukeCore core) throws DukeException {
         super.execute(core);
         Impression impression = ImpressionUtils.getImpression(core);
-        SearchResults results = ImpressionUtils.getData(cmd.getArg(), cmd.getSwitchVal("evidence"),
+        DukeData data = ImpressionUtils.getData(cmd.getArg(), cmd.getSwitchVal("evidence"),
                 cmd.getSwitchVal("treatment"), impression);
-
+        if (data == null) {
+            SearchResults results = ImpressionUtils.searchData(cmd.getArg(), cmd.getSwitchVal("evidence"),
+                    cmd.getSwitchVal("treatment"), impression);
+            processResults(core, results);
+        } else {
+            executeWithObj(core, data);
+        }
     }
 }
