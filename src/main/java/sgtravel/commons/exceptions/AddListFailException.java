f0f9c931d3c319b3e4689d56a74a5f2ed85a6d6b
Sukrut1881 <e0311151@u.nus.edu>
2019-11-08 23:41:28 +0800

    Added exception header commands

--- src/main/java/sgtravel/commons/exceptions/AddListFailException.java
+++ src/main/java/sgtravel/commons/exceptions/AddListFailException.java
@@ -2,6 +2,9 @@ package sgtravel.commons.exceptions;
 
 import sgtravel.commons.Messages;
 
+/**
+ * Exception thrown when user does not specify new name while adding a recommendation.
+ */
 public class AddListFailException extends ParseException {
     public AddListFailException() {
         super(Messages.ERROR_ADDLIST_NAME_EMPTY);
--- src/main/java/sgtravel/commons/exceptions/ChronologyBeforePresentException.java
+++ src/main/java/sgtravel/commons/exceptions/ChronologyBeforePresentException.java
@@ -2,6 +2,9 @@ package sgtravel.commons.exceptions;
 
 import sgtravel.commons.Messages;
 
+/**
+ * Exception thrown when Dates entered are before present.
+ */
 public class ChronologyBeforePresentException extends ParseException {
     public ChronologyBeforePresentException() {
         super(Messages.ERROR_DATE_BEFORE_NOW);
