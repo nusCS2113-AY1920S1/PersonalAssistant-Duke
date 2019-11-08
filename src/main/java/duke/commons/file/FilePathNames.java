2588832ddfa0730a78b09146e30d6df1199a87a0
Hashir Zahir <hashirsahad@gmail.com>
2019-11-08 16:53:11 +0800

    Change filepath to be compatible with jar resource loading

--- src/main/java/duke/commons/file/FilePathNames.java
+++ src/main/java/duke/commons/file/FilePathNames.java
@@ -2,13 +2,13 @@ package duke.commons.file;
 
 //@@author HashirZahir
 public enum FilePathNames {
-    FILE_PATH_USER_MEALS_FILE("src/main/java/duke/Data/meals.json"),
-    FILE_PATH_DEFAULT_MEAL_FILE("src/main/java/duke/Data/defaultValues.json"),
-    FILE_PATH_GOAL_FILE("src/main/java/duke/Data/goal.json"),
-    FILE_PATH_USER_FILE("src/main/java/duke/Data/user.json"),
-    FILE_PATH_AUTOCORRECT_FILE("src/main/java/duke/Data/word.txt"),
-    FILE_PATH_TRANSACTION_FILE("src/main/java/duke/Data/transaction.json"),
-    FILE_PATH_MASTER_HELP_FILE("src/main/java/duke/commons/help");
+    FILE_PATH_USER_MEALS_FILE("duke/Data/meals.json"),
+    FILE_PATH_DEFAULT_MEAL_FILE("duke/Data/defaultValues.json"),
+    FILE_PATH_GOAL_FILE("duke/Data/goal.json"),
+    FILE_PATH_USER_FILE("duke/Data/user.json"),
+    FILE_PATH_AUTOCORRECT_FILE("duke/Data/word.txt"),
+    FILE_PATH_TRANSACTION_FILE("duke/Data/transaction.json"),
+    FILE_PATH_MASTER_HELP_FILE("duke/commons/help");
 
     private String filePath;
     FilePathNames(String filePath) {
