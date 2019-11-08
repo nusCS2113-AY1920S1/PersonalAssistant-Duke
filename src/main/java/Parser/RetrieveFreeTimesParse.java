b9ad063d77be04626f4887018d08bb1c0a5fd222
hwbjerry <e0319255@u.nus.edu>
2019-11-09 04:39:54 +0800

    Update RetrieveFreeTimesParse and RetrieveFreeTimesCommand

--- src/main/java/Parser/RetrieveFreeTimesParse.java
+++ src/main/java/Parser/RetrieveFreeTimesParse.java
@@ -5,6 +5,7 @@ import Commands.FindFreeTimesCommand;
 import Commands.RetrieveFreeTimesCommand;
 import Commons.DukeLogger;
 import DukeExceptions.DukeInvalidFormatException;
+import DukeExceptions.DukeNoValidDataException;
 import javafx.util.Pair;
 
 import java.util.ArrayList;
@@ -16,10 +17,15 @@ import java.util.logging.Logger;
 public class RetrieveFreeTimesParse extends Parse {
     private final Logger LOGGER = DukeLogger.getLogger(FindFreeTimesParse.class);
     private String fullCommand;
-    private final String invalidInput = "Invalid input. Please enter the command as follows. \n" +
-            "retrieve/time 'x', where 'x' is a digit between 1 - 5";
     private final String invalidOption = "Invalid option. Please enter the command as follows. \n" +
             "retrieve/time 'x', where 'x' is a digit between 1 - 5";
+    private final String invalidEmptyOption = "Invalid input.\n" +
+            "Option cannot be blank.\nPlease enter the command as follows.\n"
+            + "retrieve/time 'x', where 'x' is a digit between 1 - 5";
+    private final String invalidNoFreeTimeFound = "Please find free times by invoking the command shown below\n"
+            + "find/time 'x' hours, where 'x' is a digit between 1 - 16\n"
+            + "Followed by the command\n"
+            + "retrieve/time 'x', where 'x' is a digit between 1- 5";
 
     /**
      * Creates RetrieveFreeTimesParse object.
@@ -30,11 +36,13 @@ public class RetrieveFreeTimesParse extends Parse {
     }
 
     @Override
-    public Command parse() throws DukeInvalidFormatException {
-        fullCommand = fullCommand.replaceFirst("retrieve/time ", "");
+    public Command parse() throws DukeInvalidFormatException, DukeNoValidDataException {
+        ArrayList<Pair<String, String>> retrievedFreeTimes = FindFreeTimesCommand.getCompiledFreeTimesList();
+        if (checkIsEmpty(retrievedFreeTimes)) throw new DukeNoValidDataException(invalidNoFreeTimeFound);//TODO: add DukeNoDataFound
+        fullCommand = fullCommand.replaceFirst("retrieve/time", "");
         fullCommand = fullCommand.trim();
         if(fullCommand.isEmpty()){
-            throw new DukeInvalidFormatException(invalidInput);
+            throw new DukeInvalidFormatException(invalidEmptyOption);
         } else {
             try {
                 Integer option = Integer.parseInt(fullCommand);
@@ -42,9 +50,29 @@ public class RetrieveFreeTimesParse extends Parse {
                 else throw new DukeInvalidFormatException(invalidOption);
             } catch (NumberFormatException e) {
                 LOGGER.info("Unable to parse string to integer" + e.getMessage());
-                throw new DukeInvalidFormatException(invalidInput);
+                throw new DukeInvalidFormatException(invalidOption);
             }
         }
+    }
+
+    public static boolean isValidOption(String input) {
+        input = input.replace("retrieve/time", "");
+        input = input.trim();
+        if(input.isEmpty()){
+            return false;
+        } else {
+            try {
+                Integer option = Integer.parseInt(input);
+                if (option >=1 && option <= 5) return true;
+                else return false;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+    }
+
+    private boolean checkIsEmpty (ArrayList<Pair<String, String>> retrievedFreeTimes){
+        return (retrievedFreeTimes.size() == 0) ? true : false;
     }
 }
 
