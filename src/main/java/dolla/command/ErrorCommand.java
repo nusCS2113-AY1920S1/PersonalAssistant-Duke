99edff3b86a191ea96e28b5041dd6a66072aac55
weng-kexin <weng.kexin98@gmail.com>
2019-11-08 14:44:49 +0800

    added error command test

--- src/main/java/dolla/command/ErrorCommand.java
+++ src/main/java/dolla/command/ErrorCommand.java
@@ -3,14 +3,13 @@ package dolla.command;
 import dolla.model.DollaData;
 
 /**
- * duke.command.ErrorCommand is a type of command that is generated if a proper command
- * could not be produced. IE. as a result of invalid inputs.
+ * A command that is generated if a proper command could not be produced (as a result of invalid inputs).
  */
 public class ErrorCommand extends Command {
 
     @Override
     public void execute(DollaData dollaData) {
-        return; // Don't do anything
+        return;
     }
 
     @Override
---
+++ src/test/java/dolla/command/ErrorCommandTest.java
@@ -0,0 +1,15 @@
+package dolla.command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ErrorCommandTest {
+
+    @Test
+    public void ErrorCommandTest1() {
+        Command commandTest = new ErrorCommand();
+        String expected = "ErrorCommand";
+        assertEquals(expected, commandTest.getCommandInfo());
+    }
+}
