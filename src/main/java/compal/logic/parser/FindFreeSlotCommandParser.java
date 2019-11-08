755b43e96562242a46119f8bb5485fcf4e59f4e5
Catherinetan99 <42688483+Catherinetan99@users.noreply.github.com>
2019-11-08 12:29:13 +0800

    Added logging

--- src/main/java/compal/logic/parser/FindFreeSlotCommandParser.java
+++ src/main/java/compal/logic/parser/FindFreeSlotCommandParser.java
@@ -1,25 +1,30 @@
 package compal.logic.parser;
 
+import compal.commons.LogUtils;
 import compal.logic.command.Command;
 import compal.logic.command.FindFreeSlotCommand;
 import compal.logic.parser.exceptions.ParserException;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.logging.Logger;
 
 //@@author Catherinetan99
 public class FindFreeSlotCommandParser implements CommandParser {
 
+    private static final Logger logger = LogUtils.getLogger(FindFreeSlotCommandParser.class);
     public static final String MESSAGE_INVALID_DATE = "Error: Date entered cannot be a past date!";
 
     @Override
     public Command parseCommand(String restOfInput) throws ParserException, ParseException {
+        logger.info("Attempting to parse findfreeslot command");
         Date date = getDate(restOfInput);
         if (!isFutureDate(date)) {
             throw new ParserException(MESSAGE_INVALID_DATE);
         }
         int hour = getHour(restOfInput);
         int min = getMin(restOfInput);
+        logger.info("Successfully parse findfreeslot command");
         return new FindFreeSlotCommand(date, hour, min);
     }
 }
