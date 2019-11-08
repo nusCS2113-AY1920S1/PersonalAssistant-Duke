76def5a449ae5ae15fb13097e4f97127f4f24e43
koushireo <koushireo@gmail.com>
2019-11-08 12:46:51 +0800

    threw unused codes

---
+++ unused/GUI.java
@@ -0,0 +1,145 @@
+package duke.gui;
+
+import duke.exceptions.DukeException;
+import duke.tasks.Meal;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.application.Platform;
+import jdk.jfr.Category;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
+//@@author koushireo-unused
+
+public class GUI extends Application {
+    private static HashMap<String, Integer> weight;
+    private static HashMap<String, ArrayList<Meal>> mealList;
+    private static String startDate;
+    private static String endDate;
+    private static String type;
+    private Stage mainStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void setWeight(HashMap<String, Integer> weight) {
+        this.weight = weight;
+    }
+
+    public void setMealList(HashMap<String, ArrayList<Meal>> mealList) {
+        this.mealList = mealList;
+    }
+
+    public void setStartDate(String date) {
+        this.startDate = date;
+    }
+
+    public void setEndDate(String date) {
+        this.endDate = date;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public void start(Stage stage) throws DukeException {
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Date");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("calorie");
+
+        LineChart<String, Number> lineChart = new LineChart<String, Number>(xAxis, yAxis);
+
+        XYChart.Series dataSeries1 = new XYChart.Series();
+        XYChart.Series dataSeries2 = new XYChart.Series();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate startDate = LocalDate.parse(this.startDate, formatter);
+        LocalDate endDate = LocalDate.parse(this.endDate, formatter);
+        int days = (int) ChronoUnit.DAYS.between(startDate, endDate);
+        Calendar cal = Calendar.getInstance();
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            Date date = sdf.parse(this.startDate);
+            cal.setTime(date);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+
+        for (int i = 0; i <= days; i += 1) {
+            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            String current = dateFormat.format(cal.getTime());
+            System.out.println(current);
+            if (this.type.equals("weight")) {
+                if (weight.containsKey(current)) {
+                    System.out.println(weight.get(current));
+                    dataSeries1.getData().add(new XYChart.Data(current, weight.get(current)));
+                    dataSeries2.getData().add(new XYChart.Data(current, 100));
+                } else {
+                    System.out.println(0);
+                    dataSeries1.getData().add(new XYChart.Data(current, 0));
+                    dataSeries2.getData().add(new XYChart.Data(current, 100));
+                }
+            } else {
+                int value = 0;
+                if (!mealList.containsKey(current)){
+                    dataSeries1.getData().add(new XYChart.Data(current, value));
+                }
+                else {
+                    ArrayList<Meal> currentMealInDay = mealList.get(current);
+                    for (int j = 0; j < currentMealInDay.size(); j += 1) {
+                        HashMap<String, Integer> currentMeal = currentMealInDay.get(j).getNutritionalValue();
+                        if (currentMeal.containsKey(this.type)) {
+                            value += currentMeal.get(this.type);
+                        }
+                    }
+                    System.out.println(value);
+                    dataSeries1.getData().add(new XYChart.Data(current, value));
+                }
+            }
+            cal.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        lineChart.getData().add(dataSeries1);
+        lineChart.getData().add(dataSeries2);
+        HBox hbox = new HBox(lineChart);
+
+        Scene scene = new Scene(hbox,500,500);
+        stage.setTitle("duke");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ unused/GUICommand.java
@@ -0,0 +1,52 @@
+package duke.commands;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import duke.exceptions.DukeException;
+import duke.gui.GUI;
+import duke.tasks.Meal;
+import duke.tasks.MealList;
+import duke.ui.Ui;
+import duke.storage.Storage;
+import duke.user.User;
+
+//@@author koushireo-unused
+
+/**
+ * Display desired data based on dates
+ */
+public class GUICommand extends Command {
+    private String startDate;
+    private String endDate;
+    private String type;
+
+    /**
+     * This is a constructor for AddCommand which create a new AddCommand object with
+     * the meal specified as the instance field meal.
+     * @param meal The meal to be added.
+     */
+    public GUICommand(String type, String startDate, String endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.type = type;
+    }
+
+    /**
+     * The object will execute the "add" command, updating the current tasks, ui, and storage in the process.
+     * @param meals the MealList object in which the meal is supposed to be added
+     * @param ui the ui object to display the user interface of an "add" command
+     * @param storage the storage object that stores the list of meals
+     * @param in the scanner object to handle secondary command IO
+     */
+    @Override
+    public void execute(MealList meals, Ui ui, Storage storage, User user, Scanner in, GUI gui) throws DukeException {
+        gui.setMealList(meals.getMealTracker());
+        gui.setWeight(user.getAllWeight());
+        gui.setStartDate(this.startDate);
+        gui.setEndDate(this.endDate);
+        gui.setType(this.type);
+        String[] args = {};
+        gui.main(args);
+    }
+}
