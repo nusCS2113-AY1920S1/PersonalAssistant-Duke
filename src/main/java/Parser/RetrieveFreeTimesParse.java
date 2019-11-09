ec354e06193d2d3eb4d319e737e599770ce9f449
hwbjerry <e0319255@u.nus.edu>
2019-11-09 20:34:09 +0800

    Update RetrieveFreeTimesCommand and RetrieveFreeTimesParse

--- src/main/java/Parser/RetrieveFreeTimesParse.java
+++ src/main/java/Parser/RetrieveFreeTimesParse.java
@@ -18,10 +18,10 @@ import java.util.logging.Logger;
 public class RetrieveFreeTimesParse extends Parse {
     private final Logger LOGGER = DukeLogger.getLogger(FindFreeTimesParse.class);
     private String fullCommand;
-    private final String invalidOption = "Invalid option. Please enter the command as follows. \n" +
-            "retrieve/time 'x', where 'x' is a digit between 1 - 5";
-    private final String invalidEmptyOption = "Invalid input.\n" +
-            "Option cannot be blank.\nPlease enter the command as follows.\n"
+    private final String invalidOption = "Invalid option. Please enter the command as follows. \n"
+            + "retrieve/time 'x', where 'x' is a digit between 1 - 5";
+    private final String invalidEmptyOption = "Invalid input.\n"
+            + "Option cannot be blank.\nPlease enter the command as follows.\n"
             + "retrieve/time 'x', where 'x' is a digit between 1 - 5";
     private final String invalidNoFreeTimeFound = "Please find free times by invoking the command shown below\n"
             + "find/time 'x' hours, where 'x' is a digit between 1 - 16\n"
@@ -39,16 +39,22 @@ public class RetrieveFreeTimesParse extends Parse {
     @Override
     public Command parse() throws DukeInvalidFormatException, DukeNoValidDataException {
         ArrayList<Pair<String, String>> retrievedFreeTimes = FindFreeTimesCommand.getCompiledFreeTimesList();
-        if (checkIsEmpty(retrievedFreeTimes)) throw new DukeNoValidDataException(invalidNoFreeTimeFound);
+        if (checkIsEmpty(retrievedFreeTimes)) {
+            throw new DukeNoValidDataException(invalidNoFreeTimeFound);
+        }
         fullCommand = fullCommand.replaceFirst(DukeConstants.RETRIEVE_TIME_HEADER, "");
         fullCommand = fullCommand.trim();
-        if(fullCommand.isEmpty()){
+        if (fullCommand.isEmpty()) {
             throw new DukeInvalidFormatException(invalidEmptyOption);
         } else {
             try {
                 Integer option = Integer.parseInt(fullCommand);
-                if (option >= DukeConstants.RETRIEVE_TIME_LOWER_BOUNDARY && option <= DukeConstants.RETRIEVE_TIME_UPPER_BOUNDARY) return new RetrieveFreeTimesCommand(option);
-                else throw new DukeInvalidFormatException(invalidOption);
+                if (option >= DukeConstants.RETRIEVE_TIME_LOWER_BOUNDARY
+                        && option <= DukeConstants.RETRIEVE_TIME_UPPER_BOUNDARY) {
+                    return new RetrieveFreeTimesCommand(option);
+                } else {
+                    throw new DukeInvalidFormatException(invalidOption);
+                }
             } catch (NumberFormatException e) {
                 LOGGER.info("Unable to parse string to integer" + e.getMessage());
                 throw new DukeInvalidFormatException(invalidOption);
@@ -56,23 +62,32 @@ public class RetrieveFreeTimesParse extends Parse {
         }
     }
 
+    /**
+     * This method checks if the option is valid.
+     * @param input The retrieveFreeTimesCommand input
+     * @return true if it is valid. Otherwise, false
+     */
     public static boolean isValidOption(String input) {
         input = input.replace(DukeConstants.RETRIEVE_TIME_HEADER, "");
         input = input.trim();
-        if(input.isEmpty()){
+        if (input.isEmpty()) {
             return false;
         } else {
             try {
                 Integer option = Integer.parseInt(input);
-                if (option >= DukeConstants.RETRIEVE_TIME_LOWER_BOUNDARY && option <= DukeConstants.RETRIEVE_TIME_UPPER_BOUNDARY) return true;
-                else return false;
+                if (option >= DukeConstants.RETRIEVE_TIME_LOWER_BOUNDARY
+                        && option <= DukeConstants.RETRIEVE_TIME_UPPER_BOUNDARY) {
+                    return true;
+                } else {
+                    return false;
+                }
             } catch (NumberFormatException e) {
                 return false;
             }
         }
     }
 
-    private boolean checkIsEmpty (ArrayList<Pair<String, String>> retrievedFreeTimes){
+    private boolean checkIsEmpty(ArrayList<Pair<String, String>> retrievedFreeTimes) {
         return (retrievedFreeTimes.size() == 0) ? true : false;
     }
 }
