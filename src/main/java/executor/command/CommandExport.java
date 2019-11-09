6209c22f988d8887af2c8dbefa13873695ab089b
priyan-coder <rajamohanpriyan@gmail.com>
2019-11-09 16:33:16 +0800

    done with checkstyle and feature is successful

--- src/main/java/executor/command/CommandExport.java
+++ src/main/java/executor/command/CommandExport.java
@@ -26,12 +26,12 @@ public class CommandExport extends Command {
     public void execute(StorageManager storageManager) {
         String userMessageForEnteringExtraFields = " ";
 
-        if(this.isThereExtraInputByUser(this.userInput)){
+        if (this.isThereExtraInputByUser(this.userInput)) {
             userMessageForEnteringExtraFields = "Incorrect Command but DUKE$$$ understands"
-                    + " you would want to export to csv !\n";
+                    + " you would want to export Wallet to csv !\n";
         }
 
-        try{
+        try {
             File csv = new File("data.csv");
             FileWriter outputFile = new FileWriter(csv);
             CSVWriter writer = new CSVWriter(outputFile);
@@ -39,7 +39,7 @@ public class CommandExport extends Command {
             writer.writeNext(header);
             storageManager.saveAllData();
             int i = 0;
-            for(Receipt receipt :storageManager.getWallet().getReceipts()){
+            for (Receipt receipt :storageManager.getWallet().getReceipts()) {
                 String eachRowOfData = (i + 1) + ". "
                         + receipt.getTags().toString().replaceAll(" ", "") + " "
                         + receipt.getCashSpent() + " "
@@ -50,16 +50,21 @@ public class CommandExport extends Command {
             writer.close();
             this.infoCapsule.setCodeCli();
             this.infoCapsule.setOutputStr("data.csv has been created ! Please check the project folder \n"
-            + userMessageForEnteringExtraFields);
+                    + userMessageForEnteringExtraFields);
         } catch (DukeException | IOException e) {
             this.infoCapsule.setCodeError();
             this.infoCapsule.setOutputStr(e.getMessage());
         }
     }
 
-
+    /**
+     * convertReceiptsToCSV is the method used to change receipts into CSV.
+     * @param dataTobeAdded String is the row of data which is to be changed to csv
+     * @param writer CSVWriter is the library used to write to CSV
+     * @throws DukeException Method throws Duke Exception if unable to write to csv
+     */
     private void convertReceiptsToCsv(String dataTobeAdded, CSVWriter writer) throws DukeException {
-        try{
+        try {
             String[] entries = dataTobeAdded.split(" ");
             writer.writeNext(entries);
         } catch (Exception e) {
@@ -67,7 +72,12 @@ public class CommandExport extends Command {
         }
     }
 
-    private boolean isThereExtraInputByUser(String userInput){
+    /**
+     * isThereExtraInputByUser is the method used to check if the user has entered misleading entries.
+     * @param userInput String is the user input entered in the GUI
+     * @return false is returned if user enters the correct command for exporting into csv
+     */
+    private boolean isThereExtraInputByUser(String userInput) {
         String additionalEntriesOfUser = Parser.parseForPrimaryInput(this.commandType, userInput);
         return !additionalEntriesOfUser.isEmpty();
     }
