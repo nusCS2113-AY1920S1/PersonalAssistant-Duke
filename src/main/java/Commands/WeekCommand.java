49ce5e820a181160487ac35aa75bb8da5e922c5b
hwbjerry <e0319255@u.nus.edu>
2019-11-09 20:32:56 +0800

    Update WeekCommand, WeekParse, and WeekList

--- src/main/java/Commands/WeekCommand.java
+++ src/main/java/Commands/WeekCommand.java
@@ -1,6 +1,10 @@
 package Commands;
 
-import Commons.*;
+import Commons.DukeConstants;
+import Commons.LookupTable;
+import Commons.Storage;
+import Commons.UserInteraction;
+import Commons.WeekList;
 import Tasks.Assignment;
 import Tasks.TaskList;
 import javafx.collections.FXCollections;
@@ -35,11 +39,12 @@ public class WeekCommand extends Command {
         this.week = fullCommand;
     }
 
-    private ArrayList<String> generateDateDay(String date, LookupTable LT){
+    private ArrayList<String> generateDateDay(String date, LookupTable lookupTable) {
         String[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
         ArrayList<String> temp = new ArrayList<>();
         for (String day : days) {
-            String dateOut = day + DukeConstants.STRING_SPACE_SPLIT_KEYWORD + LT.getValue(date + DukeConstants.STRING_SPACE_SPLIT_KEYWORD + day);
+            String dateOut = day + DukeConstants.STRING_SPACE_SPLIT_KEYWORD
+                    + lookupTable.getValue(date + DukeConstants.STRING_SPACE_SPLIT_KEYWORD + day);
             temp.add(dateOut);
         }
         return temp;
@@ -47,52 +52,70 @@ public class WeekCommand extends Command {
 
     private ArrayList<String> checkIfExist(HashMap<String, ArrayList<Assignment>> map, ArrayList<String> dates) {
         ArrayList<String> newDates = new ArrayList<>();
-        for(String s: dates){
-            if(map.containsKey(s) == true) newDates.add(s);
+        for (String s: dates) {
+            if (map.containsKey(s) == true) {
+                newDates.add(s);
+            }
         }
         return newDates;
     }
 
-    private void updateList(String day, Text toShow){
-        switch (day){
-            case "Mon":
-                monList.add(toShow);
-                break;
-            case  "Tue":
-                tueList.add(toShow);
-                break;
-            case "Wed":
-                wedList.add(toShow);
-                break;
-            case "Thu":
-                thuList.add(toShow);
-                break;
-            case "Fri":
-                friList.add(toShow);
-                break;
-            case "Sat":
-                satList.add(toShow);
-                break;
-            case "Sun":
-                sunList.add(toShow);
-                break;
+    private void updateList(String day, Text toShow) {
+        switch (day) {
+        case "Mon":
+            monList.add(toShow);
+            break;
+        case  "Tue":
+            tueList.add(toShow);
+            break;
+        case "Wed":
+            wedList.add(toShow);
+            break;
+        case "Thu":
+            thuList.add(toShow);
+            break;
+        case "Fri":
+            friList.add(toShow);
+            break;
+        case "Sat":
+            satList.add(toShow);
+            break;
+        case "Sun":
+            sunList.add(toShow);
+            break;
+        default:
+            break;
         }
     }
 
     private void sortList() {
-        if(monList.size() != 0 ) monList.sort(WeekCommand::compareByTime);
-        if(tueList.size() != 0 ) tueList.sort(WeekCommand::compareByTime);
-        if(wedList.size() != 0 ) wedList.sort(WeekCommand::compareByTime);
-        if(thuList.size() != 0 ) thuList.sort(WeekCommand::compareByTime);
-        if(friList.size() != 0 ) friList.sort(WeekCommand::compareByTime);
-        if(satList.size() != 0 ) satList.sort(WeekCommand::compareByTime);
-        if(sunList.size() != 0 ) sunList.sort(WeekCommand::compareByTime);
+        if (monList.size() != 0) {
+            monList.sort(WeekCommand::compareByTime);
+        }
+        if (tueList.size() != 0) {
+            tueList.sort(WeekCommand::compareByTime);
+        }
+        if (wedList.size() != 0) {
+            wedList.sort(WeekCommand::compareByTime);
+        }
+        if (thuList.size() != 0) {
+            thuList.sort(WeekCommand::compareByTime);
+        }
+        if (friList.size() != 0) {
+            friList.sort(WeekCommand::compareByTime);
+        }
+        if (satList.size() != 0) {
+            satList.sort(WeekCommand::compareByTime);
+        }
+        if (sunList.size() != 0) {
+            sunList.sort(WeekCommand::compareByTime);
+        }
     }
 
     private Text generateToShow(Assignment task) {
         Text toShow = new Text(task.toShow() + task.getModCode() + "\n" + task.getDescription());
         toShow.setFont(Font.font(10));
-        if (task.getStatus()){
+        if (task.getStatus()) {
             toShow.setFill(Color.GAINSBORO);
             toShow.setStrikethrough(true);
         }
@@ -100,17 +123,17 @@ public class WeekCommand extends Command {
     }
 
     /**
-     * This method generates data in day GridPane ListViews based on the week selected
+     * This method generates data in day GridPane ListViews based on the week selected.
      */
     public void setListView(LookupTable lookupTable, TaskList eventsList) {
         ArrayList<String> weekDates = generateDateDay(week, lookupTable);
-        for(String module: eventsList.getMap().keySet()) {
+        for (String module: eventsList.getMap().keySet()) {
             HashMap<String, ArrayList<Assignment>> moduleValue = eventsList.getMap().get(module);
             ArrayList<String> dates = checkIfExist(moduleValue, weekDates);
-            for(String strDate : dates) {
+            for (String strDate : dates) {
                 String[] spilt = strDate.split(DukeConstants.STRING_SPACE_SPLIT_KEYWORD, 2);
                 ArrayList<Assignment> data = moduleValue.get(strDate);
-                for(Assignment task: data){
+                for (Assignment task: data) {
                     Text toShow = generateToShow(task);
                     String day = spilt[0];
                     updateList(day, toShow);
@@ -133,12 +156,13 @@ public class WeekCommand extends Command {
         String[] rightSplit = right.split(newLine,2);
         String[] rightTimeSplit = rightSplit[0].split(DukeConstants.STRING_SPACE_SPLIT_KEYWORD);
 
-        if(leftTimeSplit[1].equals(twelveHourTimeAMPostFix) && rightTimeSplit[1].equals(twelveHourTimeAMPostFix)){
+        if (leftTimeSplit[1].equals(twelveHourTimeAMPostFix) && rightTimeSplit[1].equals(twelveHourTimeAMPostFix)) {
             String[]leftTimeSplitHourMinute = leftTimeSplit[0].split(twelveHourTimeFormatSeparator);
             String[]rightTimeSplitHourMinute = rightTimeSplit[0].split(twelveHourTimeFormatSeparator);
-            if(leftTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary) && rightTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)) {
+            if (leftTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)
+                    && rightTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)) {
                 return leftTimeSplitHourMinute[1].compareTo(rightTimeSplitHourMinute[1]);
-            } else if(leftTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)) {
+            } else if (leftTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)) {
                 return -1;
             } else if (rightTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)) {
                 return 1;
@@ -152,9 +176,10 @@ public class WeekCommand extends Command {
         } else {
             String[]leftTimeSplitHourMinute = leftTimeSplit[0].split(twelveHourTimeFormatSeparator);
             String[]rightTimeSplitHourMinute = rightTimeSplit[0].split(twelveHourTimeFormatSeparator);
-            if(leftTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary) && rightTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)) {
+            if (leftTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)
+                    && rightTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)) {
                 return leftTimeSplitHourMinute[1].compareTo(rightTimeSplitHourMinute[1]);
-            } else if(leftTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)) {
+            } else if (leftTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)) {
                 return -1;
             } else if (rightTimeSplitHourMinute[0].equals(twelveHourTimeHourBoundary)) {
                 return 1;
@@ -164,7 +189,7 @@ public class WeekCommand extends Command {
         }
     }
 
-    public static WeekList getWeekList(){
+    public static WeekList getWeekList() {
         return weekList;
     }
 
--- src/main/java/Parser/WeekParse.java
+++ src/main/java/Parser/WeekParse.java
@@ -7,13 +7,13 @@ import DukeExceptions.DukeInvalidFormatException;
 
 public class WeekParse extends Parse {
     private String fullCommand;
-    private final String invalidEmptyWeek = "Invalid Input.\n" +
-            "The week cannot be blank.\nPlease enter the command as follows.\n" +
-            "show/week 'x' , where 'x' is a digit between 1 - 13 or \n" +
-            "'x' is either 'recess', 'reading', or 'exam'";
-    private final String invalidWeek = "Invalid Week. Please enter the command as follows. \n" +
-            "show/week 'x' , where 'x' is a digit between 1 - 13 or \n" +
-            "'x' is either 'recess', 'reading', or 'exam'";
+    private final String invalidEmptyWeek = "Invalid Input.\n"
+            + "The week cannot be blank.\nPlease enter the command as follows.\n"
+            + "show/week 'x' , where 'x' is a digit between 1 - 13 or \n"
+            + "'x' is either 'recess', 'reading', or 'exam'";
+    private final String invalidWeek = "Invalid Week. Please enter the command as follows. \n"
+            + "show/week 'x' , where 'x' is a digit between 1 - 13 or \n"
+            + "'x' is either 'recess', 'reading', or 'exam'";
 
     public WeekParse(String fullCommand) {
         this.fullCommand = fullCommand;
@@ -26,39 +26,57 @@ public class WeekParse extends Parse {
             return new WeekCommand(fullCommand);
         }
         fullCommand = fullCommand.replaceFirst(DukeConstants.SHOW_WEEK_HEADER, "");
-        if(fullCommand.trim().isEmpty()) {
+        if (fullCommand.trim().isEmpty()) {
             throw new DukeInvalidFormatException(invalidEmptyWeek);
         }
         throw new DukeInvalidFormatException(invalidWeek);
     }
 
+    /**
+     * This method checks if the input is valid.
+     * @param fullCommand The command input by the user
+     * @return true if the input is valid. Otherwise, false
+     */
     public static boolean isValid(String fullCommand) {
         String strWeek = fullCommand.replaceFirst(DukeConstants.SHOW_WEEK_HEADER, "");
-        if(!strWeek.isEmpty()) {
+        if (!strWeek.isEmpty()) {
             char checkSpace = strWeek.charAt(0);
-            if (checkSpace != ' ') return false;
+            if (checkSpace != ' ') {
+                return false;
+            }
         }
         strWeek = strWeek.trim();
-        if(strWeek.isEmpty()) {
+        if (strWeek.isEmpty()) {
             return false;
-        } else if (strWeek.equals(DukeConstants.WEEK_FORMAT_KEYWORD_RECESS) || strWeek.equals(DukeConstants.WEEK_FORMAT_KEYWORD_READING)
+        } else if (strWeek.equals(DukeConstants.WEEK_FORMAT_KEYWORD_RECESS)
+                || strWeek.equals(DukeConstants.WEEK_FORMAT_KEYWORD_READING)
                 || strWeek.equals(DukeConstants.WEEK_FORMAT_KEYWORD_EXAM)) {
             return true;
-        }else {
-            try{
+        } else {
+            try {
                 Integer week = Integer.parseInt(strWeek);
-                if(week >= 1 && week <= 13) return true;
-                else return false;
+                if (week >= 1 && week <= 13) {
+                    return true;
+                } else {
+                    return false;
+                }
             } catch (NumberFormatException e) {
                 return false;
             }
         }
     }
 
+    /**
+     * This method converts the given week format to the format in the lookup table.
+     * @param fullCommand The WeekCommand with the selected week
+     * @return The week in the lookup table format
+     */
     public static String getWeek(String fullCommand) {
         String week = fullCommand.replaceFirst(DukeConstants.SHOW_WEEK_HEADER, "");
         week = week.trim();
-        if ((week.equals(DukeConstants.WEEK_FORMAT_KEYWORD_RECESS) || week.equals(DukeConstants.WEEK_FORMAT_KEYWORD_READING) || week.equals(DukeConstants.WEEK_FORMAT_KEYWORD_EXAM))) {
+        if ((week.equals(DukeConstants.WEEK_FORMAT_KEYWORD_RECESS)
+                || week.equals(DukeConstants.WEEK_FORMAT_KEYWORD_READING)
+                || week.equals(DukeConstants.WEEK_FORMAT_KEYWORD_EXAM))) {
             week = capitalizeWord(week);
             week = week + DukeConstants.STRING_SPACE_SPLIT_KEYWORD + DukeConstants.WEEK_FORMAT_KEYWORD;
         } else {
@@ -67,14 +85,24 @@ public class WeekParse extends Parse {
         return week;
     }
 
+    /**
+     * This method converts the given week format to the WeekCommand format.
+     * @param selectedWeek The chosen week
+     * @return The week in WeekCommand format
+     */
     public static String getWeekCommandFormat(String selectedWeek) {
         String week;
-        if((selectedWeek.toLowerCase().startsWith(DukeConstants.WEEK_FORMAT_KEYWORD_RECESS) || selectedWeek.toLowerCase().startsWith(DukeConstants.WEEK_FORMAT_KEYWORD_READING) || selectedWeek.toLowerCase().startsWith(DukeConstants.WEEK_FORMAT_KEYWORD_EXAM)) && selectedWeek.contains("Week")) {
+        if ((selectedWeek.toLowerCase().startsWith(DukeConstants.WEEK_FORMAT_KEYWORD_RECESS)
+                || selectedWeek.toLowerCase().startsWith(DukeConstants.WEEK_FORMAT_KEYWORD_READING)
+                || selectedWeek.toLowerCase().startsWith(DukeConstants.WEEK_FORMAT_KEYWORD_EXAM))
+                && selectedWeek.contains("Week")) {
             selectedWeek = selectedWeek.replace(DukeConstants.WEEK_FORMAT_KEYWORD, "");
             selectedWeek = selectedWeek.trim();
-            week = DukeConstants.SHOW_WEEK_HEADER + DukeConstants.STRING_SPACE_SPLIT_KEYWORD + selectedWeek.toLowerCase();
+            week = DukeConstants.SHOW_WEEK_HEADER
+                    + DukeConstants.STRING_SPACE_SPLIT_KEYWORD
+                    + selectedWeek.toLowerCase();
         } else {
-            week = DukeConstants.SHOW_WEEK_HEADER +selectedWeek.replaceFirst(DukeConstants.WEEK_FORMAT_KEYWORD, "");//reading week got problem
+            week = DukeConstants.SHOW_WEEK_HEADER + selectedWeek.replaceFirst(DukeConstants.WEEK_FORMAT_KEYWORD, "");
         }
         return week;
     }
