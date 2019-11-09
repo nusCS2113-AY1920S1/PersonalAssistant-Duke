b8f6cdc9181d2faae3161c9cf74794dc3786dd5a
Xiaoxue <e0323328@u.nus.edu>
2019-11-09 22:03:09 +0800

    refactor debts part in DollaParser

--- src/main/java/dolla/parser/DollaParser.java
+++ src/main/java/dolla/parser/DollaParser.java
@@ -39,26 +39,11 @@ public class DollaParser extends Parser {
             */
 
         } else if (commandToRun.equals(DEBT_COMMAND_OWE) || commandToRun.equals(DEBT_COMMAND_BORROW)) {
-            String type = commandToRun;
-            String name = null;
-            double amount = 0.0;
-            LocalDate date = null;
-            try {
-                name = inputArray[1];
-                amount = stringToDouble(inputArray[2]);
-
-                String[] desc = inputLine.split(inputArray[2] + SPACE);
-                String[] dateString = desc[1].split(" /due ");
-                description = dateString[0];
-                date = Time.readDate(dateString[1].trim());
-            } catch (IndexOutOfBoundsException e) {
-                DebtUi.printInvalidDebtFormatError();
-                return new ErrorCommand();
-            } catch (Exception e) {
+            if (verifyDebtCommand()) {
+                return new AddDebtsCommand(commandToRun, inputArray[1], amount, description, date);
+            } else {
                 return new ErrorCommand();
             }
-            return new AddDebtsCommand(type, name, amount, description, date);
-
         } else if (commandToRun.equals(ParserStringList.LIMIT_COMMAND_SET)) {
             if (verifySetCommand()) {
                 return new AddLimitCommand(type, amount, duration);
