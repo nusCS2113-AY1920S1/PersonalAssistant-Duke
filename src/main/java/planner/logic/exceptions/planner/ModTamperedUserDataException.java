2c865a7a2146cce0c3a7794a928b2f35f6efa7b5
LongLeCE <siromansiroas@gmail.com>
2019-11-10 03:19:29 +0800

    Updated tampered data message

--- src/main/java/planner/logic/exceptions/planner/ModTamperedUserDataException.java
+++ src/main/java/planner/logic/exceptions/planner/ModTamperedUserDataException.java
@@ -6,6 +6,6 @@ public class ModTamperedUserDataException extends ModException {
 
     @Override
     public String getMessage() {
-        return super.getMessage() + "User data has been tampered with!";
+        return super.getMessage() + "User data has been tampered, not using saved data!";
     }
 }
