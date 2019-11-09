d69b0140fc5b1165fec647b9c4833f1b101b64d2
J.J. Liu <liujiajun1999@gmail.com>
2019-11-09 17:50:48 +0800

    Change error message (#280)
    * Use formal dates
    
    * Disallow input for re
    
    * Disallow input for re
    
    * Add shortcut tests
    
    * Update docs for TimeParser
    
    * Update DG
    
    * Fix mass operation bug
    
    * Add tests; more OOP for order
    
    * Integrate demo file
    
    * Update AutoCompleter
    
    * Update Order
    
    * Remove unused commands
    
    * Clearer UI code
    
    * Code quality
    
    * Update diagram
    
    * Update DG
    
    * Change error message

--- src/main/java/duke/commons/core/Message.java
+++ src/main/java/duke/commons/core/Message.java
@@ -9,7 +9,8 @@ public class Message {
     public static final String MESSAGE_INVALID_PREFIX = "Prefix %s does not exist.";
     public static final String MESSAGE_INVALID_DATE = "Date is invalid.";
     public static final String MESSAGE_ITEM_MISSING_NAME_OR_QUANTITY = "Item name or quantity is not specified.";
-    public static final String MESSAGE_INVALID_NUMBER_FORMAT = "Number format is invalid.";
+    public static final String MESSAGE_INVALID_NUMBER_FORMAT = "Please enter a valid number";
+    public static final String MESSAGE_INVALID_QUANTITY = "Please enter a valid quantity";
     public static final String MESSAGE_INVALID_INDEX = "Index is invalid.";
     public static final String MESSAGE_INVALID_RANGE = "Range is invalid.";
     public static final String MESSAGE_INVALID_STATUS = "Status is invalid.";
