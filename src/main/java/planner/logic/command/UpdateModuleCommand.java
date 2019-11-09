3e099df4c5d949c4441eac52714ed87dc6c05d4e
namiwa <khairul.nasri895@gmail.com>
2019-11-10 01:15:27 +0800

    Fixed bug in update command

---
+++ src/main/java/planner/logic/command/UpdateModuleCommand.java
@@ -0,0 +1,48 @@
+// @@author namiwa
+
+package planner.logic.command;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+
+import planner.credential.user.User;
+import planner.logic.exceptions.legacy.ModCommandException;
+import planner.logic.exceptions.legacy.ModEmptyCommandException;
+import planner.logic.exceptions.legacy.ModException;
+import planner.logic.exceptions.planner.ModUpdateErrorException;
+import planner.logic.modules.module.ModuleInfoDetailed;
+import planner.ui.cli.PlannerUi;
+import planner.util.crawler.JsonWrapper;
+import planner.util.datetime.NattyWrapper;
+import planner.util.storage.Storage;
+
+public class UpdateModuleCommand extends ModuleCommand {
+
+    public UpdateModuleCommand(Arguments args) {
+        super(args);
+    }
+
+    @Override
+    public void execute(
+            HashMap<String, ModuleInfoDetailed> detailedMap,
+            PlannerUi plannerUi,
+            Storage store,
+            JsonWrapper jsonWrapper,
+            User profile) throws ModException {
+        String year = arg("academicYear");
+        if (year.equals("module")) {
+            NattyWrapper nattyWrapper = new NattyWrapper();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy");
+            String thisYear = nattyWrapper.dateToLocalDateTime("this year").format(formatter);
+            String nextYear = nattyWrapper.dateToLocalDateTime("next year").format(formatter);
+            year = thisYear + "-" + nextYear;
+            jsonWrapper.runRequests(year, store);
+            detailedMap.putAll(jsonWrapper.getModuleDetailedMap());
+            profile.getModules().setTasks(jsonWrapper.readJsonTaskList(store));
+            plannerUi.showUpdatedMsg();
+        } else {
+            throw new ModUpdateErrorException();
+        }
+    }
+}
--- src/main/java/planner/logic/parser/Parser.java
+++ src/main/java/planner/logic/parser/Parser.java
@@ -21,10 +21,9 @@ import planner.logic.command.ModuleCommand;
 import planner.logic.command.ReminderCommand;
 import planner.logic.command.RemoveCommand;
 import planner.logic.command.SearchThenAddCommand;
-import planner.logic.command.SetPasswordCommand;
 import planner.logic.command.ShowCommand;
 import planner.logic.command.SortCommand;
-import planner.logic.command.UpdateModuleInfo;
+import planner.logic.command.UpdateModuleCommand;
 import planner.logic.exceptions.legacy.ModException;
 import planner.logic.parser.action.Join;
 import planner.util.logger.PlannerLogger;
@@ -63,7 +62,7 @@ public class Parser {
         mapCommand("sort", SortCommand.class);
         mapCommand("cap", CapCommand.class);
         mapCommand("grade", GradeCommand.class);
-        mapCommand("update", UpdateModuleInfo.class);
+        mapCommand("update", UpdateModuleCommand.class);
         mapCommand("reminder", ReminderCommand.class);
         //mapCommand("passwd", SetPasswordCommand.class);
     }
