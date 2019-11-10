d29c934126fd9bea646a1e421398054387a65156
Xiaoxue <e0323328@u.nus.edu>
2019-11-10 12:59:46 +0800

    added SearchCommandTest

--- src/test/java/dolla/command/SearchCommandTest.java
+++ src/test/java/dolla/command/SearchCommandTest.java
@@ -1,4 +1,41 @@
 package dolla.command;
 
+import dolla.ModeStringList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SearchCommandTest {
+
+    @Test
+    public void searchCommandTest1() {
+        Command commandTest = new SearchCommand(ModeStringList.MODE_DEBT,
+                "name", "tata");
+        String expected = "name tata";
+        assertEquals(expected, commandTest.getCommandInfo());
+    }
+
+    @Test
+    public void searchCommandTest2() {
+        Command commandTest = new SearchCommand(ModeStringList.MODE_LIMIT,
+                "duration", "monthly");
+        String expected = "duration monthly";
+        assertEquals(expected, commandTest.getCommandInfo());
+    }
+
+    @Test
+    public void searchCommandTest3() {
+        Command commandTest = new SearchCommand(ModeStringList.MODE_ENTRY,
+                "date", "12");
+        String expected = "date 12";
+        assertEquals(expected, commandTest.getCommandInfo());
+    }
+
+    @Test
+    public void searchCommandTest4() {
+        Command commandTest = new SearchCommand(ModeStringList.MODE_SHORTCUT,
+                "description", "food");
+        String expected = "description food";
+        assertEquals(expected, commandTest.getCommandInfo());
+    }
 }
