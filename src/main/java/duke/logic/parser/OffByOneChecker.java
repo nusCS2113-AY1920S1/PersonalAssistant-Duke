cf4dc9a1cb877ee3fb19ecbf9d91266722a7086b
unknown <jang047@e.ntu.edu.sg>
2019-11-11 01:47:38 +0800

    update userguide and ppp

--- src/main/java/duke/logic/parser/OffByOneChecker.java
+++ src/main/java/duke/logic/parser/OffByOneChecker.java
@@ -15,8 +15,8 @@ public class OffByOneChecker {
      * @return keyword (may not be changed)
      */
     public static String offByOne(String keyword) {
-        ArrayList<String> commandList = new ArrayList<>(Arrays.asList("bye", "help", "list", "delete", "find",
-                "done", "edit", "task", "autoassign", "pomo", "clear", "undo", "event"));
+        ArrayList<String> commandList = new ArrayList<>(Arrays.asList("bye", "help", "list", "done", "find",
+                "delete", "event", "task", "autoassign", "pomo", "clear", "undo", "edit"));
         Scanner scanner = new Scanner(System.in);
         for (String command : commandList) {
             if (keyword.length() != command.length()) {
