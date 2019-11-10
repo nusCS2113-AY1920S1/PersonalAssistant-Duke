b3ad277e4673adcaf08fc2f3d5ba3621e848c2dd
hwbjerry <e0319255@u.nus.edu>
2019-11-11 05:30:42 +0800

    Update Tasks with checkstyle

--- src/main/java/Tasks/Event.java
+++ src/main/java/Tasks/Event.java
@@ -31,57 +31,70 @@ public class Event extends Assignment {
         this.start = start;
         this.end = end;
     }
+
     /**
-     * This method returns type of task.
+     * This method gets type of task.
      */
     @Override
     public String getType() {
         return DukeConstants.EVENT_INDICATOR;
     }
+
     /**
      * This method converts tasks to string for storage purposes.
      */
     @Override
     public String toString() {
-        return super.getModCode() + DukeConstants.BLANK_SPACE + getType() + super.toString() + START_OF_DATE_DELIMITER + by + START_OF_TIME_DELIMITER + start + TIME_DELIMITER + end + END_OF_DATE_TIME_DELIMITER;
+        return super.getModCode() + DukeConstants.BLANK_SPACE
+                + getType() + super.toString()
+                + START_OF_DATE_DELIMITER
+                + by + START_OF_TIME_DELIMITER
+                + start + TIME_DELIMITER
+                + end + END_OF_DATE_TIME_DELIMITER;
     }
+
     /**
-     * This method returns date and time of task.
+     * This method gets date and time of task.
      */
     @Override
     public String getDateTime() {
         return by + DukeConstants.BLANK_SPACE + start + TIME_TO_TIME + end;
     }
+
     /**
-     * This method returns start and end time for display.
+     * This method gets start and end time for display.
      */
     @Override
     public String toShow() {
-        return START_DELIMITER + start + "\n" + END_DELIMITER + end + "\n";//
+        return START_DELIMITER + start + "\n" + END_DELIMITER + end + "\n";
     }
+
     /**
-     * This method returns date of task.
+     * This method gets date of task.
      */
     @Override
     public String getDate() {
         return by;
     }
+
     /**
-     * This method returns time of task.
+     * This method gets time of task.
      */
     @Override
     public String getTime() {
         return start + TIME_TO_TIME + end;
     }
+
     /**
-     * This method returns start time of task.
+     * This method gets start time of task.
      */
     @Override
     public String getStartTime() {
         return start;
     }
+
     /**
-     * This method returns end time of task.
+     * This method gets end time of task.
      */
     @Override
     public String getEndTime() {
