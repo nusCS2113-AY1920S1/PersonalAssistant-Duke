b5e173ae6e01a35034172a1f482ce56c87d41379
leowyh <leowyh4@gmail.com>
2019-11-10 20:23:26 +0800

    Junit for Project and Project Manager, editing storage test

--- src/test/java/storagetest/StorageTest.java
+++ src/test/java/storagetest/StorageTest.java
@@ -1,4 +1,45 @@
 package storagetest;
 
+import Storage.Storage;
+import common.AlphaNUSException;
+import org.junit.jupiter.api.Test;
+import project.Project;
+
+import java.util.LinkedHashMap;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StorageTest {
+
+    private Storage storage = new Storage();
+
+    @Test
+    void test_WriteAndReadCurrentProjectName() throws AlphaNUSException {
+        try {
+            String expectedcurrentprojectname = "Rag";
+            storage.writeTocurrentprojectnameFile("Rag");
+            String outputcurrentprojectname = storage.readFromcurrentprojectnameFile();
+
+            assertEquals(expectedcurrentprojectname, outputcurrentprojectname);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void test_WriteAndReadProjectsFile() throws AlphaNUSException {
+        try {
+            LinkedHashMap<String, Project> expectedprojectmap = new LinkedHashMap<>();
+            expectedprojectmap.put("Arts Night", new Project("Arts Night"));
+            expectedprojectmap.put("Rag", new Project("Rag", 100.0));
+
+            LinkedHashMap<String, Project> outputprojectmap = expectedprojectmap;
+            String outputcurrentprojectname = storage.readFromcurrentprojectnameFile();
+
+            assertEquals(expectedprojectmap, outputprojectmap);
+        } catch (Exception e) {
+            fail();
+        }
+    }
 }
