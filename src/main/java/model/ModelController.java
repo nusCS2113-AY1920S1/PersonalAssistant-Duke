6dbbfe0e6c81abdb6477ecfdbd151805d9c4217b
yuyanglin28 <annielin9908@gmail.com>
2019-11-10 17:33:21 +0800

    model controller javadoc

--- src/main/java/model/ModelController.java
+++ src/main/java/model/ModelController.java
@@ -49,46 +49,57 @@ public class ModelController implements Model {
         return tasksManager.getTaskList();
     }
 
+    @Override
     public String getTodoTasks() {
         return tasksManager.getTodoTasks();
     }
 
+    @Override
     public String getTasks() {
         return tasksManager.getTasks();
     }
 
+    @Override
     public int getTaskListSize() {
         return tasksManager.getTaskListSize();
     }
 
+    @Override
     public String getTaskNameById(int index) {
         return tasksManager.getNameById(index);
     }
 
+    @Override
     public String getTaskNameByIdOnList(int index) {
         return tasksManager.getTaskNameByIdOnList(index);
     }
 
+    @Override
     public String getTaskIsDoneByIdOnList(int index) {
         return tasksManager.getTaskIsDoneByIdOnList(index);
     }
 
+    @Override
     public String getTaskDescriptionByIdOnList(int index) {
         return tasksManager.getTaskDescriptionByIdOnList(index);
     }
 
+    @Override
     public Date getTaskDateTimeByIdOnList(int index) {
         return tasksManager.getTaskDateTimeByIdOnList(index);
     }
 
+    @Override
     public ArrayList getMemberListOfTaskByIdOnList(int index) {
         return tasksManager.getMemberListOfTaskByIdOnList(index);
     }
 
+    @Override
     public ArrayList getSkillListOfTaskByIdOnList(int index) {
         return tasksManager.getSkillListOfTaskByIdOnList(index);
     }
 
+    @Override
     public Date getTaskReminderByIdOnList(int index) {
         return tasksManager.getTaskReminderByIdOnList(index);
     }
@@ -117,7 +128,13 @@ public class ModelController implements Model {
         return tasksManager.addReqSkill(taskName, skillName);
     }
 
-
+    //@@author yuyanglin28
+    /**
+     * This method is to update the description of a task
+     * @param index the index of task (begin from 0)
+     * @param des new description
+     * @return old description
+     */
     public String updateTaskDes(int index, String des) {
         String oldDes = tasksManager.getTaskDes(index);
         tasksManager.updateTaskDes(index, des);
@@ -198,12 +215,30 @@ public class ModelController implements Model {
     }
 
 
+    //@@author yuyanglin28
+
+    /**
+     * This method is to update the biography of a member
+     * @param name member name
+     * @param bio new bio
+     * @return old bio
+     */
+    @Override
     public String updateMemberBio(String name, String bio) {
         String oldBio = memberManager.getMemberBio(name);
         memberManager.updateMemberBio(name, bio);
         return oldBio;
     }
 
+    //@@author yuyanglin28
+
+    /**
+     * This method is to update the email of a member
+     * @param name member name
+     * @param email new email
+     * @return old email
+     * @throws DukeException throw exception when the email format is not correct
+     */
     @Override
     public String updateMemberEmail(String name, String email) throws DukeException {
         String oldEmail = memberManager.getMemberEmail(name);
@@ -211,6 +246,14 @@ public class ModelController implements Model {
         return oldEmail;
     }
 
+    //@@author yuyanglin28
+
+    /**
+     * This method is to update the phone of a member
+     * @param name member name
+     * @param phone new phone
+     * @return old phone
+     */
     @Override
     public String updateMemberPhone(String name, String phone) {
         String oldPhone = memberManager.getMemberPhone(name);
@@ -286,10 +329,12 @@ public class ModelController implements Model {
         return memberPhone;
     }
 
+    @Override
     public ArrayList<String> getTaskListOfMemberByName(String name) {
         return memberManager.getTaskListOfMember(name);
     }
 
+    @Override
     public ArrayList<String> getSkillListOfMemberByName(String name) {
         return memberManager.getSkillListOfMember(name);
     }
@@ -315,6 +360,11 @@ public class ModelController implements Model {
         memberManager.getMemberByName(memberName).deleteTask(task.getName());
     }
 
+    //@@author yuyanglin28
+    /**
+     * This method is to delete a member in member list, and also corresponding member in task list
+     * @param memberName to be deleted member name
+     */
     @Override
     public void deleteMember(String memberName) {
         tasksManager.deleteMemberInTasks(memberName);
@@ -323,6 +373,13 @@ public class ModelController implements Model {
     }
 
 
+    //@@author yuyanglin28
+
+    /**
+     * This method is to delete a task in task list and also corresponding task in member list
+     * @param index index of to be deleted task
+     * @return to be deleted task's name
+     */
     @Override
     public String deleteTask(int index) {
         String name = tasksManager.getNameById(index);
@@ -332,16 +389,41 @@ public class ModelController implements Model {
         return name;
     }
 
+    //@@author yuyanglin28
+
+    /**
+     * This method is to schedule all assigned tasks for a member
+     * @param memberName member name
+     * @return sorted tasks list
+     */
+    @Override
     public String scheduleMemberAll(String memberName) {
         ArrayList<String> tasksName = memberManager.getTaskListOfMember(memberName);
         return tasksManager.tasksAllInorderTime(tasksName);
     }
 
+    //@@author yuyanglin28
+
+    /**
+     * This method is to schedule todo assigned tasks for a member
+     * @param memberName member name
+     * @return sorted todo tasks list
+     */
+    @Override
     public String scheduleMemberTodo(String memberName) {
         ArrayList<String> tasksName = memberManager.getTaskListOfMember(memberName);
         return tasksManager.tasksTodoInorderTime(tasksName);
     }
 
+    //@@author yuyanglin28
+
+    /**
+     * This method is to check deadline crash of every member,
+     * two tasks has time crash if their deadlines are at the same day.
+     * This method will get the task list of a member and pass to tasks manager to check the crash,
+     * do it for every member.
+     * @return string of the time crash, if no time crash, return an empty string
+     */
     @Override
     public String check() {
         String result = "";
@@ -356,6 +438,15 @@ public class ModelController implements Model {
         return result;
     }
 
+    //@@author yuyanglin28
+
+    /**
+     * This method is to get the member list in order of progress.
+     * This method will first get the progress of every member through task manager and store in an array list,
+     * then pass the array list to member manager to sorted the progress.
+     *
+     * @return sorted member list, progress from high to low
+     */
     @Override
     public String membersInorderProgress() {
         ArrayList<Double> progress = new ArrayList<>();
@@ -367,6 +458,15 @@ public class ModelController implements Model {
     }
 
 
+    //@@author yuyanglin28
+
+    /**
+     * This method is to get the member list in order of number of todo tasks.
+     * This method will first get the todo task num of every member through task manager and store in an array list,
+     * then pass the array list to member manager to sorted the todo tasks num.
+     *
+     * @return sorted member list, num of todo tasks from small to big
+     */
     @Override
     public String membersInorderTodoNum() {
         ArrayList<Integer> todoNum = new ArrayList<>();
