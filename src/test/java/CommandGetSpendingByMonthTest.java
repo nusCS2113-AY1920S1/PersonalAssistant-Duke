13d2f2dd7dd020c2ea68744f775a54dc761e4625
AjeyAshok <a5000shiok@gmail.com>
2019-11-11 02:29:53 +0800

    Junit testing added to CommandSpendingByDay

--- src/test/java/CommandGetSpendingByMonthTest.java
+++ src/test/java/CommandGetSpendingByMonthTest.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommandGetSpendingByMonthTest {
-    @Test
+     @Test
    void execute() {
         StorageManager storageManager = new StorageManager();
 
