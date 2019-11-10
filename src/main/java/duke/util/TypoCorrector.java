87318a1b1ca5b4ad1f010332beefc7430cfac119
HUANG XUAN KUN <hxk123123123@gmail.com>
2019-11-10 18:53:52 +0800

    Update TypoCorrected Junit Test

--- src/main/java/duke/util/TypoCorrector.java
+++ src/main/java/duke/util/TypoCorrector.java
@@ -7,19 +7,65 @@ import org.apache.commons.text.similarity.LevenshteinDistance;
  * It provides a method TypoCorrector.CommandCorrection which takes in an invalid input command
  * and return a possible matched command.
  *
- * @author HUANG XUAN KUN
- * @version 1.2
+ * @author HUANGXUANKUN
+ * @version 1.4
  */
 public class TypoCorrector {
 
     //The maximum ratio changes of a text in % that is acceptable
-    private static final double MAX_DISTANCE_DIFF_RATIO = 0.7;
+    private static final double MAX_DISTANCE_DIFF_RATIO = 0.8;
 
     //Sets of "Dictionaries" for the command keyword, categorised by number of keywords contain in a supported commands.
-    private static final String[] simpleCommands = {"bye", "duke", "help", "list patients", "list tasks"};
-    private static final String[] otherCommands = {"update patient", "update task", "delete patient",
+    private static final String[] SIMPLE_COMMANDS = {"bye", "duke", "help", "list patients", "list tasks"};
+    private static final String[] OTHER_COMMANDS = {"update patient", "update task", "delete patient",
         "delete task", "delete assigned task", "add task", "add patient", "find patient", "find task",
         "find assigned tasks", "assign deadline task", "assign period task"};
+
+    private boolean isCorrected = false;
+    private String correctedCommand;
+    private String commandInput;
+
+    /**
+     * It takes in full command by user and process it with auto-correction.
+     * If the correctedCommand is not equal to the original command, the command is
+     * considered as corrected.
+     *
+     * @param commandInput the full command entered by user
+     */
+    public TypoCorrector(String commandInput) {
+        this.commandInput = commandInput;
+        correctedCommand = commandCorrection(commandInput);
+        String inputKeyword = commandInput.split(":", 2)[0].trim();
+        String correctedKeyword = correctedCommand.split(":", 2)[0].trim();
+        if (!inputKeyword.equals(correctedKeyword)) {
+            isCorrected = true;
+        }
+    }
+
+    /**
+     * It returns the correction status of the command.
+     * if true, the command can be corrected, user can then call the getCorrectedCommand() to get the corrected version.
+     * Otherwise, the command cannot be corrected.
+     *
+     * @return if input command can be corrected.
+     */
+    public boolean isCommandCorrected() {
+        return isCorrected;
+    }
+
+    /**
+     * It return the correctedCommand if the command can be corrected.
+     * Otherwise, return the original command.
+     *
+     * @return the full corrected command
+     */
+    public String getCorrectedCommand() {
+        if (isCommandCorrected()) {
+            return correctedCommand;
+        } else {
+            return commandInput;
+        }
+    }
 
     /**
      * This method take in an user input command with typo and return a possible matches
@@ -28,27 +74,27 @@ public class TypoCorrector {
      * @param command the full userInput command without parsing
      * @return a string of correctedCommand
      */
-    public static String commandCorrection(String command) {
+    private String commandCorrection(String command) {
         String[] splitCommand = command.split(":", 2);
         int commandSize = splitCommand.length;
         String closestMatch;
         if (commandSize == 1) {
             // Type A command with only command keywords
             String fullCommand = command.trim().toLowerCase();//Ignore spaces(back and fore)and upper/lower cases
-            closestMatch = matchStringFromDict(fullCommand, simpleCommands); //get closest match
+            closestMatch = matchStringFromDict(fullCommand, SIMPLE_COMMANDS); //get closest match
             if (isSimilar(fullCommand, closestMatch)) {
                 return closestMatch;
             }
         } else if (commandSize == 2) {
             // Type B command with command keywords and other info/data
             String keyword = splitCommand[0].trim().toLowerCase(); //Ignore spaces(back and fore)and upper/lower cases
-            closestMatch = matchStringFromDict(keyword, otherCommands); //get closest match
+            closestMatch = matchStringFromDict(keyword, OTHER_COMMANDS); //get closest match
             if (isSimilar(keyword, closestMatch)) {
                 splitCommand[0] = "";
                 return closestMatch + String.join(":", splitCommand).trim();
             }
         }
-        return command; // The input command will be return if there is no matched found
+        return command; // The original command will be return if there is no matched found
     }
 
     /**
@@ -57,7 +103,7 @@ public class TypoCorrector {
      * @param str the arbitrary string
      * @return the closest matching from the target dict
      */
-    private static String matchStringFromDict(String str, String[] targetDict) {
+    private String matchStringFromDict(String str, String[] targetDict) {
         int minDist = 256;
         String closestMatch = null;
         for (String keyword : targetDict) {
@@ -79,23 +125,23 @@ public class TypoCorrector {
      * @param str the arbitrary string
      * @return the difference of two strings measured in distance
      */
-    private static Integer getDistance(String str, String target) {
+    private Integer getDistance(String str, String target) {
         LevenshteinDistance distance = new LevenshteinDistance();
         return distance.apply(str, target);
     }
 
     /**
      * Method indicating if a message can be considered similar, based on Levenshtein distance
-     * calculation with an allowed variation of 50%.
-     *
-     * <b>Note:</b> Max tolerated distance is derived from the current scenario's error message
+     * calculation with an allowed variation of 70%.
+     * <p>
+     * Max tolerated distance is derived from the current scenario's error message
      * The arbitrary MAX_DISTANCE_DIFF_RATIO (50%) means we consider 10% change to be acceptable
      *
      * @param referenceText the reference text
      * @param targetText    the target text for the comparison
      * @return true if the can be considered similar else false
      */
-    public static boolean isSimilar(String referenceText, String targetText) {
+    public boolean isSimilar(String referenceText, String targetText) {
         int threshold = (int) Math.round(MAX_DISTANCE_DIFF_RATIO * referenceText.length());
         LevenshteinDistance levenshteinDistance = new LevenshteinDistance(threshold);
         return levenshteinDistance.apply(referenceText, targetText) != -1;
--- src/test/java/duke/util/TypoCorrectorTest.java
+++ src/test/java/duke/util/TypoCorrectorTest.java
@@ -1,7 +1,8 @@
 package duke.util;
 
-import duke.util.TypoCorrector;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,42 +12,65 @@ import java.util.Arrays;
  * Junit test for TypoCorrector class.
  *
  * @author HUANG XUAN KUN
- * @version 1.2
+ * @version 1.4
  */
 public class TypoCorrectorTest {
 
+    //ArrayList of pairs <command, commandToBeCorrected>
+    private final static ArrayList<String[]> COMMANDS_TO_BE_CORRECTED = new ArrayList<String[]>(
+        Arrays.asList(
+            new String[]{"b y e", "bye"},
+            new String[]{"d uke ", "duke"},
+            new String[]{"lsit patant", "list patients"},
+            new String[]{"DEe lte Pti ents:#12", "delete patient:#12"},
+            new String[]{"deleot tasksa :task description", "delete task:task description"},
+            new String[]{"delette   tasks:#1", "delete task:#1"},
+            new String[]{"addd patents:name :nric :room :remark", "add patient:name :nric :room :remark"},
+            new String[]{"dad tsak:a very long task name", "add task:a very long task name"},
+            new String[]{"addd task :abc", "add task:abc"},
+            new String[]{"asgn deadli task: #1 :12 :12/12/2019 1645",
+                "assign deadline task: #1 :12 :12/12/2019 1645"},
+            new String[]{"asSGn perio tk :#1 :12 :12/12/2019 1645 :12/12/2020 1645",
+                "assign period task:#1 :12 :12/12/2019 1645 :12/12/2020 1645"},
+            new String[]{"fi n d p atin t: #1", "find patient: #1"},
+            new String[]{"u p da te pa i t en s :#12:Room:2A", "update patient:#12:Room:2A"}
+        ));
+
+    private final static String[] NOT_CORRECTED_COMMANDS = {
+        "", " ", "list patients", "an invalid command"
+    };
+
+    /**
+     *
+     */
     @Test
-    void stringMatchTest() {
-        /*ArrayList<String[]> testCases = new ArrayList<String[]>(
-            Arrays.asList(
-                new String[]{"b q e", "bye"},
-                new String[]{"d uke ", "duke"},
-                new String[]{"dbKe", "duke"},
-                new String[]{"l si t patant", "list patients"},
-                new String[]{"lsf vs ta kss", "list tasks"},
-                new String[]{"DEe lte Pti ents:#12", "delete patient :#12"},
-                new String[]{"deleot tasksa :task description", "delete task :task description"},
-                new String[]{"delette   tasks:#1", "delete task :#1"},
-                new String[]{"addd patents:name :nric :room :remark", "add patient :name :nric :room :remark"},
-                new String[]{"dad tsak:a very long task name", "add task :a very long task name"},
-                new String[]{"addd task :abc", "add task :abc"},
-                new String[]{"asgn deadli task: #1 :12 :12/12/2019 1645",
-                    "assign deadline task : #1 :12 :12/12/2019 1645"},
-                new String[]{"asSGn perio tk :#1 :12 :12/12/2019 1645 :12/12/2020 1645",
-                    "assign period task :#1 :12 :12/12/2019 1645 :12/12/2020 1645"},
-                new String[]{"fi n d p atin t: #1", "find patient : #1"},
-                new String[]{"u p da te pa i t en s :#12:Room:2A", "update patient :#12:Room:2A"},
-                new String[]{"", ""},
-                new String[]{" ", " "}
-            ));
-        for (String[] testPair : testCases) {
-            String correctedOutput = TypoCorrector.commandCorrection(testPair[0]);
+    void CommandCorrectionTest() {
+        for (String[] testPair : COMMANDS_TO_BE_CORRECTED) {
+            TypoCorrector typoCorrector = new TypoCorrector(testPair[0]);
+            assertTrue(typoCorrector.isCommandCorrected(),
+                "The command < " + testPair[0] + " > should not be corrected to < " + testPair[1] + " >");
+            String correctedOutput = typoCorrector.getCorrectedCommand();
             System.out.println("Input Command:     " + testPair[0]);
             System.out.println("Expected Command:  " + testPair[1]);
-            System.out.println("Corrected Command: " + correctedOutput);
-            System.out.println();
+            System.out.println("Corrected Command: " + correctedOutput + "\n");
             assertEquals(testPair[1], correctedOutput);
         }
-*/
     }
+
+    @Test
+    void UnCorrectedCommandTest() {
+        for (String command : NOT_CORRECTED_COMMANDS) {
+            TypoCorrector typoCorrector = new TypoCorrector(command);
+            assertFalse(typoCorrector.isCommandCorrected(),
+                "The command: " + command + " should not be corrected");
+            String correctedOutput = typoCorrector.getCorrectedCommand();
+            System.out.println("Input Command:     " + command);
+            System.out.println("Expected Command:  " + command);
+            System.out.println("Corrected Command: " + correctedOutput);
+            System.out.println();
+            assertEquals(command, correctedOutput);
+        }
+    }
+
+
 }
