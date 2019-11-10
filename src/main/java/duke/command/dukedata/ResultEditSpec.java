5e4747311b46717f0cd299081f16d28df283f0c6
John Khoo <john_khoo@u.nus.edu>
2019-11-11 02:54:27 +0800

    Update ResultEdit and fix PatientPrimary

--- src/main/java/duke/command/dukedata/ResultEditSpec.java
+++ src/main/java/duke/command/dukedata/ResultEditSpec.java
@@ -1,6 +1,24 @@
 package duke.command.dukedata;
 
+import duke.command.ArgLevel;
 import duke.command.ArgSpec;
+import duke.command.Switch;
 
 public class ResultEditSpec extends ArgSpec {
+
+    private static final ResultEditSpec spec = new ResultEditSpec();
+
+    public static ResultEditSpec getSpec() {
+        return spec;
+    }
+
+    private ResultEditSpec() {
+        cmdArgLevel = ArgLevel.OPTIONAL;
+        initSwitches(
+                new Switch("append", null, true, ArgLevel.NONE, "a"),
+                new Switch("name", String.class, true, ArgLevel.REQUIRED, "n"),
+                new Switch("priority", Integer.class, true, ArgLevel.REQUIRED, "pri"),
+                new Switch("summary", String.class, true, ArgLevel.REQUIRED, "sum")
+        );
+    }
 }
