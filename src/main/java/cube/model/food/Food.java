92043096c99f747f7ecfb55bf2d34b74ce75a19b
LL-Pengfei <42534460+LL-Pengfei@users.noreply.github.com>
2019-11-10 19:01:06 +0800

    junit for food completed /20191110

--- src/main/java/cube/model/food/Food.java
+++ src/main/java/cube/model/food/Food.java
@@ -172,7 +172,7 @@ public class Food {
 	}
 
 	/**
-	 * Check if the product already exists in the inventory.
+	 * Check if the food already exists in the inventory.
 	 *
 	 * @return true if exists, false if otherwise.
 	 */
