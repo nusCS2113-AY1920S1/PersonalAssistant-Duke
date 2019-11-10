9f12b33d02e10d04bb603619e0773d6d63fee8ff
lee761 <vivianleeboongeok@gmail.com>
2019-11-11 00:09:06 +0800

    added part for developer and user guide

---
+++ src/test/java/seedu/duke/EmailKeywordPairListTest.java
@@ -0,0 +1,14 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class EmailKeywordPairListTest {
+    @Test
+    public void addAndCopy() {
+
+    }
+}
