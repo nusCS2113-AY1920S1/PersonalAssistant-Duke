ff2e5facdfea1e0dacf4efa71a0c44cb70fb4a84
zkathrynx <katherine111433@gmail.com>
2019-11-10 23:08:16 +0800

    checkstyle foodlist

--- src/main/java/cube/model/food/FoodList.java
+++ src/main/java/cube/model/food/FoodList.java
@@ -8,43 +8,74 @@ import cube.model.food.foolUtil.SortByStock;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class FoodList{
+/**
+ * Model for food list.
+ */
+public class FoodList {
 	private ArrayList<Food> foodArrayList;
 
+	/**
+	 * The way to sort.
+	 */
 	public enum SortType {
 		EXPIRY, NAME, STOCK, PRICE;
 
-        public static boolean IsDefined(String temp){
-            if(temp.equals("EXPIRY")||temp.equals("NAME")||temp.equals("STOCK")||temp.equals("PRICE")) {
+        public static boolean isDefined(String temp) {
+            if (temp.equals("EXPIRY") || temp.equals("NAME")
+					|| temp.equals("STOCK") || temp.equals("PRICE")) {
                 return true;
             }
             return false;
         }
 	}
 
+	/**
+	 * Default constructor.
+	 */
 	public FoodList() {
 		this.foodArrayList = new ArrayList<>();
 	}
 
+	/**
+	 * Constructor with a given list.
+	 * @param list the given list.
+	 */
 	public FoodList(ArrayList<Food> list) {
 		this.foodArrayList = list;
 	}
 
+	/**
+	 * Add food into the list.
+	 * @param food the food to add.
+	 */
 	public void add(Food food) {
 		this.foodArrayList.add(food);
 	}
 
-	// public getter to allow JSON read/write to work
+	/**
+	 * Public getter to allow JSON read/write to work.
+	 * @return the food list.
+	 */
 	public ArrayList<Food> getFoodList() {
 		return this.foodArrayList;
 	}
 
+	/**
+	 * Get food by index.
+	 * @param index the index.
+	 * @return the food at the index.
+	 */
 	public Food get(int index) {
 		return foodArrayList.get(index);
 	}
 
+	/**
+	 * Get food by name.
+	 * @param foodName the name.
+	 * @return the food with the name.
+	 */
 	public Food get(String foodName) {
-		for(Food food : foodArrayList) {
+		for (Food food : foodArrayList) {
 			if (food.getName().toLowerCase().equals(foodName.toLowerCase())) {
 				return food;
 			}
@@ -52,12 +83,20 @@ public class FoodList{
 		return null;
 	}
 
+	/**
+	 * Remove food by index.
+	 * @param index the index to remove.
+	 */
 	public void removeIndex(int index) {
 		foodArrayList.remove(index);
 	}
 
+	/**
+	 * Remove food by name.
+	 * @param foodName the name of the food.
+	 */
 	public void removeName(String foodName) {
-		for(int i = 0; i < foodArrayList.size(); i ++) {
+		for (int i = 0; i < foodArrayList.size(); i++) {
 			if (foodArrayList.get(i).getName().toLowerCase().equals(foodName.toLowerCase())) {
 				foodArrayList.remove(i);
 				break;
@@ -65,21 +104,32 @@ public class FoodList{
 		}
 	}
 
+	/**
+	 * Remove food by type.
+	 * @param foodType the type of the food.
+	 * @return the number of items removed.
+	 */
 	public int removeType(String foodType) {
 		int count = 0;
-		for(int i = 0; i < foodArrayList.size();) {
-			if (foodArrayList.get(i).getType()!=null && foodArrayList.get(i).getType().equals(foodType)) {
+		for (int i = 0; i < foodArrayList.size();) {
+			if (foodArrayList.get(i).getType() != null
+					&& foodArrayList.get(i).getType().equals(foodType)) {
 				foodArrayList.remove(i);
-				count ++;
+				count++;
 			} else {
-				i ++;
+				i++;
 			}
 		}
 		return count;
 	}
 
+	/**
+	 * Checks whether the name exists.
+	 * @param foodName the name to check.
+	 * @return true if exists, false otherwise.
+	 */
 	public boolean existsName(String foodName) {
-		for(Food food : foodArrayList) {
+		for (Food food : foodArrayList) {
 			if (food.getName().toLowerCase().equals(foodName.toLowerCase())) {
 				return true;
 			}
@@ -87,8 +137,13 @@ public class FoodList{
 		return false;
 	}
 
+	/**
+	 * Checks whether the type exists.
+	 * @param foodType the type to check.
+	 * @return true if exists, false otherwise.
+	 */
 	public boolean existsType(String foodType) {
-		for(Food food : foodArrayList) {
+		for (Food food : foodArrayList) {
 			if (food.getType() != null && food.getType().toLowerCase().equals(foodType.toLowerCase())) {
 				return true;
 			}
@@ -96,12 +151,20 @@ public class FoodList{
 		return false;
 	}
 
+	/**
+	 * Get list size.
+	 * @return the size of the list.
+	 */
 	public int size() {
 		return foodArrayList.size();
 	}
 
+	/**
+	 * Sort the list by sort type.
+	 * @param sortType the way to sort.
+	 */
 	public void sort(SortType sortType) {
-		switch(sortType) {
+		switch (sortType) {
 			case EXPIRY:
 				Collections.sort(foodArrayList, new SortByExpiry());
 				break;
@@ -110,19 +173,26 @@ public class FoodList{
 				break;
 			case STOCK:
 				Collections.sort(foodArrayList, new SortByStock());
+				break;
 			case PRICE:
 				Collections.sort(foodArrayList, new SortByPrice());
+				break;
+			default:
+				break;
 		}
 	}
 
-	public void clear(){
+	/**
+	 * Clear the list.
+	 */
+	public void clear() {
 		foodArrayList.clear();
 	}
 
 	@Override
 	public String toString() {
 		String result = "";
-		for(int i = 0; i < foodArrayList.size(); i++) {
+		for (int i = 0; i < foodArrayList.size(); i++) {
 			result += String.format("%1$d. %2$s.\n", i + 1, foodArrayList.get(i));
 		}
 		return result;
