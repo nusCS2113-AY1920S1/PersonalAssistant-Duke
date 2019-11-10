2dd32a2213908aa29dbaab4a8eee822a79fdf07a
rishi12438 <rishimahadevan@gmail.com>
2019-11-10 11:50:56 +0800

    added testcases and fixed bugs with overlay

--- src/main/java/ducats/Ducats.java
+++ src/main/java/ducats/Ducats.java
@@ -73,6 +73,7 @@ public class Ducats {
         while (!isExit) {
             try {
                 String fullCommand = ui.readCommand();
+
                 Command c = Parser.parse(ui,fullCommand);
                 //if the command uses the SongList
                 String output;
