b6824d47e8ae7eb8026beba27413b32310de52ce
namiwa <khairul.nasri895@gmail.com>
2019-11-10 17:45:22 +0800

    started testing on legacy exceptions

--- src/test/java/planner/logic/exceptions/legacy/LegacyExceptionTest.java
+++ src/test/java/planner/logic/exceptions/legacy/LegacyExceptionTest.java
@@ -4,6 +4,20 @@ package planner.logic.exceptions.legacy;
 
 public class LegacyExceptionTest {
 
-
+    private final String test = "test";
+    private ModException modException = new ModException();
+    private ModCommandException modCommandException = new ModCommandException();
+    private ModEmptyCommandException modEmptyCommandException = new ModEmptyCommandException();
+    private ModEmptyListException modEmptyListException = new ModEmptyListException();
+    private ModInvalidIndexException modInvalidIndexException = new ModInvalidIndexException();
+    private ModInvalidTimeException modInvalidTimeException = new ModInvalidTimeException();
+    private ModInvalidTimePeriodException modInvalidTimePeriodException = new ModInvalidTimePeriodException(test);
+    private ModMissingArgumentException modMissingArgumentException = new ModMissingArgumentException(test);
+    private ModNoTimeException modNoTimeException = new ModNoTimeException();
+    private ModOutOfBoundException modOutOfBoundException = new ModOutOfBoundException();
+    private ModScheduleException modScheduleException = new ModScheduleException();
+    private ModTimeIntervalTooCloseException modTimeIntervalTooCloseException = new ModTimeIntervalTooCloseException();
+    private ModMultipleValuesForSameArgumentException modMultipleValuesForSameArgumentException =
+            new ModMultipleValuesForSameArgumentException();
 
 }
