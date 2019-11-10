1fbffc0903fc8a4698730c566ae28c20b69c6514
jasonchanwq <jasonchanwq@gmail.com>
2019-11-11 03:04:34 +0800

    Test Travis Ci

--- src/test/java/command/EditTaskDateTimeCommandTest.java
+++ src/test/java/command/EditTaskDateTimeCommandTest.java
@@ -8,7 +8,6 @@ import model.Model;
 import model.ModelController;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -19,9 +18,7 @@ public class EditTaskDateTimeCommandTest {
     @Test
     public void editTaskDateTimeCommand_indexNotInTaskList_throwsException() throws DukeException {
         Model model = new ModelController();
-        for (int i = model.getTaskListSize() - 1; i >= 0; i--)  {
-            model.deleteTask(i);
-        }
+        model.getTaskList().clear();
         model.addTask("Create event poster");
         Command command = EditTaskDateTimeParser.parseEditTaskDateTime("5 /to 10/10/2020 1010");
         assertThrows(DukeException.class, () -> command.execute(model));
@@ -30,9 +27,7 @@ public class EditTaskDateTimeCommandTest {
     @Test
     public void editTaskDateTimeCommand_negativeIndex_throwsException() throws DukeException {
         Model model = new ModelController();
-        for (int i = model.getTaskListSize() - 1; i >= 0; i--)  {
-            model.deleteTask(i);
-        }
+        model.getTaskList().clear();
         model.addTask("Create event poster");
         Command command = EditTaskDateTimeParser.parseEditTaskDateTime("-1 /to 10/10/2020 1010");
         assertThrows(DukeException.class, () -> command.execute(model));
@@ -41,9 +36,7 @@ public class EditTaskDateTimeCommandTest {
     @Test
     public void editTaskDateTimeCommand_dateTimeBehindCurrentTime_throwsException() throws DukeException {
         Model model = new ModelController();
-        for (int i = model.getTaskListSize() - 1; i >= 0; i--)  {
-            model.deleteTask(i);
-        }
+        model.getTaskList().clear();
         model.addTask("Create event poster");
         Command command = EditTaskDateTimeParser.parseEditTaskDateTime("1 /to 10/10/1920 1010");
         assertThrows(DukeException.class, () -> command.execute(model));
@@ -52,25 +45,14 @@ public class EditTaskDateTimeCommandTest {
     @Test
     public void editTaskDateTimeCommand_success() throws DukeException {
         Model model = new ModelController();
-        for (int i = model.getTaskListSize() - 1; i >= 0; i--)  {
-            model.deleteTask(i);
-        }
-        //model.getTaskList().clear();
+        model.getTaskList().clear();
         model.addTask("Create event poster");
         Command command = EditTaskDateTimeParser.parseEditTaskDateTime("1 /to 10/10/2020 1010");
         CommandOutput out = command.execute(model);
         String expected = "The deadline has been changed to: Sat Oct 10 10:10:00 SGT 2020";
         String actual = out.getOutputToUser();
-        //CommandOutput expected = new CommandOutput(
-        //        "The deadline has been changed to: Sat Oct 10 10:10:00 SGT 2020", false);
-        //assertTrue(model.getTaskDateTimeByIdOnList(1).toString().equals("Sat Oct 10 10:10:00 SGT 2020"));
-        //assertTrue(out instanceof CommandOutput);
-        //assertEquals(expected.isExit(), out.isExit());
-        //assertEquals(expected.getOutputToUser(), out.getOutputToUser());
-        //System.out.println(out.getOutputToUser());
-        //System.out.println(expected.getOutputToUser());
-        //assertEquals(expected, out);
-        assertEquals(expected, actual);
+        boolean isSame = expected.equals(actual)? true:false;
+        assertTrue(isSame);
     }
 
 }
