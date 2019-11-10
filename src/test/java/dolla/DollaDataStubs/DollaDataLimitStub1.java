559c4804385025b31cd46f731180ff86f201847c
Penguin <quek.aik.peng@gmail.com>
2019-11-10 17:49:15 +0800

    Refactored DollaData stubs.

--- src/test/java/dolla/DollaDataStubs/DollaDataLimitStub1.java
+++ src/test/java/dolla/DollaDataStubs/DollaDataLimitStub1.java
@@ -5,22 +5,23 @@ import dolla.model.DebtList;
 import dolla.model.DollaData;
 import dolla.model.Entry;
 import dolla.model.EntryList;
+import dolla.model.Limit;
 import dolla.model.LimitList;
 import dolla.model.Record;
 import dolla.model.ShortcutList;
+import dolla.parser.ParserStringList;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class DollaDataEntryStub1 extends DollaData implements EntryArrayListInterface {
+public class DollaDataLimitStub1 extends DollaData implements LimitArrayListInterface {
 
     /**
      * Creates an instance of DollaData to store and manipulate data.
      */
-    public DollaDataEntryStub1() {
-        this.mode = MODE_ENTRY;
-        this.entryList = new EntryList(createNewEntryArrayList());
-        this.limitList = new LimitList(new ArrayList<Record>());
+    public DollaDataLimitStub1() {
+        this.entryList = new EntryList(new ArrayList<Record>());
+        this.limitList = new LimitList(createNewLimitArrayList());
         this.debtList = new DebtList(new ArrayList<Record>());
         this.shortcutList = new ShortcutList(new ArrayList<Record>());
         this.billList = new BillList(new ArrayList<Record>());
---
+++ src/test/java/dolla/DollaDataStubs/LimitArrayListInterface.java
@@ -0,0 +1,30 @@
+package dolla.DollaDataStubs;
+
+import dolla.model.Limit;
+import dolla.model.Record;
+import dolla.parser.ParserStringList;
+
+import java.util.ArrayList;
+
+public interface LimitArrayListInterface extends ParserStringList {
+
+    private Limit createNewLimit1() {
+        return new Limit(LIMIT_TYPE_S, 5, LIMIT_DURATION_D);
+    }
+
+    private Limit createNewLimit2() {
+        return new Limit(LIMIT_TYPE_S, 50, LIMIT_DURATION_W);
+    }
+
+    private Limit createNewLimit3() {
+        return new Limit(LIMIT_TYPE_B, 500, LIMIT_DURATION_M);
+    }
+
+    default ArrayList<Record> createNewLimitArrayList() {
+        ArrayList<Record> newRecordList = new ArrayList<Record>();
+        newRecordList.add(createNewLimit1());
+        newRecordList.add(createNewLimit2());
+        newRecordList.add(createNewLimit3());
+        return newRecordList;
+    }
+}
