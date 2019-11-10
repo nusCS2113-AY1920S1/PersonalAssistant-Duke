59534ca2ff26de18658bc932e6334f9d0a971a1d
Penguin <quek.aik.peng@gmail.com>
2019-11-10 16:10:27 +0800

    Added ViewDateCommandTest.

---
+++ src/test/java/dolla/command/view/ViewDateCommandTest.java
@@ -0,0 +1,19 @@
+package dolla.command.view;
+
+import dolla.Time;
+import dolla.command.Command;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ViewDateCommandTest {
+
+    @Test
+    public void ViewDateCommandInfo() {
+        LocalDate inputDate = LocalDate.parse("2001-12-03");
+        Command newViewTodayCommand = new ViewDateCommand(inputDate);
+        String expected = Time.dateToString(inputDate) + " " + Time.dateToString(inputDate);
+        assertEquals(expected, newViewTodayCommand.getCommandInfo());
+    }
+}
