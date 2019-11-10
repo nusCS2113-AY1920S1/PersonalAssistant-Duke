c0bd00c6079ba9f8b16604fc98b514b03806b894
Hashir Zahir <hashirsahad@gmail.com>
2019-11-10 19:42:00 +0800

    Updated junit test for edit command

--- src/test/java/diyeats/logic/command/EditMealTest.java
+++ src/test/java/diyeats/logic/command/EditMealTest.java
@@ -3,6 +3,7 @@ package diyeats.logic.command;
 import diyeats.logic.commands.EditCommand;
 import diyeats.model.meal.Meal;
 
+import diyeats.model.meal.MealType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -30,38 +31,39 @@ public class EditMealTest {
         nutritionMap.put("fats", "300");
         costStr = "8.50";
         meal = new Meal("biryani", currDate,  nutritionMap, costStr);
+        meal.setMealType(MealType.LUNCH);
     }
 
 
     @Test
     public void editMealEmptyTest() {
         correctUpdatedMeal = meal;
-        updatedMeal = editCommand.updateMeal(meal, details);
+        updatedMeal = editCommand.getUpdatedMeal(meal, details);
         assertEquals(correctUpdatedMeal.getDescription(), updatedMeal.getDescription());
         assertEquals(correctUpdatedMeal.getNutritionalValue(), updatedMeal.getNutritionalValue());
     }
 
-
+    @Test
     public void editMealRenameTest() {
         String newNameStr = "chicken-biryani";
         details.put("name", newNameStr);
         correctUpdatedMeal = new Meal(newNameStr, currDate, nutritionMap, costStr);
-        updatedMeal = editCommand.updateMeal(meal, details);
+        updatedMeal = editCommand.getUpdatedMeal(meal, details);
         assertEquals(correctUpdatedMeal.getDescription(), updatedMeal.getDescription());
         assertEquals(correctUpdatedMeal.getNutritionalValue(), updatedMeal.getNutritionalValue());
     }
 
-
+    @Test
     public void editMealCostTest() {
         String newCostStr = "7.45";
         details.put("cost", newCostStr);
         correctUpdatedMeal = new Meal(meal.getDescription(), currDate, nutritionMap, newCostStr);
-        updatedMeal = editCommand.updateMeal(meal, details);
+        updatedMeal = editCommand.getUpdatedMeal(meal, details);
         assertEquals(correctUpdatedMeal.getDescription(), updatedMeal.getDescription());
         assertEquals(correctUpdatedMeal.getNutritionalValue(), updatedMeal.getNutritionalValue());
     }
 
-
+    @Test
     public void editMealTagTest() {
         details.put("calorie", "1000");
         details.put("calcium", "150");
@@ -71,7 +73,7 @@ public class EditMealTest {
         nutritionMap.put("salt", "130");
 
         correctUpdatedMeal = new Meal(meal.getDescription(), currDate, nutritionMap, costStr);
-        updatedMeal = editCommand.updateMeal(meal, details);
+        updatedMeal = editCommand.getUpdatedMeal(meal, details);
         assertEquals(correctUpdatedMeal.getDescription(), updatedMeal.getDescription());
         assertEquals(correctUpdatedMeal.getNutritionalValue(), updatedMeal.getNutritionalValue());
     }
