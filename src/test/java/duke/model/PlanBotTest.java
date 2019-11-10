1895be3d3a071e4d54bfc351c4d12f5a949e7e2c
Lucas Foo <lucasfoo33@gmail.com>
2019-11-10 13:54:54 +0800

    Minor refactor of PlanBot

--- src/test/java/duke/model/PlanBotTest.java
+++ src/test/java/duke/model/PlanBotTest.java
@@ -28,11 +28,14 @@ public class PlanBotTest {
         PlanBot planBot = PlanBot.getInstance(knownAttributes);
         Assertions.assertNull(planBot.getPlanBudgetRecommendation());
         planBot.processInput("random string of inputs");
-        PlanBot.PlanDialog planDialog = new PlanBot.PlanDialog("Please enter a valid reply!", PlanBot.Agent.BOT);
+        PlanBot.PlanDialog planDialogExpected1 = new PlanBot.PlanDialog("Please enter a valid reply!",
+                PlanBot.Agent.BOT);
+        PlanBot.PlanDialog planDialogExpected2 = new PlanBot.PlanDialog(
+                "random string of inputs is not a valid amount!", PlanBot.Agent.BOT);
         int size = planBot.getDialogObservableList().size();
-        PlanBot.PlanDialog planDialog1 = planBot.getDialogObservableList().get(size - 1);
-        Assertions.assertEquals(planDialog.text, planDialog1.text);
-        Assertions.assertEquals(planDialog.agent, planDialog1.agent);
+        PlanBot.PlanDialog planDialog = planBot.getDialogObservableList().get(size - 1);
+        Assertions.assertTrue(planDialogExpected1.text.equals(planDialog.text)
+                || planDialogExpected2.text.equals(planDialog.text));
     }
 
 
