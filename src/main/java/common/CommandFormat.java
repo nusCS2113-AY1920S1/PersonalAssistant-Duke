cf807c38516da7975630d7d109d42350bebd9b77
lijiayu980606 <lijiayu0017@163.com>
2019-11-10 16:42:00 +0800

    fixed checkstyle

--- src/main/java/common/CommandFormat.java
+++ src/main/java/common/CommandFormat.java
@@ -39,7 +39,8 @@ public class CommandFormat {
     }
 
     public String getPayeeFormat() {
-        return "getpayee PAYEE"; }
+        return "getpayee PAYEE";
+    }
 
     public String deletePayeeFormat() {
         return "delete payee p/PAYEE";
