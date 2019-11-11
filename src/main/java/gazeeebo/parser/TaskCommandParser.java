9df84034be6e0f013b14ef21c16d66a8e4fb3fee
JasonLeeWeiHern <e0309651@u.nus.edu>
2019-11-11 20:31:23 +0800

    catch error in edit command

--- src/main/java/gazeeebo/parser/TaskCommandParser.java
+++ src/main/java/gazeeebo/parser/TaskCommandParser.java
@@ -34,6 +34,7 @@ import gazeeebo.commands.schedule.ScheduleDailyCommand;
 import gazeeebo.commands.schedule.ScheduleMonthlyCommand;
 import gazeeebo.commands.schedule.ScheduleWeeklyCommand;
 import gazeeebo.exception.DukeException;
+import gazeeebo.storage.TasksPageStorage;
 import gazeeebo.triviaManager.TriviaManager;
 import gazeeebo.UI.Ui;
 import gazeeebo.storage.Storage;
@@ -293,7 +294,12 @@ public class TaskCommandParser extends Command {
             } else {
                 ui.showDontKnowErrorMessage();
             }
-
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < list.size(); i++) {
+                sb.append(list.get(i).toString() + "\n");
+            }
+            TasksPageStorage tasksPageStorage = new TasksPageStorage();
+            tasksPageStorage.writeToSaveFile(sb.toString());
         }
     }
 
---
+++ src/test/java/PlacesCommandTest/PlacesAddCommandTest.java
@@ -0,0 +1,56 @@
+//@@author jessteoxizhi
+
+package PlacesCommandTest;
+
+import gazeeebo.UI.Ui;
+import gazeeebo.commands.places.AddPlacesCommand;
+import gazeeebo.storage.Storage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlacesAddCommandTest {
+    private Ui ui = new Ui();
+    private Storage storage = new Storage();
+    private ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private PrintStream mine = new PrintStream(output);
+    private PrintStream original = System.out;
+
+    @BeforeEach
+    void setupStream() {
+        System.setOut(mine);
+    }
+
+    @AfterEach
+    void restoreStream() {
+        System.out.flush();
+        System.setOut(original);
+    }
+
+    @Test
+    void testAddPlacesCommand() throws IOException {
+        HashMap<String, String> map = new HashMap<>(); //Read the file
+        Map<String, String> places = new TreeMap<String, String>(map);
+        ui.fullCommand = "add-Test,COM3";
+        AddPlacesCommand test = new AddPlacesCommand(ui, places);
+        assertEquals("Successfully added :Test,COM3\r\n", output.toString());
+    }
+
+    @Test
+    void testAddWrongPlacesCommand() {
+        HashMap<String, String> map = new HashMap<>(); //Read the file
+        Map<String, String> places = new TreeMap<String, String>(map);
+        ui.fullCommand = "add-TestCOM3";
+        AddPlacesCommand test = new AddPlacesCommand(ui, places);
+        assertEquals("Please input add command in the correct format\r\n", output.toString());
+    }
+}
