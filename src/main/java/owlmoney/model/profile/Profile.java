5633de086e96878fe943588a7a9c3abbfe57977e
TerenceTanWT <e0357478@u.nus.edu>
2019-11-11 20:53:02 +0800

    Updated PPP, DG, and fixed printing inconsistencies for card bill

--- src/main/java/owlmoney/model/profile/Profile.java
+++ src/main/java/owlmoney/model/profile/Profile.java
@@ -1254,7 +1254,7 @@ public class Profile {
     public void addCardBill(String card, String bank, Expenditure expenditure, Deposit deposit,
             YearMonth cardDate, Ui ui, String type) throws CardException, BankException {
         bankList.bankListAddExpenditure(bank, expenditure, ui, type);
-        ui.printMessage("\n");
+        ui.printMessage("");
         bankList.bankListAddDeposit(bank, deposit, ui, type);
         try {
             cardList.transferExpUnpaidToPaid(card, cardDate, type);
@@ -1287,6 +1287,7 @@ public class Profile {
         checkBillAmountNotZero(getCardPaidBillAmount(card, cardDate), card, cardDate);
         int expenditureNumber = profileGetCardBillExpenditureId(bank,getCardId(card), cardDate) + ARRAY_INDEX;
         profileDeleteExpenditure(expenditureNumber, bank, ui, type, true);
+        ui.printMessage("");
         int depositNumber = profileGetCardBillDepositId(bank,getCardId(card), cardDate) + ARRAY_INDEX;
         profileDeleteDeposit(depositNumber, bank, ui, true);
         cardList.transferExpPaidToUnpaid(card, cardDate, type);
