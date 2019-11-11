491efd3dac5f42df1422e45100b4337fb73ed555
liushiru <shiruliu55@hotmail.com>
2019-11-11 14:12:19 +0800

    sync

--- src/test/java/duke/logic/command/product/ProductCommandTestUtil.java
+++ src/test/java/duke/logic/command/product/ProductCommandTestUtil.java
@@ -1,14 +1,7 @@
 package duke.logic.command.product;
 
-import duke.model.product.Product;
-
-import static duke.testutil.TypicalProducts.CHEESE_CAKE;
-
 public class ProductCommandTestUtil {
 
     public static final String VALID_NAME = "_name";
     public static final Double VALID_COST = 3000.0;
-    Product product = CHEESE_CAKE;
-    //public static final String VALID_CHEESE_CAKE_USER_INPUT = "product add -name Cheese cake -cost 3.0 "
-    //        + "-price 5.9 -ingt [Cream cheese, 3.0]";
 }
