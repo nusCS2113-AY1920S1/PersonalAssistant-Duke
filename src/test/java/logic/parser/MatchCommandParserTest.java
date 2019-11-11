7efb37b53c5a8df52e9a90d7b3ccc6c28997ae60
JustinChia1997 <justinchiajj@hotmail.com>
2019-11-11 16:42:29 +0800

    More tests for matching

--- src/test/java/logic/parser/MatchCommandParserTest.java
+++ src/test/java/logic/parser/MatchCommandParserTest.java
@@ -2,6 +2,7 @@ package logic.parser;
 
 import common.DukeException;
 import logic.command.Command;
+import logic.command.match.MatchMemberCommand;
 import logic.command.match.MatchTaskCommand;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,4 +18,27 @@ public class MatchCommandParserTest {
         MatchTaskCommand correctCommand = new MatchTaskCommand(1);
         assertEquals(correctCommand.getClass(), testingCommand.getClass());
     }
+
+    @Test
+    public void matchCommandParser_MatchMember_Success() throws DukeException {
+        Command testingCommand = MatchCommandParser.parseMatch("member john");
+        MatchMemberCommand correctCommand = new MatchMemberCommand("john");
+        assertEquals(correctCommand.getClass(), testingCommand.getClass());
+    }
+
+    @Test
+    public void matchCommandParser_MatchTaskWrongInputString_Failure() throws DukeException {
+        assertThrows(DukeException.class, () -> MatchCommandParser.parseMatch("task testing"));
+    }
+
+    @Test
+    public void matchCommandParser_MatchTaskNoInput_Failure() throws DukeException {
+        assertThrows(DukeException.class, () -> MatchCommandParser.parseMatch("task "));
+    }
+
+    @Test
+    public void matchCommandParser_MatchMemberNoInput_Failure() throws DukeException {
+        assertThrows(DukeException.class, () -> MatchCommandParser.parseMatch("member "));
+    }
+
 }
