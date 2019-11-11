aa6525a7343bb09a02d6a234655bec02981e2d5e
jessteoxizhi <e0309463@u.nus.edu.sg>
2019-11-11 16:35:36 +0800

    refactor

---
+++ src/test/java/TaskCommandTest/DoAfterCommandTest.java
@@ -0,0 +1,22 @@
+package TaskCommandTest;//@@author JasonLeeWeiHern
+
+import gazeeebo.tasks.DoAfter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DoAfterCommandTest {
+
+    DoAfter testdoAfter = new DoAfter("read book","read book","return book");
+
+    @Test
+    void testtoString() {
+        assertEquals("DA|ND|return book|read book", testdoAfter.toString());
+    }
+
+    @Test
+    void testlistformat() {
+        assertEquals("[DA][ND] return book(/after:read book)", testdoAfter.listFormat());
+    }
+
+}
