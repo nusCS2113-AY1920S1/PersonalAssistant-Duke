2abfec26f3ff06aa5a0a2848bbd7b4370d130d41
koushireo <koushireo@gmail.com>
2019-11-13 15:23:10 +0800

    no message

--- src/main/java/diyeats/ui/GraphUi.java
+++ src/main/java/diyeats/ui/GraphUi.java
@@ -19,22 +19,49 @@ public class GraphUi {
      * @param month month of the data collected
      * @param type type of the data collected
      */
-    public void show(String[][] graph, int month, String type) {
+    public void show(String[][] graph, int month, String type, int highest) {
         System.out.println(this.month[month - 1] + " \n" + type);
+        int factor = findFactor(highest);
         for (int i = 0; i < graph.length; i += 1) {
+            int currentValue = (int)(((20.0 - i)/20.0) * highest);
+            for (int k = 0; k < (factor - findFactor(currentValue)); k += 1) {
+                System.out.print(" ");
+            }
+            System.out.print(currentValue);
             System.out.print(" |");
             for (int j = 0; j < graph[i].length; j += 1) {
                 System.out.print(graph[i][j]);
             }
             System.out.println();
         }
-        System.out.print("  ");
+        printPadding(factor + 2);
         for (int i = 0; i < graph[0].length; i += 1) {
             System.out.print("-");
         }
         System.out.println();
-        System.out.println("   1 2 3 4 5 6 7 8 9 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 3 ");
-        System.out.println("                     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 ");
-        System.out.println("                              days                               ");
+        printPadding(factor + 2);
+        System.out.println("1 2 3 4 5 6 7 8 9 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 3 ");
+        printPadding(factor + 2);
+        System.out.println("                  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 ");
+        printPadding(factor + 2);
+        System.out.println("                           days                               ");
+    }
+
+    public int findFactor(int highest) {
+        if (highest == 0) {
+            return 1;
+        }
+        int factor = 0;
+        while (highest != 0) {
+            factor += 1;
+            highest /= 10;
+        }
+        return factor;
+    }
+
+    private void printPadding(int padding) {
+        for (int i = 0; i < padding; i += 1) {
+            System.out.print(" ");
+        }
     }
 }
