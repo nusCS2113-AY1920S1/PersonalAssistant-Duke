0523017056d5758b15b1b9af2b43e753f792e329
yueyeah <yuejunyi@u.nus.edu>
2020-07-19 11:44:45 +0800

    Add documentation for MainLauncher

--- src/main/java/compal/MainLauncher.java
+++ src/main/java/compal/MainLauncher.java
@@ -4,6 +4,13 @@ package compal;
  * MainLauncher is the entrypoint to the COMPal application.
  */
 public class MainLauncher {
+    /**
+     *
+     * The Java main method is the entry point of any Java program.
+     * This is the Java main method for COMPal. This will call the main method
+     * of the Main class.
+     * @param args Java command line arguments. At the moment there are none.
+     */
     public static void main(String[] args) {
         Main.main(args);
     }
